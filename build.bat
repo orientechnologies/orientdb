483a69d214a9eff9dbb7cae54b48fbcdd71aba7f
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2010-04-21 00:20:58 +0000

    Added 2 new script to build the entire project.

---
+++ build.bat
@@ -0,0 +1,7 @@
+@REM 
+@REM  Copyright (c) 1999-2010 Luca Garulli
+@REM 
+call ant -f build-db.xml 
+call ant -f build-kv.xml 
+
+pause
---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# Copyright (c) 1999-2010 Luca Garulli
+#
+ant -f build-db.xml 
+ant -f build-kv.xml 
