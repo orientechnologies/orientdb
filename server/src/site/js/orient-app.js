44e2e3d48e1ea66b5320eec548b80077d0c956f2
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2010-05-13 22:35:17 +0000

    Improved GUI OrientDB Studio

---
+++ server/src/site/js/orient-app.js
@@ -0,0 +1,79 @@
+function executeRequest(iRequest, iCallback) {
+	$.ajax( {
+		type : 'GET',
+		url : iRequest,
+		success : function(msg) {
+			iCallback.apply(this, [ JSON.parse(msg) ]);
+		},
+		error : function(msg) {
+			jQuery("#output").text("Command response: " + msg);
+		}
+	});
+}
+
+function fillDynaTable(iTable, iTitle, iColumnsNames, iColumnsModel, iData,
+		iCustomConfig) {
+	var columnModel = iColumnsModel;
+	if (!columnModel) {
+		var columnModel = new Array();
+		for (col in iColumnsNames) {
+			columnModel.push( {
+				"name" : iColumnsNames[col],
+				"index" : iColumnsNames[col]
+			});
+		}
+	}
+
+	var config = {
+		datatype : "local",
+		autowidth : true,
+		multiselect : false,
+		viewrecords : true,
+		caption : iTitle,
+		colNames : iColumnsNames,
+		colModel : columnModel
+	};
+
+	if (iCustomConfig)
+		// MERGE SETTINGS
+		for (property in iCustomConfig)
+			config[property] = iCustomConfig[property];
+
+	jQuery(iTable).jqGrid(config);
+
+	fillDynaTableRows(iTable, iData);
+}
+
+function fillDynaTableRows(iTable, iData) {
+	jQuery(iTable).jqGrid('clearGridData');
+
+	if (iData)
+		for ( var i = 0; i <= iData.length; i++)
+			jQuery(iTable).jqGrid('addRowData', i + 1, iData[i]);
+}
+
+function fillStaticTable(iTable, iColumns, iData) {
+	$(iTable).text("");
+
+	var line = "";
+	for (col in iColumns) {
+		line += '<td><b>' + iColumns[col] + '</b></td>';
+	}
+	$(iTable).append('<tr>' + line + '</tr>');
+
+	for (row in iData) {
+		var values = iData[row];
+
+		var line = ""
+		var i = 0;
+		for (col in values) {
+			if (i++ >= iColumns.length)
+				break;
+
+			line += '<td>' + values[col] + '</td>';
+		}
+		$(iTable).append('<tr>' + line + '</tr>');
+	}
+
+	jQuery("#output").text("Command executed");
+}
