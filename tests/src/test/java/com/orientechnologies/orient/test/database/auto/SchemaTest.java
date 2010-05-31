4ebe58fd0593dfc73c6e26588ac72b689cd42fd8
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2010-05-31 09:24:16 +0000

    Added Danny test case to the automatic test suite

--- tests/src/test/java/com/orientechnologies/orient/test/database/auto/SchemaTest.java
+++ tests/src/test/java/com/orientechnologies/orient/test/database/auto/SchemaTest.java
@@ -61,6 +61,7 @@ public class SchemaTest {
 		profile.createProperty("lastAccessOn", OType.DATE).setMin("2010-01-01 00:00:00");
 
 		OClass whiz = database.getMetadata().getSchema().createClass("Whiz");
+		whiz.createProperty("id", OType.INTEGER);
 		whiz.createProperty("account", OType.LINK, profile).setMandatory(true).setNotNull(true);
 		whiz.createProperty("date", OType.DATE).setMin("2010-01-01 00:00:00");
 		whiz.createProperty("text", OType.STRING).setMandatory(true).setMin("1").setMax("140");
