4b2e979d48d6510a70c592abbf1ac139f4955189
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2010-06-03 17:54:49 +0000

    Issue #14:http://code.google.com/p/orient/issues/detail?id=14 New Unit Test and Speed test for indexes.

---
+++ tests/src/test/java/com/orientechnologies/orient/test/database/auto/IndexTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 1999-2010 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.orient.test.database.auto;
+
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.orientechnologies.orient.client.remote.OEngineRemote;
+import com.orientechnologies.orient.core.Orient;
+import com.orientechnologies.orient.core.db.object.ODatabaseObjectTx;
+import com.orientechnologies.orient.core.exception.ODatabaseException;
+import com.orientechnologies.orient.core.index.OIndexException;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
+import com.orientechnologies.orient.test.database.base.OrientTest;
+import com.orientechnologies.orient.test.domain.whiz.Profile;
+
+@Test(groups = { "index" }, sequential = true)
+public class IndexTest {
+	private ODatabaseObjectTx	database;
+	protected long						startRecordNumber;
+
+	@Parameters(value = "url")
+	public IndexTest(String iURL) {
+		Orient.instance().registerEngine(new OEngineRemote());
+
+		database = new ODatabaseObjectTx(iURL);
+		database.getEntityManager().registerEntityClasses("com.orientechnologies.orient.test.domain");
+	}
+
+	@Test
+	public void testDuplicatedIndex() {
+		database.open("admin", "admin");
+
+		Profile jayMiner = new Profile("Jay", "Jay", "Miner", null);
+		database.save(jayMiner);
+
+		Profile jacobMiner = new Profile("Jay", "Jacob", "Miner", null);
+
+		try {
+			database.save(jacobMiner);
+
+			// IT SHOULD GIVE ERROR ON DUPLICATED KEY
+			Assert.assertTrue(false);
+
+		} catch (ODatabaseException e) {
+			Assert.assertTrue(e.getCause() instanceof OIndexException);
+		}
+		database.close();
+	}
+
+	@Test
+	public void testUseOfIndex() {
+		database.open("admin", "admin");
+
+		final List<ODocument> result = database.command(new OSQLSynchQuery<ODocument>("select * from Profile where nick = 'Jay'"))
+				.execute();
+
+		ODocument record;
+		for (int i = 0; i < result.size(); ++i) {
+			record = result.get(i);
+
+			OrientTest.printRecord(i, record);
+
+			Assert.assertTrue(record.getClassName().equalsIgnoreCase("Profile"));
+			Assert.assertTrue(record.field("name").toString().equalsIgnoreCase("Jay"));
+		}
+
+		database.close();
+	}
+}
---
+++ tests/src/test/java/com/orientechnologies/orient/test/database/speed/LocalCreateIndexedDocumentSpeedTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 1999-2010 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.orient.test.database.speed;
+
+import org.testng.annotations.Test;
+
+import com.orientechnologies.common.profiler.OProfiler;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.intent.OIntentMassiveInsert;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.tx.OTransaction.TXTYPE;
+import com.orientechnologies.orient.test.database.base.OrientMonoThreadTest;
+
+@Test(enabled = false)
+public class LocalCreateIndexedDocumentSpeedTest extends OrientMonoThreadTest {
+	private ODatabaseDocument	database;
+	private ODocument					record;
+
+	public static void main(String[] iArgs) throws InstantiationException, IllegalAccessException {
+		LocalCreateIndexedDocumentSpeedTest test = new LocalCreateIndexedDocumentSpeedTest();
+		test.data.go(test);
+	}
+
+	public LocalCreateIndexedDocumentSpeedTest() throws InstantiationException, IllegalAccessException {
+		super(1000000);
+	}
+
+	@Override
+	public void init() {
+		OProfiler.getInstance().startRecording();
+
+		database = new ODatabaseDocumentTx(System.getProperty("url")).open("admin", "admin");
+		record = database.newInstance();
+
+		// REMOVE THE INDEX
+		System.out.println("Remove index...");
+		database.getMetadata().getSchema().getClass("Profile").getProperty("nick").removeIndex();
+		System.out.println("Ok");
+
+		database.declareIntent(new OIntentMassiveInsert());
+		database.begin(TXTYPE.NOTX);
+	}
+
+	@Override
+	public void cycle() {
+		record.reset();
+
+		record.setClassName("Profile");
+		record.field("nick", String.valueOf(100+data.getCyclesDone()));
+		record.field("name", "Luca");
+		record.field("surname", "Garulli");
+
+		record.save();
+
+		if (data.getCyclesDone() == data.getCycles() - 1) {
+			database.commit();
+
+			System.out.println("\nIndexing...");
+			database.getMetadata().getSchema().getClass("Profile").getProperty("nick").createIndex(true);
+			System.out.println("Done");
+		}
+	}
+
+	@Override
+	public void deinit() {
+		database.close();
+		super.deinit();
+	}
+}
