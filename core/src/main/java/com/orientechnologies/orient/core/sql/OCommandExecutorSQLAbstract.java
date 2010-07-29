b125205d92f793266188decbbf1b5b2c44503d19
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2010-07-29 09:15:04 +0000

    Fixed bug of not-trimmed sql string (Thanks lmolino!)

--- core/src/main/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLAbstract.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLAbstract.java
@@ -32,9 +32,9 @@ public abstract class OCommandExecutorSQLAbstract extends OCommandExecutorAbstra
 	public static final String	CLUSTER_PREFIX	= "CLUSTER:";
 	public static final String	CLASS_PREFIX		= "CLASS:";
 
-	@Override
-	public OCommandExecutorSQLAbstract init(final ODatabaseRecord<?> iDatabase, final String iText) {
-		text = iText;
+	@Override	
+	public OCommandExecutorSQLAbstract init(final ODatabaseRecord<?> iDatabase, String iText) {
+		iText = iText.trim();
 		textUpperCase = iText.toUpperCase();
 		return (OCommandExecutorSQLAbstract) super.init(iDatabase, iText);
 	}
