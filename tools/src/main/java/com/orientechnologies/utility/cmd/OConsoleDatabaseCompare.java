6f064fa670b727446c0bc85e6280fd51390c23b4
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2010-07-29 14:42:49 +0000

    Improved import/export tools

---
+++ tools/src/main/java/com/orientechnologies/utility/cmd/OConsoleDatabaseCompare.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright 1999-2010 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.utility.cmd;
+
+import java.io.IOException;
+
+import com.orientechnologies.orient.core.Orient;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.record.impl.ORecordColumn;
+import com.orientechnologies.orient.core.record.impl.ORecordFlat;
+import com.orientechnologies.orient.core.storage.ORawBuffer;
+import com.orientechnologies.orient.core.storage.OStorage;
+import com.orientechnologies.utility.console.OCommandListener;
+
+public class OConsoleDatabaseCompare {
+	private OStorage					storage1;
+	private OStorage					storage2;
+	private OCommandListener	listener;
+	private int								differences	= 0;
+
+	public OConsoleDatabaseCompare(final String iDb1URL, final String iDb2URL, final OCommandListener iListener) throws IOException {
+		listener = iListener;
+		listener.onMessage("\nComparing two databases:\n1) " + iDb1URL + "\n2) " + iDb2URL + "\n");
+
+		storage1 = Orient.instance().accessToLocalStorage(iDb1URL, "rw");
+		storage1.open(0, null, null);
+
+		storage2 = Orient.instance().accessToLocalStorage(iDb2URL, "rw");
+		storage2.open(0, null, null);
+	}
+
+	public boolean compare() {
+		try {
+			compareClusters();
+			compareRecords();
+
+			if (differences == 0) {
+				listener.onMessage("\n\nDatabases match.");
+				return true;
+			} else {
+				listener.onMessage("\n\nDatabases not match. Found " + differences + " difference(s).");
+				return false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ODatabaseExportException("Error on compare of databases '" + storage1.getName() + "' against '"
+					+ storage2.getName() + "'", e);
+		} finally {
+			storage1.close();
+			storage2.close();
+		}
+	}
+
+	private boolean compareClusters() {
+		listener.onMessage("\nStarting shallow comparison of clusters:");
+
+		listener.onMessage("\nChecking the number of clusters...");
+
+		if (storage1.getClusterNames().size() != storage1.getClusterNames().size()) {
+			listener.onMessage("KO: cluster sizes are different: " + storage1.getClusterNames().size() + " <-> "
+					+ storage1.getClusterNames().size());
+			++differences;
+		}
+
+		int cluster2Id;
+		boolean ok;
+
+		for (String cl1 : storage1.getClusterNames()) {
+			ok = true;
+			cluster2Id = storage2.getClusterIdByName(cl1);
+
+			listener.onMessage("\n- Checking cluster " + String.format("%-25s: ", "'" + cl1 + "'"));
+
+			if (cluster2Id == -1) {
+				listener.onMessage("KO: cluster name " + cl1 + " was not found on database " + storage2);
+				++differences;
+				ok = false;
+			}
+
+			if (cluster2Id != storage1.getClusterIdByName(cl1)) {
+				listener.onMessage("KO: cluster id is different for cluster " + cl1 + ": " + storage1.getClusterIdByName(cl1) + " <-> "
+						+ cluster2Id);
+				++differences;
+				ok = false;
+			}
+
+			if (storage1.count(cluster2Id) != storage2.count(cluster2Id)) {
+				listener.onMessage("KO: record number are different in cluster '" + cl1 + "' (id=" + cluster2Id + "): "
+						+ storage1.count(cluster2Id) + " <-> " + storage2.count(cluster2Id));
+				++differences;
+				ok = false;
+			}
+
+			if (ok)
+				listener.onMessage("OK");
+		}
+
+		listener.onMessage("\n\nShallow analysis done.");
+		return true;
+	}
+
+	private boolean compareRecords() {
+		listener.onMessage("\nStarting deep comparison record by record. It can takes some minutes. Wait please...");
+
+		int clusterId;
+		long clusterMax;
+
+		ORawBuffer buffer1, buffer2;
+
+		for (String clusterName : storage1.getClusterNames()) {
+			clusterId = storage1.getClusterIdByName(clusterName);
+			clusterMax = storage1.getClusterLastEntryPosition(clusterId);
+			for (int i = 0; i < clusterMax; ++i) {
+				buffer1 = storage1.readRecord(null, 0, clusterId, i, null);
+				buffer2 = storage2.readRecord(null, 0, clusterId, i, null);
+
+				if (buffer1 == null && buffer2 == null || buffer1.buffer == null && buffer2.buffer == null)
+					continue;
+
+				if (buffer1.recordType != buffer2.recordType) {
+					listener.onMessage("\n- KO: RID=" + clusterId + ":" + i + " recordType is different: " + (char) buffer1.recordType
+							+ " <-> " + (char) buffer2.recordType);
+					++differences;
+				}
+
+				if (buffer1.buffer == null && buffer2.buffer != null) {
+					listener.onMessage("\n- KO: RID=" + clusterId + ":" + i + " content is different: null <-> " + buffer2.buffer.length);
+					++differences;
+				} else if (buffer1.buffer != null && buffer2.buffer == null) {
+					listener
+							.onMessage("\n- KO: RID=" + clusterId + ":" + i + " content is different: " + buffer1.buffer.length + " <-> null");
+					++differences;
+				} else if (buffer1.buffer.length != buffer2.buffer.length) {
+					listener.onMessage("\n- KO: RID=" + clusterId + ":" + i + " content length is different: " + buffer1.buffer.length
+							+ " <-> " + buffer2.buffer.length);
+					if (buffer1.recordType == ODocument.RECORD_TYPE || buffer1.recordType == ORecordFlat.RECORD_TYPE
+							|| buffer1.recordType == ORecordColumn.RECORD_TYPE)
+						listener.onMessage("\n--- REC1: " + new String(buffer1.buffer));
+					if (buffer2.recordType == ODocument.RECORD_TYPE || buffer2.recordType == ORecordFlat.RECORD_TYPE
+							|| buffer2.recordType == ORecordColumn.RECORD_TYPE)
+						listener.onMessage("\n--- REC2: " + new String(buffer2.buffer));
+
+					++differences;
+				} else {
+					// CHECK BYTE PER BYTE
+					for (int b = 0; b < buffer1.buffer.length; ++b) {
+						if (buffer1.buffer[b] != buffer2.buffer[b]) {
+							listener.onMessage("\n--- KO: RID=" + clusterId + ":" + i + " content is different at byte #" + b + ": "
+									+ buffer1.buffer[b] + " <-> " + buffer2.buffer[b]);
+							++differences;
+						}
+					}
+				}
+			}
+		}
+
+		return true;
+	}
+}
---
+++ tools/src/main/java/com/orientechnologies/utility/cmd/OConsoleDatabaseExport.java
@@ -0,0 +1,279 @@
+/*
+ * Copyright 1999-2010 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.utility.cmd;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.dictionary.ODictionary;
+import com.orientechnologies.orient.core.index.OPropertyIndex;
+import com.orientechnologies.orient.core.intent.OIntentMassiveRead;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OProperty;
+import com.orientechnologies.orient.core.metadata.schema.OSchema;
+import com.orientechnologies.orient.core.record.ORecordInternal;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.serialization.serializer.OJSONWriter;
+import com.orientechnologies.orient.core.storage.impl.local.OClusterLogical;
+import com.orientechnologies.utility.console.OCommandListener;
+
+public class OConsoleDatabaseExport extends OConsoleDatabaseImpExpAbstract {
+	private OJSONWriter	writer;
+	private long				recordExported;
+
+	public OConsoleDatabaseExport(final ODatabaseDocument iDatabase, final String iFileName, final OCommandListener iListener)
+			throws IOException {
+		super(iDatabase, iFileName, iListener);
+
+		writer = new OJSONWriter(new FileWriter(fileName));
+		writer.beginObject();
+
+		iDatabase.declareIntent(new OIntentMassiveRead());
+	}
+
+	public OConsoleDatabaseExport exportDatabase() {
+		try {
+			long time = System.currentTimeMillis();
+
+			if (includeInfo)
+				exportInfo();
+			exportClusters();
+			if (includeSchema)
+				exportSchema();
+			exportRecords();
+			if (includeDictionary)
+				exportDictionary();
+
+			listener.onMessage("\n\nExport of database completed in " + (System.currentTimeMillis() - time) + "ms");
+
+			writer.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ODatabaseExportException("Error on exporting database '" + database.getName() + "' to: " + fileName, e);
+		} finally {
+			close();
+		}
+
+		return this;
+	}
+
+	public long exportRecords() throws IOException {
+		long totalRecords = 0;
+		int level = 1;
+		listener.onMessage("\nExporting records...");
+
+		writer.beginCollection(level, true, "records");
+		for (String clusterName : database.getClusterNames()) {
+			// CHECK IF THE CLUSTER IS INCLUDED
+			if (includeClusters != null) {
+				if (!includeClusters.contains(clusterName))
+					continue;
+			} else if (excludeClusters != null) {
+				if (excludeClusters.contains(clusterName))
+					continue;
+			}
+
+			if (excludeClusters.contains(clusterName))
+				continue;
+
+			long recordTot = database.countClusterElements(clusterName);
+			listener.onMessage("\n- Exporting record of cluster '" + clusterName + "'...");
+
+			long recordNum = 0;
+			for (ORecordInternal<?> rec : database.browseCluster(clusterName)) {
+
+				if (rec instanceof ODocument) {
+					// CHECK IF THE CLASS OF THE DOCUMENT IS INCLUDED
+					ODocument doc = (ODocument) rec;
+					if (includeClasses != null) {
+						if (!includeClasses.contains(doc.getClassName()))
+							continue;
+					} else if (excludeClasses != null) {
+						if (excludeClasses.contains(doc.getClassName()))
+							continue;
+					}
+				}
+
+				exportRecord(recordTot, recordNum++, rec);
+			}
+
+			listener.onMessage("OK (records=" + recordTot + ")");
+
+			totalRecords += recordTot;
+		}
+		writer.endCollection(level, true);
+
+		listener.onMessage("\n\nDone. Exported " + totalRecords + " records\n");
+
+		return totalRecords;
+	}
+
+	public void close() {
+		database.declareIntent(null);
+
+		if (writer == null)
+			return;
+
+		try {
+			writer.endObject();
+			writer.close();
+			writer = null;
+		} catch (IOException e) {
+		}
+	}
+
+	private void exportClusters() throws IOException {
+		listener.onMessage("\nExporting clusters...");
+
+		writer.beginCollection(1, true, "clusters");
+		int totalClusters = 0;
+
+		for (String clusterName : database.getClusterNames()) {
+			// CHECK IF THE CLUSTER IS INCLUDED
+			if (includeClusters != null) {
+				if (!includeClusters.contains(clusterName))
+					continue;
+			} else if (excludeClusters != null) {
+				if (excludeClusters.contains(clusterName))
+					continue;
+			}
+
+			writer.beginObject(2, true, null);
+			writer.writeAttribute(0, false, "name", clusterName);
+			writer.writeAttribute(0, false, "id", database.getClusterIdByName(clusterName));
+			writer.writeAttribute(0, false, "type", database.getClusterType(clusterName));
+
+			if (database.getClusterType(clusterName).equals("LOGICAL")) {
+				OClusterLogical cluster = (OClusterLogical) database.getStorage().getClusterById(database.getClusterIdByName(clusterName));
+				writer.writeAttribute(0, false, "rid", cluster.getRID());
+			}
+
+			totalClusters++;
+			writer.endObject(2, false);
+		}
+
+		listener.onMessage("OK (" + totalClusters + " clusters)");
+
+		writer.endCollection(1, true);
+	}
+
+	private void exportInfo() throws IOException {
+		listener.onMessage("\nExporting database info...");
+
+		writer.beginObject(1, true, "info");
+		writer.writeAttribute(2, true, "name", database.getName().replace('\\', '/'));
+		writer.writeAttribute(2, true, "default-cluster-id", database.getDefaultClusterId());
+		writer.endObject(1, true);
+
+		listener.onMessage("OK");
+	}
+
+	private void exportDictionary() throws IOException {
+		listener.onMessage("\nExporting dictionary...");
+
+		long tot = 0;
+		writer.beginObject(1, true, "dictionary");
+		ODictionary<ODocument> d = database.getDictionary();
+		if (d != null) {
+			Entry<String, ODocument> entry;
+			for (Iterator<Entry<String, ODocument>> iterator = d.iterator(); iterator.hasNext();) {
+				entry = iterator.next();
+				writer.writeAttribute(2, true, "key", entry.getKey());
+				writer.writeAttribute(0, false, "value", OJSONWriter.writeValue(entry.getValue()));
+				++tot;
+			}
+		}
+		writer.endObject(1, true);
+
+		listener.onMessage("OK (" + tot + " entries)");
+	}
+
+	private void exportSchema() throws IOException {
+		listener.onMessage("\nExporting schema...");
+
+		writer.beginObject(1, true, "schema");
+		OSchema s = database.getMetadata().getSchema();
+		writer.writeAttribute(2, true, "version", s.getDocument().getVersion());
+
+		if (s.getClasses().size() > 0) {
+			writer.beginCollection(2, true, "classes");
+			for (OClass cls : s.getClasses()) {
+				writer.beginObject(3, true, null);
+				writer.writeAttribute(0, false, "name", cls.getName());
+				writer.writeAttribute(0, false, "id", cls.getId());
+				writer.writeAttribute(0, false, "default-cluster-id", cls.getDefaultClusterId());
+				writer.writeAttribute(0, false, "cluster-ids", cls.getClusterIds());
+				if (cls.getSuperClass() != null)
+					writer.writeAttribute(0, false, "super-class", cls.getSuperClass().getName());
+
+				if (cls.properties().size() > 0) {
+					writer.beginCollection(4, true, "properties");
+					for (OProperty p : cls.declaredProperties()) {
+						writer.beginObject(5, true, null);
+						writer.writeAttribute(0, false, "name", p.getName());
+						writer.writeAttribute(0, false, "id", p.getId());
+						writer.writeAttribute(0, false, "type", p.getType().toString());
+						if (p.getLinkedClass() != null)
+							writer.writeAttribute(0, false, "linked-class", p.getLinkedClass().getName());
+						if (p.getLinkedType() != null)
+							writer.writeAttribute(0, false, "linked-type", p.getLinkedType().toString());
+						if (p.getMin() != null)
+							writer.writeAttribute(0, false, "min", p.getMin());
+						if (p.getMax() != null)
+							writer.writeAttribute(0, false, "max", p.getMax());
+						if (p.getIndex() != null) {
+							writer.writeAttribute(0, false, "index-rid", p.getIndex().getRecord().getIdentity());
+
+							OPropertyIndex idx = p.getIndex();
+							writer.writeAttribute(0, false, "index-type", idx.isUnique() ? "unique" : "not-unique");
+						}
+						writer.endObject(0, false);
+					}
+					writer.endCollection(4, true);
+				}
+
+				writer.endObject(3, true);
+			}
+			writer.endCollection(2, true);
+		}
+
+		writer.endObject(1, true);
+
+		listener.onMessage("OK (" + s.getClasses().size() + " classes)");
+	}
+
+	private void exportRecord(long recordTot, long recordNum, ORecordInternal<?> rec) throws IOException {
+		if (rec == null)
+			return;
+
+		if (rec.getIdentity().isValid())
+			rec.load();
+
+		if (recordExported > 0)
+			writer.append(",");
+
+		writer.append(rec.toJSON("rid,type,version,class,attribSameRow,indent:4"));
+
+		recordExported++;
+		recordNum++;
+
+		if (recordTot > 10 && (recordNum + 1) % (recordTot / 10) == 0)
+			listener.onMessage(".");
+	}
+}
