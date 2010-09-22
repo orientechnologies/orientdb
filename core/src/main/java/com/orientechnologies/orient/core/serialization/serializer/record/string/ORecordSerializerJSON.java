5fe2446fd17f1def5d8a0d735f959be88623c985
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2010-09-22 20:59:21 +0000

    Support for Unicode in JSON output

--- core/src/main/java/com/orientechnologies/orient/core/serialization/serializer/record/string/ORecordSerializerJSON.java
+++ core/src/main/java/com/orientechnologies/orient/core/serialization/serializer/record/string/ORecordSerializerJSON.java
@@ -362,10 +362,34 @@ public class ORecordSerializerJSON extends ORecordSerializerStringAbstract {
 
   private Object encode(final Object iValue) {
     if (iValue instanceof String) {
-      final String encoded = ((String) iValue).replace('"', '\'');
-      return encoded;
+      return convert2unicode(((String) iValue).replace('"', '\''));
     } else
       return iValue;
+  }
+
+  public static String convert2unicode(String str) {
+    StringBuffer ostr = new StringBuffer();
+
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+
+      if ((ch >= 0x0020) && (ch <= 0x007e)) // Does the char need to be converted to unicode?
+      {
+        ostr.append(ch); // No.
+      } else // Yes.
+      {
+        ostr.append("\\u"); // standard unicode format.
+        String hex = Integer.toHexString(str.charAt(i) & 0xFFFF); // Get hex value of the char.
+        for (int j = 0; j < 4 - hex.length(); j++)
+          // Prepend zeros because unicode requires 4 digits
+          ostr.append("0");
+        ostr.append(hex.toLowerCase()); // standard unicode format.
+        // ostr.append(hex.toLowerCase(Locale.ENGLISH));
+      }
+    }
+
+    return (new String(ostr)); // Return the stringbuffer cast as a string.
+
   }
 
   @Override
