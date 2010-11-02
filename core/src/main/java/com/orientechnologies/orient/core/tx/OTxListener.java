698f442f2f6c69b89e904f27134fd4e2c952c3c7
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2010-11-02 17:17:35 +0000

    Fixed issues on RB+Tree and transactions. There's yet a memory leak to fix.

---
+++ core/src/main/java/com/orientechnologies/orient/core/tx/OTxListener.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 1999-2010 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.orient.core.tx;
+
+import com.orientechnologies.orient.core.record.ORecord;
+
+/**
+ * Listener interface to catch events during transactions phases.
+ * 
+ * @author Luca Garulli
+ * 
+ */
+public interface OTxListener {
+	public enum EVENT {
+		BEFORE_COMMIT, AFTER_COMMIT, BEFORE_ROLLBACK, AFTER_ROLLBACK
+	}
+
+	public void onEvent(OTransactionEntry<? extends ORecord<?>> iTxEntry, EVENT iEvent);
+}
