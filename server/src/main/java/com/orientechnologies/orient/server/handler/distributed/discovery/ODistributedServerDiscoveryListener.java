21f99502214967e41b0acbc54b234be9fc109bd3
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2010-11-06 16:07:11 +0000

    Refactored all classes from "*cluster*" to "*distributed*"

---
+++ server/src/main/java/com/orientechnologies/orient/server/handler/distributed/discovery/ODistributedServerDiscoveryListener.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 1999-2010 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.orient.server.handler.distributed.discovery;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.MulticastSocket;
+
+import com.orientechnologies.common.log.OLogManager;
+import com.orientechnologies.common.thread.OSoftThread;
+import com.orientechnologies.orient.core.security.OSecurityManager;
+import com.orientechnologies.orient.server.OServer;
+import com.orientechnologies.orient.server.network.OServerNetworkListener;
+
+public class ODistributedServerDiscoveryListener extends OSoftThread {
+	private final byte[]												recvBuffer	= new byte[50000];
+	private DatagramPacket											dgram;
+	private ODistributedServerDiscoveryManager	serverNode;
+	private OServerNetworkListener							binaryNetworkListener;
+
+	private MulticastSocket											socket;
+
+	public ODistributedServerDiscoveryListener(final ODistributedServerDiscoveryManager iServerNode,
+			final OServerNetworkListener iNetworkListener) {
+		super(OServer.getThreadGroup(), "DiscoveryListener");
+
+		serverNode = iServerNode;
+		binaryNetworkListener = iNetworkListener;
+
+		OLogManager.instance().info(
+				this,
+				"Listening for distributed nodes on IP multicast " + iServerNode.networkMulticastAddress + ":"
+						+ iServerNode.networkMulticastPort);
+
+		dgram = new DatagramPacket(recvBuffer, recvBuffer.length);
+		try {
+			socket = new MulticastSocket(iServerNode.networkMulticastPort);
+			socket.joinGroup(iServerNode.networkMulticastAddress);
+		} catch (IOException e) {
+			OLogManager.instance().error(this, "Can't startup the Discovery Listener service to catch distributed server nodes", e);
+		}
+
+		start();
+	}
+
+	@Override
+	protected void execute() throws Exception {
+		try {
+			// RESET THE LENGTH TO RE-RECEIVE THE PACKET
+			dgram.setLength(recvBuffer.length);
+
+			// BLOCKS UNTIL SOMETHING IS RECEIVED OR SOCKET SHUTDOWN
+			socket.receive(dgram);
+
+			OLogManager.instance().debug(this, "Received multicast packet %d bytes from %s:%d", dgram.getLength(), dgram.getAddress(),
+					dgram.getPort());
+
+			byte[] buffer = new byte[dgram.getLength()];
+			System.arraycopy(dgram.getData(), 0, buffer, 0, buffer.length);
+
+			try {
+				String packet = new String(OSecurityManager.instance()
+						.decrypt(serverNode.securityAlgorithm, serverNode.securityKey, buffer));
+
+				// UNPACK DATA
+				String[] parts = packet.trim().split("\\|");
+
+				int i = 0;
+
+				if (!parts[i].startsWith(ODistributedServerDiscoveryManager.PACKET_HEADER))
+					return;
+
+				if (Integer.parseInt(parts[++i]) != ODistributedServerDiscoveryManager.PROTOCOL_VERSION) {
+					OLogManager.instance().debug(this, "Received bad multicast packet with version %s not equals to the current %d",
+							parts[i], ODistributedServerDiscoveryManager.PROTOCOL_VERSION);
+					return;
+				}
+
+				if (!parts[++i].equals(serverNode.name)) {
+					OLogManager.instance().debug(this, "Received bad multicast packet with cluster name %s not equals to the current %s",
+							parts[i], serverNode.name);
+					return;
+				}
+
+				final String serverAddress = parts[++i];
+				final int serverPort = Integer.parseInt(parts[++i]);
+
+				// CHECK IF THE PACKET WAS SENT BY MYSELF
+				if (serverAddress.equals(binaryNetworkListener.getInboundAddr().getHostName())
+						&& serverPort == binaryNetworkListener.getInboundAddr().getPort())
+					// IT'S ME, JUST IGNORE
+					return;
+
+				// GOOD PACKET, PASS TO THE DISTRIBUTED NODE MANAGER THIS INFO
+				serverNode.receivedDistributedServerNodePresence(serverAddress, serverPort);
+
+			} catch (Exception e) {
+				// WRONG PACKET
+				OLogManager.instance().debug(this, "Received wrong packet from multicast IP", e);
+			}
+		} catch (Throwable t) {
+			OLogManager.instance().error(this, "Error on executing request", t);
+		} finally {
+		}
+	}
+}
---
+++ server/src/main/java/com/orientechnologies/orient/server/handler/distributed/discovery/ODistributedServerDiscoverySignaler.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 1999-2010 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.orient.server.handler.distributed.discovery;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+import com.orientechnologies.common.log.OLogManager;
+import com.orientechnologies.common.thread.OPollerThread;
+import com.orientechnologies.orient.core.OConstants;
+import com.orientechnologies.orient.core.security.OSecurityManager;
+import com.orientechnologies.orient.server.OServer;
+import com.orientechnologies.orient.server.network.OServerNetworkListener;
+
+/**
+ * Sends regularly packets using IP multicast protocol to signal the presence in the network.
+ * 
+ * @author Luca Garulli (l.garulli--at--orientechnologies.com)
+ * 
+ */
+public class ODistributedServerDiscoverySignaler extends OPollerThread {
+	private byte[]					discoveryPacket;
+	private DatagramPacket	dgram;
+	private DatagramSocket	socket;
+
+	public ODistributedServerDiscoverySignaler(final ODistributedServerDiscoveryManager iClusterNode,
+			final OServerNetworkListener iNetworkListener) {
+		super(iClusterNode.networkMulticastHeartbeat, OServer.getThreadGroup(), "DiscoverySignaler");
+
+		String buffer = ODistributedServerDiscoveryManager.PACKET_HEADER + OConstants.ORIENT_VERSION + "|"
+				+ ODistributedServerDiscoveryManager.PROTOCOL_VERSION + "|" + iClusterNode.name + "|"
+				+ iNetworkListener.getInboundAddr().getHostName() + "|" + iNetworkListener.getInboundAddr().getPort();
+
+		discoveryPacket = OSecurityManager.instance().encrypt(iClusterNode.securityAlgorithm, iClusterNode.securityKey,
+				buffer.getBytes());
+
+		try {
+			dgram = new DatagramPacket(discoveryPacket, discoveryPacket.length, iClusterNode.networkMulticastAddress,
+					iClusterNode.networkMulticastPort);
+			socket = new DatagramSocket();
+			start();
+		} catch (Exception e) {
+			OLogManager.instance().error(this, "Can't startup distributed server discovery signaler", e);
+		}
+	}
+
+	@Override
+	protected void execute() throws Exception {
+		OLogManager.instance().debug(this, "Discovering distributed server nodes using IP Multicast with address %s, port %d...",
+				dgram.getAddress(), dgram.getPort());
+
+		try {
+			socket.send(dgram);
+		} catch (Throwable t) {
+			OLogManager.instance().error(this, "Error on sending signal for distributed server presence", t);
+		} finally {
+		}
+	}
+
+	@Override
+	public void shutdown() {
+		socket.close();
+		socket = null;
+		dgram = null;
+		super.shutdown();
+	}
+}
