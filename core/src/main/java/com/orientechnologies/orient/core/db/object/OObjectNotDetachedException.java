24e6840addd3e277b308581346ee7b59702483c4
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2010-12-07 18:55:05 +0000

    Fixed issue 102 Committed contribution by Luca Molino about attach/detach checks

--- core/src/main/java/com/orientechnologies/orient/core/db/object/OObjectNotDetachedException.java
+++ core/src/main/java/com/orientechnologies/orient/core/db/object/OObjectNotDetachedException.java
@@ -19,24 +19,3 @@ public class OObjectNotDetachedException extends RuntimeException {
 	}
 
 }
-package com.orientechnologies.orient.core.db.object;
-
-public class OObjectNotDetachedException extends RuntimeException {
-
-	public OObjectNotDetachedException() {
-		super();
-	}
-
-	public OObjectNotDetachedException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public OObjectNotDetachedException(String message) {
-		super(message);
-	}
-
-	public OObjectNotDetachedException(Throwable cause) {
-		super(cause);
-	}
-
-}
