1ed395d9e9f1769860d260f93a7ec7b9fba8bbf1
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2011-02-05 01:12:33 +0000

    Fixed bug on SQL and float/double conversion reported on ML

--- core/src/main/java/com/orientechnologies/orient/core/sql/filter/OSQLFilterCondition.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/filter/OSQLFilterCondition.java
@@ -57,7 +57,7 @@ public class OSQLFilterCondition {
 		Object l = evaluate(iRecord, left);
 		Object r = evaluate(iRecord, right);
 
-		Object[] convertedValues = checkForConversion(iRecord, l, r);
+		final Object[] convertedValues = checkForConversion(iRecord, l, r);
 		if (convertedValues != null) {
 			l = convertedValues[0];
 			r = convertedValues[1];
@@ -74,12 +74,12 @@ public class OSQLFilterCondition {
 		Object[] result = null;
 
 		// INTEGERS
-		if (r instanceof Integer && !(l instanceof Integer)) {
+		if (r instanceof Integer && !(l instanceof Number)) {
 			if (l instanceof String && ((String) l).indexOf(".") > -1)
 				result = new Object[] { new Float((String) l).intValue(), r };
 			else if (!(l instanceof OQueryRuntimeValueMulti))
 				result = new Object[] { getInteger(l), r };
-		} else if (l instanceof Integer && !(r instanceof Integer)) {
+		} else if (l instanceof Integer && !(r instanceof Number)) {
 			if (r instanceof String && ((String) r).indexOf(".") > -1)
 				result = new Object[] { l, new Float((String) r).intValue() };
 			else if (!(r instanceof OQueryRuntimeValueMulti))
