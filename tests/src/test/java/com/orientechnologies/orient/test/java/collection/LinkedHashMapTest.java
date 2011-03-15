88993b643d74fb69679fe453b1816e2a0acb1fa6
l.garulli@gmail.com <l.garulli@gmail.com@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2011-03-15 22:00:36 +0000

    New test to see if LinkedHashMap is buggy

---
+++ tests/src/test/java/com/orientechnologies/orient/test/java/collection/LinkedHashMapTest.java
@@ -0,0 +1,25 @@
+package com.orientechnologies.orient.test.java.collection;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+public class LinkedHashMapTest {
+
+	@SuppressWarnings("serial")
+	public static void main(String[] args) {
+		LinkedHashMap<Integer, Integer> lhm = new LinkedHashMap<Integer, Integer>(100, 0.7f, true) {
+			@Override
+			protected boolean removeEldestEntry(final Map.Entry<Integer, Integer> iEldest) {
+				return size() > 10;
+			}
+		};
+
+		for (int i = 0; i < 1000000; ++i) {
+			lhm.put(i, i);
+		}
+
+		Assert.assertEquals(lhm.size(), 10);
+	}
+}
