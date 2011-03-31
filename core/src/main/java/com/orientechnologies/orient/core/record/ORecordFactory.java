c9e852071ca963b50bd825d5562b047fdafaea29
l.garulli@gmail.com <l.garulli@gmail.com@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2011-03-31 12:22:27 +0000

    Committed contribution of Sylvain Spinelli about the ORecordFactory class. Now is dynamic and allow external record implementation to being registered and used by OrientDB engine.

--- core/src/main/java/com/orientechnologies/orient/core/record/ORecordFactory.java
+++ core/src/main/java/com/orientechnologies/orient/core/record/ORecordFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2010 Luca Garulli (l.garulli--at--orientechnologies.com)
+ * Copyright 1999-2011 Luca Garulli (l.garulli--at--orientechnologies.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,34 +15,46 @@
  */
 package com.orientechnologies.orient.core.record;
 
+import com.orientechnologies.common.exception.OException;
 import com.orientechnologies.orient.core.db.record.ODatabaseRecord;
 import com.orientechnologies.orient.core.exception.OConfigurationException;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.record.impl.ORecordBytes;
 import com.orientechnologies.orient.core.record.impl.ORecordFlat;
 
+/**
+ * Record factory. To use your own record implementation use the declareRecordType() method. Example of registration of the record
+ * MyRecord:
+ * <p>
+ * <code>
+ * declareRecordType('m', "myrecord", MyRecord.class);
+ * </code>
+ * </p>
+ * 
+ * @author Sylvain Spinelli
+ * @author Luca Garulli (l.garulli--at--orientechnologies.com)
+ * 
+ */
 @SuppressWarnings("unchecked")
 public class ORecordFactory {
-	private static final ORecordFactory	instance	= new ORecordFactory();
+	private static final ORecordFactory							instance				= new ORecordFactory();
+	protected static String[]												recordTypeNames	= new String[Byte.MAX_VALUE];
+	protected static Class<? extends ORecord<?>>[]	recordTypes			= new Class[Byte.MAX_VALUE];
+
+	static {
+		declareRecordType(ODocument.RECORD_TYPE, "document", ODocument.class);
+		declareRecordType(ORecordFlat.RECORD_TYPE, "flat", ORecordFlat.class);
+		declareRecordType(ORecordBytes.RECORD_TYPE, "bytes", ORecordBytes.class);
+	}
 
 	public <T extends ORecord<?>> T newInstance(final ODatabaseRecord iDatabase, final Class<?> iClass) {
 		if (iClass == null)
 			return null;
-
 		try {
-			if (iClass.equals(ODocument.class))
-				return (T) new ODocument(iDatabase);
-
-			else if (iClass.equals(ORecordFlat.class))
-				return (T) new ORecordFlat(iDatabase);
-
-			else if (iClass.equals(ORecordBytes.class))
-				return (T) new ORecordBytes(iDatabase);
-
-			return (T) iClass.newInstance();
-
+			T result = (T) iClass.newInstance();
+			result.setDatabase(iDatabase);
+			return result;
 		} catch (Exception e) {
-
 			throw new OConfigurationException("Error on the creation of the record of class " + iClass, e);
 		}
 	}
@@ -52,24 +64,24 @@ public class ORecordFactory {
 	}
 
 	public static String getRecordTypeName(final byte iRecordType) {
-		if (iRecordType == ODocument.RECORD_TYPE)
-			return "document";
-		else if (iRecordType == ORecordFlat.RECORD_TYPE)
-			return "flat";
-		else if (iRecordType == ORecordBytes.RECORD_TYPE)
-			return "bytes";
-
-		throw new IllegalArgumentException("Unsupported record type: " + iRecordType);
+		String name = recordTypeNames[iRecordType];
+		if (name == null)
+			throw new IllegalArgumentException("Unsupported record type: " + iRecordType);
+		return name;
 	}
 
 	public static ORecordInternal<?> newInstance(final byte iRecordType) {
-		if (iRecordType == ODocument.RECORD_TYPE)
-			return new ODocument();
-		else if (iRecordType == ORecordFlat.RECORD_TYPE)
-			return new ORecordFlat();
-		else if (iRecordType == ORecordBytes.RECORD_TYPE)
-			return new ORecordBytes();
+		try {
+			return (ORecordInternal<?>) recordTypes[iRecordType].newInstance();
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Unsupported record type: " + iRecordType, e);
+		}
+	}
 
-		throw new IllegalArgumentException("Unsupported record type: " + iRecordType);
+	public static void declareRecordType(byte iByte, String iName, Class<? extends ORecordInternal<?>> iClass) {
+		if (recordTypes[iByte] != null)
+			throw new OException("Record type byte '" + iByte + "' already in used : " + recordTypes[iByte].getName());
+		recordTypeNames[iByte] = iName;
+		recordTypes[iByte] = iClass;
 	}
 }
