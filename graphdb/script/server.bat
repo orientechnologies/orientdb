acf24df8bc528faa70d52c781c35c5bf3b868018
l.garulli@gmail.com <l.garulli@gmail.com@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2011-07-09 13:39:54 +0000

    GraphDB: customized scripts to report "GRAPH-DB" on startup

---
+++ graphdb/script/server.bat
@@ -0,0 +1,75 @@
+@echo off
+rem
+rem Copyright (c) 1999-2011 Luca Garulli @www.orientechnologies.com
+rem
+
+echo           .                                              
+echo          .`        `                                     
+echo          ,      `:.                                      
+echo         `,`    ,:`                                       
+echo         .,.   :,,                                        
+echo         .,,  ,,,                                         
+echo    .    .,.:::::  ````                                   
+echo    ,`   .::,,,,::.,,,,,,`;;                      .:      
+echo    `,.  ::,,,,,,,:.,,.`  `                       .:      
+echo     ,,:,:,,,,,,,,::.   `        `         ``     .:      
+echo      ,,:.,,,,,,,,,: `::, ,,   ::,::`   : :,::`  ::::     
+echo       ,:,,,,,,,,,,::,:   ,,  :.    :   ::    :   .:      
+echo        :,,,,,,,,,,:,::   ,,  :      :  :     :   .:      
+echo  `     :,,,,,,,,,,:,::,  ,, .::::::::  :     :   .:      
+echo  `,...,,:,,,,,,,,,: .:,. ,, ,,         :     :   .:      
+echo    .,,,,::,,,,,,,:  `: , ,,  :     `   :     :   .:      
+echo      ...,::,,,,::.. `:  .,,  :,    :   :     :   .:      
+echo           ,::::,,,. `:   ,,   :::::    :     :   .:      
+echo           ,,:` `,,.                                      
+echo          ,,,    .,`                                      
+echo         ,,.     `,                  GRAPH-DB Server        
+echo       ``        `.                                       
+echo                 ``                                       
+echo                 `                                        
+
+rem Guess ORIENTDB_HOME if not defined
+set CURRENT_DIR=%cd%
+
+if exist "%JAVA_HOME%\bin\java.exe" goto setJavaHome
+set JAVA=java
+goto okJava
+
+:setJavaHome
+set JAVA="%JAVA_HOME%\bin\java"
+
+:okJava
+if not "%ORIENTDB_HOME%" == "" goto gotHome
+set ORIENTDB_HOME=%CURRENT_DIR%
+if exist "%ORIENTDB_HOME%\bin\server.bat" goto okHome
+cd ..
+set ORIENTDB_HOME=%cd%
+cd %CURRENT_DIR%
+
+:gotHome
+if exist "%ORIENTDB_HOME%\bin\server.bat" goto okHome
+echo The ORIENTDB_HOME environment variable is not defined correctly
+echo This environment variable is needed to run this program
+goto end
+
+:okHome
+rem Get remaining unshifted command line arguments and save them in the
+set CMD_LINE_ARGS=
+
+:setArgs
+if ""%1""=="""" goto doneSetArgs
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+shift
+goto setArgs
+
+:doneSetArgs
+set CONFIG_FILE=%ORIENTDB_HOME%/config/orientdb-server-config.xml
+set LOG_FILE=%ORIENTDB_HOME%/config/orientdb-server-log.properties
+set LOG_CONSOLE_LEVEL=info
+set LOG_FILE_LEVEL=fine
+set WWW_PATH=%ORIENTDB_HOME%/www
+REM set JAVA_OPTS=-Xms1024m -Xmx1024m
+
+call %JAVA% -server %JAVA_OPTS% -XX:+UseParallelGC -XX:+AggressiveOpts -XX:CompileThreshold=200 -Djava.util.logging.config.file="%LOG_FILE%" -Dorientdb.config.file="%CONFIG_FILE%" -Dorientdb.www.path="%WWW_PATH%" -Dlog.console.level=%LOG_CONSOLE_LEVEL% -Dlog.file.level=%LOG_FILE_LEVEL% -Dorientdb.build.number=@BUILD@ -jar "%ORIENTDB_HOME%\lib\orientdb-server-@VERSION@.jar" %CMD_LINE_ARGS%
+
+:end
---
+++ graphdb/script/server.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#
+# Copyright (c) 1999-2010 Luca Garulli
+#
+
+echo "           .                                              "
+echo "          .\`        \`                                     "
+echo "          ,      \`:.                                      "
+echo "         \`,\`    ,:\`                                       "
+echo "         .,.   :,,                                        "
+echo "         .,,  ,,,                                         "
+echo "    .    .,.:::::  \`\`\`\`                                   "
+echo "    ,\`   .::,,,,::.,,,,,,\`;;                      .:      "
+echo "    \`,.  ::,,,,,,,:.,,.\`  \`                       .:      "
+echo "     ,,:,:,,,,,,,,::.   \`        \`         \`\`     .:      "
+echo "      ,,:.,,,,,,,,,: \`::, ,,   ::,::\`   : :,::\`  ::::     "
+echo "       ,:,,,,,,,,,,::,:   ,,  :.    :   ::    :   .:      "
+echo "        :,,,,,,,,,,:,::   ,,  :      :  :     :   .:      "
+echo "  \`     :,,,,,,,,,,:,::,  ,, .::::::::  :     :   .:      "
+echo "  \`,...,,:,,,,,,,,,: .:,. ,, ,,         :     :   .:      "
+echo "    .,,,,::,,,,,,,:  \`: , ,,  :     \`   :     :   .:      "
+echo "      ...,::,,,,::.. \`:  .,,  :,    :   :     :   .:      "
+echo "           ,::::,,,. \`:   ,,   :::::    :     :   .:      "
+echo "           ,,:\` \`,,.                                      "
+echo "          ,,,    .,\`                                      "
+echo "         ,,.     \`,                     GRAPH-DB Server   "
+echo "       \`\`        \`.                                       "
+echo "                 \`\`                                       "
+echo "                 \`                                        "
+
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+# Get standard environment variables
+PRGDIR=`dirname "$PRG"`
+
+# Only set ORIENTDB_HOME if not already set
+[ -f "$ORIENTDB_HOME"/bin/orient.sh ] || ORIENTDB_HOME=`cd "$PRGDIR/.." ; pwd`
+export ORIENTDB_HOME
+
+CONFIG_FILE=$ORIENTDB_HOME/config/orientdb-server-config.xml
+LOG_FILE=$ORIENTDB_HOME/config/orientdb-server-log.properties
+LOG_CONSOLE_LEVEL=info
+LOG_FILE_LEVEL=fine
+WWW_PATH=$ORIENTDB_HOME/www
+#JAVA_OPTS=-Xms1024m -Xmx1024m
+
+java -server $JAVA_OPTS -XX:+UseParallelGC -XX:+AggressiveOpts -XX:CompileThreshold=200 -Djava.util.logging.config.file="$LOG_FILE" -Dorientdb.config.file="$CONFIG_FILE" -Dorientdb.www.path="$WWW_PATH" -Dlog.console.level=$LOG_CONSOLE_LEVEL -Dlog.file.level=$LOG_FILE_LEVEL -Dorientdb.build.number=@BUILD@ -jar "$ORIENTDB_HOME/lib/orientdb-server-@VERSION@.jar"
