9a15cab296f6e31157ea715cb2d194f4c8c80fba
l.garulli@gmail.com <l.garulli@gmail.com@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2011-07-09 13:41:07 +0000

    GraphDB: added Test Graph Loader taken by Blueprints example (n.2)

--- graphdb/test/main/java/com/orientechnologies/orient/graph/gremlin/TestLoadGraph.java
+++ graphdb/test/main/java/com/orientechnologies/orient/graph/gremlin/TestLoadGraph.java
@@ -6,18 +6,24 @@ import com.tinkerpop.blueprints.pgm.util.graphml.GraphMLReader;
 
 public class TestLoadGraph {
 	private static final String	INPUT_FILE	= "/graph-example-2.xml";
-	private static final String	DBURL				= "local:../../databases/tinkerpop";
+	private static final String	DBURL				= "local:target/databases/tinkerpop";
 
 	// private static final String DBURL = "remote:localhost/graph";
 
 	public static void main(String[] args) throws Exception {
 		OrientGraph g = new OrientGraph(DBURL);
 
-		System.out.println("Importing graph from file '" + INPUT_FILE + "' into database: " + g);
+		System.out.println("Importing graph from file '" + INPUT_FILE + "' into database: " + g + "...");
+
+		final long startTime = System.currentTimeMillis();
 
 		g.setTransactionMode(Mode.MANUAL);
 
 		GraphMLReader.inputGraph(g, Thread.class.getResourceAsStream(INPUT_FILE), 0, null, null, null);
 		g.shutdown();
+
+		System.out.println("Imported in " + (System.currentTimeMillis() - startTime) + "ms. Vertexes: "
+				+ g.getRawGraph().countVertexes() + " Edges: " + g.getRawGraph().countEdges());
+
 	}
 }
