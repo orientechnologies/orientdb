f1967b5bcd4f5cfb8d9d3859eb44150e6eaa0f02
l.garulli@gmail.com <l.garulli@gmail.com@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2011-07-18 16:01:46 +0000

    Added test case to try if Collections are taken as parameter of query

--- tests/src/test/java/com/orientechnologies/orient/test/database/auto/SQLSelectTest.java
+++ tests/src/test/java/com/orientechnologies/orient/test/database/auto/SQLSelectTest.java
@@ -410,6 +410,20 @@ public class SQLSelectTest {
 	}
 
 	@Test
+	public void queryInAsParameter() {
+		database.open("admin", "admin");
+
+		List<ODocument> roles = database.query(new OSQLSynchQuery<Object>("select from orole"));
+		roles.remove(0);
+
+		List<ODocument> result = database.command(new OSQLSynchQuery<ODocument>("select * from OUser where roles in ?")).execute(roles);
+
+		Assert.assertEquals(result.size(), 2);
+
+		database.close();
+	}
+
+	@Test
 	public void queryAnyOperator() {
 		database.open("admin", "admin");
 
