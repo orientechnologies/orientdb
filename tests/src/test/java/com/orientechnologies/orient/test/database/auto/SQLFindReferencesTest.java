3f01e4f82d4bb00155a540c9861af9d17f2213b6
l.garulli@gmail.com <l.garulli@gmail.com@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2011-07-29 14:33:07 +0000

    Fix by Luca Molino about the full rewriting of SQLFindReferencesTest. All the next tests should made in this way!

--- tests/src/test/java/com/orientechnologies/orient/test/database/auto/SQLFindReferencesTest.java
+++ tests/src/test/java/com/orientechnologies/orient/test/database/auto/SQLFindReferencesTest.java
@@ -15,76 +15,242 @@
  */
 package com.orientechnologies.orient.test.database.auto;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.id.ORID;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.OCommandSQL;
 
 @Test(groups = "sql-findReferences")
 public class SQLFindReferencesTest {
-	private ODatabaseDocument	database;
+
+	private static final String	WORKPLACE	= "Workplace";
+	private static final String	WORKER		= "Worker";
+	private static final String	CAR				= "Car";
+
+	private ODatabaseDocument		database;
+
+	private ORID								carID;
+
+	private ORID								johnDoeID;
+
+	private ORID								janeDoeID;
+
+	private ORID								chuckNorrisID;
+
+	private ORID								jackBauerID;
+
+	private ORID								ctuID;
+
+	private ORID								fbiID;
 
 	@Parameters(value = "url")
 	public SQLFindReferencesTest(String iURL) {
 		database = new ODatabaseDocumentTx(iURL);
 	}
-//
-//	@Test
-//	public void findSimpleReference() {
-//		if (database.isClosed())
-//			database.open("admin", "admin");
-//
-//		Collection<ORID> result = database.command(new OCommandSQL("find references 14:58")).execute();
-//
-//		Assert.assertTrue(result.size() == 1);
-//		Assert.assertTrue(result.iterator().next().toString().equals("#13:54"));
-//
-//		result = database.command(new OCommandSQL("find references 19:0")).execute();
-//
-//		Assert.assertTrue(result.size() == 2);
-//		ORID rid = result.iterator().next();
-//		Assert.assertTrue(rid.toString().equals("#22:0") || rid.toString().equals("#21:0"));
-//		rid = result.iterator().next();
-//		Assert.assertTrue(rid.toString().equals("#22:0") || rid.toString().equals("#21:0"));
-//
-//		result = database.command(new OCommandSQL("find references 9:0")).execute();
-//		Assert.assertTrue(result.size() == 0);
-//
-//		result.clear();
-//		result = null;
-//
-//		database.close();
-//	}
-//
-//	@Test
-//	public void findReferenceByClassAndClusters() {
-//		if (database.isClosed())
-//			database.open("admin", "admin");
-//
-//		Collection<ORID> result = database.command(new OCommandSQL("find references #19:0 [GraphCar]")).execute();
-//
-//		Assert.assertEquals(result.size(), 1);
-//		Assert.assertTrue(result.iterator().next().toString().equals("#21:0"));
-//
-//		result = database.command(new OCommandSQL("find references 19:0 [company,cluster:GraphMotocycle]")).execute();
-//
-//		Assert.assertTrue(result.size() == 1);
-//		Assert.assertTrue(result.iterator().next().toString().equals("#22:0"));
-//
-//		result = database.command(new OCommandSQL("find references 19:0 [company,account,cluster:OGraphEdge]")).execute();
-//
-//		Assert.assertTrue(result.size() == 0);
-//
-//		result.clear();
-//		result = null;
-//
-//		database.close();
-//	}
+
+	@Test
+	public void findSimpleReference() {
+		if (database.isClosed())
+			database.open("admin", "admin");
+
+		Collection<ORID> result = database.command(new OCommandSQL("find references " + carID.toString())).execute();
+
+		Assert.assertTrue(result.size() == 1);
+		Assert.assertTrue(result.iterator().next().toString().equals(johnDoeID.toString()));
+
+		result = database.command(new OCommandSQL("find references " + chuckNorrisID.toString())).execute();
+
+		Assert.assertTrue(result.size() == 2);
+		ORID rid = result.iterator().next();
+		Assert.assertTrue(rid.toString().equals(ctuID.toString()) || rid.toString().equals(fbiID.toString()));
+		rid = result.iterator().next();
+		Assert.assertTrue(rid.toString().equals(ctuID.toString()) || rid.toString().equals(fbiID.toString()));
+
+		result = database.command(new OCommandSQL("find references " + johnDoeID.toString())).execute();
+		Assert.assertTrue(result.size() == 0);
+
+		result.clear();
+		result = null;
+
+		database.close();
+	}
+
+	@Test
+	public void findReferenceByClassAndClusters() {
+		if (database.isClosed())
+			database.open("admin", "admin");
+
+		Collection<ORID> result = database.command(new OCommandSQL("find references " + janeDoeID.toString() + " [" + WORKPLACE + "]"))
+				.execute();
+
+		Assert.assertEquals(result.size(), 1);
+		Assert.assertTrue(result.iterator().next().toString().equals(ctuID.toString()));
+
+		result = database.command(
+				new OCommandSQL("find references " + jackBauerID.toString() + " [" + WORKPLACE + ",cluster:" + CAR + "]")).execute();
+
+		Assert.assertTrue(result.size() == 3);
+		ORID rid = result.iterator().next();
+		Assert.assertTrue(rid.toString().equals(ctuID.toString()) || rid.toString().equals(fbiID.toString())
+				|| rid.toString().equals(carID.toString()));
+		rid = result.iterator().next();
+		Assert.assertTrue(rid.toString().equals(ctuID.toString()) || rid.toString().equals(fbiID.toString())
+				|| rid.toString().equals(carID.toString()));
+		rid = result.iterator().next();
+		Assert.assertTrue(rid.toString().equals(ctuID.toString()) || rid.toString().equals(fbiID.toString())
+				|| rid.toString().equals(carID.toString()));
+
+		result = database.command(
+				new OCommandSQL("find references " + johnDoeID.toString() + " [" + WORKPLACE + "," + CAR + ",cluster:" + WORKER + "]"))
+				.execute();
+
+		Assert.assertTrue(result.size() == 0);
+
+		result.clear();
+		result = null;
+
+		database.close();
+	}
+
+	@BeforeClass
+	public void createTestEnviroment() {
+		if (database.isClosed())
+			database.open("admin", "admin");
+		createSchema();
+		populateDatabase();
+		database.close();
+	}
+
+	private void createSchema() {
+		OClass worker = database.getMetadata().getSchema().createClass(WORKER);
+		OClass workplace = database.getMetadata().getSchema().createClass(WORKPLACE);
+		OClass car = database.getMetadata().getSchema().createClass(CAR);
+
+		worker.createProperty("name", OType.STRING);
+		worker.createProperty("surname", OType.STRING);
+		worker.createProperty("colleagues", OType.LINKLIST, worker);
+		worker.createProperty("car", OType.LINK, car);
+
+		workplace.createProperty("name", OType.STRING);
+		workplace.createProperty("boss", OType.LINK, workplace);
+		workplace.createProperty("workers", OType.LINKLIST, workplace);
+
+		car.createProperty("plate", OType.STRING);
+		car.createProperty("owner", OType.LINK, worker);
+
+		database.getMetadata().getSchema().save();
+	}
+
+	private void populateDatabase() {
+		ODocument car = new ODocument(database, CAR);
+		car.field("plate", "JINF223S");
+
+		ODocument johnDoe = new ODocument(database, WORKER);
+		johnDoe.field("name", "John");
+		johnDoe.field("surname", "Doe");
+		johnDoe.field("car", car);
+		johnDoe.save();
+		johnDoeID = johnDoe.getIdentity().copy();
+
+		ODocument janeDoe = new ODocument(database, WORKER);
+		janeDoe.field("name", "Jane");
+		janeDoe.field("surname", "Doe");
+		janeDoe.save();
+		janeDoeID = janeDoe.getIdentity().copy();
+
+		ODocument chuckNorris = new ODocument(database, WORKER);
+		chuckNorris.field("name", "Chuck");
+		chuckNorris.field("surname", "Norris");
+		chuckNorris.save();
+		chuckNorrisID = chuckNorris.getIdentity().copy();
+
+		ODocument jackBauer = new ODocument(database, WORKER);
+		jackBauer.field("name", "Jack");
+		jackBauer.field("surname", "Bauer");
+		jackBauer.save();
+		jackBauerID = jackBauer.getIdentity().copy();
+
+		ODocument ctu = new ODocument(database, WORKPLACE);
+		ctu.field("name", "CTU");
+		ctu.field("boss", jackBauer);
+		List<ODocument> workplace1Workers = new ArrayList<ODocument>();
+		workplace1Workers.add(chuckNorris);
+		workplace1Workers.add(janeDoe);
+		ctu.field("workers", workplace1Workers);
+		ctu.save();
+		ctuID = ctu.getIdentity().copy();
+
+		ODocument fbi = new ODocument(database, WORKPLACE);
+		fbi.field("name", "FBI");
+		fbi.field("boss", chuckNorris);
+		List<ODocument> workplace2Workers = new ArrayList<ODocument>();
+		workplace2Workers.add(chuckNorris);
+		workplace2Workers.add(jackBauer);
+		fbi.field("workers", workplace2Workers);
+		fbi.save();
+		fbiID = fbi.getIdentity().copy();
+
+		car.field("owner", jackBauer);
+		car.save();
+		carID = car.getIdentity().copy();
+	}
+
+	@AfterClass
+	public void deleteTestEnviroment() {
+		if (database.isClosed())
+			database.open("admin", "admin");
+		carID.reset();
+		carID = null;
+		johnDoeID.reset();
+		johnDoeID = null;
+		janeDoeID.reset();
+		janeDoeID = null;
+		chuckNorrisID.reset();
+		chuckNorrisID = null;
+		jackBauerID.reset();
+		jackBauerID = null;
+		ctuID.reset();
+		ctuID = null;
+		fbiID.reset();
+		fbiID = null;
+		deleteSchema();
+		database.close();
+	}
+
+	private void deleteSchema() {
+		dropClass(CAR);
+		dropClass(WORKER);
+		dropClass(WORKPLACE);
+	}
+
+	private void dropClass(String iClass) {
+		OCommandSQL dropClassCommand = new OCommandSQL("drop class " + iClass);
+		database.command(dropClassCommand).execute();
+		database.getMetadata().getSchema().save();
+		database.getMetadata().getSchema().reload();
+		database.reload();
+		while (database.getMetadata().getSchema().existsClass(iClass)) {
+			database.getMetadata().getSchema().dropClass(iClass);
+			database.getMetadata().getSchema().save();
+			database.reload();
+		}
+		while (database.getClusterIdByName(iClass) > -1) {
+			database.dropCluster(iClass);
+			database.reload();
+		}
+	}
 
 }
