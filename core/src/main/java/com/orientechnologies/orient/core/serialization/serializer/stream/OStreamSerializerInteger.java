f07edb8d470a37c564fbb662144da9c87f18770a
l.garulli@gmail.com <l.garulli@gmail.com@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2011-08-17 07:45:09 +0000

    Minor: added integer serialized to build ad-hoc indexes with integer values

---
+++ core/src/main/java/com/orientechnologies/orient/core/serialization/serializer/stream/OStreamSerializerInteger.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 1999-2010 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.orient.core.serialization.serializer.stream;
+
+import java.io.IOException;
+
+import com.orientechnologies.orient.core.db.record.ODatabaseRecord;
+import com.orientechnologies.orient.core.serialization.OBinaryProtocol;
+
+public class OStreamSerializerInteger implements OStreamSerializer {
+	public static final String										NAME			= "i";
+
+	public static final OStreamSerializerInteger	INSTANCE	= new OStreamSerializerInteger();
+
+	public String getName() {
+		return NAME;
+	}
+
+	public Object fromStream(final ODatabaseRecord iDatabase, final byte[] iStream) throws IOException {
+		return OBinaryProtocol.bytes2int(iStream);
+	}
+
+	public byte[] toStream(final ODatabaseRecord iDatabase, final Object iObject) throws IOException {
+		return OBinaryProtocol.int2bytes((Integer) iObject);
+	}
+}
