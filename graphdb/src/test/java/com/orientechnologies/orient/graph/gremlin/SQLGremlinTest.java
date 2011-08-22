014ff1e1bb9efa8c4a482bc5450f020c8f5376d2
l.garulli@gmail.com <l.garulli@gmail.com@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2011-08-22 00:45:10 +0000

    GraphDB: Fixed Gremlin test

--- graphdb/src/test/java/com/orientechnologies/orient/graph/gremlin/SQLGremlinTest.java
+++ graphdb/src/test/java/com/orientechnologies/orient/graph/gremlin/SQLGremlinTest.java
@@ -5,24 +5,24 @@ import java.util.List;
 import org.testng.annotations.Test;
 
 import com.orientechnologies.orient.core.db.graph.OGraphDatabase;
+import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.OSQLEngine;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
-import com.orientechnologies.orient.graph.gremlin.OSQLFunctionGremlin;
 
 public class SQLGremlinTest {
 	@Test
 	public void f() {
 		OSQLEngine.getInstance().registerFunction(OSQLFunctionGremlin.NAME, OSQLFunctionGremlin.class);
 
-		OGraphDatabase db = new OGraphDatabase("local:C:/temp/databases/gremlin");
+		OGraphDatabase db = new OGraphDatabase("local:target/databases/gremlin");
 		if (db.exists())
 			db.open("admin", "admin");
 		else
 			db.create();
 
-		// ODocument vertex1 = (ODocument) db.createVertex().field("label", "car").save();
-		// ODocument vertex2 = (ODocument) db.createVertex().field("label", "pilot").save();
-		// ODocument edge = (ODocument) db.createEdge(vertex1, vertex2).field("label", "drives").save();
+		ODocument vertex1 = (ODocument) db.createVertex().field("label", "car").save();
+		ODocument vertex2 = (ODocument) db.createVertex().field("label", "pilot").save();
+		ODocument edge = (ODocument) db.createEdge(vertex1, vertex2).field("label", "drives").save();
 
 		List<?> result = db.query(new OSQLSynchQuery<Object>(
 				"select gremlin('current.out.in') as value from V where out.size() > 0 limit 3"));
