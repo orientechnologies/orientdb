ea42c245cbbe3387968c7e41376fe5b5bd419d88
l.garulli@gmail.com <l.garulli@gmail.com@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2011-10-11 17:47:25 +0000

    Fixed issue 405: huge patch by Andrey and Artem about a complete refactoring of indexes to support multiple fields at all the levels, plus improvements in indexes and mvrb-tree. And as icing on the cake about 150 new test cases!

---
+++ tests/src/test/java/com/orientechnologies/orient/test/database/auto/SQLDropPropertyIndexTest.java
@@ -0,0 +1,146 @@
+package com.orientechnologies.orient.test.database.auto;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.exception.OCommandExecutionException;
+import com.orientechnologies.orient.core.index.OCompositeIndexDefinition;
+import com.orientechnologies.orient.core.index.OIndex;
+import com.orientechnologies.orient.core.index.OIndexDefinition;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OSchema;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.sql.OCommandSQL;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+@Test(groups = {"index"})
+public class SQLDropPropertyIndexTest {
+    private final ODatabaseDocumentTx database;
+    private static final OType EXPECTED_PROP1_TYPE = OType.DOUBLE;
+    private static final OType EXPECTED_PROP2_TYPE = OType.INTEGER;
+
+    @Parameters(value = "url")
+    public SQLDropPropertyIndexTest(final String iURL) {
+        database =  new ODatabaseDocumentTx(iURL);
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        database.open("admin", "admin");
+
+        final OSchema schema = database.getMetadata().getSchema();
+        final OClass oClass = schema.createClass("DropPropertyIndexTestClass");
+        oClass.createProperty("prop1", EXPECTED_PROP1_TYPE);
+        oClass.createProperty("prop2", EXPECTED_PROP2_TYPE);
+
+        schema.save();
+    }
+
+    @AfterMethod
+    public void afterMethod() throws Exception {
+        database.command(new OCommandSQL("drop class DropPropertyIndexTestClass")).execute();
+        database.getMetadata().getSchema().reload();
+        database.close();
+    }
+
+    @Test
+    public void testForcePropertyEnabled() throws Exception {
+        database.command(new OCommandSQL("CREATE INDEX DropPropertyIndexCompositeIndex ON DropPropertyIndexTestClass (prop2, prop1) UNIQUE")).execute();
+        database.getMetadata().getIndexManager().reload();
+
+        OIndex index = database.getMetadata().getSchema().getClass("DropPropertyIndexTestClass").
+                getClassIndex("DropPropertyIndexCompositeIndex");
+        Assert.assertNotNull(index);
+
+        database.command(new OCommandSQL("DROP PROPERTY DropPropertyIndexTestClass.prop1 FORCE")).execute();
+        database.getMetadata().getIndexManager().reload();
+
+        index = database.getMetadata().getSchema().getClass("DropPropertyIndexTestClass").
+                getClassIndex("DropPropertyIndexCompositeIndex");
+
+        Assert.assertNull(index);
+    }
+
+    @Test
+    public void testForcePropertyEnabledBrokenCase() throws Exception {
+        database.command(new OCommandSQL("CREATE INDEX DropPropertyIndexCompositeIndex ON DropPropertyIndexTestClass (prop2, prop1) UNIQUE")).
+                execute();
+        database.getMetadata().getIndexManager().reload();
+
+        OIndex index = database.getMetadata().getSchema().getClass("DropPropertyIndexTestClass").
+                getClassIndex("DropPropertyIndexCompositeIndex");
+        Assert.assertNotNull(index);
+
+        database.command(new OCommandSQL("DROP PROPERTY DropPropertyIndextestclasS.pRoP1 FORCE")).execute();
+        database.getMetadata().getIndexManager().reload();
+
+        index = database.getMetadata().getSchema().getClass("DropPropertyIndexTestClass").
+                getClassIndex("DropPropertyIndexCompositeIndex");
+
+        Assert.assertNull(index);
+    }
+
+    @Test
+    public void testForcePropertyDisabled() throws Exception {
+        database.command(new OCommandSQL("CREATE INDEX DropPropertyIndexCompositeIndex ON DropPropertyIndexTestClass (prop1, prop2) UNIQUE")).execute();
+        database.getMetadata().getIndexManager().reload();
+
+        OIndex index = database.getMetadata().getSchema().getClass("DropPropertyIndexTestClass").
+                getClassIndex("DropPropertyIndexCompositeIndex");
+        Assert.assertNotNull(index);
+
+        try {
+            database.command(new OCommandSQL("DROP PROPERTY DropPropertyIndexTestClass.prop1")).execute();
+            Assert.fail();
+        } catch (OCommandExecutionException e) {
+            Assert.assertEquals(e.getMessage(), "Property contains in indexes (" +
+                    "DropPropertyIndexCompositeIndex" +
+                    ").Please, drop this indexes before removing property or use FORCE parameter.");
+        }
+
+        database.getMetadata().getIndexManager().reload();
+
+        index = database.getMetadata().getSchema().getClass("DropPropertyIndexTestClass").
+                getClassIndex("DropPropertyIndexCompositeIndex");
+
+        Assert.assertNotNull(index);
+
+        final OIndexDefinition indexDefinition = index.getDefinition();
+
+        Assert.assertTrue(indexDefinition instanceof OCompositeIndexDefinition);
+        Assert.assertEquals(indexDefinition.getFields(), Arrays.asList("prop1", "prop2"));
+        Assert.assertEquals(indexDefinition.getTypes(), new OType[]{EXPECTED_PROP1_TYPE, EXPECTED_PROP2_TYPE});
+        Assert.assertEquals(index.getType(), "UNIQUE");
+    }
+
+    @Test
+    public void testForcePropertyDisabledBrokenCase() throws Exception {
+        database.command(new OCommandSQL("CREATE INDEX DropPropertyIndexCompositeIndex ON DropPropertyIndexTestClass (prop1, prop2) UNIQUE")).execute();
+
+        try {
+            database.command(new OCommandSQL("DROP PROPERTY DropPropertyIndextestclaSS.proP1")).execute();
+            Assert.fail();
+        } catch (OCommandExecutionException e) {
+            Assert.assertEquals(e.getMessage(), "Property contains in indexes (" +
+                    "DropPropertyIndexCompositeIndex" +
+                    ").Please, drop this indexes before removing property or use FORCE parameter.");
+        }
+
+        database.getMetadata().getIndexManager().reload();
+        final OIndex index = database.getMetadata().getSchema().getClass("DropPropertyIndexTestClass").
+                getClassIndex("DropPropertyIndexCompositeIndex");
+
+        Assert.assertNotNull(index);
+
+        final OIndexDefinition indexDefinition = index.getDefinition();
+
+        Assert.assertTrue(indexDefinition instanceof OCompositeIndexDefinition);
+        Assert.assertEquals(indexDefinition.getFields(), Arrays.asList("prop1", "prop2"));
+        Assert.assertEquals(indexDefinition.getTypes(), new OType[]{EXPECTED_PROP1_TYPE, EXPECTED_PROP2_TYPE});
+        Assert.assertEquals(index.getType(), "UNIQUE");
+    }
+}
---
+++ tests/src/test/java/com/orientechnologies/orient/test/database/auto/SchemaIndexTest.java
@@ -0,0 +1,82 @@
+package com.orientechnologies.orient.test.database.auto;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.exception.OSchemaException;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OSchema;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.sql.OCommandSQL;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+@Test(groups = {"index"})
+public class SchemaIndexTest {
+    private final ODatabaseDocumentTx database;
+
+    @Parameters(value = "url")
+    public SchemaIndexTest(final String iURL) {
+        database =  new ODatabaseDocumentTx(iURL);
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        database.open("admin", "admin");
+
+        final OSchema schema = database.getMetadata().getSchema();
+        final OClass superTest = schema.createClass("SchemaSharedIndexSuperTest");
+        final OClass test = schema.createClass("SchemaIndexTest", superTest);
+        test.createProperty("prop1", OType.DOUBLE);
+        test.createProperty("prop2", OType.DOUBLE);
+
+        schema.save();
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        database.command(new OCommandSQL("drop class SchemaIndexTest")).execute();
+        database.command(new OCommandSQL("drop class SchemaSharedIndexSuperTest")).execute();
+        database.getMetadata().getSchema().reload();
+
+        database.close();
+    }
+
+    @Test
+    public void testDropClass() throws Exception {
+        database.command(new OCommandSQL("CREATE INDEX SchemaSharedIndexCompositeIndex ON SchemaIndexTest (prop1, prop2) UNIQUE")).execute();
+        database.getMetadata().getIndexManager().reload();
+        Assert.assertNotNull(database.getMetadata().getIndexManager().getIndex("SchemaSharedIndexCompositeIndex"));
+
+        database.getMetadata().getSchema().dropClass("SchemaIndexTest");
+        database.getMetadata().getSchema().reload();
+        database.getMetadata().getIndexManager().reload();
+
+        Assert.assertNull(database.getMetadata().getSchema().getClass("SchemaIndexTest"));
+        Assert.assertNotNull(database.getMetadata().getSchema().getClass("SchemaSharedIndexSuperTest"));
+
+        Assert.assertNull(database.getMetadata().getIndexManager().getIndex("SchemaSharedIndexCompositeIndex"));
+    }
+
+    @Test
+    public void testDropSuperClass() throws Exception {
+        database.command(new OCommandSQL("CREATE INDEX SchemaSharedIndexCompositeIndex ON SchemaIndexTest (prop1, prop2) UNIQUE")).execute();
+        database.getMetadata().getIndexManager().reload();
+
+        try {
+            database.getMetadata().getSchema().dropClass("SchemaSharedIndexSuperTest");
+            Assert.fail();
+        } catch (OSchemaException e) {
+            Assert.assertEquals(e.getMessage(), "Class SchemaSharedIndexSuperTest can't be dropped since has sub classes. Remove the dependencies before to drop it again");
+        }
+
+        database.getMetadata().getSchema().reload();
+
+        Assert.assertNotNull(database.getMetadata().getSchema().getClass("SchemaIndexTest"));
+        Assert.assertNotNull(database.getMetadata().getSchema().getClass("SchemaSharedIndexSuperTest"));
+
+        Assert.assertNotNull(database.getMetadata().getIndexManager().getIndex("SchemaSharedIndexCompositeIndex"));
+    }
+}
