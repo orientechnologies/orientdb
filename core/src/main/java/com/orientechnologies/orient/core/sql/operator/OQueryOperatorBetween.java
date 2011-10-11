acacbb3a4be10a03c7420965fa4f7145b0f4f233
l.garulli@gmail.com <l.garulli@gmail.com@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2011-10-11 12:51:20 +0000

    Minor: remove warning

--- core/src/main/java/com/orientechnologies/orient/core/sql/operator/OQueryOperatorBetween.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/operator/OQueryOperatorBetween.java
@@ -15,13 +15,12 @@
  */
 package com.orientechnologies.orient.core.sql.operator;
 
-import com.orientechnologies.common.collection.OIterableObject;
+import java.util.Iterator;
+
 import com.orientechnologies.common.collection.OMultiValue;
 import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.ORecordInternal;
 import com.orientechnologies.orient.core.sql.filter.OSQLFilterCondition;
-
-import java.util.Iterator;
 
 /**
  * BETWEEN operator.
@@ -39,11 +38,11 @@ public class OQueryOperatorBetween extends OQueryOperatorEqualityNotNulls {
 	@SuppressWarnings("unchecked")
 	protected boolean evaluateExpression(final ORecordInternal<?> iRecord, final OSQLFilterCondition iCondition, final Object iLeft,
 			final Object iRight) {
-        if(!OMultiValue.isMultiValue(iRight.getClass())){
-            throw new IllegalArgumentException("Found '" + iRight + "' while was expected: " + getSyntax());
-        }
+		if (!OMultiValue.isMultiValue(iRight.getClass())) {
+			throw new IllegalArgumentException("Found '" + iRight + "' while was expected: " + getSyntax());
+		}
 
-        final Iterator valueIterator = OMultiValue.getMultiValueIterator(iRight);
+		final Iterator<?> valueIterator = OMultiValue.getMultiValueIterator(iRight);
 
 		if (OMultiValue.getSize(iRight) != 3)
 			throw new IllegalArgumentException("Found '" + OMultiValue.toString(iRight) + "' while was expected: " + getSyntax());
@@ -51,7 +50,7 @@ public class OQueryOperatorBetween extends OQueryOperatorEqualityNotNulls {
 		final Object right1 = OType.convert(valueIterator.next(), iLeft.getClass());
 		if (right1 == null)
 			return false;
-        valueIterator.next();
+		valueIterator.next();
 		final Object right2 = OType.convert(valueIterator.next(), iLeft.getClass());
 		if (right2 == null)
 			return false;
