aee082437edc1654820ae5d05bf0f6fe51e50523
l.garulli@gmail.com <l.garulli@gmail.com@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-01-16 19:12:42 +0000

    Added more test case to Gremlin test suite

--- graphdb/src/test/java/com/orientechnologies/orient/graph/gremlin/LocalGremlinTest.java
+++ graphdb/src/test/java/com/orientechnologies/orient/graph/gremlin/LocalGremlinTest.java
@@ -1,8 +1,11 @@
 package com.orientechnologies.orient.graph.gremlin;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import junit.framework.Assert;
 
 import org.testng.annotations.Test;
 
@@ -68,4 +71,32 @@ public class LocalGremlinTest {
 		db.close();
 	}
 
+	@Test
+	public void testMultipleExpressions() {
+		OGraphDatabase db = new OGraphDatabase("local:target/databases/tinkerpop");
+		db.open("admin", "admin");
+
+		List<OIdentifiable> result = db.command(new OCommandSQL("SELECT gremlin('m = []; m << 1; m;') FROM #6:1")).execute();
+
+		Assert.assertEquals(1, result.size());
+		Assert.assertEquals(1, ((Collection) ((ODocument) result.get(0)).field("gremlin")).iterator().next());
+
+		db.close();
+	}
+
+	@Test
+	public void testMultipleExpressionsSideEffects() {
+		OGraphDatabase db = new OGraphDatabase("local:target/databases/tinkerpop");
+		db.open("admin", "admin");
+
+		List<OIdentifiable> result = db.command(
+				new OCommandSQL(
+						"SELECT gremlin('m = []; current.out.sideEffect({ m << it.id }).out.out.sideEffect({ m << it.id })') FROM #6:1"))
+				.execute();
+
+		Assert.assertEquals(1, result.size());
+		System.out.println("Query result: " + result);
+
+		db.close();
+	}
 }
