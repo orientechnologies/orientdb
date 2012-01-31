ef8d93b6efd8551a41a048854c659d1568d2f23f
l.garulli@gmail.com <l.garulli@gmail.com@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-01-31 14:16:05 +0000

    Changed AtomicReference<OLevel2RecordCache> in just OLevel2RecordCache because the class is used by 1 thread per time

--- core/src/main/java/com/orientechnologies/orient/core/cache/OLevel1RecordCache.java
+++ core/src/main/java/com/orientechnologies/orient/core/cache/OLevel1RecordCache.java
@@ -15,6 +15,8 @@
  */
 package com.orientechnologies.orient.core.cache;
 
+import static com.orientechnologies.orient.core.storage.OStorage.CLUSTER_INDEX_NAME;
+
 import com.orientechnologies.common.profiler.OProfiler;
 import com.orientechnologies.orient.core.config.OGlobalConfiguration;
 import com.orientechnologies.orient.core.db.ODatabaseRecordThreadLocal;
@@ -22,128 +24,124 @@ import com.orientechnologies.orient.core.db.record.ODatabaseRecord;
 import com.orientechnologies.orient.core.id.ORID;
 import com.orientechnologies.orient.core.record.ORecordInternal;
 
-import java.util.concurrent.atomic.AtomicReference;
-
-import static com.orientechnologies.orient.core.storage.OStorage.CLUSTER_INDEX_NAME;
-
 /**
  * Per database primary cache of documents
- *
+ * 
  * @author Luca Garulli
  */
 public class OLevel1RecordCache extends OAbstractRecordCache {
-  private AtomicReference<OLevel2RecordCache> secondary = new AtomicReference<OLevel2RecordCache>();
-  private String CACHE_HIT;
-  private String CACHE_MISS;
+	private OLevel2RecordCache	secondary	= null;
+	private String							CACHE_HIT;
+	private String							CACHE_MISS;
 
-  public OLevel1RecordCache() {
-    super(new OCacheLocator().primaryCache());
-  }
+	public OLevel1RecordCache() {
+		super(new OCacheLocator().primaryCache());
+	}
 
-  @Override
-  public void startup() {
-    ODatabaseRecord db = ODatabaseRecordThreadLocal.INSTANCE.get();
-    secondary.set(db.getLevel2Cache());
+	@Override
+	public void startup() {
+		ODatabaseRecord db = ODatabaseRecordThreadLocal.INSTANCE.get();
+		secondary = db.getLevel2Cache();
 
-    profilerPrefix = "db." + db.getName();
-    CACHE_HIT = profilerPrefix + ".cache.found";
-    CACHE_MISS = profilerPrefix + ".cache.notFound";
+		profilerPrefix = "db." + db.getName();
+		CACHE_HIT = profilerPrefix + ".cache.found";
+		CACHE_MISS = profilerPrefix + ".cache.notFound";
 
-    excludedCluster = db.getClusterIdByName(CLUSTER_INDEX_NAME);
+		excludedCluster = db.getClusterIdByName(CLUSTER_INDEX_NAME);
 
-    super.startup();
-    setEnable(OGlobalConfiguration.CACHE_LEVEL1_ENABLED.getValueAsBoolean());
-  }
+		super.startup();
+		setEnable(OGlobalConfiguration.CACHE_LEVEL1_ENABLED.getValueAsBoolean());
+	}
 
-  /**
-   * Push record to cache. Identifier of record used as access key
-   *
-   * @param record record that should be cached
-   */
-  public void updateRecord(final ORecordInternal<?> record) {
-    if (!isEnabled() ||
-        record.getIdentity().getClusterId() == excludedCluster ||
-        !record.getIdentity().isValid())
-      return;
+	/**
+	 * Push record to cache. Identifier of record used as access key
+	 * 
+	 * @param record
+	 *          record that should be cached
+	 */
+	public void updateRecord(final ORecordInternal<?> record) {
+		if (!isEnabled() || record.getIdentity().getClusterId() == excludedCluster || !record.getIdentity().isValid())
+			return;
 
-    underlying.lock(record.getIdentity());
-    try {
-      if (underlying.get(record.getIdentity()) != record)
-        underlying.put(record);
-    } finally {
-      underlying.unlock(record.getIdentity());
-    }
+		underlying.lock(record.getIdentity());
+		try {
+			if (underlying.get(record.getIdentity()) != record)
+				underlying.put(record);
+		} finally {
+			underlying.unlock(record.getIdentity());
+		}
 
-    secondary.get().updateRecord(record);
-  }
+		secondary.updateRecord(record);
+	}
 
-  /**
-   * Look up for record in cache by it's identifier.
-   * Optionally look up in secondary cache and update primary with found record
-   *
-   * @param rid unique identifier of record
-   * @return record stored in cache if any, otherwise - {@code null}
-   */
-  public ORecordInternal<?> findRecord(final ORID rid) {
-    if (!isEnabled())
-      return null;
+	/**
+	 * Look up for record in cache by it's identifier. Optionally look up in secondary cache and update primary with found record
+	 * 
+	 * @param rid
+	 *          unique identifier of record
+	 * @return record stored in cache if any, otherwise - {@code null}
+	 */
+	public ORecordInternal<?> findRecord(final ORID rid) {
+		if (!isEnabled())
+			return null;
 
-    ORecordInternal<?> record;
-    underlying.lock(rid);
-    try {
-      record = underlying.get(rid);
+		ORecordInternal<?> record;
+		underlying.lock(rid);
+		try {
+			record = underlying.get(rid);
 
-      if (record == null) {
-        record = secondary.get().retrieveRecord(rid);
+			if (record == null) {
+				record = secondary.retrieveRecord(rid);
 
-        if (record != null)
-          underlying.put(record);
-      }
-    } finally {
-      underlying.unlock(rid);
-    }
+				if (record != null)
+					underlying.put(record);
+			}
+		} finally {
+			underlying.unlock(rid);
+		}
 
-    OProfiler.getInstance().updateCounter(record != null ? CACHE_HIT : CACHE_MISS, 1L);
+		OProfiler.getInstance().updateCounter(record != null ? CACHE_HIT : CACHE_MISS, 1L);
 
-    return record;
-  }
+		return record;
+	}
 
-  /**
-   * Remove record with specified identifier from both primary and secondary caches
-   *
-   * @param rid unique identifier of record
-   */
-  public void deleteRecord(final ORID rid) {
-    super.deleteRecord(rid);
-    secondary.get().freeRecord(rid);
-  }
+	/**
+	 * Remove record with specified identifier from both primary and secondary caches
+	 * 
+	 * @param rid
+	 *          unique identifier of record
+	 */
+	public void deleteRecord(final ORID rid) {
+		super.deleteRecord(rid);
+		secondary.freeRecord(rid);
+	}
 
-  public void shutdown() {
-    super.shutdown();
-    secondary.set(null);
-  }
+	public void shutdown() {
+		super.shutdown();
+		secondary = null;
+	}
 
-  @Override
-  public void clear() {
-    moveRecordsToSecondaryCache();
-    super.clear();
-  }
+	@Override
+	public void clear() {
+		moveRecordsToSecondaryCache();
+		super.clear();
+	}
 
-  private void moveRecordsToSecondaryCache() {
-    if (secondary.get() == null)
-      return;
+	private void moveRecordsToSecondaryCache() {
+		if (secondary == null)
+			return;
 
-    for (ORID rid : underlying.keys()) {
-      secondary.get().updateRecord(underlying.get(rid));
-    }
-  }
+		for (ORID rid : underlying.keys()) {
+			secondary.updateRecord(underlying.get(rid));
+		}
+	}
 
-  public void invalidate() {
-    underlying.clear();
-  }
+	public void invalidate() {
+		underlying.clear();
+	}
 
-  @Override
-  public String toString() {
-    return "DB level1 cache records = " + getSize() + ", maxSize= " + getMaxSize();
-  }
+	@Override
+	public String toString() {
+		return "DB level1 cache records = " + getSize() + ", maxSize= " + getMaxSize();
+	}
 }
