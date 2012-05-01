6b194e2360ff3c455b00f43de520dac816cf92ac
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-05-01 10:07:24 +0000

    Added dump of heap in case of server crash

--- server/script/server.sh
+++ server/script/server.sh
@@ -53,7 +53,8 @@ LOG_FILE=$ORIENTDB_HOME/config/orientdb-server-log.properties
 LOG_CONSOLE_LEVEL=info
 LOG_FILE_LEVEL=fine
 WWW_PATH=$ORIENTDB_HOME/www
+#UNCOMMENT BELOW TO DISABLE THE CACHE
 #ORIENTDB_SETTINGS=-Dcache.level1.enabled=false -Dcache.level2.enabled=false
-#JAVA_OPTS=-Xmx1024m
+JAVA_OPTS=-XX:+HeapDumpOnOutOfMemoryError
 
 java -server $JAVA_OPTS $ORIENTDB_SETTINGS -Djava.util.logging.config.file="$LOG_FILE" -Dorientdb.config.file="$CONFIG_FILE" -Dorientdb.www.path="$WWW_PATH" -Dorientdb.build.number="@BUILD@" -cp "$ORIENTDB_HOME/lib/orientdb-server-@VERSION@.jar:$ORIENTDB_HOME/lib/*" com.orientechnologies.orient.server.OServerMain
