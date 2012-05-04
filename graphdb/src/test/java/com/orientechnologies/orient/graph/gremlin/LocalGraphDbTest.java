48754f3baaf1e0e19cb692c8f3587e99089cddb9
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-05-04 07:37:55 +0000

    Updated GraphDB dependencies and get graphdb test working

--- graphdb/src/test/java/com/orientechnologies/orient/graph/gremlin/LocalGraphDbTest.java
+++ graphdb/src/test/java/com/orientechnologies/orient/graph/gremlin/LocalGraphDbTest.java
@@ -3,48 +3,61 @@ package com.orientechnologies.orient.graph.gremlin;
 import java.io.IOException;
 import java.util.List;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.orientechnologies.orient.core.config.OGlobalConfiguration;
 import com.orientechnologies.orient.core.db.graph.OGraphDatabase;
 import com.orientechnologies.orient.core.db.graph.OGraphDatabasePool;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 
 public class LocalGraphDbTest {
-	public static void main(String[] args) throws IOException {
-		new LocalGraphDbTest().multipleDatabasesSameThread();
-	}
+  public static void main(String[] args) throws IOException {
+    new LocalGraphDbTest().multipleDatabasesSameThread();
+  }
 
-	public LocalGraphDbTest() {
-		OGremlinHelper.global().create();
-	}
+  public LocalGraphDbTest() {
+    OGremlinHelper.global().create();
+  }
 
-	@Test
-	public void multipleDatabasesSameThread() throws IOException {
-		String db1URL = "local:target/databases/tinkerpop";
-		String db2URL = "local:target/databases/tinkerpop";// + System.currentTimeMillis();
+  @BeforeClass
+  public void before() {
+    OGlobalConfiguration.STORAGE_KEEP_OPEN.setValue(false);
+  }
 
-		OGraphDatabase db1 = OGraphDatabasePool.global().acquire(db1URL, "admin", "admin");
-		ODocument doc1 = db1.createVertex();
+  @AfterClass
+  public void after() {
+    OGlobalConfiguration.STORAGE_KEEP_OPEN.setValue(true);
+  }
 
-		doc1.field("key", "value");
-		doc1.save();
-		db1.close();
+  @Test
+  public void multipleDatabasesSameThread() throws IOException {
+    String db1URL = "local:target/databases/tinkerpop";
+    String db2URL = "local:target/databases/tinkerpop";// + System.currentTimeMillis();
 
-		OGraphDatabase db2 = OGraphDatabasePool.global().acquire(db2URL, "admin", "admin");
+    OGraphDatabase db1 = OGraphDatabasePool.global().acquire(db1URL, "admin", "admin");
+    ODocument doc1 = db1.createVertex();
 
-		ODocument doc2 = db2.createVertex();
-		doc2.field("key", "value");
-		doc2.save();
-		db2.close();
+    doc1.field("key", "value");
+    doc1.save();
+    db1.close();
 
-		db1 = OGraphDatabasePool.global().acquire(db1URL, "admin", "admin"); // this line throws the Exception
+    OGraphDatabase db2 = OGraphDatabasePool.global().acquire(db2URL, "admin", "admin");
 
-		final List<?> result = db1.query(new OSQLSynchQuery<ODocument>("select out[weight=3].size() from V where out.size() > 0"));
+    ODocument doc2 = db2.createVertex();
+    doc2.field("key", "value");
+    doc2.save();
+    db2.close();
 
-		doc1 = db1.createVertex();
-		doc1.field("newkey", "newvalue");
-		doc1.save();
-		db1.close();
-	}
+    db1 = OGraphDatabasePool.global().acquire(db1URL, "admin", "admin"); // this line throws the Exception
+
+    final List<?> result = db1.query(new OSQLSynchQuery<ODocument>("select out[weight=3].size() from V where out.size() > 0"));
+
+    doc1 = db1.createVertex();
+    doc1.field("newkey", "newvalue");
+    doc1.save();
+    db1.close();
+  }
 }
--- graphdb/src/test/java/com/orientechnologies/orient/graph/gremlin/RemoteGremlinTest.java
+++ graphdb/src/test/java/com/orientechnologies/orient/graph/gremlin/RemoteGremlinTest.java
@@ -12,86 +12,93 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.orientechnologies.orient.core.Orient;
 import com.orientechnologies.orient.core.db.graph.OGraphDatabase;
 import com.orientechnologies.orient.core.db.record.OIdentifiable;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.OCommandSQL;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
+import com.orientechnologies.orient.core.storage.OStorage;
 import com.orientechnologies.orient.server.OServer;
 import com.orientechnologies.orient.server.OServerMain;
 
 public class RemoteGremlinTest {
-	private final OServer		server;
-	private OGraphDatabase	graphDatabase;
+  private final OServer  server;
+  private OGraphDatabase graphDatabase;
 
-	public RemoteGremlinTest() throws Exception {
-		if (System.getProperty("ORIENTDB_HOME") == null) {
-			System.setProperty("ORIENTDB_HOME", "target");
-		}
-		OGremlinHelper.global().create();
-		server = OServerMain.create();
-	}
+  public RemoteGremlinTest() throws Exception {
+    if (System.getProperty("ORIENTDB_HOME") == null) {
+      System.setProperty("ORIENTDB_HOME", "target");
+    }
+    OGremlinHelper.global().create();
+    server = OServerMain.create();
+  }
 
-	@BeforeClass
-	public void setUp() throws Exception {
-		server.startup(new File(getClass().getResource("db-config.xml").getFile()));
-		server.activate();
-	}
+  @BeforeClass
+  public void setUp() throws Exception {
+    server.startup(new File(getClass().getResource("db-config.xml").getFile()));
+    server.activate();
+  }
 
-	@AfterClass
-	public void tearDown() throws Exception {
-		if (server != null)
-			server.shutdown();
-	}
+  @AfterClass
+  public void tearDown() throws Exception {
+    if (server != null)
+      server.shutdown();
+  }
 
-	@BeforeMethod
-	public void beforeMethod() {
-		graphDatabase = new OGraphDatabase("remote:localhost/tinkerpop");
-		graphDatabase.open("admin", "admin");
-	}
+  @BeforeMethod
+  public void beforeMethod() {
+    for (OStorage stg : Orient.instance().getStorages()) {
+      System.out.println("Closing storage: " + stg);
+      stg.close(true);
+    }
 
-	@AfterMethod
-	public void afterMethod() {
-		graphDatabase.close();
-	}
+    graphDatabase = new OGraphDatabase("remote:localhost/tinkerpop");
+    graphDatabase.open("admin", "admin");
+  }
 
-	@Test
-	public void function() throws IOException {
-		ODocument vertex1 = (ODocument) graphDatabase.createVertex().field("label", "car").save();
-		ODocument vertex2 = (ODocument) graphDatabase.createVertex().field("label", "pilot").save();
-		ODocument edge = (ODocument) graphDatabase.createEdge(vertex1, vertex2).field("label", "drives").save();
+  @AfterMethod
+  public void afterMethod() {
+    graphDatabase.close();
+  }
 
-		List<?> result = graphDatabase.query(new OSQLSynchQuery<Object>(
-				"select gremlin('current.out.in') as value from V where out.size() > 0 limit 3"));
-		System.out.println("Query result: " + result);
+  @Test
+  public void function() throws IOException {
+    ODocument vertex1 = (ODocument) graphDatabase.createVertex().field("label", "car").save();
+    ODocument vertex2 = (ODocument) graphDatabase.createVertex().field("label", "pilot").save();
+    ODocument edge = (ODocument) graphDatabase.createEdge(vertex1, vertex2).field("label", "drives").save();
 
-		result = graphDatabase.query(new OSQLSynchQuery<Object>("select gremlin('current.out') as value from V"));
-		System.out.println("Query result: " + result);
+    List<?> result = graphDatabase.query(new OSQLSynchQuery<Object>(
+        "select gremlin('current.out.in') as value from V where out.size() > 0 limit 3"));
+    System.out.println("Query result: " + result);
 
-		int clusterId = graphDatabase.getVertexBaseClass().getDefaultClusterId();
+    result = graphDatabase.query(new OSQLSynchQuery<Object>("select gremlin('current.out') as value from V"));
+    System.out.println("Query result: " + result);
 
-		result = graphDatabase.query(new OSQLSynchQuery<Object>("select gremlin('current.out.in') as value from " + clusterId + ":1"));
-		System.out.println("Query result: " + result);
+    int clusterId = graphDatabase.getVertexBaseClass().getDefaultClusterId();
 
-		result = graphDatabase.query(new OSQLSynchQuery<Object>("select gremlin('current.out(\"drives\").count()') as value from V"));
-		System.out.println("Query result: " + result);
-	}
+    result = graphDatabase.query(new OSQLSynchQuery<Object>("select gremlin('current.out.in') as value from " + clusterId + ":1"));
+    System.out.println("Query result: " + result);
 
-	@Test
-	public void command() throws IOException {
-		List<OIdentifiable> result = graphDatabase.command(new OCommandGremlin("g.V[0..10]")).execute();
-		if (result != null) {
-			for (OIdentifiable doc : result) {
-				System.out.println(doc.getRecord().toJSON());
-			}
-		}
+    result = graphDatabase.query(new OSQLSynchQuery<Object>("select gremlin('current.out(\"drives\").count()') as value from V"));
+    System.out.println("Query result: " + result);
+  }
 
-		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("par1", 100);
+  @Test
+  public void command() throws IOException {
+    List<OIdentifiable> result = graphDatabase.command(new OCommandGremlin("g.V[0..10]")).execute();
+    if (result != null) {
+      for (OIdentifiable doc : result) {
+        System.out.println(doc.getRecord().toJSON());
+      }
+    }
 
-		result = graphDatabase.command(new OCommandSQL("select gremlin('current.out.filter{ it.performances > par1 }') from V"))
-				.execute(params);
-		System.out.println("Command result: " + result);
-	}
+    Map<String, Object> params = new HashMap<String, Object>();
+    params.put("par1", 100);
+
+    result = graphDatabase.command(new OCommandSQL("select gremlin('current.out.filter{ it.performances > par1 }') from V"))
+        .execute(params);
+    System.out.println("Command result: " + result);
+  }
 
 }
