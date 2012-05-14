e6084d5e03ecf481dbabf954c8dd30273fcc30aa
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-05-14 18:00:29 +0000

    Updated Copyright years to 2010-2012

--- core/src/main/java/com/orientechnologies/orient/core/query/nativ/OQueryContextNativeSchema.java
+++ core/src/main/java/com/orientechnologies/orient/core/query/nativ/OQueryContextNativeSchema.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2011 Luca Garulli (l.garulli--at--orientechnologies.com)
+ * Copyright 2010-2012 Luca Garulli (l.garulli--at--orientechnologies.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
