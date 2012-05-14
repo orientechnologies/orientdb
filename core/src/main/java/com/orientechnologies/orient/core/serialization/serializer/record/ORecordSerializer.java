c7617bcdeaf99ea74e105a0055137b506ad93d01
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-05-14 18:05:07 +0000

    Updated Copyright years to 2010-2012

--- core/src/main/java/com/orientechnologies/orient/core/serialization/serializer/record/ORecordSerializer.java
+++ core/src/main/java/com/orientechnologies/orient/core/serialization/serializer/record/ORecordSerializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2010 Luca Garulli (l.garulli--at--orientechnologies.com)
+ * Copyright 2010-2012 Luca Garulli (l.garulli--at--orientechnologies.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 /*
- * Copyright 1999-2010 Luca Garulli (l.garulli--at--orientechnologies.com)
+ * Copyright 2010-2012 Luca Garulli (l.garulli--at--orientechnologies.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
