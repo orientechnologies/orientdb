e23d900d41ed072b04bb383c82ce4c2bf1de77d8
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-07-02 11:33:51 +0000

    Minor: removed warnings

--- commons/src/main/java/com/orientechnologies/common/util/OCollections.java
+++ commons/src/main/java/com/orientechnologies/common/util/OCollections.java
@@ -24,48 +24,51 @@ import java.util.List;
  * Set of utility methods to work with collections.
  */
 public class OCollections {
-    /**
-     * This method is used to find item in collection using passed in comparator.
-     * Only 0 value (requested object is found) returned by comparator is taken into account the rest is ignored.
-     *
-     *
-     * @param list         List in which value should be found.
-     * @param object       Object to find.
-     * @param comparator   Comparator is sued for search.
-     * @param <T>          Type of collection elements.
-     * @return             Index of found item or <code>-1</code> otherwise.
-     */
-    public static <T> int indexOf(final List<T> list, final T object, final Comparator<T> comparator) {
-        int i = 0;
-        for(final T item : list) {
-            if(comparator.compare(item, object) == 0)
-                return i;
-            i++;
-        }
-        return -1;
+  /**
+   * This method is used to find item in collection using passed in comparator. Only 0 value (requested object is found) returned by
+   * comparator is taken into account the rest is ignored.
+   * 
+   * 
+   * @param list
+   *          List in which value should be found.
+   * @param object
+   *          Object to find.
+   * @param comparator
+   *          Comparator is sued for search.
+   * @param <T>
+   *          Type of collection elements.
+   * @return Index of found item or <code>-1</code> otherwise.
+   */
+  public static <T> int indexOf(final List<T> list, final T object, final Comparator<T> comparator) {
+    int i = 0;
+    for (final T item : list) {
+      if (comparator.compare(item, object) == 0)
+        return i;
+      i++;
     }
-    
-    /**
-     * Create a string representation of all objects in the given Iterable.
-     * example : [value1,value2,value3]
-     * 
-     * @param iterable
-     * @return String
-     */
-    public static String toString(Iterable<?> iterable){
-        final StringBuilder builder = new StringBuilder();
-        builder.append('[');
-        int cnt = 0;
-        final Iterator ite = iterable.iterator();
-        while(ite.hasNext()){
-            if(cnt!=0){
-                builder.append(',');
-            }
-            cnt++;
-            final Object obj = ite.next();
-            builder.append(obj);
-        }        
-        builder.append(']');
-        return builder.toString();
+    return -1;
+  }
+
+  /**
+   * Create a string representation of all objects in the given Iterable. example : [value1,value2,value3]
+   * 
+   * @param iterable
+   * @return String
+   */
+  public static String toString(Iterable<?> iterable) {
+    final StringBuilder builder = new StringBuilder();
+    builder.append('[');
+    int cnt = 0;
+    final Iterator<?> ite = iterable.iterator();
+    while (ite.hasNext()) {
+      if (cnt != 0) {
+        builder.append(',');
+      }
+      cnt++;
+      final Object obj = ite.next();
+      builder.append(obj);
     }
+    builder.append(']');
+    return builder.toString();
+  }
 }
