597a90c2af1a09dfa99ca081c71301cdb35a9f99
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-07-10 13:36:09 +0000

    Created a new speed test case

---
+++ tests/src/test/java/com/orientechnologies/orient/test/database/speed/DictionaryPutDocumentSpeedTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2010-2012 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.orient.test.database.speed;
+
+import org.testng.annotations.Test;
+
+import com.orientechnologies.common.profiler.OProfiler;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.index.OIndex;
+import com.orientechnologies.orient.core.intent.OIntentMassiveInsert;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.test.database.base.OrientMonoThreadTest;
+
+@Test(enabled = false)
+public class DictionaryPutDocumentSpeedTest extends OrientMonoThreadTest {
+  private ODatabaseDocumentTx database;
+  private ODocument           record;
+  private long                startNum;
+
+  public static void main(String[] iArgs) throws InstantiationException, IllegalAccessException {
+    // System.setProperty("url", "remote:localhost:2424/demo");
+    DictionaryPutDocumentSpeedTest test = new DictionaryPutDocumentSpeedTest();
+    test.data.go(test);
+  }
+
+  public DictionaryPutDocumentSpeedTest() throws InstantiationException, IllegalAccessException {
+    super(1000000);
+
+    String url = System.getProperty("url");
+
+    database = new ODatabaseDocumentTx(url).open("admin", "admin");
+    for (OIndex idx : database.getMetadata().getSchema().getClass("Account").getIndexes()) {
+      System.out.println("Deleting existent index: " + idx);
+      idx.delete();
+    }
+
+    OProfiler.getInstance().startRecording();
+
+    startNum = database.getDictionary().size();
+
+    System.out.println("Total element in the dictionary at the beginning: " + startNum);
+
+    record = new ODocument();
+    database.declareIntent(new OIntentMassiveInsert());
+  }
+
+  @Override
+  public void cycle() {
+    int id = (int) (startNum + data.getCyclesDone());
+
+    record.reset();
+
+    record.setClassName("Account");
+    record.field("id", data.getCyclesDone());
+    record.field("name", "Luca");
+    record.field("surname", "Garulli");
+    record.field("salary", 3000f + data.getCyclesDone());
+
+    database.getDictionary().put("doc-" + id, record);
+  }
+
+  @Override
+  public void deinit() {
+    System.out.println("Total element in the dictionary at the end: " + database.getDictionary().size());
+
+    database.close();
+    super.deinit();
+  }
+
+}
