d8c7b3fd4e872299aaab31fd9c6f05454dc14a13
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-07-21 08:43:52 +0000

    Fixed type with cache enabled in server.sh/bat

--- graphdb/script/server.sh
+++ graphdb/script/server.sh
@@ -53,7 +53,7 @@ LOG_FILE=$ORIENTDB_HOME/config/orientdb-server-log.properties
 LOG_CONSOLE_LEVEL=info
 LOG_FILE_LEVEL=fine
 WWW_PATH=$ORIENTDB_HOME/www
-ORIENTDB_SETTINGS=-Dcache.level1.enabled=false -Dcache.level2.strategy=1
+ORIENTDB_SETTINGS="-Dcache.level1.enabled=false -Dcache.level2.strategy=1"
 JAVA_OPTS_SCRIPT=-XX:+HeapDumpOnOutOfMemoryError
 
 java -server $JAVA_OPTS $JAVA_OPTS_SCRIPT $ORIENTDB_SETTINGS -Djava.util.logging.config.file="$LOG_FILE" -Dorientdb.config.file="$CONFIG_FILE" -Dorientdb.www.path="$WWW_PATH" -Dlog.console.level=$LOG_CONSOLE_LEVEL -Dlog.file.level=$LOG_FILE_LEVEL -Dorientdb.build.number="@BUILD@" -cp "$ORIENTDB_HOME/lib/*:" com.orientechnologies.orient.server.OServerMain
