e6f5208fa01e79665f4c147cc40fbadd13a01790
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-07-31 10:40:33 +0000

    Patch by Fabrizio Fortino to let the shutdown.sh command to wait for completition of server's shutdown

--- server/script/shutdown.sh
+++ server/script/shutdown.sh
@@ -2,9 +2,18 @@
 #
 # Copyright (c) 1999-2010 Luca Garulli
 #
-
+# 2012-07-31 - Added -w option 
+#
 # resolve links - $0 may be a softlink
 PRG="$0"
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -w|--wait)
+      wait="yes"
+      shift 1 ;;
+  esac
+done
 
 while [ -h "$PRG" ]; do
   ls=`ls -ld "$PRG"`
@@ -30,3 +39,10 @@ WWW_PATH=$ORIENTDB_HOME/www
 #JAVA_OPTS=-Xms1024m -Xmx1024m
 
 java -client $JAVA_OPTS -Dorientdb.config.file="$CONFIG_FILE" -cp "$ORIENTDB_HOME/lib/orientdb-tools-@VERSION@.jar:$ORIENTDB_HOME/lib/*" com.orientechnologies.orient.server.OServerShutdownMain $*
+
+if [ "x$wait" == "xyes" ] ; then
+  while true ; do
+    ps -ef | grep java | grep $ORIENTDB_HOME/lib/orientdb-server > /dev/null || break
+    sleep 1;
+  done
+fi
