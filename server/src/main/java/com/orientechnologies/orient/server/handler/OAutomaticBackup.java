139b1e7e6c67864c7e20758fffd034b18a4b1531
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-08-26 14:25:42 +0000

    Applied patch by Vaclav about automatic backup. Fixed issue 1031

--- server/src/main/java/com/orientechnologies/orient/server/handler/OAutomaticBackup.java
+++ server/src/main/java/com/orientechnologies/orient/server/handler/OAutomaticBackup.java
@@ -2,6 +2,7 @@ package com.orientechnologies.orient.server.handler;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
@@ -26,127 +27,139 @@ import com.orientechnologies.orient.server.OServerMain;
 import com.orientechnologies.orient.server.config.OServerParameterConfiguration;
 
 public class OAutomaticBackup extends OServerHandlerAbstract {
-	public enum VARIABLES {
-		DBNAME, DATE
-	}
+  public enum VARIABLES {
+    DBNAME, DATE
+  }
 
-	private long				delay							= -1;
-	private String			targetDirectory		= "backup";
-	private String			targetFileName;
-	private Set<String>	includeDatabases	= new HashSet<String>();
-	private Set<String>	excludeDatabases	= new HashSet<String>();
+  private Date        firstTime        = null;
+  private long        delay            = -1;
+  private String      targetDirectory  = "backup";
+  private String      targetFileName;
+  private Set<String> includeDatabases = new HashSet<String>();
+  private Set<String> excludeDatabases = new HashSet<String>();
 
-	@Override
-	public void config(final OServer iServer, final OServerParameterConfiguration[] iParams) {
-		for (OServerParameterConfiguration param : iParams) {
-			if (param.name.equalsIgnoreCase("enabled")) {
-				if (!Boolean.parseBoolean(param.value))
-					// DISABLE IT
-					return;
-			} else if (param.name.equalsIgnoreCase("delay"))
-				delay = OIOUtils.getTimeAsMillisecs(param.value);
-			else if (param.name.equalsIgnoreCase("target.directory"))
-				targetDirectory = param.value;
-			else if (param.name.equalsIgnoreCase("db.include") && param.value.trim().length() > 0)
-				for (String db : param.value.split(","))
-					includeDatabases.add(db);
-			else if (param.name.equalsIgnoreCase("db.exclude") && param.value.trim().length() > 0)
-				for (String db : param.value.split(","))
-					excludeDatabases.add(db);
-			else if (param.name.equalsIgnoreCase("target.fileName"))
-				targetFileName = param.value;
-		}
+  @Override
+  public void config(final OServer iServer, final OServerParameterConfiguration[] iParams) {
+    for (OServerParameterConfiguration param : iParams) {
+      if (param.name.equalsIgnoreCase("enabled")) {
+        if (!Boolean.parseBoolean(param.value))
+          // DISABLE IT
+          return;
+      } else if (param.name.equalsIgnoreCase("delay"))
+        delay = OIOUtils.getTimeAsMillisecs(param.value);
+      else if (param.name.equalsIgnoreCase("firsttime")) {
+        try {
+          firstTime = OIOUtils.getTodayWithTime(param.value);
+        } catch (ParseException e) {
+          throw new OConfigurationException("Parameter 'firstTime' has invalid format, expected: HH:mm:ss", e);
+        }
+      } else if (param.name.equalsIgnoreCase("target.directory"))
+        targetDirectory = param.value;
+      else if (param.name.equalsIgnoreCase("db.include") && param.value.trim().length() > 0)
+        for (String db : param.value.split(","))
+          includeDatabases.add(db);
+      else if (param.name.equalsIgnoreCase("db.exclude") && param.value.trim().length() > 0)
+        for (String db : param.value.split(","))
+          excludeDatabases.add(db);
+      else if (param.name.equalsIgnoreCase("target.fileName"))
+        targetFileName = param.value;
+    }
 
-		if (delay <= 0)
-			throw new OConfigurationException("Cannot find mandatory parameter 'delay'");
-		if (!targetDirectory.endsWith("/"))
-			targetDirectory += "/";
+    if (delay <= 0)
+      throw new OConfigurationException("Cannot find mandatory parameter 'delay'");
+    if (!targetDirectory.endsWith("/"))
+      targetDirectory += "/";
 
-		final File filePath = new File(targetDirectory);
-		if (filePath.exists()) {
-			if (!filePath.isDirectory())
-				throw new OConfigurationException("Parameter 'path' points to a file, not a directory");
-		} else
-			// CREATE BACKUP FOLDER(S) IF ANY
-			filePath.mkdirs();
+    final File filePath = new File(targetDirectory);
+    if (filePath.exists()) {
+      if (!filePath.isDirectory())
+        throw new OConfigurationException("Parameter 'path' points to a file, not a directory");
+    } else
+      // CREATE BACKUP FOLDER(S) IF ANY
+      filePath.mkdirs();
 
-		OLogManager.instance().info(this, "Automatic backup handler installed and active: delay=%dms, targetDirectory=%s", delay,
-				targetDirectory);
+    OLogManager.instance().info(this,
+        "Automatic backup handler installed and active: delay=%dms, firstTime=%s, targetDirectory=%s", delay, firstTime,
+        targetDirectory);
 
-		Orient.getTimer().schedule(new TimerTask() {
+    final TimerTask timerTask = new TimerTask() {
+      @Override
+      public void run() {
+        OLogManager.instance().info(this, "[OAutomaticBackup] Scanning databases to backup...");
 
-			@Override
-			public void run() {
-				OLogManager.instance().info(this, "[OAutomaticBackup] Scanning databases to backup...");
+        int ok = 0, errors = 0;
 
-				int ok = 0, errors = 0;
+        final Map<String, String> databaseNames = OServerMain.server().getAvailableStorageNames();
+        for (final Entry<String, String> dbName : databaseNames.entrySet()) {
+          boolean include;
 
-				final Map<String, String> databaseNames = OServerMain.server().getAvailableStorageNames();
-				for (final Entry<String, String> dbName : databaseNames.entrySet()) {
-					boolean include;
+          if (includeDatabases.size() > 0)
+            include = includeDatabases.contains(dbName.getKey());
+          else
+            include = true;
 
-					if (includeDatabases.size() > 0)
-						include = includeDatabases.contains(dbName.getKey());
-					else
-						include = true;
+          if (excludeDatabases.contains(dbName.getKey()))
+            include = false;
 
-					if (excludeDatabases.contains(dbName.getKey()))
-						include = false;
+          if (include) {
+            final String fileName = OVariableParser.resolveVariables(targetFileName, OSystemVariableResolver.VAR_BEGIN,
+                OSystemVariableResolver.VAR_END, new OVariableParserListener() {
+                  @Override
+                  public String resolve(final String iVariable) {
+                    if (iVariable.equalsIgnoreCase(VARIABLES.DBNAME.toString()))
+                      return dbName.getKey();
+                    else if (iVariable.startsWith(VARIABLES.DATE.toString())) {
+                      return new SimpleDateFormat(iVariable.substring(VARIABLES.DATE.toString().length() + 1)).format(new Date());
+                    }
 
-					if (include) {
-						final String fileName = OVariableParser.resolveVariables(targetFileName, OSystemVariableResolver.VAR_BEGIN,
-								OSystemVariableResolver.VAR_END, new OVariableParserListener() {
-									@Override
-									public String resolve(final String iVariable) {
-										if (iVariable.equalsIgnoreCase(VARIABLES.DBNAME.toString()))
-											return dbName.getKey();
-										else if (iVariable.startsWith(VARIABLES.DATE.toString())) {
-											return new SimpleDateFormat(iVariable.substring(VARIABLES.DATE.toString().length() + 1)).format(new Date());
-										}
+                    // NOT FOUND
+                    throw new IllegalArgumentException("Variable '" + iVariable + "' wasn't found");
+                  }
+                });
 
-										// NOT FOUND
-										throw new IllegalArgumentException("Variable '" + iVariable + "' wasn't found");
-									}
-								});
+            final String exportFilePath = targetDirectory + fileName;
+            final ODatabaseDocumentTx db = new ODatabaseDocumentTx(dbName.getValue());
 
-						final String exportFilePath = targetDirectory + fileName;
-						final ODatabaseDocumentTx db = new ODatabaseDocumentTx(dbName.getValue());
+            try {
+              db.setProperty(ODatabase.OPTIONS.SECURITY.toString(), Boolean.FALSE);
+              db.open("admin", "aaa");
 
-						try {
-							db.setProperty(ODatabase.OPTIONS.SECURITY.toString(), Boolean.FALSE);
-							db.open("admin", "aaa");
+              final long begin = System.currentTimeMillis();
 
-							final long begin = System.currentTimeMillis();
+              new ODatabaseExport(db, exportFilePath, new OCommandOutputListener() {
+                @Override
+                public void onMessage(final String iText) {
 
-							new ODatabaseExport(db, exportFilePath, new OCommandOutputListener() {
-								@Override
-								public void onMessage(final String iText) {
+                }
+              }).exportDatabase();
 
-								}
-							}).exportDatabase();
+              OLogManager.instance().info(
+                  this,
+                  "[OAutomaticBackup] - Backup of database '" + dbName.getValue() + "' completed in "
+                      + (System.currentTimeMillis() - begin) + "ms");
+              ok++;
 
-							OLogManager.instance().info(
-									this,
-									"[OAutomaticBackup] - Backup of database '" + dbName.getValue() + "' completed in "
-											+ (System.currentTimeMillis() - begin) + "ms");
-							ok++;
+            } catch (IOException e) {
+              OLogManager.instance().error(this,
+                  "[OAutomaticBackup] - Error on exporting database '" + dbName.getValue() + "' to file: " + exportFilePath, e);
+              errors++;
+            } finally {
+              db.close();
+            }
+          }
+        }
+        OLogManager.instance().info(this, "[OAutomaticBackup] Backup finished: %d ok, %d errors", ok, errors);
+      }
+    };
 
-						} catch (IOException e) {
-							OLogManager.instance().error(this,
-									"[OAutomaticBackup] - Error on exporting database '" + dbName.getValue() + "' to file: " + exportFilePath, e);
-							errors++;
-						} finally {
-							db.close();
-						}
-					}
-				}
-				OLogManager.instance().info(this, "[OAutomaticBackup] Backup finished: %d ok, %d errors", ok, errors);
-			}
-		}, delay, delay);
-	}
+    if (firstTime == null)
+      Orient.getTimer().schedule(timerTask, delay, delay);
+    else
+      Orient.getTimer().schedule(timerTask, firstTime, delay);
+  }
 
-	@Override
-	public String getName() {
-		return "automaticBackup";
-	}
+  @Override
+  public String getName() {
+    return "automaticBackup";
+  }
 }
