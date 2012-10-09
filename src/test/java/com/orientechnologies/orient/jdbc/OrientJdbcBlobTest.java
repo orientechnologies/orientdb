8ed26c18e50225c5d8ddd45a3cc50e1c5cfd8694
Luca Garulli <l.garulli@gmail.com>
2012-10-09 16:32:47 +0200

    Changed management of temporary resources to get working tests

--- src/test/java/com/orientechnologies/orient/jdbc/OrientJdbcBlobTest.java
+++ src/test/java/com/orientechnologies/orient/jdbc/OrientJdbcBlobTest.java
@@ -1,5 +1,10 @@
 package com.orientechnologies.orient.jdbc;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -17,89 +22,105 @@ import java.sql.SQLException;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+public class OrientJdbcBlobTest extends OrientJdbcBaseTest {
 
-public class OrientJdbcBlobTest extends OrientJdbcBaseTest{
+  @Test
+  public void shouldLoadBlob() throws SQLException, FileNotFoundException, IOException, NoSuchAlgorithmException {
+    File binaryFile = getOutFile();
 
-	@Test
-	public void shouldLoadBlob() throws SQLException, FileNotFoundException, IOException, NoSuchAlgorithmException {
-		File binaryFile = new File("./working/output_blob.pdf");
-		binaryFile.delete();
+    String digest = this.calculateMD5checksum(ClassLoader.getSystemResourceAsStream("file.pdf"));
 
-		String digest = this.calculateMD5checksum(ClassLoader.getSystemResourceAsStream("file.pdf"));
+    PreparedStatement stmt = conn.prepareStatement("SELECT FROM Article WHERE uuid = 1 ");
 
-		PreparedStatement stmt = conn.prepareStatement("SELECT FROM Article WHERE uuid = 1 ");
-		
-		ResultSet rs = stmt.executeQuery();
-		assertTrue(rs.next());
-		rs.next();
-		
-		Blob blob = rs.getBlob("attachment");
-		
-		assertNotNull(blob);
-		new FileOutputStream(binaryFile).write(blob.getBytes(1, (int) blob.length()));
+    ResultSet rs = stmt.executeQuery();
+    assertTrue(rs.next());
+    rs.next();
 
-		assertTrue("The file '" + binaryFile.getName() + "' does not exist", binaryFile.exists());
-		this.verifyMD5checksum(binaryFile, digest);
-		
-		
-		
-	}
+    Blob blob = rs.getBlob("attachment");
 
-	@Test
-	public void shouldLoadChunckedBlob() throws SQLException, FileNotFoundException, IOException, NoSuchAlgorithmException {
-		File binaryFile = new File("./working/output_blob.pdf");
-		binaryFile.delete();
+    assertNotNull(blob);
 
-		String digest = this.calculateMD5checksum(ClassLoader.getSystemResourceAsStream("file.pdf"));
+    FileOutputStream s = null;
+    try {
+      s = new FileOutputStream(binaryFile);
+      s.write(blob.getBytes(1, (int) blob.length()));
+    } finally {
+      if (s != null)
+        s.close();
+    }
 
-		PreparedStatement stmt = conn.prepareStatement("SELECT FROM Article WHERE uuid = 2 ");
-		
-		ResultSet rs = stmt.executeQuery();
-		assertTrue(rs.next());
-		rs.next();
-		
-		Blob blob = rs.getBlob("attachment");
-		
-		assertNotNull(blob);
-		new FileOutputStream(binaryFile).write(blob.getBytes(1, (int) blob.length()));
+    assertTrue("The file '" + binaryFile.getName() + "' does not exist", binaryFile.exists());
+    this.verifyMD5checksum(binaryFile, digest);
 
-		assertTrue("The file '" + binaryFile.getName() + "' does not exist", binaryFile.exists());
-		this.verifyMD5checksum(binaryFile, digest);
-		
-		
-		
-	}
+  }
 
-	private void verifyMD5checksum(File fileToBeChecked, String digest) {
-		try {
-			assertEquals("The MD5 checksum of the file '" + fileToBeChecked.getAbsolutePath() + "' does not match the given one.", digest, calculateMD5checksum(new FileInputStream(fileToBeChecked)));
-		} catch (NoSuchAlgorithmException e) {
-			fail(e.getMessage());
-		} catch (IOException e) {
-			fail(e.getMessage());
-		}
-	}
+  @Test
+  public void shouldLoadChunckedBlob() throws SQLException, FileNotFoundException, IOException, NoSuchAlgorithmException {
+    File binaryFile = getOutFile();
 
-	private String calculateMD5checksum(InputStream fileStream) throws NoSuchAlgorithmException, IOException {
-		MessageDigest md = MessageDigest.getInstance("MD5");
+    String digest = this.calculateMD5checksum(ClassLoader.getSystemResourceAsStream("file.pdf"));
 
-		try {
-			fileStream = new DigestInputStream(fileStream, md);
-			while (fileStream.read() != -1)
-				;
-		} finally {
-			try {
-				fileStream.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return new BigInteger(1, md.digest()).toString(16);
-	}
+    PreparedStatement stmt = conn.prepareStatement("SELECT FROM Article WHERE uuid = 2 ");
 
+    ResultSet rs = stmt.executeQuery();
+    assertTrue(rs.next());
+    rs.next();
+
+    Blob blob = rs.getBlob("attachment");
+
+    assertNotNull(blob);
+
+    FileOutputStream s = null;
+    try {
+      s = new FileOutputStream(binaryFile);
+      s.write(blob.getBytes(1, (int) blob.length()));
+    } finally {
+      if (s != null)
+        s.close();
+    }
+
+    assertTrue("The file '" + binaryFile.getName() + "' does not exist", binaryFile.exists());
+    this.verifyMD5checksum(binaryFile, digest);
+
+  }
+
+  private void verifyMD5checksum(File fileToBeChecked, String digest) {
+    try {
+      assertEquals("The MD5 checksum of the file '" + fileToBeChecked.getAbsolutePath() + "' does not match the given one.",
+          digest, calculateMD5checksum(new FileInputStream(fileToBeChecked)));
+    } catch (NoSuchAlgorithmException e) {
+      fail(e.getMessage());
+    } catch (IOException e) {
+      fail(e.getMessage());
+    }
+  }
+
+  private String calculateMD5checksum(InputStream fileStream) throws NoSuchAlgorithmException, IOException {
+    MessageDigest md = MessageDigest.getInstance("MD5");
+
+    try {
+      fileStream = new DigestInputStream(fileStream, md);
+      while (fileStream.read() != -1)
+        ;
+    } finally {
+      try {
+        fileStream.close();
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+    return new BigInteger(1, md.digest()).toString(16);
+  }
+
+  protected File getOutFile() {
+    File binaryFile = new File("./target/working/output_blob.pdf");
+    if (binaryFile.exists()) {
+      do {
+        binaryFile.delete();
+      } while (binaryFile.exists());
+    } else
+      binaryFile.mkdirs();
+    return binaryFile;
+  }
 }
