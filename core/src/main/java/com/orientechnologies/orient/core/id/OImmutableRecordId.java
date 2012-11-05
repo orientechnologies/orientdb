86a71e3e3f3034d9bd0d0cfcbacb9942bed8ae9d
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-11-05 21:10:57 +0000

    Added new property on database to be altered: LOCALECOUNTRY and LOCALELANGUAGE

---
+++ core/src/main/java/com/orientechnologies/orient/core/id/OImmutableRecordId.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2010-2012 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.orient.core.id;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.orientechnologies.orient.core.serialization.OMemoryStream;
+
+/**
+ * Immutable ORID implementation. To be really immutable fields must not be public anymore. TODO!
+ * 
+ * @author Luca Garulli (l.garulli--at--orientechnologies.com)
+ * 
+ */
+public class OImmutableRecordId extends ORecordId {
+  private static final long serialVersionUID = 1L;
+
+  public OImmutableRecordId(final int iClusterId, final long iClusterPosition) {
+    super(iClusterId, iClusterPosition);
+  }
+
+  public OImmutableRecordId(final ORecordId iRID) {
+    super(iRID);
+  }
+
+  @Override
+  public void copyFrom(final ORID iSource) {
+    throw new UnsupportedOperationException("copyFrom");
+  }
+
+  @Override
+  public ORecordId fromStream(byte[] iBuffer) {
+    throw new UnsupportedOperationException("fromStream");
+  }
+
+  @Override
+  public ORecordId fromStream(OMemoryStream iStream) {
+    throw new UnsupportedOperationException("fromStream");
+  }
+
+  @Override
+  public ORecordId fromStream(InputStream iStream) throws IOException {
+    throw new UnsupportedOperationException("fromStream");
+  }
+
+  @Override
+  public void fromString(String iRecordId) {
+    throw new UnsupportedOperationException("fromString");
+  }
+
+  @Override
+  public void reset() {
+    throw new UnsupportedOperationException("reset");
+  }
+}
