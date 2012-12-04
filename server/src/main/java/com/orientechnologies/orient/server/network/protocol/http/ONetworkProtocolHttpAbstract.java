e2b759aac189ce578dbf63b0c6e65093eab3e23f
l.garulli <l.garulli@3625ad7b-9c83-922f-a72b-73d79161f2ea>
2012-12-04 22:10:10 +0000

    Fixed bug on using decoding() in log warn message in case the command is wrong

--- server/src/main/java/com/orientechnologies/orient/server/network/protocol/http/ONetworkProtocolHttpAbstract.java
+++ server/src/main/java/com/orientechnologies/orient/server/network/protocol/http/ONetworkProtocolHttpAbstract.java
@@ -139,7 +139,7 @@ public abstract class ONetworkProtocolHttpAbstract extends ONetworkProtocol {
           OLogManager.instance().warn(
               this,
               "->" + channel.socket.getInetAddress().getHostAddress() + ": Command not found: " + request.httpMethod + "."
-                  + command);
+                  + URLDecoder.decode(command, "UTF-8"));
 
           sendTextContent(OHttpUtils.STATUS_INVALIDMETHOD_CODE, OHttpUtils.STATUS_INVALIDMETHOD_DESCRIPTION, null,
               OHttpUtils.CONTENT_TEXT_PLAIN, "Command not found: " + command);
@@ -343,7 +343,7 @@ public abstract class ONetworkProtocolHttpAbstract extends ONetworkProtocol {
           else if (OStringSerializerHelper.startsWithIgnoreCase(line, OHttpUtils.HEADER_AUTHENTICATION))
             iRequest.authentication = line.substring(OHttpUtils.HEADER_AUTHENTICATION.length());
           else if (OStringSerializerHelper.startsWithIgnoreCase(line, "Expect: 100-continue"))
-            // SUPPORT THE CONTINUE TO AUTHORIZE THE CLIENT TO SEND THE CONTENT WITHOUT WAITING THE DELAY 
+            // SUPPORT THE CONTINUE TO AUTHORIZE THE CLIENT TO SEND THE CONTENT WITHOUT WAITING THE DELAY
             sendTextContent(100, null, null, null, null);
 
         }
