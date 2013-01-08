71b84d984925f7a2949d0094988670be18b44062
Artem Orobets <enisher@gmail.com>
2013-01-08 15:40:42 +0200

    ODocument serialization fixed

--- core/src/main/java/com/orientechnologies/orient/core/version/OSimpleVersion.java
+++ core/src/main/java/com/orientechnologies/orient/core/version/OSimpleVersion.java
@@ -65,17 +65,17 @@ public final class OSimpleVersion implements ORecordVersion {
   @Override
   public boolean isUntracked() {
     return version == -1;
-	}
+  }
 
-	@Override
-	public boolean isTemporary() {
-		return version < -1;
-	}
+  @Override
+  public boolean isTemporary() {
+    return version < -1;
+  }
 
-	@Override
-	public boolean isValid() {
-		return version > -1;
-	}
+  @Override
+  public boolean isValid() {
+    return version > -1;
+  }
 
   @Override
   public boolean isTombstone() {
@@ -89,6 +89,19 @@ public final class OSimpleVersion implements ORecordVersion {
 
     version++;
     version = -version;
+  }
+
+  @Override
+  public byte[] toStream() {
+    final byte[] buffer = new byte[OBinaryProtocol.SIZE_INT];
+    getSerializer().writeTo(buffer, 0, this);
+
+    return buffer;
+  }
+
+  @Override
+  public void fromStream(byte[] stream) {
+    getSerializer().readFrom(stream, 0, this);
   }
 
   @Override
