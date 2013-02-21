386522441686543098513ff686cb54dfdd4b381a
lvca <l.garulli@gmail.com>
2013-02-21 10:42:00 +0100

    Disabled auto-flush to let suite test to pass

--- core/src/main/java/com/orientechnologies/orient/core/config/OGlobalConfiguration.java
+++ core/src/main/java/com/orientechnologies/orient/core/config/OGlobalConfiguration.java
@@ -199,7 +199,7 @@ public enum OGlobalConfiguration {
       "Manager that will be used to handle mmap files. true = USE OLD MANAGER, false = USE NEW MANAGER", boolean.class, false),
 
   FILE_MMAP_AUTOFLUSH_TIMER("file.mmap.autoFlush.timer", "Auto flushes memory mapped blocks every X seconds. 0 = disabled",
-      int.class, 60),
+      int.class, 0),
 
   FILE_MMAP_AUTOFLUSH_UNUSED_TIME("file.mmap.autoFlush.unusedTime",
       "Remove memory mapped blocks with unused time major than this value. Time is in seconds", int.class, 60),
