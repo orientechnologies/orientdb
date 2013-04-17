400d5c68643f82852024aa413304323dce6eda6a
logart <logart2007@gmail.com>
2013-04-17 23:15:39 +0300

    adding copyrights fixing ant build

--- commons/src/main/java/com/orientechnologies/common/directmemory/OUnsafeMemoryJava7.java
+++ commons/src/main/java/com/orientechnologies/common/directmemory/OUnsafeMemoryJava7.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright 1999-2012 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.orientechnologies.common.directmemory;
 
 /**
- * Created with IntelliJ IDEA. User: artem Date: 4/16/13 Time: 9:43 PM To change this template use File | Settings | File Templates.
+ * @author logart (logart2007@gmail.com)
+ * @since 15.04.2013
  */
 public class OUnsafeMemoryJava7 extends OUnsafeMemory {
 
