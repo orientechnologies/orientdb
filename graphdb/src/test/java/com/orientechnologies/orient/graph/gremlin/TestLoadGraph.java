854f87a34ace1b878a706cff3052c265345a6890
lvca <l.garulli@gmail.com>
2013-04-26 00:15:25 +0200

    Restored graph test

--- graphdb/src/test/java/com/orientechnologies/orient/graph/gremlin/TestLoadGraph.java
+++ graphdb/src/test/java/com/orientechnologies/orient/graph/gremlin/TestLoadGraph.java
@@ -1,5 +1,15 @@
 package com.orientechnologies.orient.graph.gremlin;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.orientechnologies.orient.client.db.ODatabaseHelper;
+import com.orientechnologies.orient.core.config.OGlobalConfiguration;
+import com.orientechnologies.orient.core.db.graph.OGraphDatabase;
+import com.tinkerpop.blueprints.impls.orient.OrientBaseGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientGraphNoTx;
+import com.tinkerpop.blueprints.util.io.graphml.GraphMLReader;
 
 public class TestLoadGraph {
   private static final String INPUT_FILE = "target/test-classes/graph-example-2.xml";
@@ -8,7 +18,7 @@ public class TestLoadGraph {
   private String              dbURL      = DBURL;
 
   public static void main(final String[] args) throws Exception {
-//    new TestLoadGraph(args).testImport();
+    new TestLoadGraph(args).testImport();
   }
 
   public TestLoadGraph() {
@@ -21,22 +31,24 @@ public class TestLoadGraph {
     dbURL = args.length > 1 ? args[1] : DBURL;
   }
 
-//  @Test
-//  public void testImport() throws IOException, FileNotFoundException {
-//    OGlobalConfiguration.STORAGE_KEEP_OPEN.setValue(false);
-//    OGraphDatabase db = new OGraphDatabase(DBURL);
-//    ODatabaseHelper.deleteDatabase(db);
-//    OrientBaseGraph g = new OrientBatchGraph(dbURL);
-//
-//    System.out.println("Importing graph from file '" + inputFile + "' into database: " + g + "...");
-//
-//    final long startTime = System.currentTimeMillis();
-//
-//    GraphMLReader.inputGraph(g, new FileInputStream(inputFile), 10000, null, null, null);
-//
-//    System.out.println("Imported in " + (System.currentTimeMillis() - startTime) + "ms. Vertexes: " + g.countVertices()
-//        + ", Edges: " + g.countEdges("E"));
-//
-//    g.shutdown();
-//  }
+  // @Test
+  public void testImport() throws IOException, FileNotFoundException {
+    OGlobalConfiguration.STORAGE_KEEP_OPEN.setValue(false);
+
+    OGraphDatabase db = new OGraphDatabase(DBURL);
+    ODatabaseHelper.deleteDatabase(db);
+
+    OrientBaseGraph g = new OrientGraphNoTx(dbURL);
+
+    System.out.println("Importing graph from file '" + inputFile + "' into database: " + g + "...");
+
+    final long startTime = System.currentTimeMillis();
+
+    GraphMLReader.inputGraph(g, new FileInputStream(inputFile), 10000, null, null, null);
+
+    System.out.println("Imported in " + (System.currentTimeMillis() - startTime) + "ms. Vertexes: " + g.countVertices()
+        + ", Edges: " + g.countEdges("E"));
+
+    g.shutdown();
+  }
 }
