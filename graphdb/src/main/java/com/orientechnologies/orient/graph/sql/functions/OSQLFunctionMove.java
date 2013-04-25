4e4586506c42666f893d4d58ee2c5a4a76f3a9b3
lvca <l.garulli@gmail.com>
2013-04-26 00:07:55 +0200

    Improved management of collection by recognizing when it's internal avoiding to iterate for the count()

---
+++ graphdb/src/main/java/com/orientechnologies/orient/graph/sql/functions/OSQLFunctionMove.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2010-2012 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.orient.graph.sql.functions;
+
+import com.orientechnologies.common.collection.OMultiValue;
+import com.orientechnologies.common.util.OCallable;
+import com.orientechnologies.orient.core.command.OCommandContext;
+import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.serialization.serializer.OStringSerializerHelper;
+import com.orientechnologies.orient.core.sql.OSQLEngine;
+import com.orientechnologies.orient.core.sql.functions.OSQLFunctionConfigurableAbstract;
+import com.orientechnologies.orient.graph.sql.OGraphCommandExecutorSQLFactory;
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.impls.orient.OrientBaseGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientEdge;
+import com.tinkerpop.blueprints.impls.orient.OrientVertex;
+
+/**
+ * Hi-level function to move inside a graph. Return the incoming connections. If the current element is a vertex, then will be
+ * returned edges otherwise vertices.
+ * 
+ * @author Luca Garulli (l.garulli--at--orientechnologies.com)
+ * 
+ */
+public class OSQLFunctionMove extends OSQLFunctionConfigurableAbstract {
+  public static final String NAME = "move";
+
+  public OSQLFunctionMove() {
+    super(NAME, 1, 2);
+  }
+
+  public OSQLFunctionMove(final String iName, final int iMin, final int iMax) {
+    super(iName, iMin, iMax);
+  }
+
+  public Object execute(OIdentifiable iCurrentRecord, Object iCurrentResult, final Object[] iParameters,
+      final OCommandContext iContext) {
+    final OrientBaseGraph graph = OGraphCommandExecutorSQLFactory.getGraph();
+
+    final Direction direction = Direction
+        .valueOf(OStringSerializerHelper.getStringContent(iParameters[0].toString().toUpperCase()));
+    final String[] labels;
+    if (iParameters.length > 1 && iParameters[1] != null)
+      labels = OMultiValue.array(iParameters[1], String.class);
+    else
+      labels = null;
+
+    if (iCurrentRecord == null) {
+      return OSQLEngine.foreachRecord(new OCallable<Object, OIdentifiable>() {
+        @Override
+        public Object call(final OIdentifiable iArgument) {
+          return move(graph, iArgument, direction, labels);
+        }
+      }, iCurrentResult, iContext);
+    } else
+      return move(graph, iCurrentRecord.getRecord(), direction, labels);
+  }
+
+  public Object executeSub(OIdentifiable iCurrentRecord, Object iCurrentResult, final Object[] iParameters,
+      final OCommandContext iContext, final Direction iDirection) {
+    final OrientBaseGraph graph = OGraphCommandExecutorSQLFactory.getGraph();
+
+    final String[] labels;
+    if (iParameters.length > 0 && iParameters[0] != null)
+      labels = OMultiValue.array(iParameters[0], String.class);
+    else
+      labels = null;
+
+    if (iCurrentRecord == null) {
+      return OSQLEngine.foreachRecord(new OCallable<Object, OIdentifiable>() {
+        @Override
+        public Object call(final OIdentifiable iArgument) {
+          return move(graph, iArgument, iDirection, labels);
+        }
+      }, iCurrentResult, iContext);
+    } else
+      return move(graph, iCurrentRecord.getRecord(), iDirection, labels);
+  }
+
+  protected Object move(final OrientBaseGraph graph, final OIdentifiable iRecord, final Direction iDirection, final String[] iLabels) {
+    final ODocument rec = iRecord.getRecord();
+
+    if (rec.getSchemaClass() != null)
+      if (rec.getSchemaClass().isSubClassOf(OrientVertex.CLASS_NAME)) {
+        // VERTEX
+        final OrientVertex vertex = graph.getVertex(rec);
+        if (vertex != null)
+          return vertex.getEdges(iDirection, iLabels);
+
+      } else if (rec.getSchemaClass().isSubClassOf(OrientEdge.CLASS_NAME)) {
+        // EDGE
+        final OrientEdge edge = graph.getEdge(rec);
+        if (edge != null) {
+          final OrientVertex out = (OrientVertex) edge.getVertex(iDirection);
+
+          if (iLabels == null || iLabels.length == 0)
+            return out;
+
+          // FILTER BY LABEL
+          final String vLabel = out.getLabel();
+          if (vLabel != null)
+            for (String l : iLabels)
+              if (vLabel.equals(l))
+                return out;
+        }
+      }
+
+    return null;
+  }
+
+  public String getSyntax() {
+    return "Syntax error: move(<direction>, [<labels>])";
+  }
+}
---
+++ graphdb/src/main/java/com/orientechnologies/orient/graph/sql/functions/OSQLFunctionOut.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2010-2012 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.orient.graph.sql.functions;
+
+import com.orientechnologies.orient.core.command.OCommandContext;
+import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.tinkerpop.blueprints.Direction;
+
+/**
+ * Hi-level function that return the outgoing connections. If the current element is a vertex, then will be returned edges otherwise
+ * vertices.
+ * 
+ * @author Luca Garulli (l.garulli--at--orientechnologies.com)
+ * 
+ */
+public class OSQLFunctionOut extends OSQLFunctionMove {
+  public static final String NAME = "out";
+
+  public OSQLFunctionOut() {
+    super(NAME, 0, 1);
+  }
+
+  public Object execute(OIdentifiable iCurrentRecord, Object iCurrentResult, final Object[] iParameters,
+      final OCommandContext iContext) {
+    return executeSub(iCurrentRecord, iCurrentResult, iParameters, iContext, Direction.OUT);
+  }
+
+  public String getSyntax() {
+    return "Syntax error: out([<labels>])";
+  }
+}
