f85c3fb7e457d00521e8b935c1a079fab0e85d5b
laa <lomakin.andrey@gmail.com>
2013-06-29 17:42:05 +0300

    Issue #1404  Storage keep open is set to true by default for embedded DB.

--- core/src/main/java/com/orientechnologies/orient/core/config/OGlobalConfiguration.java
+++ core/src/main/java/com/orientechnologies/orient/core/config/OGlobalConfiguration.java
@@ -116,7 +116,7 @@ public enum OGlobalConfiguration {
   STORAGE_KEEP_OPEN(
       "storage.keepOpen",
       "Tells to the engine to not close the storage when a database is closed. Storages will be closed when the process shuts down",
-      Boolean.class, Boolean.FALSE),
+      Boolean.class, Boolean.TRUE),
 
   STORAGE_LOCK_TIMEOUT("storage.lockTimeout", "Maximum timeout in milliseconds to lock the storage", Integer.class, 600000),
 
