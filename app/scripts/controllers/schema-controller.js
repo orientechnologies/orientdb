23730c0b8e4da1c26a2a5cf4f76d47773f6bf4c9
Marco Lazzara <marco.lazzara@gmail.com>
2013-07-14 21:30:23 +0200

    now available 'save properties' for a class

--- app/scripts/controllers/schema-controller.js
+++ app/scripts/controllers/schema-controller.js
@@ -1,30 +1,85 @@
-var schemaModule = angular.module('schema.controller',['database.services']);
-schemaModule.controller("SchemaController",['$scope','$routeParams','$location','Database','CommandApi',function($scope,$routeParams,$location,Database,CommandApi){
+	var schemaModule = angular.module('schema.controller',['database.services']);
+	schemaModule.controller("SchemaController",['$scope','$routeParams','$location','Database','CommandApi',function($scope,$routeParams,$location,Database,CommandApi){
 
-	$scope.database = Database;
+		$scope.database = Database;
 
-	$scope.list = $scope.database.listClasses();
-	
-	$scope.headers = ['name','superClass','alias','abstract','clusters','defaultCluster','records'];
+		$scope.list = $scope.database.listClasses();
+		
+		$scope.headers = ['name','superClass','alias','abstract','clusters','defaultCluster','records'];
 
-	
+		
 
-	$scope.setClass = function(clazz){
-		$scope.classClicked = clazz;
-	}  
-	$scope.openClass = function(clazz){
-		$location.path("/database/" + $scope.database.getName() + "/browse/editclass/" + clazz.name);
-	}
-}]);
-schemaModule.controller("ClassEditController",['$scope','$routeParams','$location','Database','CommandApi',function($scope,$routeParams,$location,Database,CommandApi){
-
-
-	var clazz = $routeParams.clazz;
+		$scope.setClass = function(clazz){
+			$scope.classClicked = clazz;
+		}  
+		$scope.openClass = function(clazz){
+			$location.path("/database/" + $scope.database.getName() + "/browse/editclass/" + clazz.name);
+		}
+	}]);
+	schemaModule.controller("ClassEditController",['$scope','$routeParams','$location','Database','CommandApi',function($scope,$routeParams,$location,Database,CommandApi){
 
 
-	$scope.classClickedHeaders = ['name','type','linkedType','linkedClass','mandatory','readonly','notNull','min','max'];
+		var clazz = $routeParams.clazz;
 
-	$scope.property = Database.listPropertiesForClass(clazz);
-	
+		$scope.database = Database;
+		$scope.limit = 20;
+		$scope.queries = new Array;
 
-}]);
+		$scope.classClickedHeaders = ['name','type','linkedType','linkedClass','mandatory','readonly','notNull','min','max'];
+
+		$scope.property = Database.listPropertiesForClass(clazz);
+		$scope.queryText = ""
+		$scope.modificati = new Array;
+		$scope.prova = new Array;
+		
+		
+console.log($scope.property[0]['linkedType'])
+		$scope.modificato = function(result,prop){
+			var key = result['name'];
+			if($scope.modificati[result['name']] == undefined){
+				$scope.modificati[result['name']] = new Array(prop);
+			}
+
+			else {
+
+				var elem= $scope.modificati[result['name']] 
+				var already = false;
+				for(i in elem){
+					if(prop == elem[i]){
+						already=true
+
+					}
+				}
+				if(already == false){
+
+					elem.push(prop);
+				}
+
+			}
+
+		}
+		$scope.saveProperty = function(properties){
+
+			for (result in properties ){
+
+	//il nome da andare a cercare nella lista dei modificati
+	var keyName = properties[result]['name'];
+	//l'array da modificare
+	var arrayToUpdate = $scope.modificati[keyName];
+
+	if(arrayToUpdate != undefined){
+
+		for(i in arrayToUpdate){
+
+			var prop = arrayToUpdate[i];
+			var sql = 'ALTER PROPERTY ' + clazz + '.' + keyName +' ' +prop+ ' ' +properties[result][prop];
+			console.log(sql);
+							CommandApi.queryText({database : $routeParams.database, language : 'sql', text : sql, limit : $scope.limit},function(data){
+
+							});
+}
+}
+}
+//clear
+$scope.modificati = new Array;
+}}]);
