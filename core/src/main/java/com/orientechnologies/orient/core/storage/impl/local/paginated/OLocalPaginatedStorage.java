4eafc0da4facfd1af3b8fd4d4c8686d8224e51d6
lvca <l.garulli@gmail.com>
2013-07-25 11:29:03 +0200

    Added log when record returns NULL stream in transaction

--- core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/paginated/OLocalPaginatedStorage.java
+++ core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/paginated/OLocalPaginatedStorage.java
@@ -1527,6 +1527,10 @@ public class OLocalPaginatedStorage extends OStorageLocalAbstract {
     case ORecordOperation.CREATED: {
       // CHECK 2 TIMES TO ASSURE THAT IT'S A CREATE OR AN UPDATE BASED ON RECURSIVE TO-STREAM METHOD
       byte[] stream = txEntry.getRecord().toStream();
+      if (stream == null) {
+        OLogManager.instance().warn(this, "Null serialization on committing new record %s in transaction", rid);
+        break;
+      }
 
       final ORecordId oldRID = rid.isNew() ? rid.copy() : rid;
 
@@ -1559,6 +1563,11 @@ public class OLocalPaginatedStorage extends OStorageLocalAbstract {
 
     case ORecordOperation.UPDATED: {
       byte[] stream = txEntry.getRecord().toStream();
+      if (stream == null) {
+        OLogManager.instance().warn(this, "Null serialization on committing updated record %s in transaction", rid);
+        break;
+      }
+
       txEntry
           .getRecord()
           .getRecordVersion()
