0164fe6263f3c52f37d6dd669891d070bf588445
lvca <l.garulli@gmail.com>
2013-07-30 15:14:34 +0200

    Released 1.5.0

--- enterprise/src/main/java/com/orientechnologies/orient/enterprise/channel/binary/OChannelBinaryProtocol.java
+++ enterprise/src/main/java/com/orientechnologies/orient/enterprise/channel/binary/OChannelBinaryProtocol.java
@@ -100,6 +100,7 @@ public class OChannelBinaryProtocol {
   public static final short RECORD_NULL                            = -2;
   public static final short RECORD_RID                             = -3;
 
+  // FOR MORE INFO: https://github.com/orientechnologies/orientdb/wiki/Network-Binary-Protocol#wiki-Compatibility
   public static final int   CURRENT_PROTOCOL_VERSION               = 17; // SENT AS SHORT AS FIRST PACKET AFTER SOCKET CONNECTION
 
   public static OIdentifiable readIdentifiable(final OChannelBinaryClient network) throws IOException {
