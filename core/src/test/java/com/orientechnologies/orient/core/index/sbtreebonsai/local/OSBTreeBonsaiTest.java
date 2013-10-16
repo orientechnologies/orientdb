c696ec57ab457e1cdf2abb9c5b220d8032360f38
Artem Orobets <enisher@gmail.com>
2013-10-16 16:32:28 +0300

    Fixed maven tests

--- core/src/test/java/com/orientechnologies/orient/core/index/sbtreebonsai/local/OSBTreeBonsaiTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/index/sbtreebonsai/local/OSBTreeBonsaiTest.java
@@ -52,7 +52,7 @@ public class OSBTreeBonsaiTest {
     databaseDocumentTx.create();
 
     sbTree = new OSBTreeBonsai<Integer, OIdentifiable>(".sbt", 1, false);
-    sbTree.create("sbTree", OIntegerSerializer.INSTANCE, OLinkSerializer.INSTANCE,
+    sbTree.create("OSBTreeBonsaiTest", OIntegerSerializer.INSTANCE, OLinkSerializer.INSTANCE,
         (OStorageLocalAbstract) databaseDocumentTx.getStorage());
   }
 
