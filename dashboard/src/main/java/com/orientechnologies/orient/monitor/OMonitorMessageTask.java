346a7a9aba34e7b348fa6eee17d0f5a9fa457c8b
Enrico Risa <maggiolo00@gmail.com>
2013-11-07 22:14:39 +0100

    hardcoded url

--- dashboard/src/main/java/com/orientechnologies/orient/monitor/OMonitorMessageTask.java
+++ dashboard/src/main/java/com/orientechnologies/orient/monitor/OMonitorMessageTask.java
@@ -6,7 +6,7 @@ import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -46,16 +46,18 @@ public class OMonitorMessageTask extends TimerTask {
 			licenses += license;
 			i++;
 		}
+
 		final List<ODocument> response = this.handler.getDb().query(osqlQuery);
 		licenses = licenses.isEmpty() ? "none" : licenses;
 		if (response.size() > 0) {
 			ODocument config = response.iterator().next();
-			String url = config.field("orientdbSite");
+			String url = "http://www.orientechnologies.com/"; //config.field("orientdbSite");
 			OLogManager.instance().info(this, "MONITOR contacting [%s] ", url);
 			if (url != null) {
 				try {
-					URL remoteUrl = new java.net.URL(url + "" + cId + "/" + licenses);
+					URL remoteUrl = new java.net.URL(url + "pro/function/business/check/" + cId + "/" + licenses + "/true");
 					URLConnection urlConnection = null;
+
 					ODocument proxy = config.field("proxyConfiguration");
 					String ip = proxy.field("proxyIp");
 					Integer port = proxy.field("proxyPort");
@@ -79,12 +81,25 @@ public class OMonitorMessageTask extends TimerTask {
 
 					final ODocument docMetrics = new ODocument().fromJSON(result);
 
-					List<ODocument> messages = docMetrics.field("messages");
+					List<Map<String, Object>> results = docMetrics.field("result");
+					Map<String, Object> map = results.get(0);
+					Object obj = map.get("messages");
+					List<Map<String, Object>> oDocument = (List<Map<String, Object>>) map.get("messages");
+					for (Map<String, Object> values : oDocument) {
 
-					for (ODocument oDocument : messages) {
-						ODocument saved = new ODocument("Message");
-						saved.field("message", oDocument.field("text"));
-						saved.save();
+						Object text = values.get("text");
+						Object date = values.get("date");
+						Map<String, Object> params = new HashMap<String, Object>();
+						params.put("message", text);
+						params.put("date", date);
+						List<ODocument> resultSet = handler.getDb().query(
+								new OSQLSynchQuery<ORecordSchemaAware<?>>("select from Message where message = :message and date = :date"), params);
+						if (resultSet.isEmpty()) {
+							ODocument saved = new ODocument("Message");
+							saved.field("message", text);
+							saved.field("date", date);
+							saved.save();
+						}
 					}
 				} catch (Exception e) {
 					OLogManager.instance().error(this, "MONITOR error contacting [%s] ", url);
