13e8be365bcb0f471f07fafd5abf6f9ddd4815dc
lvca <l.garulli@gmail.com>
2013-11-08 15:10:40 +0100

    Fixed issue #1802

---
+++ server/src/main/java/com/orientechnologies/orient/server/network/protocol/http/command/post/OServerCommandPostBackupDatabase.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2010-2012 Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.orientechnologies.orient.server.network.protocol.http.command.post;
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.Date;
+
+import com.orientechnologies.orient.core.command.OCommandOutputListener;
+import com.orientechnologies.orient.core.db.record.ODatabaseRecord;
+import com.orientechnologies.orient.server.network.protocol.http.OHttpRequest;
+import com.orientechnologies.orient.server.network.protocol.http.OHttpResponse;
+import com.orientechnologies.orient.server.network.protocol.http.OHttpUtils;
+import com.orientechnologies.orient.server.network.protocol.http.command.OServerCommandAuthenticatedDbAbstract;
+
+public class OServerCommandPostBackupDatabase extends OServerCommandAuthenticatedDbAbstract implements OCommandOutputListener {
+  private static final String[] NAMES = { "POST|backup/*" };
+
+  @Override
+  public boolean execute(final OHttpRequest iRequest, final OHttpResponse iResponse) throws Exception {
+    String[] urlParts = checkSyntax(iRequest.url, 2, "Syntax error: backup/<database>/[<name>][?params*]");
+
+    if (urlParts.length > 2) {
+    } else {
+      backupStandard(iRequest, iResponse);
+    }
+    return false;
+  }
+
+  protected void backupStandard(final OHttpRequest iRequest, final OHttpResponse iResponse) throws InterruptedException,
+      IOException {
+    iRequest.data.commandInfo = "Database backup";
+    final ODatabaseRecord database = getProfiledDatabaseInstance(iRequest);
+    try {
+      iResponse.writeStatus(OHttpUtils.STATUS_OK_CODE, OHttpUtils.STATUS_OK_DESCRIPTION);
+      iResponse.writeHeaders(OHttpUtils.CONTENT_GZIP);
+      iResponse.writeLine("Content-Disposition: attachment; filename=" + database.getName() + ".gz");
+      iResponse.writeLine("Date: " + new Date());
+      iResponse.writeLine(null);
+      database.backup(iResponse.getOutputStream(), null, null);
+
+      try {
+        iResponse.flush();
+      } catch (SocketException e) {
+      }
+    } finally {
+      if (database != null)
+        database.close();
+    }
+  }
+
+  @Override
+  public void onMessage(String iText) {
+  }
+
+  @Override
+  public String[] getNames() {
+    return NAMES;
+  }
+}
