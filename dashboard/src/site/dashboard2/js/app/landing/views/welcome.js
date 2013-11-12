3d61d2179e4450f65de846da6977ef631b5e0067
lvca <l.garulli@gmail.com>
2013-11-12 15:31:40 +0100

    Removed old sources

--- dashboard/src/site/dashboard2/js/app/landing/views/welcome.js
+++
@@ -1,7 +0,0 @@
-define([ 'marionette', 'landing/templates' ], function(Marionette, templates) {
-  "use strict";
-
-  return Marionette.ItemView.extend({
-    template : templates.welcome,
-  });
-});
