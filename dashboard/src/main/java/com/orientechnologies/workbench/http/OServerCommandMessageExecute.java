a1f1fcebc4b7bfc80ed99c5f6fad5bbfa5cd0ac8
Enrico Risa <maggiolo00@gmail.com>
2013-11-12 14:41:44 +0100

    added utils for unzipping

--- dashboard/src/main/java/com/orientechnologies/workbench/http/OServerCommandMessageExecute.java
+++ dashboard/src/main/java/com/orientechnologies/workbench/http/OServerCommandMessageExecute.java
@@ -84,34 +84,6 @@ public class OServerCommandMessageExecute extends OServerCommandAuthenticatedDbA
 				urlConnection.connect();
 				File zip = WUtils.unpackArchive(remoteUrl, new File("download"));
 				zip.delete();
-				/*InputStream is = urlConnection.getInputStream();
-				ZipInputStream stream = new ZipInputStream(is);
-
-				// create a buffer to improve copy performance later.
-				byte[] buffer = new byte[2048];
-				ZipEntry entry;
-				stream.available();
-				while ((entry = stream.getNextEntry()) != null) {
-					String s = String.format("Entry: %s len %d added %TD", entry.getName(), entry.getSize(), new Date(entry.getTime()));
-					System.out.println(s);
-
-					// Once we get the entry from the stream, the stream is
-					// positioned read to read the raw data, and we keep
-					// reading until read returns 0 or less.
-					String outpath = "download/" + entry.getName();
-					FileOutputStream output = null;
-					try {
-						output = new FileOutputStream(outpath);
-						int len = 0;
-						while ((len = stream.read(buffer)) > 0) {
-							output.write(buffer, 0, len);
-						}
-					} finally {
-						// we must always close the output file
-						if (output != null)
-							output.close();
-					}
-				}*/
 			}
 
 			message.field("type", "news");
