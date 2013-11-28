119425159d1a92d43da1a26816dababde18e8558
lvca <l.garulli@gmail.com>
2013-11-28 18:20:45 +0100

    Fixed problem with Hazelcast shutdown

--- distributed/src/main/java/com/orientechnologies/orient/server/hazelcast/OHazelcastPlugin.java
+++ distributed/src/main/java/com/orientechnologies/orient/server/hazelcast/OHazelcastPlugin.java
@@ -220,7 +220,13 @@ public class OHazelcastPlugin extends ODistributedAbstractPlugin implements Memb
       hazelcastInstance.getCluster().removeMembershipListener(membershipListenerRegistration);
     }
 
-    hazelcastInstance.shutdown();
+    try {
+      hazelcastInstance.shutdown();
+    } catch (Exception e) {
+      OLogManager.instance().error(this, "Error on shutting down Hazelcast instance", e);
+    } finally {
+      hazelcastInstance = null;
+    }
 
     setStatus(STATUS.OFFLINE);
 
