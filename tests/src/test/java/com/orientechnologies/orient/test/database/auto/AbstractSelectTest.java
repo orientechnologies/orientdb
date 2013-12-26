b3703dcb8ff6c0140fd19c70952f30ea56746b6c
laa <lomakin.andrey@gmail.com>
2013-12-26 14:37:46 +0200

    Issue #1912 was fixed.

--- tests/src/test/java/com/orientechnologies/orient/test/database/auto/AbstractSelectTest.java
+++ tests/src/test/java/com/orientechnologies/orient/test/database/auto/AbstractSelectTest.java
@@ -11,7 +11,6 @@ import org.testng.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public abstract class AbstractSelectTest {
