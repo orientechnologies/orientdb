c2c4f14af6c64facf78b3fae6c2bf8847b3c3171
lvca <l.garulli@gmail.com>
2014-01-05 23:24:18 +0100

    Added new test case for special characters

--- graphdb/src/test/java/com/orientechnologies/orient/graph/blueprints/BlueprintsTest.java
+++ graphdb/src/test/java/com/orientechnologies/orient/graph/blueprints/BlueprintsTest.java
@@ -68,7 +68,7 @@ public class BlueprintsTest {
 
     Vertex v1 = graph.addVertex(null);
     Vertex v2 = graph.addVertex(null);
-    OrientEdge e = graph.addEdge(null, v1, v2, null);
+    OrientEdge e = graph.addEdge(null, v1, v2, "anyLabel");
     e.setProperty("key", "forceCreationOfDocument");
 
     Iterable<Edge> result = graph.command(new OSQLSynchQuery<Edge>("select from e where key = 'forceCreationOfDocument'"))
@@ -82,4 +82,20 @@ public class BlueprintsTest {
     result = graph.command(new OSQLSynchQuery<Edge>("select from e where key = 'forceCreationOfDocument'")).execute();
     Assert.assertFalse(result.iterator().hasNext());
   }
+
+  @Test
+  public void testQueryWithSpecialCharacters() {
+    graph.setAutoStartTx(false);
+
+    graph.addVertex(null).setProperty("name", "Jay");
+    graph.addVertex(null).setProperty("name", "Smith's");
+    graph.addVertex(null).setProperty("name", "Smith\"s");
+
+    graph.commit(); // transaction not-reopened
+
+    Assert.assertTrue(graph.getVertices("name", "Jay").iterator().hasNext());
+    Assert.assertTrue(graph.getVertices("name", "Smith's").iterator().hasNext());
+    Assert.assertTrue(graph.getVertices("name", "Smith\"s").iterator().hasNext());
+  }
+
 }
