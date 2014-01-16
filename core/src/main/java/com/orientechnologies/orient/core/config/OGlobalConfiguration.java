7a19257fc551be220501332ccc48826c45f15f51
laa <lomakin.andrey@gmail.com>
2014-01-16 21:15:57 +0200

    Build fix.

--- core/src/main/java/com/orientechnologies/orient/core/config/OGlobalConfiguration.java
+++ core/src/main/java/com/orientechnologies/orient/core/config/OGlobalConfiguration.java
@@ -290,9 +290,9 @@ public enum OGlobalConfiguration {
   MVRBTREE_RID_BINARY_THRESHOLD(
       "mvrbtree.ridBinaryThreshold",
       "Valid for set of rids. It's the threshold as number of entries to use the binary streaming instead of classic string streaming. -1 means never use binary streaming",
-          Integer.class, -1),
+      Integer.class, -1),
 
-    MVRBTREE_RID_NODE_PAGE_SIZE("mvrbtree.ridNodePageSize",
+  MVRBTREE_RID_NODE_PAGE_SIZE("mvrbtree.ridNodePageSize",
       "Page size of each treeset node. 16 means that 16 entries can be stored inside each node", Integer.class, 64),
 
   MVRBTREE_RID_NODE_SAVE_MEMORY("mvrbtree.ridNodeSaveMemory",
