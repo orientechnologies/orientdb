ffea089f1bb2bbcd87958ad273ee763a756323ed
laa <lomakin.andrey@gmail.com>
2014-02-03 14:36:37 +0200

    Problem with server shutdown was fixed.

--- server/src/main/java/com/orientechnologies/orient/server/network/protocol/binary/ONetworkProtocolBinary.java
+++ server/src/main/java/com/orientechnologies/orient/server/network/protocol/binary/ONetworkProtocolBinary.java
@@ -1616,7 +1616,7 @@ public class ONetworkProtocolBinary extends OBinaryNetworkProtocolAbstract {
   protected void endResponse() throws IOException {
     // reseting transaction state. Commands are stateless and connection should be cleared
     // otherwise reused connection (connections pool) may lead to unpredicted errors
-    if (connection.database!=null && connection.database.getTransaction()!=null)
+    if (connection != null && connection.database!=null && connection.database.getTransaction()!=null)
             connection.database.getTransaction().close();
     channel.flush();
     channel.releaseWriteLock();
