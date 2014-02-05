f8b368a185f8e9f83c3649e636606fa37c83066a
laa <lomakin.andrey@gmail.com>
2014-02-05 20:05:54 +0200

    1.7-rc1 version was set

--- core/src/main/java/com/orientechnologies/orient/core/OConstants.java
+++ core/src/main/java/com/orientechnologies/orient/core/OConstants.java
@@ -16,7 +16,8 @@
 package com.orientechnologies.orient.core;
 
 public class OConstants {
-  public static final String ORIENT_VERSION = "1.7-SNAPSHOT";
+  public static final String ORIENT_VERSION = "1.7-rc1";
+
   public static final String ORIENT_URL     = "www.orientechnologies.com";
 
   public static String getVersion() {
