9d45da21a7ee6b4c3bf74b98bf65786f05a66c90
kowalot <kowalot@gmail.com>
2014-03-17 18:34:28 +0100

    Revert "Revert "#2088 LOCK not released in SQL UPDATE-PESSIMISTIC LOCK""
    This reverts commit 2508b3e6411b5d0cffb0fdc8256426c30673d20c.

--- core/src/main/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLSelect.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLSelect.java
@@ -73,6 +73,7 @@ import com.orientechnologies.orient.core.sql.operator.OQueryOperatorMinorEquals;
 import com.orientechnologies.orient.core.sql.operator.OQueryOperatorOr;
 import com.orientechnologies.orient.core.sql.query.OSQLQuery;
 import com.orientechnologies.orient.core.storage.OStorage;
+import com.orientechnologies.orient.core.storage.OStorageEmbedded;
 
 /**
  * Executes the SQL SELECT statement. the parse() method compiles the query and builds the meta information needed by the execute().
@@ -361,8 +362,9 @@ public class OCommandExecutorSQLSelect extends OCommandExecutorSQLResultsetAbstr
 
     final OStorage.LOCKING_STRATEGY lockingStrategy = context.getVariable("$locking") != null ? (OStorage.LOCKING_STRATEGY) context
         .getVariable("$locking") : OStorage.LOCKING_STRATEGY.DEFAULT;
-
-    final ORecordInternal<?> record = id instanceof ORecordInternal<?> ? (ORecordInternal<?>) id : getDatabase().load(
+    ORecordInternal<?> record = null;
+    try{
+        record = id instanceof ORecordInternal<?> ? (ORecordInternal<?>) id : getDatabase().load(
         id.getIdentity(), null, false, false, lockingStrategy);
 
     context.updateMetric("recordReads", +1);
@@ -377,7 +379,16 @@ public class OCommandExecutorSQLSelect extends OCommandExecutorSQLResultsetAbstr
       if (!handleResult(record, true))
         // END OF EXECUTION
         return false;
-
+    }finally{
+          // lock must be released (no matter if filtered or not)
+          if (lockingStrategy==OStorage.LOCKING_STRATEGY.KEEP_EXCLUSIVE_LOCK)
+          {
+              if (record!=null)
+              {
+                  ((OStorageEmbedded) getDatabase().getStorage()).releaseWriteLock(record.getIdentity());
+              }
+          }
+      }
     return true;
   }
 
@@ -878,7 +889,7 @@ public class OCommandExecutorSQLSelect extends OCommandExecutorSQLResultsetAbstr
       return -1;
 
     int upperBound = OStringSerializerHelper.getLowerIndexOf(parserTextUpperCase, parserGetCurrentPosition(), KEYWORD_FROM_2FIND,
-        KEYWORD_LET_2FIND);
+            KEYWORD_LET_2FIND);
     if (upperBound == -1)
       // UP TO THE END
       upperBound = parserText.length();
