6c7e78e67563c4ad7bed3feb403e64635767c8f5
lvca <l.garulli@gmail.com>
2014-04-03 11:33:23 +0200

    Fixed compilation problem with Java<7

--- tests/src/test/java/com/orientechnologies/orient/test/internal/BackupTest.java
+++ tests/src/test/java/com/orientechnologies/orient/test/internal/BackupTest.java
@@ -3,11 +3,10 @@ package com.orientechnologies.orient.test.internal;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphNoTx;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Enumeration;
 import java.util.zip.ZipFile;
 
@@ -16,14 +15,14 @@ import java.util.zip.ZipFile;
  */
 public class BackupTest {
   public static void main(String[] args) throws IOException {
-    Path backupFile = Paths.get("testbackup.zip");
-    OutputStream oS = new FileOutputStream(backupFile.toFile());
+    File backupFile = new File("testbackup.zip");
+    OutputStream oS = new FileOutputStream(backupFile);
 
     OrientGraphFactory factory = new OrientGraphFactory("plocal:backupTest", "admin", "admin");
     OrientGraphNoTx graphNoTx = factory.getNoTx();
     graphNoTx.getRawGraph().backup(oS, null, null, null, 1, 1024);
 
-    ZipFile zipFile = new ZipFile(backupFile.toFile());
+    ZipFile zipFile = new ZipFile(backupFile);
     Enumeration enumeration = zipFile.entries();
     System.out.format("ZipFile : %s%n", zipFile);
     while (enumeration.hasMoreElements()) {
