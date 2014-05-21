76fb6a08f41ce654ca4a80c7e4d5870632a18eb2
lvca <l.garulli@gmail.com>
2014-05-21 20:08:06 +0200

    Benchmarks: fixed typos

--- server/benchmarks/bench_persistent_get.sh
+++ server/benchmarks/bench_persistent_get.sh
@@ -2,4 +2,4 @@
 #
 # Copyright (c) 1999-2014 Luca Garulli
 #
-ab -n1000 -A admin:admin -k -v -c10 http://127.0.0.1:2480/document/GratefulDeadConcerts/9:1
+ab -n10000 -A admin:admin -k -v -c16 http://127.0.0.1:2480/document/GratefulDeadConcerts/9:1
