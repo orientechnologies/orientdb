b258b23282763bb044c157abcfacc1af68985d08
Jon Mort <jonmort@gmail.com>
2014-06-06 17:16:53 +0100

    Set API var to be relative to the current URL

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -1,4 +1,6 @@
-var API = '/api/';
-//var API = '/';
+var API = (function() {
+    var m = window.location.pathname.match(/(.*\/)studio\/index.html/);
+    return m && m[1] ? m[1] : '/';
+})();
 
 var STUDIO_VERSION = "1.7";
