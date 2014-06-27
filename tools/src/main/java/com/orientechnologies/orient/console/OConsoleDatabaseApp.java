f6b49ca18f1317c39229119e92beb768d86b194a
enisher <enisher@gmail.com>
2014-06-27 18:30:31 +0300

    Fixed `repair database` command for graph model

--- tools/src/main/java/com/orientechnologies/orient/console/OConsoleDatabaseApp.java
+++ tools/src/main/java/com/orientechnologies/orient/console/OConsoleDatabaseApp.java
@@ -15,6 +15,27 @@
  */
 package com.orientechnologies.orient.console;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+
 import com.orientechnologies.common.collection.OMultiValue;
 import com.orientechnologies.common.console.TTYConsoleReader;
 import com.orientechnologies.common.console.annotation.ConsoleCommand;
@@ -73,18 +94,6 @@ import com.orientechnologies.orient.core.storage.OStorage;
 import com.orientechnologies.orient.core.storage.impl.local.ODataHoleInfo;
 import com.orientechnologies.orient.core.storage.impl.local.OStorageLocal;
 import com.orientechnologies.orient.core.storage.impl.local.OStorageLocalAbstract;
-
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.Map.Entry;
 
 public class OConsoleDatabaseApp extends OrientConsole implements OCommandOutputListener, OProgressListener {
   protected ODatabaseDocument   currentDatabase;
@@ -1486,8 +1495,8 @@ public class OConsoleDatabaseApp extends OrientConsole implements OCommandOutput
                       message("\n--- reset link " + ((OIdentifiable) fieldValue).getIdentity() + " in field '" + fieldName
                           + "' (rid=" + doc.getIdentity() + ")");
                   }
-                } else if (fieldValue instanceof Collection<?>) {
-                  final Iterator<Object> it = ((Collection) fieldValue).iterator();
+                } else if (fieldValue instanceof Iterable<?>) {
+                  final Iterator<Object> it = ((Iterable) fieldValue).iterator();
                   for (int i = 0; it.hasNext(); ++i) {
                     final Object v = it.next();
                     if (fixLink(v)) {
