e7d9b49813854eb041521bc122c4c034c574ed1d
julian.neuhaus <julian.neuhaus@brain-activit.com>
2014-08-15 14:49:12 +0200

    added negative test-cases to OObjectEnumLazyListTest to check correct behaviour of incorrect method-calls.

--- object/src/test/java/com/orientechnologies/orient/object/enumerations/OObjectEnumLazyListTest.java
+++ object/src/test/java/com/orientechnologies/orient/object/enumerations/OObjectEnumLazyListTest.java
@@ -1,5 +1,6 @@
 package com.orientechnologies.orient.object.enumerations;
 
+import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,13 @@ import com.orientechnologies.orient.object.db.OObjectDatabaseTx;
 public class OObjectEnumLazyListTest
 {
 	public enum TESTENUM
+	{
+		TEST_VALUE_1,
+		TEST_VALUE_2,
+		TEST_VALUE_3
+	}
+	
+	public enum WRONG_TESTENUM
 	{
 		TEST_VALUE_1,
 		TEST_VALUE_2,
@@ -48,6 +56,13 @@ public class OObjectEnumLazyListTest
 		assertTrue(hasListWithEnums.getEnumList().contains(TESTENUM.TEST_VALUE_1));
 		assertTrue(hasListWithEnums.getEnumList().contains(TESTENUM.TEST_VALUE_2));
 		assertTrue(hasListWithEnums.getEnumList().contains(TESTENUM.TEST_VALUE_3));
+		
+		assertFalse(hasListWithEnums.getEnumList().contains(WRONG_TESTENUM.TEST_VALUE_1));
+		assertFalse(hasListWithEnums.getEnumList().contains(WRONG_TESTENUM.TEST_VALUE_2));
+		assertFalse(hasListWithEnums.getEnumList().contains(WRONG_TESTENUM.TEST_VALUE_3));
+		
+		assertFalse(hasListWithEnums.getEnumList().contains(null));		
+		assertFalse(hasListWithEnums.getEnumList().contains("INVALID TYPE"));
 	}	
 	
 	@Test
@@ -58,6 +73,13 @@ public class OObjectEnumLazyListTest
 		assertTrue(hasListWithEnums.getEnumList().indexOf(TESTENUM.TEST_VALUE_1) == 0);
 		assertTrue(hasListWithEnums.getEnumList().indexOf(TESTENUM.TEST_VALUE_2) == 1);
 		assertTrue(hasListWithEnums.getEnumList().indexOf(TESTENUM.TEST_VALUE_3) == 2);
+		
+		assertTrue(hasListWithEnums.getEnumList().indexOf(WRONG_TESTENUM.TEST_VALUE_1) == -1);
+		assertTrue(hasListWithEnums.getEnumList().indexOf(WRONG_TESTENUM.TEST_VALUE_2) == -1);
+		assertTrue(hasListWithEnums.getEnumList().indexOf(WRONG_TESTENUM.TEST_VALUE_3) == -1);
+		
+		assertTrue(hasListWithEnums.getEnumList().indexOf(null) == -1);
+		assertTrue(hasListWithEnums.getEnumList().indexOf("INVALID TYPE") == -1);
 	}	
 	
 	@Test
@@ -68,6 +90,13 @@ public class OObjectEnumLazyListTest
 		assertTrue(hasListWithEnums.getEnumList().lastIndexOf(TESTENUM.TEST_VALUE_1) == 3);
 		assertTrue(hasListWithEnums.getEnumList().lastIndexOf(TESTENUM.TEST_VALUE_2) == 4);
 		assertTrue(hasListWithEnums.getEnumList().lastIndexOf(TESTENUM.TEST_VALUE_3) == 5);
+		
+		assertTrue(hasListWithEnums.getEnumList().lastIndexOf(WRONG_TESTENUM.TEST_VALUE_1) == -1);
+		assertTrue(hasListWithEnums.getEnumList().lastIndexOf(WRONG_TESTENUM.TEST_VALUE_2) == -1);
+		assertTrue(hasListWithEnums.getEnumList().lastIndexOf(WRONG_TESTENUM.TEST_VALUE_3) == -1);
+		
+		assertTrue(hasListWithEnums.getEnumList().lastIndexOf(null) == -1);
+		assertTrue(hasListWithEnums.getEnumList().lastIndexOf("INVALID TYPE") == -1);
 	}	
 	
 	private EntityWithEnumList getTestObject()
