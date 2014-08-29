b5c4ee19fe32e54f6862179cbacc79c025c8ead4
laa <lomakin.andrey@gmail.com>
2014-08-29 06:50:45 +0300

    Issue #2697 thread local counters were integrated in RW lock.

--- commons/src/main/java/com/orientechnologies/common/concur/lock/OReadersWriterSpinLock.java
+++ commons/src/main/java/com/orientechnologies/common/concur/lock/OReadersWriterSpinLock.java
@@ -1,12 +1,7 @@
 package com.orientechnologies.common.concur.lock;
 
 import com.orientechnologies.common.types.OModifiableInteger;
-import com.orientechnologies.common.util.MersenneTwister;
-
-import java.util.Arrays;
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.AbstractOwnableSynchronizer;
 import java.util.concurrent.locks.LockSupport;
 
@@ -15,30 +10,16 @@ import java.util.concurrent.locks.LockSupport;
  * @since 8/18/14
  */
 public class OReadersWriterSpinLock extends AbstractOwnableSynchronizer {
-  private static final int                      NCPU           = Runtime.getRuntime().availableProcessors();
-  private static final int                      COUNTERS_SIZE  = 1 << (32 - Integer.numberOfLeadingZeros((NCPU << 2) - 1));
-  private static final int                      MASK           = COUNTERS_SIZE - 1;
-
-  private final MersenneTwister                 random         = new MersenneTwister();
-  private final ThreadLocal<Integer>            threadHashCode = new ThreadLocal<Integer>() {
-                                                                 @Override
-                                                                 protected Integer initialValue() {
-                                                                   return random.nextInt();
-                                                                 }
-                                                               };
-
-  private final AtomicInteger                   writeState     = new AtomicInteger(0);
-
-  private final PaddedCounter[]                 readCounters;
-
+  private final OThreadCountersHashTable threadCountersHashTable = new OThreadCountersHashTable();
+  private final AtomicInteger                   writeState              = new AtomicInteger(0);
   private final long                            writeDelay;
 
-  private final ThreadLocal<OModifiableInteger> lockHolds      = new ThreadLocal<OModifiableInteger>() {
-                                                                 @Override
-                                                                 protected OModifiableInteger initialValue() {
-                                                                   return new OModifiableInteger();
-                                                                 }
-                                                               };
+  private final ThreadLocal<OModifiableInteger> lockHolds               = new ThreadLocal<OModifiableInteger>() {
+                                                                          @Override
+                                                                          protected OModifiableInteger initialValue() {
+                                                                            return new OModifiableInteger();
+                                                                          }
+                                                                        };
 
   public OReadersWriterSpinLock() {
     this(100 * 1000); // 0.1 ms
@@ -46,12 +27,6 @@ public class OReadersWriterSpinLock extends AbstractOwnableSynchronizer {
 
   public OReadersWriterSpinLock(final long writeDelay) {
     this.writeDelay = writeDelay;
-
-    final PaddedCounter[] counters = new PaddedCounter[COUNTERS_SIZE];
-    for (int i = 0; i < counters.length; i++)
-      counters[i] = new PaddedCounter();
-
-    readCounters = counters;
   }
 
   public void acquireReadLock() {
@@ -60,25 +35,24 @@ public class OReadersWriterSpinLock extends AbstractOwnableSynchronizer {
     final int holds = lHolds.intValue();
     if (holds > 0) {
       // we have already acquire read lock
-      lockHolds.get().increment();
+      lHolds.increment();
       return;
     } else if (holds < 0) {
       // write lock is acquired before, do nothing
       return;
     }
 
-    final PaddedCounter counter = readCounters[threadHashCode.get() & MASK];
-    incrementCounter(counter);
+    threadCountersHashTable.increment();
 
     int wstate = writeState.get();
 
     while (wstate > 0) {
-      decrementCounter(counter);
+      threadCountersHashTable.decrement();
 
       while (writeState.get() == 1)
         LockSupport.parkNanos(writeDelay);
 
-      incrementCounter(counter);
+      threadCountersHashTable.increment();
 
       wstate = writeState.get();
     }
@@ -98,25 +72,10 @@ public class OReadersWriterSpinLock extends AbstractOwnableSynchronizer {
       return;
     }
 
-    final PaddedCounter counter = readCounters[threadHashCode.get() & MASK];
-    decrementCounter(counter);
+    threadCountersHashTable.decrement();
 
     lHolds.decrement();
     assert lHolds.intValue() == 0;
-  }
-
-  private void incrementCounter(PaddedCounter counter) {
-    long attempts = 0;
-
-    long cnt = counter.get();
-    while (!counter.compareAndSet(cnt, cnt + 1)) {
-      attempts++;
-
-      if (attempts > NCPU)
-        LockSupport.parkNanos(1);
-
-      cnt = counter.get();
-    }
   }
 
   public void acquireWriteLock() {
@@ -130,18 +89,8 @@ public class OReadersWriterSpinLock extends AbstractOwnableSynchronizer {
     while (!writeState.compareAndSet(0, 1))
       LockSupport.parkNanos(writeDelay);
 
-    boolean cnt = false;
-    while (!cnt) {
-      cnt = true;
-
-      for (PaddedCounter counter : readCounters) {
-        cnt = counter.get() == 0;
-
-        if (!cnt)
-          break;
-      }
-
-    }
+    while (!threadCountersHashTable.isEmpty())
+      ;
 
     setExclusiveOwnerThread(Thread.currentThread());
     lHolds.decrement();
@@ -162,84 +111,5 @@ public class OReadersWriterSpinLock extends AbstractOwnableSynchronizer {
 
     lHolds.increment();
     assert lHolds.intValue() == 0;
-  }
-
-  private void decrementCounter(PaddedCounter counter) {
-    long attempts = 0;
-
-    long cnt = counter.get();
-    assert cnt > 0;
-    while (!counter.compareAndSet(cnt, cnt - 1)) {
-      attempts++;
-      if (attempts > NCPU)
-        LockSupport.parkNanos(1);
-
-      cnt = counter.get();
-    }
-  }
-
-  void modPadding() {
-    for (PaddedCounter counter : readCounters)
-      counter.modPadding();
-  }
-
-  @Override
-  public String toString() {
-    modPadding();
-    return "OReadersWriterSpinLock{" + "writeState=" + writeState + ", readCounters=" + Arrays.toString(readCounters) + '}';
-  }
-
-  private static final class PaddedCounter extends AtomicLong {
-    private long p0 = 0, p1 = 1, p2 = 2, p3 = 3, p4 = 4, p5 = 5, p6 = 6;
-
-    void modPadding() {
-      Random random = new Random();
-      p0 = random.nextLong();
-      p1 = random.nextLong();
-      p2 = random.nextLong();
-      p3 = random.nextLong();
-      p4 = random.nextLong();
-      p5 = random.nextLong();
-      p6 = random.nextLong();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o)
-        return true;
-      if (o == null || getClass() != o.getClass())
-        return false;
-
-      PaddedCounter that = (PaddedCounter) o;
-
-      if (p0 != that.p0)
-        return false;
-      if (p1 != that.p1)
-        return false;
-      if (p2 != that.p2)
-        return false;
-      if (p3 != that.p3)
-        return false;
-      if (p4 != that.p4)
-        return false;
-      if (p5 != that.p5)
-        return false;
-      if (p6 != that.p6)
-        return false;
-
-      return true;
-    }
-
-    @Override
-    public int hashCode() {
-      int result = (int) (p0 ^ (p0 >>> 32));
-      result = 31 * result + (int) (p1 ^ (p1 >>> 32));
-      result = 31 * result + (int) (p2 ^ (p2 >>> 32));
-      result = 31 * result + (int) (p3 ^ (p3 >>> 32));
-      result = 31 * result + (int) (p4 ^ (p4 >>> 32));
-      result = 31 * result + (int) (p5 ^ (p5 >>> 32));
-      result = 31 * result + (int) (p6 ^ (p6 >>> 32));
-      return result;
-    }
   }
 }
