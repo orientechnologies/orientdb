4d01eff369480f4a9de247f017666624d5c54492
lvca <l.garulli@gmail.com>
2014-09-14 17:37:48 +0200

    Fixed issue #2704

---
+++ core/src/main/java/com/orientechnologies/orient/core/sql/functions/misc/OSQLFunctionUUID.java
@@ -0,0 +1,62 @@
+/*
+ *
+ *  *  Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
+ *  *
+ *  *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  *  You may obtain a copy of the License at
+ *  *
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *  Unless required by applicable law or agreed to in writing, software
+ *  *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  See the License for the specific language governing permissions and
+ *  *  limitations under the License.
+ *  *
+ *  * For more information: http://www.orientechnologies.com
+ *  
+ */
+package com.orientechnologies.orient.core.sql.functions.misc;
+
+import com.orientechnologies.orient.core.command.OCommandContext;
+import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.core.sql.functions.OSQLFunctionAbstract;
+
+import java.util.UUID;
+
+/**
+ * Generates a UUID as a 128-bits value using the Leach-Salz variant. For more information look at:
+ * http://docs.oracle.com/javase/6/docs/api/java/util/UUID.html.
+ * 
+ * @author Luca Garulli (l.garulli--at--orientechnologies.com)
+ *
+ */
+public class OSQLFunctionUUID extends OSQLFunctionAbstract {
+  public static final String NAME = "uuid";
+
+  /**
+   * Get the date at construction to have the same date for all the iteration.
+   */
+  public OSQLFunctionUUID() {
+    super(NAME, 0, 0);
+  }
+
+  public Object execute(Object iThis, final OIdentifiable iCurrentRecord, final Object iCurrentResult, final Object[] iParams,
+      OCommandContext iContext) {
+    return UUID.randomUUID();
+  }
+
+  public boolean aggregateResults(final Object[] configuredParameters) {
+    return false;
+  }
+
+  public String getSyntax() {
+    return "uuid()";
+  }
+
+  @Override
+  public Object getResult() {
+    return null;
+  }
+}
