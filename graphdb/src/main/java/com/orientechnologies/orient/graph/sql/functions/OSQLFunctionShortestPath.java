0ede2c3795099f475c51ce6560752c79c7273bdf
lvca <l.garulli@gmail.com>
2014-09-17 18:11:55 +0200

    Fixed issue #2809

--- graphdb/src/main/java/com/orientechnologies/orient/graph/sql/functions/OSQLFunctionShortestPath.java
+++ graphdb/src/main/java/com/orientechnologies/orient/graph/sql/functions/OSQLFunctionShortestPath.java
@@ -18,12 +18,23 @@ package com.orientechnologies.orient.graph.sql.functions;
 import com.orientechnologies.common.collection.OMultiValue;
 import com.orientechnologies.orient.core.command.OCommandContext;
 import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.core.id.ORID;
 import com.orientechnologies.orient.core.record.ORecordInternal;
 import com.orientechnologies.orient.core.sql.OSQLHelper;
+import com.orientechnologies.orient.core.sql.functions.math.OSQLFunctionMathAbstract;
 import com.orientechnologies.orient.graph.sql.OGraphCommandExecutorSQLFactory;
 import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientBaseGraph;
 import com.tinkerpop.blueprints.impls.orient.OrientVertex;
+import com.tinkerpop.pipes.util.structures.ArrayQueue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Shortest path algorithm to find the shortest path from one node to another node in a directed graph.
@@ -31,7 +42,7 @@ import com.tinkerpop.blueprints.impls.orient.OrientVertex;
  * @author Luca Garulli (l.garulli--at--orientechnologies.com)
  * 
  */
-public class OSQLFunctionShortestPath extends OSQLFunctionPathFinder {
+public class OSQLFunctionShortestPath extends OSQLFunctionMathAbstract {
   public static final String   NAME     = "shortestPath";
 
   protected static final float DISTANCE = 1f;
@@ -52,7 +63,7 @@ public class OSQLFunctionShortestPath extends OSQLFunctionPathFinder {
         throw new IllegalArgumentException("Only one sourceVertex is allowed");
       source = OMultiValue.getFirstValue(source);
     }
-    paramSourceVertex = graph.getVertex(OSQLHelper.getValue(source, record, iContext));
+    OrientVertex sourceVertex = graph.getVertex(OSQLHelper.getValue(source, record, iContext));
 
     Object dest = iParams[1];
     if (OMultiValue.isMultiValue(dest)) {
@@ -60,20 +71,65 @@ public class OSQLFunctionShortestPath extends OSQLFunctionPathFinder {
         throw new IllegalArgumentException("Only one destinationVertex is allowed");
       dest = OMultiValue.getFirstValue(dest);
     }
-    paramDestinationVertex = graph.getVertex(OSQLHelper.getValue(dest, record, iContext));
+    OrientVertex destinationVertex = graph.getVertex(OSQLHelper.getValue(dest, record, iContext));
 
+    if (sourceVertex.equals(destinationVertex)) {
+      final List<ORID> result = new ArrayList<ORID>(1);
+      result.add(destinationVertex.getIdentity());
+      return result;
+    }
+
+    Direction direction = Direction.BOTH;
     if (iParams.length > 2)
-      paramDirection = Direction.valueOf(iParams[2].toString().toUpperCase());
+      direction = Direction.valueOf(iParams[2].toString().toUpperCase());
 
-    return super.execute(iContext);
-  }
+    final ArrayQueue<OrientVertex> queue = new ArrayQueue<OrientVertex>();
+    final Set<ORID> visited = new HashSet<ORID>();
+    final Map<ORID, ORID> previouses = new HashMap<ORID, ORID>();
 
-  @Override
-  protected float getDistance(final OrientVertex node, final OrientVertex target) {
-    return DISTANCE;
+    queue.add(sourceVertex);
+    visited.add(sourceVertex.getIdentity());
+
+    OrientVertex current;
+    while (!queue.isEmpty()) {
+      current = queue.poll();
+
+      final Iterable<Vertex> neighbors = current.getVertices(direction);
+      for (Vertex neighbor : neighbors) {
+        final OrientVertex v = (OrientVertex) neighbor;
+        final ORID neighborIdentity = v.getIdentity();
+
+        if (!visited.contains(neighborIdentity)) {
+
+          previouses.put(neighborIdentity, current.getIdentity());
+
+          if (destinationVertex.equals(neighbor))
+            return computePath(previouses, neighborIdentity);
+
+          queue.offer(v);
+          visited.add(neighborIdentity);
+        }
+
+      }
+    }
+
+    return new ArrayList<ORID>();
   }
 
   public String getSyntax() {
     return "shortestPath(<sourceVertex>, <destinationVertex>, [<direction>])";
+  }
+
+  private Object computePath(final Map<ORID, ORID> distances, final ORID neighbor) {
+    final List<ORID> result = new ArrayList<ORID>();
+
+    ORID current = neighbor;
+    while (current != null) {
+      result.add(0, current);
+
+      current = distances.get(current);
+    }
+
+    return result;
   }
 }
