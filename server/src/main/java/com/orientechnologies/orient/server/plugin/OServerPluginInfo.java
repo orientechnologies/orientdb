81a9c9cb48a13528493a4061f23ef2a1b695a009
lvca <l.garulli@gmail.com>
2014-09-29 20:09:54 +0200

    Issue #2544: Updated copyright

--- server/src/main/java/com/orientechnologies/orient/server/plugin/OServerPluginInfo.java
+++ server/src/main/java/com/orientechnologies/orient/server/plugin/OServerPluginInfo.java
@@ -1,111 +1,111 @@
 /*
-  *
-  *  *  Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
-  *  *
-  *  *  Licensed under the Apache License, Version 2.0 (the "License");
-  *  *  you may not use this file except in compliance with the License.
-  *  *  You may obtain a copy of the License at
-  *  *
-  *  *       http://www.apache.org/licenses/LICENSE-2.0
-  *  *
-  *  *  Unless required by applicable law or agreed to in writing, software
-  *  *  distributed under the License is distributed on an "AS IS" BASIS,
-  *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  *  *  See the License for the specific language governing permissions and
-  *  *  limitations under the License.
-  *  *
-  *  * For more information: http://www.orientechnologies.com
-  *
-  */
+      *
+      *  *  Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
+      *  *
+      *  *  Licensed under the Apache License, Version 2.0 (the "License");
+      *  *  you may not use this file except in compliance with the License.
+      *  *  You may obtain a copy of the License at
+      *  *
+      *  *       http://www.apache.org/licenses/LICENSE-2.0
+      *  *
+      *  *  Unless required by applicable law or agreed to in writing, software
+      *  *  distributed under the License is distributed on an "AS IS" BASIS,
+      *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      *  *  See the License for the specific language governing permissions and
+      *  *  limitations under the License.
+      *  *
+      *  * For more information: http://www.orientechnologies.com
+      *
+      */
 package com.orientechnologies.orient.server.plugin;
 
 import java.lang.reflect.Method;
-import java.net.URLClassLoader;
-import java.util.HashMap;
-import java.util.Map;
+ import java.net.URLClassLoader;
+ import java.util.HashMap;
+ import java.util.Map;
 
-import com.orientechnologies.common.log.OLogManager;
+ import com.orientechnologies.common.log.OLogManager;
 
 /**
- * Server plugin information
- * 
- * @author Luca Garulli (l.garulli--at--orientechnologies.com)
- * 
- */
-public class OServerPluginInfo {
-  private final String              name;
-  private final String              version;
-  private final String              description;
-  private final String              web;
-  private final OServerPlugin       instance;
-  private final Map<String, Object> parameters;
-  private final long                loadedOn;
-  private final URLClassLoader      pluginClassLoader;
+  * Server plugin information
+  *
+  * @author Luca Garulli (l.garulli--at--orientechnologies.com)
+  *
+  */
+ public class OServerPluginInfo {
+   private final String              name;
+   private final String              version;
+   private final String              description;
+   private final String              web;
+   private final OServerPlugin       instance;
+   private final Map<String, Object> parameters;
+   private final long                loadedOn;
+   private final URLClassLoader      pluginClassLoader;
 
-  public OServerPluginInfo(final String name, final String version, final String description, final String web,
-      final OServerPlugin instance, final Map<String, Object> parameters, final long loadedOn,
-      final URLClassLoader pluginClassLoader) {
-    this.name = name;
-    this.version = version;
-    this.description = description;
-    this.web = web;
-    this.instance = instance;
-    this.parameters = parameters != null ? parameters : new HashMap<String, Object>();
-    this.loadedOn = loadedOn;
-    this.pluginClassLoader = pluginClassLoader;
-  }
+   public OServerPluginInfo(final String name, final String version, final String description, final String web,
+       final OServerPlugin instance, final Map<String, Object> parameters, final long loadedOn,
+       final URLClassLoader pluginClassLoader) {
+     this.name = name;
+     this.version = version;
+     this.description = description;
+     this.web = web;
+     this.instance = instance;
+     this.parameters = parameters != null ? parameters : new HashMap<String, Object>();
+     this.loadedOn = loadedOn;
+     this.pluginClassLoader = pluginClassLoader;
+   }
 
-  public void shutdown() {
-    if (instance != null)
-      instance.sendShutdown();
+   public void shutdown() {
+     if (instance != null)
+       instance.sendShutdown();
 
-    if (pluginClassLoader != null) {
-      // JAVA7 ONLY
-      Method m;
-      try {
-        m = pluginClassLoader.getClass().getMethod("close");
-        if (m != null)
-          m.invoke(pluginClassLoader);
-      } catch (NoSuchMethodException e) {
-      } catch (Exception e) {
-        OLogManager.instance().error(this, "Error on closing plugin classloader", e);
-      }
-    }
-  }
+     if (pluginClassLoader != null) {
+       // JAVA7 ONLY
+       Method m;
+       try {
+         m = pluginClassLoader.getClass().getMethod("close");
+         if (m != null)
+           m.invoke(pluginClassLoader);
+       } catch (NoSuchMethodException e) {
+       } catch (Exception e) {
+         OLogManager.instance().error(this, "Error on closing plugin classloader", e);
+       }
+     }
+   }
 
-  public boolean isDynamic() {
-    return loadedOn > 0;
-  }
+   public boolean isDynamic() {
+     return loadedOn > 0;
+   }
 
-  public String getName() {
-    return name;
-  }
+   public String getName() {
+     return name;
+   }
 
-  public OServerPlugin getInstance() {
-    return instance;
-  }
+   public OServerPlugin getInstance() {
+     return instance;
+   }
 
-  public long getLoadedOn() {
-    return loadedOn;
-  }
+   public long getLoadedOn() {
+     return loadedOn;
+   }
 
-  public String getVersion() {
-    return version;
-  }
+   public String getVersion() {
+     return version;
+   }
 
-  public String getDescription() {
-    return description;
-  }
+   public String getDescription() {
+     return description;
+   }
 
-  public String getWeb() {
-    return web;
-  }
+   public String getWeb() {
+     return web;
+   }
 
-  public Object getParameter(final String iName) {
-    return parameters.get(iName);
-  }
+   public Object getParameter(final String iName) {
+     return parameters.get(iName);
+   }
 
-  public URLClassLoader getClassLoader() {
-    return pluginClassLoader;
-  }
-}
+   public URLClassLoader getClassLoader() {
+     return pluginClassLoader;
+   }
+ }
