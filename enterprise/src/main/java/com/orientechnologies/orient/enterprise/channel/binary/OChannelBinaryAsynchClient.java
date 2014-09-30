95ca48f38d460c0923fd5e383a404acdc0513c94
tglman <tglman@tglman.com>
2014-09-30 20:38:12 +0100

    fixed connetion not close on connect negotiation failure.

--- enterprise/src/main/java/com/orientechnologies/orient/enterprise/channel/binary/OChannelBinaryAsynchClient.java
+++ enterprise/src/main/java/com/orientechnologies/orient/enterprise/channel/binary/OChannelBinaryAsynchClient.java
@@ -1,22 +1,22 @@
 /*
-  *
-  *  *  Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
-  *  *
-  *  *  Licensed under the Apache License, Version 2.0 (the "License");
-  *  *  you may not use this file except in compliance with the License.
-  *  *  You may obtain a copy of the License at
-  *  *
-  *  *       http://www.apache.org/licenses/LICENSE-2.0
-  *  *
-  *  *  Unless required by applicable law or agreed to in writing, software
-  *  *  distributed under the License is distributed on an "AS IS" BASIS,
-  *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  *  *  See the License for the specific language governing permissions and
-  *  *  limitations under the License.
-  *  *
-  *  * For more information: http://www.orientechnologies.com
-  *
-  */
+ *
+ *  *  Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
+ *  *
+ *  *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  *  You may obtain a copy of the License at
+ *  *
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *  Unless required by applicable law or agreed to in writing, software
+ *  *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  See the License for the specific language governing permissions and
+ *  *  limitations under the License.
+ *  *
+ *  * For more information: http://www.orientechnologies.com
+ *
+ */
 package com.orientechnologies.orient.enterprise.channel.binary;
 
 import com.orientechnologies.common.concur.OTimeoutException;
@@ -83,18 +83,20 @@ public class OChannelBinaryAsynchClient extends OChannelBinary {
       setReadResponseTimeout();
       connected();
     } catch (java.net.SocketTimeoutException e) {
+      if (socket.isConnected())
+        socket.close();
       throw new IOException("Cannot connect to host " + remoteHost + ":" + remotePort, e);
     }
-
-    inStream = new BufferedInputStream(socket.getInputStream(), socketBufferSize);
-    outStream = new BufferedOutputStream(socket.getOutputStream(), socketBufferSize);
-
-    in = new DataInputStream(inStream);
-    out = new DataOutputStream(outStream);
-
     try {
+      inStream = new BufferedInputStream(socket.getInputStream(), socketBufferSize);
+      outStream = new BufferedOutputStream(socket.getOutputStream(), socketBufferSize);
+
+      in = new DataInputStream(inStream);
+      out = new DataOutputStream(outStream);
+
       srvProtocolVersion = readShort();
     } catch (IOException e) {
+      socket.close();
       throw new ONetworkProtocolException("Cannot read protocol version from remote server " + socket.getRemoteSocketAddress()
           + ": " + e);
     }
