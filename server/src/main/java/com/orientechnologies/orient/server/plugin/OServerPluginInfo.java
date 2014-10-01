05d74e7adfb4663b42bff991d794bff8193b1b40
maggiolo00 <maggiolo00@gmail.com>
2014-10-01 13:19:19 +0200

    cherry picked 029c5dd

--- server/src/main/java/com/orientechnologies/orient/server/plugin/OServerPluginInfo.java
+++ server/src/main/java/com/orientechnologies/orient/server/plugin/OServerPluginInfo.java
@@ -1,17 +1,21 @@
 /*
- * Copyright 2010-2013 Luca Garulli (l.garulli--at--orientechnologies.com)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  *  Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
+ *  *
+ *  *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  *  You may obtain a copy of the License at
+ *  *
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *  Unless required by applicable law or agreed to in writing, software
+ *  *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  See the License for the specific language governing permissions and
+ *  *  limitations under the License.
+ *  *
+ *  * For more information: http://www.orientechnologies.com
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package com.orientechnologies.orient.server.plugin;
 
@@ -24,9 +28,9 @@ import com.orientechnologies.common.log.OLogManager;
 
 /**
  * Server plugin information
- * 
+ *
  * @author Luca Garulli (l.garulli--at--orientechnologies.com)
- * 
+ *
  */
 public class OServerPluginInfo {
   private final String              name;
@@ -52,10 +56,14 @@ public class OServerPluginInfo {
   }
 
   public void shutdown() {
+    shutdown(true);
+  }
+
+  public void shutdown(boolean closeClassLoader) {
     if (instance != null)
       instance.sendShutdown();
 
-    if (pluginClassLoader != null) {
+    if (pluginClassLoader != null && closeClassLoader) {
       // JAVA7 ONLY
       Method m;
       try {
