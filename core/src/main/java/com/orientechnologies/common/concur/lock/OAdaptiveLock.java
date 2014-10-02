ea66031d9124a06b8b737fb8b3fd0b03db797e3f
laa <lomakin.andrey@gmail.com>
2014-10-02 16:42:20 +0300

    New lock exception info added. Now when dead lock is detected information about both thread which own lock] is provided.

--- core/src/main/java/com/orientechnologies/common/concur/lock/OAdaptiveLock.java
+++ core/src/main/java/com/orientechnologies/common/concur/lock/OAdaptiveLock.java
@@ -1,27 +1,33 @@
 /*
-  *
-  *  *  Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
-  *  *
-  *  *  Licensed under the Apache License, Version 2.0 (the "License");
-  *  *  you may not use this file except in compliance with the License.
-  *  *  You may obtain a copy of the License at
-  *  *
-  *  *       http://www.apache.org/licenses/LICENSE-2.0
-  *  *
-  *  *  Unless required by applicable law or agreed to in writing, software
-  *  *  distributed under the License is distributed on an "AS IS" BASIS,
-  *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  *  *  See the License for the specific language governing permissions and
-  *  *  limitations under the License.
-  *  *
-  *  * For more information: http://www.orientechnologies.com
-  *
-  */
+ *
+ *  *  Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
+ *  *
+ *  *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  *  You may obtain a copy of the License at
+ *  *
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *  Unless required by applicable law or agreed to in writing, software
+ *  *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  See the License for the specific language governing permissions and
+ *  *  limitations under the License.
+ *  *
+ *  * For more information: http://www.orientechnologies.com
+ *
+ */
 package com.orientechnologies.common.concur.lock;
 
 import com.orientechnologies.common.concur.OTimeoutException;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -85,8 +91,9 @@ public class OAdaptiveLock extends OAbstractLock {
           throw new OLockException("Thread interrupted while waiting for resource of class '" + getClass() + "' with timeout="
               + timeout);
         }
-        throw new OTimeoutException("Timeout on acquiring lock against resource of class: " + getClass() + " with timeout="
-            + timeout);
+
+        throwTimeoutException(lock);
+
       } else
         lock.lock();
   }
@@ -122,6 +129,51 @@ public class OAdaptiveLock extends OAbstractLock {
         lock.unlock();
     } catch (Exception e) {
     }
+  }
+
+  private void throwTimeoutException(Lock lock) {
+    final String owner = extractLockOwnerStackTrace(lock);
+
+    throw new OTimeoutException("Timeout on acquiring exclusive lock against resource of class: " + getClass() + " with timeout="
+        + timeout + (owner != null ? "\n" + owner : ""));
+  }
+
+  private String extractLockOwnerStackTrace(Lock lock) {
+    try {
+      Field syncField = lock.getClass().getDeclaredField("sync");
+      syncField.setAccessible(true);
+
+      Object sync = syncField.get(lock);
+      Method getOwner = sync.getClass().getSuperclass().getDeclaredMethod("getOwner");
+      getOwner.setAccessible(true);
+
+      final Thread owner = (Thread) getOwner.invoke(sync);
+      if (owner == null)
+        return null;
+
+      StringWriter stringWriter = new StringWriter();
+      PrintWriter printWriter = new PrintWriter(stringWriter);
+
+      printWriter.append("Owner thread : ").append(owner.toString()).append("\n");
+
+      StackTraceElement[] stackTrace = owner.getStackTrace();
+      for (StackTraceElement traceElement : stackTrace)
+        printWriter.println("\tat " + traceElement);
+
+      printWriter.flush();
+      return stringWriter.toString();
+    } catch (RuntimeException e) {
+      return null;
+    } catch (NoSuchFieldException e) {
+      return null;
+    } catch (IllegalAccessException e) {
+      return null;
+    } catch (NoSuchMethodException e) {
+      return null;
+    } catch (InvocationTargetException e) {
+      return null;
+    }
+
   }
 
   public boolean isConcurrent() {
