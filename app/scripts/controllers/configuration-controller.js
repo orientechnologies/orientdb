991dca218f29f609542f7f47441be3fdbf4ede9b
maggiolo00 <maggiolo00@gmail.com>
2014-10-02 11:14:56 +0200

    fixed #159

--- app/scripts/controllers/configuration-controller.js
+++ app/scripts/controllers/configuration-controller.js
@@ -207,7 +207,7 @@ configModule.controller("DbConfigController", ['$scope', '$routeParams', '$locat
         }
     });
     if (!found) {
-        $scope.properties.push({name: 'useLightweightEdges', value: 'false' });
+        $scope.properties.push({name: 'useLightweightEdges', value: 'true' });
     }
 
     $scope.canChange = ["clusterSelection", "minimumClusters", "localeCountry", "useLightweightEdges", "conflictStrategy"];
