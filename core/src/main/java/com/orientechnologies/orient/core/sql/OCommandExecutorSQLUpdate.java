1e5a2aed4db4ef1f6e81e256ac5666beb1c99459
tglman <tglman@tglman.com>
2014-10-22 13:07:49 +0100

    fixed conversion from linkmap to embeddedmap on update command, issue #1161

--- core/src/main/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLUpdate.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLUpdate.java
@@ -1,22 +1,22 @@
 /*
-  *
-  *  *  Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
-  *  *
-  *  *  Licensed under the Apache License, Version 2.0 (the "License");
-  *  *  you may not use this file except in compliance with the License.
-  *  *  You may obtain a copy of the License at
-  *  *
-  *  *       http://www.apache.org/licenses/LICENSE-2.0
-  *  *
-  *  *  Unless required by applicable law or agreed to in writing, software
-  *  *  distributed under the License is distributed on an "AS IS" BASIS,
-  *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  *  *  See the License for the specific language governing permissions and
-  *  *  limitations under the License.
-  *  *
-  *  * For more information: http://www.orientechnologies.com
-  *
-  */
+ *
+ *  *  Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
+ *  *
+ *  *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  *  You may obtain a copy of the License at
+ *  *
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *  Unless required by applicable law or agreed to in writing, software
+ *  *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  See the License for the specific language governing permissions and
+ *  *  limitations under the License.
+ *  *
+ *  * For more information: http://www.orientechnologies.com
+ *
+ */
 package com.orientechnologies.orient.core.sql;
 
 import java.util.ArrayList;
@@ -36,6 +36,7 @@ import com.orientechnologies.orient.core.command.OCommandRequestText;
 import com.orientechnologies.orient.core.command.OCommandResultListener;
 import com.orientechnologies.orient.core.db.record.ODatabaseRecord;
 import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.core.db.record.OTrackedMap;
 import com.orientechnologies.orient.core.db.record.ridbag.ORidBag;
 import com.orientechnologies.orient.core.exception.OCommandExecutionException;
 import com.orientechnologies.orient.core.exception.OConcurrentModificationException;
@@ -461,6 +462,7 @@ public class OCommandExecutorSQLUpdate extends OCommandExecutorSQLRetryAbstract 
     return updated;
   }
 
+  @SuppressWarnings({ "unchecked", "rawtypes" })
   private boolean handlePutEntries(ODocument record) {
     boolean updated = false;
     if (!putEntries.isEmpty()) {
@@ -482,13 +484,22 @@ public class OCommandExecutorSQLUpdate extends OCommandExecutorSQLRetryAbstract 
         }
 
         if (fieldValue instanceof Map<?, ?>) {
-          @SuppressWarnings("unchecked")
           Map<String, Object> map = (Map<String, Object>) fieldValue;
 
           OPair<String, Object> pair = entry.getValue();
 
           Object value = extractValue(record, pair);
 
+          if (record.getSchemaClass() != null) {
+            final OProperty property = record.getSchemaClass().getProperty(entry.getKey());
+            if (property != null && property.getType().equals(OType.LINKMAP) && !(value instanceof OIdentifiable)) {
+              throw new OCommandExecutionException("field " + entry.getKey() + " defined of type LINKMAP accept only link values");
+            }
+          }
+          if (OType.LINKMAP.equals(record.fieldType(entry.getKey())) && !(value instanceof OIdentifiable)) {
+            map = new OTrackedMap(record, map, Object.class);
+            record.field(entry.getKey(), map, OType.EMBEDDEDMAP);
+          }
           map.put(pair.getKey(), value);
           updated = true;
         }
