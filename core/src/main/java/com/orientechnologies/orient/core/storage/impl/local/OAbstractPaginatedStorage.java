f9c1275efd5f02c796f2c15c517374bab1c48e99
henryzhao81 <henryzhao81@gmail.com>
2014-10-27 16:08:15 -0700

    Unrecongnized charactor (extend acsii code) gives me trouble during complile

--- core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/OAbstractPaginatedStorage.java
+++ core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/OAbstractPaginatedStorage.java
@@ -1037,21 +1037,21 @@ public abstract class OAbstractPaginatedStorage extends OStorageEmbedded impleme
     return new HashSet<String>(clusterMap.keySet());
   }
 
-  public int getClusterIdByName(final String сlusterName) {
+  public int getClusterIdByName(final String clusterName) {
     checkOpeness();
 
-    if (сlusterName == null)
+    if (clusterName == null)
       throw new IllegalArgumentException("Cluster name is null");
 
-    if (сlusterName.length() == 0)
+    if (clusterName.length() == 0)
       throw new IllegalArgumentException("Cluster name is empty");
 
-    if (Character.isDigit(сlusterName.charAt(0)))
-      return Integer.parseInt(сlusterName);
+    if (Character.isDigit(clusterName.charAt(0)))
+      return Integer.parseInt(clusterName);
 
     // SEARCH IT BETWEEN PHYSICAL CLUSTERS
 
-    final OCluster segment = clusterMap.get(сlusterName.toLowerCase());
+    final OCluster segment = clusterMap.get(clusterName.toLowerCase());
     if (segment != null)
       return segment.getId();
 
@@ -1264,11 +1264,11 @@ public abstract class OAbstractPaginatedStorage extends OStorageEmbedded impleme
   }
 
   @Override
-  public OCluster getClusterByName(final String сlusterName) {
-    final OCluster cluster = clusterMap.get(сlusterName.toLowerCase());
+  public OCluster getClusterByName(final String clusterName) {
+    final OCluster cluster = clusterMap.get(clusterName.toLowerCase());
 
     if (cluster == null)
-      throw new IllegalArgumentException("Cluster " + сlusterName + " does not exist in database '" + name + "'");
+      throw new IllegalArgumentException("Cluster " + clusterName + " does not exist in database '" + name + "'");
     return cluster;
   }
 
