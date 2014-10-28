4028370fd6183b9656829db4f10e36d611069c18
laa <lomakin.andrey@gmail.com>
2014-10-28 16:18:13 +0200

    Issue #2968 was fixed.

--- core/src/main/java/com/orientechnologies/orient/core/db/record/OTrackedList.java
+++ core/src/main/java/com/orientechnologies/orient/core/db/record/OTrackedList.java
@@ -140,7 +140,16 @@ public class OTrackedList<T> extends ArrayList<T> implements ORecordElement, OTr
     return false;
   }
 
-  @Override
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		boolean removed = false;
+		for (Object o : c)
+		  removed = removed | remove(o);
+
+		return removed;
+	}
+
+	@Override
   public void clear() {
     final List<T> origValues;
     if (changeListeners.isEmpty())
