b4888869b8c8e3c384ad47c33776537cdc474715
tglman <tglman@tglman.com>
2014-11-07 17:33:20 +0000

    reformated jwt source code with the orient formatter

--- token-auth-jwt/src/main/java/com/emrul/orient/jwt/impl/DefaultJwtKeyProvider.java
+++ token-auth-jwt/src/main/java/com/emrul/orient/jwt/impl/DefaultJwtKeyProvider.java
@@ -1,6 +1,5 @@
 package com.emrul.orient.jwt.impl;
 
-
 import com.orientechnologies.orient.core.metadata.security.jwt.IJwtHeader;
 import com.orientechnologies.orient.core.metadata.security.jwt.IJwtKeyProvider;
 
@@ -10,19 +9,18 @@ import java.security.Key;
 /**
  * Created by emrul on 28/09/2014.
  *
- * @author Emrul Islam <emrul@emrul.com>
- *         Copyright 2014 Emrul Islam
+ * @author Emrul Islam <emrul@emrul.com> Copyright 2014 Emrul Islam
  */
 public class DefaultJwtKeyProvider implements IJwtKeyProvider {
 
-    private SecretKeySpec secret_key;
+  private SecretKeySpec secret_key;
 
-    public DefaultJwtKeyProvider(byte[] secret) {
-        secret_key = new SecretKeySpec(secret, "HmacSHA256");
-    }
+  public DefaultJwtKeyProvider(byte[] secret) {
+    secret_key = new SecretKeySpec(secret, "HmacSHA256");
+  }
 
-    @Override
-    public Key getKey(IJwtHeader header) {
-        return secret_key;
-    }
+  @Override
+  public Key getKey(IJwtHeader header) {
+    return secret_key;
+  }
 }
--- token-auth-jwt/src/main/java/com/emrul/orient/jwt/mixin/IJwtPayloadMixin.java
+++ token-auth-jwt/src/main/java/com/emrul/orient/jwt/mixin/IJwtPayloadMixin.java
@@ -1,55 +1,53 @@
 package com.emrul.orient.jwt.mixin;
 
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Created by emrul on 28/09/2014.
  *
- * @author Emrul Islam <emrul@emrul.com>
- *         Copyright 2014 Emrul Islam
+ * @author Emrul Islam <emrul@emrul.com> Copyright 2014 Emrul Islam
  */
 public interface IJwtPayloadMixin {
 
-    @JsonProperty(value="iss")
-    public String getIssuer();
+  @JsonProperty(value = "iss")
+  public String getIssuer();
 
-    @JsonProperty(value="iss")
-    public void setIssuer(String iss);
+  @JsonProperty(value = "iss")
+  public void setIssuer(String iss);
 
-    @JsonProperty(value="exp")
-    public long getExpiry();
+  @JsonProperty(value = "exp")
+  public long getExpiry();
 
-    @JsonProperty(value="exp")
-    public void setExpiry(long exp);
+  @JsonProperty(value = "exp")
+  public void setExpiry(long exp);
 
-    @JsonProperty(value="iat")
-    public long getIssuedAt();
+  @JsonProperty(value = "iat")
+  public long getIssuedAt();
 
-    @JsonProperty(value="iat")
-    public void setIssuedAt(long iat);
+  @JsonProperty(value = "iat")
+  public void setIssuedAt(long iat);
 
-    @JsonProperty(value="nbf")
-    public long getNotBefore();
+  @JsonProperty(value = "nbf")
+  public long getNotBefore();
 
-    @JsonProperty(value="nbf")
-    public void setNotBefore(long nbf);
+  @JsonProperty(value = "nbf")
+  public void setNotBefore(long nbf);
 
-    @JsonProperty(value="sub")
-    public String getSubject();
+  @JsonProperty(value = "sub")
+  public String getSubject();
 
-    @JsonProperty(value="sub")
-    public void setSubject(String sub);
+  @JsonProperty(value = "sub")
+  public void setSubject(String sub);
 
-    @JsonProperty(value="aud")
-    public String getAudience();
+  @JsonProperty(value = "aud")
+  public String getAudience();
 
-    @JsonProperty(value="aud")
-    public void setAudience(String aud);
+  @JsonProperty(value = "aud")
+  public void setAudience(String aud);
 
-    @JsonProperty(value="jti")
-    public String getTokenId();
+  @JsonProperty(value = "jti")
+  public String getTokenId();
 
-    @JsonProperty(value="jti")
-    public void setTokenId(String jti);
+  @JsonProperty(value = "jti")
+  public void setTokenId(String jti);
 }
