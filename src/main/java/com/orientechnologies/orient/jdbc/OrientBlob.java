b938bca11dc86c621cfc08b9a507406985bd2124
fabriziofortino <fabrizio.fortino@gmail.com>
2014-12-31 10:17:28 +0000

    aligned license headers using license-maven-plugin

--- src/main/java/com/orientechnologies/orient/jdbc/OrientBlob.java
+++ src/main/java/com/orientechnologies/orient/jdbc/OrientBlob.java
@@ -1,10 +1,11 @@
-/***************************************************************************
- * Copyright 2011 TXT e-solutions SpA
+/**
+ * Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * 	http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,12 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Authors:
- *      Salvatore Piccione (TXT e-solutions SpA)
- *
- * Contributors:
- *        Domenico Rotondi (TXT e-solutions SpA)
- **************************************************************************/
+ * For more information: http://www.orientechnologies.com
+ */
 package com.orientechnologies.orient.jdbc;
 
 import java.io.IOException;
