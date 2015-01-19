f43983adc1ad248ba3eec72f374ad8592e8ed1e1
lvca <l.garulli@gmail.com>
2015-01-19 13:49:46 +0100

    Fixed issue #1371

--- server/src/main/java/com/orientechnologies/orient/server/OServer.java
+++ server/src/main/java/com/orientechnologies/orient/server/OServer.java
@@ -874,8 +874,6 @@ public class OServer {
   }
 
   protected void defaultSettings() {
-    OGlobalConfiguration.TX_USE_LOG.setValue(true);
-    OGlobalConfiguration.TX_COMMIT_SYNCH.setValue(true);
   }
 
   private boolean isStorageOfCurrentServerInstance(OStorage storage) {
