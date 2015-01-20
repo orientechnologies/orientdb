3ac65d7698bda17c3aca9a23de7e9a3a6664eceb
lvca <l.garulli@gmail.com>
2015-01-20 20:08:36 +0100

    Switched to 2.1-SNAPSHOT

--- core/src/main/java/com/orientechnologies/orient/core/OConstants.java
+++ core/src/main/java/com/orientechnologies/orient/core/OConstants.java
@@ -20,7 +20,7 @@
 package com.orientechnologies.orient.core;
 
 public class OConstants {
-  public static final String ORIENT_VERSION = "2.0";
+  public static final String ORIENT_VERSION = "2.1-SNAPSHOT";
 
   public static final String ORIENT_URL     = "www.orientechnologies.com";
 
