90dc69dcbb55d11daba09de25cdfe7229b174e1a
Tglman <tglman@tglman.com>
2015-03-31 21:02:20 +0100

    fix error on unique index on values of type date. issue #3799
    Conflicts:
    	core/src/main/java/com/orientechnologies/orient/core/record/impl/ODocument.java

---
+++ core/src/test/java/com/orientechnologies/orient/core/index/UniqueHashIndexForDate.java
@@ -0,0 +1,50 @@
+package com.orientechnologies.orient.core.index;
+
+import java.text.ParseException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.orientechnologies.common.exception.OException;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OClass.INDEX_TYPE;
+import com.orientechnologies.orient.core.metadata.schema.OProperty;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+
+public class UniqueHashIndexForDate {
+
+  @Test
+  public void testSimpleUniqueDateIndex() throws ParseException {
+    ODatabaseDocument db = new ODatabaseDocumentTx("memory:" + UniqueHashIndexForDate.class.getSimpleName());
+    db.create();
+    try {
+      OClass clazz = db.getMetadata().getSchema().createClass("test_edge");
+      OProperty prop = clazz.createProperty("date", OType.DATETIME);
+      prop.createIndex(INDEX_TYPE.UNIQUE);
+      ODocument doc = new ODocument("test_edge");
+      doc.field("date", "2015-03-24 08:54:49");
+
+      ODocument doc1 = new ODocument("test_edge");
+      doc1.field("date", "2015-03-24 08:54:49");
+
+      db.save(doc);
+      try {
+        db.begin();
+        db.save(doc1);
+        doc1.field("date", "2015-03-24 08:54:49");
+        db.save(doc1);
+        db.commit();
+        Assert.fail("expected exception for duplicate ");
+      } catch (OException e) {
+
+      }
+
+    } finally {
+      db.drop();
+    }
+  }
+
+}
