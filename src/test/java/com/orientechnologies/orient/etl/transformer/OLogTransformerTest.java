55b72af382d83a63c6c74edfddf82b3204749c21
Oleksy Ostanin <tnd_prof@yahoo.com>
2015-04-19 22:24:51 +0300

    proceed TODO in OLogTransformer.java, add test coverage for LogTransformer

--- src/test/java/com/orientechnologies/orient/etl/transformer/OLogTransformerTest.java
+++ src/test/java/com/orientechnologies/orient/etl/transformer/OLogTransformerTest.java
@@ -16,53 +16,30 @@ import static org.junit.Assert.assertEquals;
 public class OLogTransformerTest extends ETLBaseTest {
 
     private final PrintStream OUT = System.out;
-    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(output, true));
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(OUT);
-    }
-
-    @Test
-    public void testGetConfiguration() throws Exception {
-
-    }
-
-    @Test
-    public void testConfigure() throws Exception {
-
-    }
-
-    @Test
-    public void testGetName() throws Exception {
-
-    }
 
     @Test
     public void testPrefix() throws Exception {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
         System.setOut(new PrintStream(output, true));
-        String cfgJson = "{source: { content: { value: 'id,postId,text\n1,,Hello'} }, extractor : { row : {} }, transformers : [{ csv : {} },{ log : {prefix:'-> '}}], loader : { test: {} } }";
+        String cfgJson = "{source: { content: { value: 'id,text\n1,Hello\n2,Bye'} }, extractor : { row : {} }, transformers : [{ csv : {} },{ log : {prefix:'-> '}}], loader : { test: {} } }";
         process(cfgJson);
         List<ODocument> res = getResult();
         ODocument doc = res.get(0);
-        assertEquals("-> ", output.toString());
-
+        String[] stringList = output.toString().split("\n");
+        assertEquals("[1:log] INFO -> {id:1,text:Hello}", stringList[1]);
+        assertEquals("[2:log] INFO -> {id:2,text:Bye}", stringList[2]);
     }
-
     @Test
     public void testPostfix() throws Exception {
-        String cfgJson = "{source: { content: { value: 'id,postId,text\n1,,Hello'} }, extractor : { row : {} }, transformers : [{ csv : {} },{ log : {postfix:'-> '}}], loader : { test: {} } }";
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output, true));
+        String cfgJson = "{source: { content: { value: 'id,text\n1,Hello\n2,Bye'} }, extractor : { row : {} }, transformers : [{ csv : {} },{ log : {postfix:'-> '}}], loader : { test: {} } }";
         process(cfgJson);
         List<ODocument> res = getResult();
         ODocument doc = res.get(0);
-        assertEquals("-> ", output.toString());
+        String[] stringList = output.toString().split("\n");
+        assertEquals("[1:log] INFO {id:1,text:Hello}-> ", stringList[1]);
+        assertEquals("[2:log] INFO {id:2,text:Bye}-> ", stringList[2]);
     }
-
 
 }
