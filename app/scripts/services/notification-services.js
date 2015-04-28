9799441d183ed824679da3cbf7cc4e013816f0c5
maggiolo00 <maggiolo00@gmail.com>
2015-04-28 16:36:10 +0200

    implemented #186 related to https://github.com/orientechnologies/orientdb/issues/4003

--- app/scripts/services/notification-services.js
+++ app/scripts/services/notification-services.js
@@ -3,54 +3,59 @@ var notification = angular.module('notification.services', []);
 notification.factory('Notification', function ($timeout, $rootScope) {
 
 
-    var notiService = {
-        notifications: new Array,
-        errors: new Array,
-        warnings: new Array,
+  var notiService = {
+    notifications: new Array,
+    errors: new Array,
+    warnings: new Array,
 
-        push: function (notification) {
-            this.notifications.splice(0, this.notifications.length);
-            this.errors.splice(0, this.errors.length);
-            this.warnings.splice(0, this.warnings.length);
+    push: function (notification) {
 
 
-            if (notification.error) {
-                this.errors.push(notification);
-            } else if (notification.warning) {
-                this.warnings.push(notification);
-            }
-            else {
-                this.notifications.push(notification);
-            }
-            var self = this;
-            self.stopTimer();
-            self.startTimer();
-        },
-        startTimer: function () {
-            var self = this;
-            self.timePromise = $timeout(function () {
-                self.clear();
-            }, 3000)
-        },
-        stopTimer: function () {
-            var self = this;
-            if (self.timePromise) {
-                $timeout.cancel(self.timePromise);
-            }
-        },
-        clear: function () {
-            this.notifications.splice(0, this.notifications.length);
-            this.errors.splice(0, this.errors.length);
-            this.warnings.splice(0, this.warnings.length);
+      this.notifications.splice(0, this.notifications.length);
+      this.errors.splice(0, this.errors.length);
+      this.warnings.splice(0, this.warnings.length);
+
+
+      if (notification.error) {
+        if (typeof notification.content != 'string') {
+          notification.content = notification.content.errors[0].content;
         }
-
+        this.errors.push(notification);
+      } else if (notification.warning) {
+        this.warnings.push(notification);
+      }
+      else {
+        this.notifications.push(notification);
+      }
+      var self = this;
+      self.stopTimer();
+      self.startTimer();
+    },
+    startTimer: function () {
+      var self = this;
+      self.timePromise = $timeout(function () {
+        self.clear();
+      }, 3000)
+    },
+    stopTimer: function () {
+      var self = this;
+      if (self.timePromise) {
+        $timeout.cancel(self.timePromise);
+      }
+    },
+    clear: function () {
+      this.notifications.splice(0, this.notifications.length);
+      this.errors.splice(0, this.errors.length);
+      this.warnings.splice(0, this.warnings.length);
     }
 
-    $rootScope.$on('alert:hover', function () {
-        notiService.stopTimer();
-    })
-    $rootScope.$on('alert:out', function () {
-        notiService.startTimer();
-    })
-    return notiService;
+  }
+
+  $rootScope.$on('alert:hover', function () {
+    notiService.stopTimer();
+  })
+  $rootScope.$on('alert:out', function () {
+    notiService.startTimer();
+  })
+  return notiService;
 });
