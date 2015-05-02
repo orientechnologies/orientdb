9b15421da64f9ac19129736a7164bbe4dcc99499
Zach Dorian <zachdorian17@gmail.com>
2015-05-02 09:39:17 +0200

    added 'focus' directive

--- app/scripts/widgets/widget.js
+++ app/scripts/widgets/widget.js
@@ -632,4 +632,22 @@ Widget.directive('fontpicker', function ($timeout) {
 
     }
 });
-
+// Common directive for Focus
+Widget.directive('focus',
+    function($timeout) {
+        return {
+            scope : {
+                trigger : '@focus'
+            },
+            link : function(scope, element) {
+                scope.$watch('trigger', function(value) {
+                    if (value === "true") {
+                        $timeout(function() {
+                            element[0].focus();
+                        });
+                    }
+                });
+            }
+        };
+    }
+);
