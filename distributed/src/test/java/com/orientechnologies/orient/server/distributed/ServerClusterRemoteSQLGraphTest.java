6e8a9aea70072365cfa5264d6cde1546f7216280
lvca <l.garulli@gmail.com>
2015-05-05 16:18:13 +0200

    Releasing OrientDB 2.1-rc2

--- distributed/src/test/java/com/orientechnologies/orient/server/distributed/ServerClusterRemoteSQLGraphTest.java
+++ distributed/src/test/java/com/orientechnologies/orient/server/distributed/ServerClusterRemoteSQLGraphTest.java
@@ -2,11 +2,11 @@
  *
  *  *  Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
  *  *
- *  *  Licensed under the Apache License, Version 2.1-SNAPSHOT (the "License");
+ *  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  *  you may not use this file except in compliance with the License.
  *  *  You may obtain a copy of the License at
  *  *
- *  *       http://www.apache.org/licenses/LICENSE-2.1-SNAPSHOT
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
  *  *
  *  *  Unless required by applicable law or agreed to in writing, software
  *  *  distributed under the License is distributed on an "AS IS" BASIS,
