c454b4dbe01fb54b791224d4156acc9782ef46e1
lvca <l.garulli@gmail.com>
2015-06-13 01:01:39 +0200

    Minor: changed website to orientdb.com

--- core/src/main/java/com/orientechnologies/orient/core/OConstants.java
+++ core/src/main/java/com/orientechnologies/orient/core/OConstants.java
@@ -22,7 +22,7 @@ package com.orientechnologies.orient.core;
 public class OConstants {
   public static final String ORIENT_VERSION = "2.1-SNAPSHOT";
 
-  public static final String ORIENT_URL     = "www.orientechnologies.com";
+  public static final String ORIENT_URL     = "www.orientdb.com";
   public static final String COPYRIGHT      = "Copyrights (c) 2015 Orient Technologies LTD";
 
   public static String getVersion() {
