1f5733ef8692b682515e353060036808eb4f416f
ylopotun <yuriy.lopotun@grassvalley.com>
2015-06-18 14:46:30 -0400

    Added unit test for demonstrating bug #4533.

--- distributed/src/test/java/com/orientechnologies/orient/server/distributed/asynch/UpdateHook.java
+++ distributed/src/test/java/com/orientechnologies/orient/server/distributed/asynch/UpdateHook.java
@@ -2,6 +2,9 @@ package com.orientechnologies.orient.server.distributed.asynch;
 
 import com.orientechnologies.orient.core.hook.ORecordHook;
 import com.orientechnologies.orient.core.record.ORecord;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.tinkerpop.blueprints.impls.orient.OrientBaseGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientEdgeType;
 
 public class UpdateHook implements ORecordHook {
 
@@ -11,7 +14,21 @@ public class UpdateHook implements ORecordHook {
 
   @Override
   public RESULT onTrigger(TYPE iType, ORecord iRecord) {
-    if (iType.equals(TYPE.AFTER_CREATE) || iType.equals(TYPE.AFTER_UPDATE) || iType.equals(TYPE.AFTER_DELETE)) {
+    if (iType.equals(TYPE.AFTER_DELETE)) {
+      if (iRecord instanceof ODocument) {
+        ODocument doc = (ODocument) iRecord;
+        if (doc.getSchemaClass() != null && doc.getSchemaClass().isSubClassOf(OrientEdgeType.CLASS_NAME)) {
+          if (doc.field(OrientBaseGraph.CONNECTION_OUT) == null) {
+            throw new RuntimeException(OrientBaseGraph.CONNECTION_OUT + " vertex can't be null");
+          }
+          if (doc.field(OrientBaseGraph.CONNECTION_IN) == null) {
+            throw new RuntimeException(OrientBaseGraph.CONNECTION_IN + " vertex can't be null");
+          }
+        }
+      }
+    }
+
+  	if (iType.equals(TYPE.AFTER_CREATE) || iType.equals(TYPE.AFTER_UPDATE) || iType.equals(TYPE.AFTER_DELETE)) {
       // OLogManager.instance().info(this, iType + ": " + iRecord + " at: " + System.currentTimeMillis());
     }
     return null;
