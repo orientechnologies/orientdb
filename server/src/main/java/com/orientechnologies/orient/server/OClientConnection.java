affea7f302c8ef512345314b516ddbd9771d3412
lvca <l.garulli@gmail.com>
2015-06-19 11:42:39 +0200

    Fixed issue about concurrent usage of the same db if drivers use the same connId against multiple socket channels

--- server/src/main/java/com/orientechnologies/orient/server/OClientConnection.java
+++ server/src/main/java/com/orientechnologies/orient/server/OClientConnection.java
@@ -21,7 +21,6 @@ package com.orientechnologies.orient.server;
 
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.enterprise.channel.binary.OChannelBinary;
-import com.orientechnologies.orient.enterprise.channel.binary.ONetworkProtocolException;
 import com.orientechnologies.orient.server.config.OServerUserConfiguration;
 import com.orientechnologies.orient.server.network.protocol.ONetworkProtocol;
 import com.orientechnologies.orient.server.network.protocol.ONetworkProtocolData;
@@ -29,7 +28,8 @@ import com.orientechnologies.orient.server.network.protocol.ONetworkProtocolData
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class OClientConnection {
   public final int                         id;
@@ -37,9 +37,9 @@ public class OClientConnection {
   public volatile ONetworkProtocol         protocol;
   public volatile ODatabaseDocumentTx      database;
   public volatile OServerUserConfiguration serverUser;
-  private AtomicBoolean                    inUse = new AtomicBoolean(false);
+  private Lock                             lock = new ReentrantLock();
 
-  public ONetworkProtocolData              data  = new ONetworkProtocolData();
+  public ONetworkProtocolData              data = new ONetworkProtocolData();
 
   public OClientConnection(final int id, final ONetworkProtocol protocol) throws IOException {
     this.id = id;
@@ -58,13 +58,18 @@ public class OClientConnection {
     }
   }
 
+  /**
+   * Acquires the connection. This is fundamental to manage concurrent requests using the same session id.
+   */
   public void acquire() {
-    if (!inUse.compareAndSet(false, true))
-      throw new ONetworkProtocolException("Error on reusing the same session by multiple connections");
+    lock.lock();
   }
 
+  /**
+   * Releases an acquired connection.
+   */
   public void release() {
-    inUse.set(false);
+    lock.unlock();
   }
 
   @Override
