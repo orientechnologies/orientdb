f7f8c716bc3c9c94e4b757d8fae95b072d756db2
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2015-07-09 18:04:40 +0200

    fixed issues #4366 #3969 - difference function

--- core/src/test/java/com/orientechnologies/orient/core/sql/functions/coll/SQLFunctionDifferenceTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/sql/functions/coll/SQLFunctionDifferenceTest.java
@@ -15,14 +15,13 @@
  */
 package com.orientechnologies.orient.core.sql.functions.coll;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import com.orientechnologies.orient.core.command.OBasicCommandContext;
+import org.testng.annotations.Test;
 
 import java.util.*;
 
-import org.testng.annotations.Test;
-
-import com.orientechnologies.orient.core.command.OBasicCommandContext;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 /**
  * @author edegtyarenko
@@ -31,69 +30,30 @@ import com.orientechnologies.orient.core.command.OBasicCommandContext;
 @Test
 public class SQLFunctionDifferenceTest {
 
-  @Test
-  public void testOperator() {
-    final OSQLFunctionDifference differenceFunction = new OSQLFunctionDifference() {
-      @Override
-      protected boolean returnDistributedResult() {
-        return false;
-      }
-    };
-
-    final List<Object> income = Arrays.<Object> asList(1, 2, 3, 1, 4, 5, 2, 2, 1, 1);
-    final Set<Object> expectedResult = new HashSet<Object>(Arrays.asList(3, 4, 5));
-
-    for (Object i : income) {
-      differenceFunction.execute(null, null, null, new Object[] { i }, null);
-    }
-
-    final Set<Object> actualResult = differenceFunction.getResult();
-
-    assertSetEquals(actualResult, expectedResult);
-  }
-
-  @Test
-  public void testOperatorMerge() {
-    final OSQLFunctionDifference merger = new OSQLFunctionDifference();
-    final List<OSQLFunctionDifference> differences = new ArrayList<OSQLFunctionDifference>(3);
-    for (int i = 0; i < 3; i++) {
-      differences.add(new OSQLFunctionDifference() {
-        @Override
-        protected boolean returnDistributedResult() {
-          return true;
-        }
-      });
-    }
-
-    final List<List<Object>> incomes = Arrays.asList(Arrays.<Object> asList(1, 2, 3, 4, 5, 1),
-        Arrays.<Object> asList(3, 5, 6, 7, 0, 1, 3, 3, 6), Arrays.<Object> asList(2, 2, 8, 9));
-
-    final Set<Object> expectedResult = new HashSet<Object>(Arrays.<Object> asList(4, 7, 8, 9, 0));
-
-    for (int j = 0; j < 3; j++) {
-      for (Object i : incomes.get(j)) {
-        differences.get(j).execute(null, null, null, new Object[] { i }, null);
-      }
-    }
-
-    final Set<Object> actualResult = (Set<Object>) merger.mergeDistributedResult(Arrays.asList((Object) differences.get(0)
-        .getResult(), differences.get(1).getResult(), differences.get(2).getResult()));
-
-    assertSetEquals(actualResult, expectedResult);
-  }
 
   @Test
   public void testExecute() {
     final OSQLFunctionDifference function = new OSQLFunctionDifference();
 
-    final List<List<Object>> incomes = Arrays.asList(Arrays.<Object> asList(1, 2, 3, 4, 5, 1),
+     List<List<Object>> incomes = Arrays.asList(Arrays.<Object> asList(1, 2, 3, 4, 5, 1),
         Arrays.<Object> asList(3, 5, 6, 7, 0, 1, 3, 3, 6), Arrays.<Object> asList(2, 2, 8, 9));
 
-    final Set<Object> expectedResult = new HashSet<Object>(Arrays.<Object> asList(4, 7, 8, 9, 0));
+    Set<Object> expectedResult = new HashSet<Object>(Arrays.<Object> asList(4));
 
-    final Set<Object> actualResult = (Set<Object>) function.execute(null, null, null, incomes.toArray(), new OBasicCommandContext());
+    Set<Object> actualResult = (Set<Object>) function.execute(null, null, null, incomes.toArray(), new OBasicCommandContext());
+
+
 
     assertSetEquals(actualResult, expectedResult);
+
+    incomes = Arrays.asList(Arrays.<Object> asList(1, 2, 3, 4, 5, 1),
+        Arrays.<Object> asList(3, 5, 6, 7, 0, 1, 3, 3, 6));
+
+    expectedResult = new HashSet<Object>(Arrays.<Object> asList(2, 4));
+
+    actualResult = (Set<Object>) function.execute(null, null, null, incomes.toArray(), new OBasicCommandContext());
+    assertSetEquals(actualResult, expectedResult);
+
   }
 
   private void assertSetEquals(Set<Object> actualResult, Set<Object> expectedResult) {
