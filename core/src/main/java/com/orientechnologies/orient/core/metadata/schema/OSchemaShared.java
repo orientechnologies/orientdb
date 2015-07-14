fddfadf059a5735ca4cc9307fe5449b9c922228d
lvca <l.garulli@gmail.com>
2015-07-14 16:28:33 +0200

    Fixed issue about drop cluster: it didn't remove records from cache

--- core/src/main/java/com/orientechnologies/orient/core/metadata/schema/OSchemaShared.java
+++ core/src/main/java/com/orientechnologies/orient/core/metadata/schema/OSchemaShared.java
@@ -19,17 +19,6 @@
  */
 package com.orientechnologies.orient.core.metadata.schema;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.Callable;
-
 import com.orientechnologies.common.concur.lock.OReadersWriterSpinLock;
 import com.orientechnologies.common.concur.resource.OCloseable;
 import com.orientechnologies.common.log.OLogManager;
@@ -65,6 +54,17 @@ import com.orientechnologies.orient.core.storage.OStorageProxy;
 import com.orientechnologies.orient.core.storage.impl.local.OAbstractPaginatedStorage;
 import com.orientechnologies.orient.core.type.ODocumentWrapper;
 import com.orientechnologies.orient.core.type.ODocumentWrapperNoClass;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Callable;
 
 /**
  * Shared schema class. It's shared by all the database instances that point to the same storage.
@@ -547,6 +547,9 @@ public class OSchemaShared extends ODocumentWrapperNoClass implements OSchema, O
         reload();
       } else
         dropClassInternal(className);
+
+      // FREE THE RECORD CACHE
+      getDatabase().getLocalCache().freeCluster(cls.getDefaultClusterId());
 
     } finally {
       releaseSchemaWriteLock();
@@ -1177,13 +1180,16 @@ public class OSchemaShared extends ODocumentWrapperNoClass implements OSchema, O
     }
   }
 
-  private void deleteDefaultCluster(OClass clazz) {
+  private void deleteDefaultCluster(final OClass clazz) {
     final ODatabaseDocumentInternal database = getDatabase();
     final int clusterId = clazz.getDefaultClusterId();
     final OCluster cluster = database.getStorage().getClusterById(clusterId);
 
     if (cluster.getName().equalsIgnoreCase(clazz.getName()))
       database.getStorage().dropCluster(clusterId, true);
+
+    // FREE THE RECORD CACHE
+    getDatabase().getLocalCache().freeCluster(clusterId);
   }
 
   private void saveInternal() {
