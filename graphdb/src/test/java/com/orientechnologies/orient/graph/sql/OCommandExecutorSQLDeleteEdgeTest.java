20770e263400b3d6da5aa9587a4656cefe7f962d
lvca <l.garulli@gmail.com>
2015-07-17 14:41:19 +0200

    Fixed issue #4583

--- graphdb/src/test/java/com/orientechnologies/orient/graph/sql/OCommandExecutorSQLDeleteEdgeTest.java
+++ graphdb/src/test/java/com/orientechnologies/orient/graph/sql/OCommandExecutorSQLDeleteEdgeTest.java
@@ -1,16 +1,22 @@
 package com.orientechnologies.orient.graph.sql;
 
-import org.junit.*;
-
 import com.orientechnologies.orient.core.Orient;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.db.record.OIdentifiable;
 import com.orientechnologies.orient.core.id.ORID;
 import com.orientechnologies.orient.core.metadata.schema.OSchema;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.OCommandSQL;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.List;
 
 /**
  * @author Artem Orobets (enisher-at-gmail.com)
@@ -21,10 +27,12 @@ public class OCommandExecutorSQLDeleteEdgeTest {
   private static ODatabaseDocumentTx db;
   private static ORID                folderId1;
   private static ORID                userId1;
+  private List<OIdentifiable>        edges;
 
   @BeforeClass
   public static void init() throws Exception {
-    db = Orient.instance().getDatabaseFactory().createDatabase("graph", "memory:" + OCommandExecutorSQLDeleteEdgeTest.class.getSimpleName());
+    db = Orient.instance().getDatabaseFactory()
+        .createDatabase("graph", "memory:" + OCommandExecutorSQLDeleteEdgeTest.class.getSimpleName());
     if (db.exists()) {
       db.open("admin", "admin");
       db.drop();
@@ -58,7 +66,7 @@ public class OCommandExecutorSQLDeleteEdgeTest {
     folderId1 = new ODocument("Folder").field("keyId", "01234567893").save().getIdentity();
     ORID folderId2 = new ODocument("Folder").field("keyId", "01234567894").save().getIdentity();
 
-    db.command(new OCommandSQL("create edge CanAccess from " + userId1 + " to " + folderId1)).execute();
+    edges = db.command(new OCommandSQL("create edge CanAccess from " + userId1 + " to " + folderId1)).execute();
   }
 
   @Test
@@ -78,5 +86,11 @@ public class OCommandExecutorSQLDeleteEdgeTest {
         .execute();
     Assert.assertEquals(res, 1);
     Assert.assertTrue(db.query(new OSQLSynchQuery<Object>("select flatten(out(CanAccess)) from " + userId1)).isEmpty());
+  }
+
+  @Test
+  public void testDeleteByRID() throws Exception {
+    final int res = (Integer) db.command(new OCommandSQL("delete edge [" + edges.get(0).getIdentity() + "]")).execute();
+    Assert.assertEquals(res, 1);
   }
 }
