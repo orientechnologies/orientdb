923183a18bf26bd9cae5ed0e70d446c934fda1b7
lvca <l.garulli@gmail.com>
2015-07-17 10:44:24 +0200

    Added test cases for HTTP layer

---
+++ server/src/test/java/com/orientechnologies/orient/server/network/http/BaseHttpDatabaseTest.java
@@ -0,0 +1,29 @@
+package com.orientechnologies.orient.server.network.http;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Test HTTP "query" command.
+ * 
+ * @author Luca Garulli (l.garulli--at-orientechnologies.com)
+ */
+@Test
+public abstract class BaseHttpDatabaseTest extends BaseHttpTest {
+  @BeforeClass
+  public void createDatabase() throws Exception {
+    super.startServer();
+    Assert.assertEquals(post("database/" + getDatabaseName() + "/memory").setUserName("root").setUserPassword("root").getResponse()
+        .getStatusLine().getStatusCode(), 200);
+  }
+
+  @AfterClass
+  public void dropDatabase() throws Exception {
+    Assert.assertEquals(delete("database/" + getDatabaseName()).setUserName("root").setUserPassword("root").getResponse()
+        .getStatusLine().getStatusCode(), 204);
+    super.stopServer();
+  }
+
+}
