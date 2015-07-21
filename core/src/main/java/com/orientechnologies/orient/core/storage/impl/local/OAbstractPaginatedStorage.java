76fa72432701525f55f4655fbc26cf20feb706a4
Tglman <tglman@tglman.com>
2015-07-21 16:46:37 +0100

    removed wrong cluster id assegnation, issue #2620

--- core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/OAbstractPaginatedStorage.java
+++ core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/OAbstractPaginatedStorage.java
@@ -2457,7 +2457,6 @@ public abstract class OAbstractPaginatedStorage extends OStorageAbstract impleme
         final ORecordId oldRID;
         if (rid.isNew()) {
           oldRID = rid.copy();
-          rid.clusterId = clusterId;
         } else
           oldRID = rid;
 
