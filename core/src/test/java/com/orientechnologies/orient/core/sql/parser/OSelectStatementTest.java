382b22a30059690ce8457fc66d1a8aefa971d9c1
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2015-07-27 15:42:10 +0200

    fixed test case on SELECT syntax

--- core/src/test/java/com/orientechnologies/orient/core/sql/parser/OSelectStatementTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/sql/parser/OSelectStatementTest.java
@@ -431,7 +431,7 @@ public class OSelectStatementTest {
         "select from Foo where (a=2 and b=3 and (a = 4 and (b=5 and d lucene 'foo')))) or select from Foo where (a=2 and b=3 and (a = 4 and (b=5 and d lucene 'foo'))))");
 
     checkWrongSyntax("select from cluster:foo where a lucene 'b'");
-    checkWrongSyntax("select from index:foo where a lucene 'b'");
+    checkRightSyntax("select from index:foo where a lucene 'b'");
     checkWrongSyntax("select from #12:0 where a lucene 'b'");
     checkWrongSyntax("select from [#12:0, #12:1] where a lucene 'b'");
 
