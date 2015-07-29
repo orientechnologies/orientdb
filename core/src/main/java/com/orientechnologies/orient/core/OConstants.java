61e25ddff095696666496467a25135924e8375bc
lvca <l.garulli@gmail.com>
2015-07-29 10:42:07 +0200

    Switching again to 2.1-SNAPSHOT

--- core/src/main/java/com/orientechnologies/orient/core/OConstants.java
+++ core/src/main/java/com/orientechnologies/orient/core/OConstants.java
@@ -20,7 +20,7 @@
 package com.orientechnologies.orient.core;
 
 public class OConstants {
-  public static final String ORIENT_VERSION = "2.1-rc6";
+  public static final String ORIENT_VERSION = "2.1-SNAPSHOT";
 
   public static final String ORIENT_URL     = "www.orientdb.com";
   public static final String COPYRIGHT      = "Copyrights (c) 2015 Orient Technologies LTD";
