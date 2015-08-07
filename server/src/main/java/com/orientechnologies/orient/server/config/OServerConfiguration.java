a3e9935a67e22a57b6bf6c91a6adef20272c0820
tglman <tglman@tglman.com>
2015-08-07 11:59:48 +0100

    Revert "Disable 'root' user"

--- server/src/main/java/com/orientechnologies/orient/server/config/OServerConfiguration.java
+++ server/src/main/java/com/orientechnologies/orient/server/config/OServerConfiguration.java
@@ -60,15 +60,12 @@ public class OServerConfiguration {
   @XmlElementRef(type = OServerEntryConfiguration.class)
   public OServerEntryConfiguration[]       properties;
 
-  public boolean                           isAfterFirstTime;
-
   public static final String               DEFAULT_CONFIG_FILE  = "config/orientdb-server-config.xml";
 
   public static final String               PROPERTY_CONFIG_FILE = "orientdb.config.file";
 
-  public static final String               DEFAULT_ROOT_USER    = "root";
-  public static final String               GUEST_USER           = "guest";
-  public static final String               GUEST_PASS           = "guest";
+  public static final String               SRV_ROOT_ADMIN       = "root";
+  public static final String               SRV_ROOT_GUEST       = "guest";
 
   /**
    * Empty constructor for JAXB
