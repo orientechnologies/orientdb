919a36b316857011fc0a9b0b70180743e222dc0d
lvca <l.garulli@gmail.com>
2015-08-12 01:16:31 +0200

    Fixed issue #4734

--- distributed/src/test/java/com/orientechnologies/orient/server/distributed/asynch/TestAsyncReplMode2ServersAddEdge.java
+++ distributed/src/test/java/com/orientechnologies/orient/server/distributed/asynch/TestAsyncReplMode2ServersAddEdge.java
@@ -1,5 +1,6 @@
 package com.orientechnologies.orient.server.distributed.asynch;
 
+import com.orientechnologies.common.log.OLogManager;
 import com.orientechnologies.orient.core.config.OGlobalConfiguration;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientBaseGraph;
@@ -8,9 +9,9 @@ import com.tinkerpop.blueprints.impls.orient.OrientVertex;
 
 public class TestAsyncReplMode2ServersAddEdge extends BareBoneBase2ServerTest {
 
-  private static final int    NUM_OF_LOOP_ITERATIONS = 100;
+  private static final int NUM_OF_LOOP_ITERATIONS = 100;
 
-  private Object              parentV1Id;
+  private Object           parentV1Id;
 
   @Override
   protected String getDatabaseName() {
@@ -35,12 +36,17 @@ public class TestAsyncReplMode2ServersAddEdge extends BareBoneBase2ServerTest {
 
           parentV1.addEdge("edgetype", childV);
           graph.commit();
-          assertEquals(i+2, ((OrientVertex) parentV1).getRecord().getVersion());
+
+          OLogManager.instance().error(this, "parentV1 %s v%d should be v%d", parentV1.getIdentity(),
+              parentV1.getRecord().getVersion(), i + 2);
+
+          assertEquals(i + 2, ((OrientVertex) parentV1).getRecord().getVersion());
           assertEquals(2, ((OrientVertex) childV).getRecord().getVersion());
         }
 
         pause();
       } catch (Throwable e) {
+        OLogManager.instance().error(this, "Exception", e);
         if (exceptionInThread == null) {
           exceptionInThread = e;
         }
@@ -58,7 +64,7 @@ public class TestAsyncReplMode2ServersAddEdge extends BareBoneBase2ServerTest {
     synchronized (LOCK) {
       OrientBaseGraph graph = new OrientGraph(getLocalURL2());
       try {
-        sleep(1000);
+        sleep(5000);
         OrientVertex parentV1 = graph.getVertex(parentV1Id);
         assertEquals(NUM_OF_LOOP_ITERATIONS + 1, parentV1.getRecord().getVersion());
       } catch (Throwable e) {
