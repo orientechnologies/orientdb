63e2f8535b5b7889e85db869b75b7d1d3c93bd46
lvca <l.garulli@gmail.com>
2015-08-12 14:03:16 +0200

    deploy.sh: added the optional symbolic link for databases

--- _base/script/deploy.sh
+++ _base/script/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$1" ]
 then
-    echo "SYNTAX ERROR, USE: deploy.sh <version>"
+    echo "SYNTAX ERROR, USE: deploy.sh <version> [<path-of-databases-to-link>]"
     exit
 fi
 
@@ -18,6 +18,14 @@ cp ../modules/orientdb-lucene/target/orientdb-lucene-$1-dist.jar $DIR/plugins/
 cp ../modules/orientdb-etl/target/orientdb-etl-$1.jar $DIR/lib/
 cp ../modules/orientdb-etl/script/oetl.* $DIR/bin/
 
+cd $DIR
+
+if [ -n "$2" ]
+then
+    echo "Linking databases folder in $2..."
+    rm -rf databases
+    ln -s $2 databases
+fi
+
 echo "Switching to the fresh built OrientDB $1"
 
-cd $DIR
