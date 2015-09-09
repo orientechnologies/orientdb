c7767628b97918d8b6f0e8faa2ff71c772ce46f2
lvca <l.garulli@gmail.com>
2015-09-09 23:57:29 +0200

    Released OrientDB 2.1.2

--- graphdb/src/test/java/com/orientechnologies/orient/graph/sql/GraphIntersectLightweightEdges.java
+++ graphdb/src/test/java/com/orientechnologies/orient/graph/sql/GraphIntersectLightweightEdges.java
@@ -100,7 +100,7 @@ public class GraphIntersectLightweightEdges extends GraphNoTxAbstractTest {
 
   @BeforeClass
   public static void init() {
-    System.setProperty("orientdb.test.env", "ci");
+    // System.setProperty("orientdb.test.env", "ci");
     init(GraphIntersectLightweightEdges.class.getSimpleName());
   }
 }
