316dde859cc32ef383f781989496ca698253f814
Matan Shukry <matanshukry@gmail.com>
2015-09-18 20:13:31 +0300

    Added parameter (true/false)

--- core/src/main/java/com/orientechnologies/orient/core/sql/method/misc/OSQLMethodEach.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/method/misc/OSQLMethodEach.java
@@ -12,12 +12,16 @@ public class OSQLMethodEach extends OAbstractSQLMethod {
   public static final String VARIABLE_PROCESS_EACH = "$processEach";
 
   public OSQLMethodEach() {
-    super(NAME);
+    super(NAME, 0, 1);
   }
 
   @Override
   public Object execute(Object iThis, OIdentifiable iCurrentRecord, OCommandContext iContext, Object ioResult, Object[] iParams) {
-    iContext.setVariable(VARIABLE_PROCESS_EACH, true);
-    return ioResult;
+    Boolean processEach = Boolean.TRUE;
+    if (iParams != null && iParams.length >= 1) {
+      processEach = Boolean.valueOf(iParams[0].toString());
+    }
+    iContext.setVariable(VARIABLE_PROCESS_EACH, processEach);
+    return iThis;
   }
 }
