005cd7273b7d2ac62ccfc7dbc173f6922a36f467
lvca <l.garulli@gmail.com>
2015-10-04 19:18:18 +0200

    Supported incremental backup and restore from console. Issue #2029

--- server/src/main/java/com/orientechnologies/orient/server/distributed/ODistributedStorage.java
+++ server/src/main/java/com/orientechnologies/orient/server/distributed/ODistributedStorage.java
@@ -948,12 +948,12 @@ public class ODistributedStorage implements OStorage, OFreezableStorage, OAutosh
   }
 
   @Override
-  public void incrementalBackup(String backupDirectory) {
+  public void incrementalBackup(final String backupDirectory) {
     wrapped.incrementalBackup(backupDirectory);
   }
 
   @Override
-  public void restoreFromIncrementalBackup(String filePath) {
+  public void restoreFromIncrementalBackup(final String filePath) {
     wrapped.restoreFromIncrementalBackup(filePath);
   }
 
@@ -963,7 +963,7 @@ public class ODistributedStorage implements OStorage, OFreezableStorage, OAutosh
   }
 
   @Override
-  public void close(final boolean iForce, boolean onDelete) {
+  public void close(final boolean iForce, final boolean onDelete) {
     wrapped.close(iForce, onDelete);
 
     if (isClosed())
