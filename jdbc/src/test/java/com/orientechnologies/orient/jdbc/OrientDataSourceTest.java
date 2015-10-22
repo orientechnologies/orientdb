a8dafc527ad257c699ddb24b307462e5d3a41544
robfrank <ro.franchini@gmail.com>
2015-10-22 08:38:25 +0200

    add test for pool used through DataSource

--- jdbc/src/test/java/com/orientechnologies/orient/jdbc/OrientDataSourceTest.java
+++ jdbc/src/test/java/com/orientechnologies/orient/jdbc/OrientDataSourceTest.java
@@ -4,8 +4,10 @@ import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
@@ -24,6 +26,39 @@ public class OrientDataSourceTest extends OrientJdbcBaseTest {
     assertThat(conn, is(notNullValue()));
     conn.close();
     assertThat(conn.isClosed(), is(true));
+
+  }
+
+  @Test
+  public void shouldConnectPooled() throws SQLException {
+
+    OrientDataSource ds = new OrientDataSource();
+    ds.setUrl("jdbc:orient:memory:test");
+    ds.setUsername("admin");
+    ds.setPassword("admin");
+
+    Properties info = new Properties();
+    info.setProperty("db.usePool", "TRUE");
+    info.setProperty("db.pool.min", "1");
+    info.setProperty("db.pool.max", "1");
+
+    //pool size is 1: database should be the same on different connection
+    //NOTE: not safe in production!
+    OrientJdbcConnection conn = (OrientJdbcConnection) ds.getConnection(info);
+    assertThat(conn, is(notNullValue()));
+
+    OrientJdbcConnection conn2 = (OrientJdbcConnection) ds.getConnection(info);
+    assertThat(conn2, is(notNullValue()));
+    conn.getDatabase();
+
+    assertThat(conn.getDatabase(), is(sameInstance(conn2.getDatabase())));
+
+    conn.close();
+    assertThat(conn.isClosed(), is(true));
+
+    conn2.close();
+    assertThat(conn2.isClosed(), is(true));
+
 
   }
 
