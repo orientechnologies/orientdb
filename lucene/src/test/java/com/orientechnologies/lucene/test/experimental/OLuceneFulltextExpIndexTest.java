64722e18fa4bda8c63025b0b38c51f9db019d118
Roberto Franchini <ro.franchini@gmail.com>
2015-10-24 19:35:22 +0200

    test excluded

--- lucene/src/test/java/com/orientechnologies/lucene/test/experimental/OLuceneFulltextExpIndexTest.java
+++ lucene/src/test/java/com/orientechnologies/lucene/test/experimental/OLuceneFulltextExpIndexTest.java
@@ -26,7 +26,7 @@ import java.util.logging.Level;
 /**
  * Created by frank on 9/30/15.
  */
-@Test
+@Test(enabled = false)
 public class OLuceneFulltextExpIndexTest extends BaseLuceneTest {
 
   public OLuceneFulltextExpIndexTest() {
