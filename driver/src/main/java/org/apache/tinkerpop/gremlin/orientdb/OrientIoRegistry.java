39d3131157e6f30be55b52123bc5c3adde1b96db
Marvin Froeder <velo.br@gmail.com>
2015-11-07 10:37:43 +1300

    Clean up OrientIoRegistry

--- driver/src/main/java/org/apache/tinkerpop/gremlin/orientdb/OrientIoRegistry.java
+++ driver/src/main/java/org/apache/tinkerpop/gremlin/orientdb/OrientIoRegistry.java
@@ -1,7 +1,6 @@
 package org.apache.tinkerpop.gremlin.orientdb;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.tinkerpop.gremlin.structure.io.AbstractIoRegistry;
@@ -16,30 +15,27 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.JsonTokenId;
-import com.fasterxml.jackson.core.ObjectCodec;
-import com.fasterxml.jackson.core.TreeNode;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.deser.std.UntypedObjectDeserializer.Vanilla;
 import com.fasterxml.jackson.databind.jsontype.TypeDeserializer;
 import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.node.NumericNode;
-import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.orientechnologies.orient.core.db.record.ridbag.ORidBag;
 import com.orientechnologies.orient.core.id.ORecordId;
 
 @SuppressWarnings("serial")
 public class OrientIoRegistry extends AbstractIoRegistry {
 
+    private static final String CLUSTER_ID = "clusterId";
+    private static final String CLUSTER_POSITION = "clusterPosition";
+
     private static final OrientIoRegistry INSTANCE = new OrientIoRegistry();
 
     private OrientIoRegistry() {
         register(GryoIo.class, ORecordId.class, new ORecordIdKyroSerializer());
+        register(GryoIo.class, ORidBag.class, new ORidBagKyroSerializer());
         SimpleModule serializer = new SimpleModule();
         serializer.addSerializer(ORecordId.class, new ORecordIdJacksonSerializer());
         serializer.addDeserializer(Object.class, new OObjectJacksonDeserializer());
@@ -69,84 +65,11 @@ public class OrientIoRegistry extends AbstractIoRegistry {
             if (result instanceof Map) {
                 @SuppressWarnings("unchecked")
                 Map<String, Number> map = (Map<String, Number>) result;
-                if (map.containsKey("clusterId") && map.containsKey("clusterPosition")) {
-                    return new ORecordId(map.get("clusterId").intValue(), map.get("clusterPosition").longValue());
+                if (map.containsKey(CLUSTER_ID) && map.containsKey(CLUSTER_POSITION)) {
+                    return new ORecordId(map.get(CLUSTER_ID).intValue(), map.get(CLUSTER_POSITION).longValue());
                 }
             }
             return result;
-        }
-
-    }
-
-    final static class OMapJacksonDeserializer extends StdDeserializer<Map<String, Object>> {
-
-        private static final TypeReference<Map<String, Object>> mapTypeReference = new TypeReference<Map<String, Object>>() {
-        };
-
-        public OMapJacksonDeserializer() {
-            super(TypeFactory.defaultInstance().constructType(mapTypeReference));
-        }
-
-        @Override
-        public Object deserializeWithType(JsonParser jp, DeserializationContext ctxt, TypeDeserializer typeDeserializer) throws IOException {
-            return deserialize(jp, ctxt);
-        }
-
-        @Override
-        public Map<String, Object> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-            ObjectCodec codec = p.getCodec();
-            TreeNode tree = codec.readTree(p);
-            if (!tree.isObject())
-                throw new JsonMappingException("Upexpected error");
-
-            Map<String, Object> result = nodeToMap((JsonNode) tree);
-            return result;
-        }
-
-        private Map<String, Object> nodeToMap(JsonNode node) {
-            Map<String, Object> result = new HashMap<>();
-            node.fields().forEachRemaining(entry -> {
-                JsonNode entryValue = entry.getValue();
-                Object value;
-                if (entryValue.isObject()) {
-                    if (entryValue.has("clusterId") && entryValue.has("clusterPosition")) {
-                        value = new ORecordId(entryValue.get("clusterId").asInt(), entryValue.get("clusterPosition").asLong());
-                    } else {
-                        value = nodeToMap(entryValue);
-                    }
-                } else {
-                    if (entryValue.isInt())
-                        value = entryValue.asInt();
-                    else if (entryValue.isBoolean())
-                        value = entryValue.asBoolean();
-                    else if (entryValue.isDouble())
-                        value = entryValue.asDouble();
-                    else
-                        value = entryValue.asText();
-                }
-                result.put(entry.getKey(), value);
-            });
-            return result;
-        }
-
-    }
-
-    final static class ORecordIdJacksonDeserializer extends StdDeserializer<ORecordId> {
-
-        protected ORecordIdJacksonDeserializer() {
-            super(ORecordId.class);
-        }
-
-        @Override
-        public ORecordId deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-            ObjectCodec codec = p.getCodec();
-            TreeNode tree = codec.readTree(p);
-            return new ORecordId(((NumericNode) tree.get("clusterId")).asInt(), ((NumericNode) tree.get("clusterPosition")).asLong());
-        }
-
-        @Override
-        public Object deserializeWithType(JsonParser p, DeserializationContext ctxt, TypeDeserializer typeDeserializer) throws IOException {
-            return deserialize(p, ctxt);
         }
 
     }
@@ -156,8 +79,10 @@ public class OrientIoRegistry extends AbstractIoRegistry {
         @Override
         public void serialize(ORecordId value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
             jgen.writeStartObject();
-            jgen.writeObjectField("clusterId", value.clusterId);
-            jgen.writeObjectField("clusterPosition", value.clusterPosition);
+            jgen.writeFieldName(CLUSTER_ID);
+            jgen.writeNumber(value.clusterId);
+            jgen.writeFieldName(CLUSTER_POSITION);
+            jgen.writeNumber(value.clusterPosition);
             jgen.writeEndObject();
         }
 
@@ -178,6 +103,27 @@ public class OrientIoRegistry extends AbstractIoRegistry {
         @Override
         public void write(final Kryo kryo, final Output output, final ORecordId rid) {
             output.writeString(rid.toString());
+        }
+
+    }
+
+    final static class ORidBagKyroSerializer extends Serializer<ORidBag> {
+
+        @Override
+        public ORidBag read(final Kryo kryo, final Input input, final Class<ORidBag> tinkerGraphClass) {
+            ORidBag bag = new ORidBag();
+            String[] ids = input.readString().split(";");
+
+            for (String id : ids)
+                bag.add(new ORecordId(id));
+            return bag;
+        }
+
+        @Override
+        public void write(final Kryo kryo, final Output output, final ORidBag bag) {
+            StringBuilder ids = new StringBuilder();
+            bag.forEach(rid -> ids.append(rid.getIdentity()).append(";"));
+            output.writeString(ids);
         }
 
     }
