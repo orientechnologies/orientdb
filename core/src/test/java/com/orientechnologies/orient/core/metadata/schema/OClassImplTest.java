be60fa04ad23c11000535dab4fd1d9714049f294
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2015-12-07 11:31:06 +0100

    Fix cluster name clash with class name, when class name suffix is _<number>

--- core/src/test/java/com/orientechnologies/orient/core/metadata/schema/OClassImplTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/metadata/schema/OClassImplTest.java
@@ -472,6 +472,7 @@ public class OClassImplTest {
     assertNotNull(oSchema.createClass("_OClassImplTesttestClassNameSyntax"));
     assertNotNull(oSchema.createClass("_OClassImplTesttestClassNameSyntax_"));
     assertNotNull(oSchema.createClass("_OClassImplTestte_stClassNameSyntax_"));
+    assertNotNull(oSchema.createClass("_OClassImplTesttestClassNameSyntax_1"));
     assertNotNull(oSchema.createClass("_OClassImplTesttestClassNameSyntax_12"));
     assertNotNull(oSchema.createClass("_OClassImplTesttestCla23ssNameSyntax_12"));
     assertNotNull(oSchema.createClass("$OClassImplTesttestCla23ssNameSyntax_12"));
