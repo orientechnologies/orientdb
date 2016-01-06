f6baac52e7ee169b6efe843c780b9d3be0c121be
laa <lomakin.andrey@gmail.com>
2016-01-06 16:20:57 +0200

    Issue #5343 pokec full scan speed test was added.

---
+++ tests/src/test/java/com/orientechnologies/orient/test/database/speed/PokecTest.java
@@ -0,0 +1,26 @@
+package com.orientechnologies.orient.test.database.speed;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
+import org.testng.annotations.Test;
+
+@Test(enabled = false)
+public class PokecTest {
+  public void fullQueryBenchmark() {
+    ODatabaseDocumentTx databaseDocumentTx = new ODatabaseDocumentTx("plocal:E:\\pokec");
+    databaseDocumentTx.open("admin", "admin");
+
+    for (int i = 0; i < 100; i++) {
+      databaseDocumentTx.query(new OSQLSynchQuery<ODocument>("select AGE, count(*) from Profile group by AGE limit -1"));
+    }
+
+    long start = System.nanoTime();
+    for (int i = 0; i < 100; i++) {
+      databaseDocumentTx.query(new OSQLSynchQuery<ODocument>("select AGE, count(*) from Profile group by AGE limit -1"));
+    }
+    long end = System.nanoTime();
+
+    System.out.println((end - start) / (100 * 1000000L));
+  }
+}
