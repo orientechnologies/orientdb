19a974655d9a43f2d7074375de9530b56f9032a1
laa <lomakin.andrey@gmail.com>
2016-01-29 13:17:04 +0200

    Scema not created exception was added in schema load function.

---
+++ core/src/main/java/com/orientechnologies/orient/core/exception/OSchemaNotCreatedException.java
@@ -0,0 +1,19 @@
+package com.orientechnologies.orient.core.exception;
+
+import com.orientechnologies.common.exception.OHighLevelException;
+
+public class OSchemaNotCreatedException extends OSchemaException implements OHighLevelException {
+  public OSchemaNotCreatedException(String message) {
+    super(message);
+  }
+
+  /**
+   * This constructor is needed to restore and reproduce exception on client side in case of remote storage exception handling.
+   * Please create "copy constructor" for each exception which has current one as a parent.
+   *
+   * @param exception
+   */
+  public OSchemaNotCreatedException(OSchemaNotCreatedException exception) {
+    super(exception);
+  }
+}
--- core/src/main/java/com/orientechnologies/orient/core/exception/OStorageExistsException.java
+++ core/src/main/java/com/orientechnologies/orient/core/exception/OStorageExistsException.java
@@ -3,7 +3,7 @@ package com.orientechnologies.orient.core.exception;
 import com.orientechnologies.common.exception.OHighLevelException;
 
 public class OStorageExistsException extends OStorageException implements OHighLevelException {
-  public OStorageExistsException(OStorageException exception) {
+  public OStorageExistsException(OStorageExistsException exception) {
     super(exception);
   }
 
