0b4ef4fd992f20080ea53ab3efb2d1dd78aa987f
robfrank <ro.franchini@gmail.com>
2016-02-02 15:45:02 +0100

    releasing OrientDB 2.0.18

--- _base/script/release_enterprise.sh
+++ _base/script/release_enterprise.sh
@@ -7,9 +7,9 @@ fi
 
 echo "Releasing OrientDB Enterprise $1..."
 
-cp -R distribution/target/orientdb-community-$1-distribution.dir/orientdb-community-$1/ distribution/target/orientdb-community-$1-distribution.dir/orientdb-enterprise-$1/
+cp -R distribution/target/orientdb-community-$1.dir/orientdb-community-$1/ distribution/target/orientdb-community-$1.dir/orientdb-enterprise-$1/
 
-DIR=distribution/target/orientdb-community-$1-distribution.dir/orientdb-enterprise-$1/
+DIR=distribution/target/orientdb-community-$1.dir/orientdb-enterprise-$1/
 
 cp ../drivers/orientdb-jdbc/target/orientdb-jdbc-$1.jar $DIR/lib/
 
@@ -18,13 +18,13 @@ cp ../modules/orientdb-lucene/target/orientdb-lucene-$1-dist.jar $DIR/plugins/
 cp ../modules/orientdb-etl/target/orientdb-etl-$1.jar $DIR/lib/
 cp ../modules/orientdb-etl/script/oetl.* $DIR/bin/
 
-cp ../../orientdb-enterprise/agent/target/agent-$1.zip $DIR/plugins/
+cp ../orientdb-enterprise/agent/target/agent-$1.zip $DIR/plugins/
 
 cd distribution/target/
 rm orientdb-enterprise-$1.tar.gz 
 rm orientdb-enterprise-$1.zip
 
-cd orientdb-community-$1-distribution.dir
+cd orientdb-community-$1.dir
 rm `find . -name ".DS_Store" -print`
 rm `find . -name "*.wal" -print`
 
