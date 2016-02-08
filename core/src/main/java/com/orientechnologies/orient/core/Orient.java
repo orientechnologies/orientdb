d27f806d28b89fa4024149d9fe6015202d667192
laa <lomakin.andrey@gmail.com>
2016-02-08 18:52:47 +0200

    Issue #4805 was fixed.

--- core/src/main/java/com/orientechnologies/orient/core/Orient.java
+++ core/src/main/java/com/orientechnologies/orient/core/Orient.java
@@ -41,6 +41,7 @@ import com.orientechnologies.common.log.OLogManager;
 import com.orientechnologies.common.parser.OSystemVariableResolver;
 import com.orientechnologies.common.profiler.OProfiler;
 import com.orientechnologies.common.profiler.OProfilerStub;
+import com.orientechnologies.common.serialization.types.OShortSerializer;
 import com.orientechnologies.orient.core.cache.OLocalRecordCacheFactory;
 import com.orientechnologies.orient.core.cache.OLocalRecordCacheFactoryImpl;
 import com.orientechnologies.orient.core.command.script.OScriptManager;
@@ -53,6 +54,7 @@ import com.orientechnologies.orient.core.engine.local.OEngineLocalPaginated;
 import com.orientechnologies.orient.core.engine.memory.OEngineMemory;
 import com.orientechnologies.orient.core.exception.OConfigurationException;
 import com.orientechnologies.orient.core.record.ORecordFactoryManager;
+import com.orientechnologies.orient.core.shutdown.OShutdownHandler;
 import com.orientechnologies.orient.core.storage.OIdentifiableStorage;
 import com.orientechnologies.orient.core.storage.OStorage;
 
@@ -96,6 +98,20 @@ public class Orient extends OListenerManger<OOrientListener> {
       .newSetFromMap(new ConcurrentHashMap<WeakHashSetValueHolder<OOrientStartupListener>, Boolean>());
   private final Set<WeakHashSetValueHolder<OOrientShutdownListener>> weakShutdownListeners         = Collections
       .newSetFromMap(new ConcurrentHashMap<WeakHashSetValueHolder<OOrientShutdownListener>, Boolean>());
+
+  private final PriorityQueue<OShutdownHandler> shutdownHandlers = new PriorityQueue<OShutdownHandler>(
+      new Comparator<OShutdownHandler>() {
+        @Override
+        public int compare(OShutdownHandler handlerOne, OShutdownHandler handlerTwo) {
+          if (handlerOne.getPriority() > handlerTwo.getPriority())
+            return 1;
+
+          if (handlerOne.getPriority() < handlerTwo.getPriority())
+            return -1;
+
+          return 0;
+        }
+      });
 
   private final OLocalRecordCacheFactory localRecordCache = new OLocalRecordCacheFactoryImpl();
 
@@ -264,6 +280,7 @@ public class Orient extends OListenerManger<OOrientListener> {
           OLogManager.instance().error(this, "Error on startup", e);
         }
 
+      initShutdownQueue();
     } finally {
       engineLock.writeLock().unlock();
     }
@@ -271,6 +288,39 @@ public class Orient extends OListenerManger<OOrientListener> {
     return this;
   }
 
+  /**
+   * Add handler which will be executed during {@link #shutdown()} call.
+   *
+   * @param shutdownHandler Shutdown handler instance.
+   */
+  public void addShutdownHandler(OShutdownHandler shutdownHandler) {
+    engineLock.writeLock().lock();
+    try {
+      shutdownHandlers.add(shutdownHandler);
+    } finally {
+      engineLock.writeLock().unlock();
+    }
+  }
+
+  /**
+   * Adds shutdown handlers in order which will be used during execution of shutdown.
+   */
+  private void initShutdownQueue() {
+    addShutdownHandler(new OShutdownWorkersHandler());
+    addShutdownHandler(new OShutdownEnginesHandler());
+    addShutdownHandler(new OShutdownPendingThreadsHandler());
+    addShutdownHandler(new OShutdownProfilerHandler());
+    addShutdownHandler(new OShutdownCallListenersHandler());
+  }
+
+  /**
+   * Shutdown whole OrientDB ecosystem. Usually is called during JVM shutdown by JVM shutdown handler.
+   * During shutdown all handlers which were registered by the call of {@link #addShutdownHandler(OShutdownHandler)}
+   * are called together with pre-registered system shoutdown handlers according to their priority.
+   *
+   * @see OShutdownWorkersHandler
+   * @see
+   */
   public Orient shutdown() {
     engineLock.writeLock().lock();
     try {
@@ -279,66 +329,16 @@ public class Orient extends OListenerManger<OOrientListener> {
 
       active = false;
 
-      workers.shutdown();
-      try {
-        workers.awaitTermination(2, TimeUnit.MINUTES);
-      } catch (InterruptedException e) {
-      }
-
-      OLogManager.instance().debug(this, "Orient Engine is shutting down...");
-
-      shutdownAllStorages();
-
-      // SHUTDOWN ENGINES
-      for (OEngine engine : engines.values())
-        engine.shutdown();
-      engines.clear();
-
-      if (threadGroup != null)
-        // STOP ALL THE PENDING THREADS
-        threadGroup.interrupt();
-
-      if (shutdownHook != null) {
-        shutdownHook.cancel();
-        shutdownHook = null;
-      }
-      if (signalHandler != null) {
-        signalHandler.cancel();
-        signalHandler = null;
-      }
-
-      timer.cancel();
-      timer = null;
-
-      // NOTE: DON'T REMOVE PROFILER TO AVOID NPE AROUND THE CODE IF ANY THREADS IS STILL WORKING
-      profiler.shutdown();
-
-      purgeWeakShutdownListeners();
-      for (final WeakHashSetValueHolder<OOrientShutdownListener> wl : weakShutdownListeners)
+      OLogManager.instance().info(this, "Orient Engine is shutting down...");
+      for (OShutdownHandler handler : shutdownHandlers) {
         try {
-          if (wl != null) {
-            final OOrientShutdownListener l = wl.get();
-            if (l != null) {
-              l.onShutdown();
-            }
-          }
-
+          OLogManager.instance().info(this, "Shutdown handler %s is going to be called", handler);
+          handler.shutdown();
+          OLogManager.instance().info(this, "Shutdown handler %s completed", handler);
         } catch (Exception e) {
-          OLogManager.instance().error(this, "Error during orient shutdown", e);
+          OLogManager.instance().error(this, "Exception during calling of shutdown handler %s", handler);
         }
-
-      // CALL THE SHUTDOWN ON ALL THE LISTENERS
-      for (OOrientListener l : browseListeners()) {
-        if (l != null)
-          try {
-            l.onShutdown();
-          } catch (Exception e) {
-            OLogManager.instance().error(this, "Error during orient shutdown", e);
-          }
-
       }
-
-      System.gc();
 
       OLogManager.instance().info(this, "OrientDB Engine shutdown complete");
       OLogManager.instance().flush();
@@ -571,8 +571,7 @@ public class Orient extends OListenerManger<OOrientListener> {
   /**
    * Returns the engine by its name.
    *
-   * @param engineName
-   *          Engine name to retrieve
+   * @param engineName Engine name to retrieve
    * @return OEngine instance of found, otherwise null
    */
   public OEngine getEngine(final String engineName) {
@@ -814,6 +813,147 @@ public class Orient extends OListenerManger<OOrientListener> {
         ref = (WeakHashSetValueHolder<OOrientShutdownListener>) removedShutdownListenersQueue.poll();
       }
 
+    }
+  }
+
+  /**
+   * Shutdown thread group which is used in methods {@link #submit(Callable)} and {@link #submit(Runnable)}.
+   */
+  public class OShutdownWorkersHandler implements OShutdownHandler {
+    @Override
+    public int getPriority() {
+      return SHUTDOWN_WORKERS_PRIORITY;
+    }
+
+    @Override
+    public void shutdown() throws Exception {
+      workers.shutdown();
+      try {
+        workers.awaitTermination(2, TimeUnit.MINUTES);
+      } catch (InterruptedException e) {
+      }
+    }
+
+    @Override
+    public String toString() {
+      return getClass().getSimpleName();
+    }
+  }
+
+  /**
+   * Closes all storages and shutdown all engines.
+   */
+  public class OShutdownEnginesHandler implements OShutdownHandler {
+    @Override
+    public int getPriority() {
+      return SHUTDOWN_ENGINES_PRIORITY;
+    }
+
+    @Override
+    public void shutdown() throws Exception {
+      shutdownAllStorages();
+
+      // SHUTDOWN ENGINES
+      for (OEngine engine : engines.values())
+        engine.shutdown();
+      engines.clear();
+    }
+
+    @Override
+    public String toString() {
+      return getClass().getSimpleName();
+    }
+  }
+
+  /**
+   * Interrupts all threads in OrientDB thread group and stops timer is used in methods {@link #scheduleTask(TimerTask, Date, long)} and
+   * {@link #scheduleTask(TimerTask, long, long)}.
+   */
+  private class OShutdownPendingThreadsHandler implements OShutdownHandler {
+    @Override
+    public int getPriority() {
+      return SHUTDOWN_PENDING_THREADS_PRIORITY;
+    }
+
+    @Override
+    public void shutdown() throws Exception {
+      if (threadGroup != null)
+        // STOP ALL THE PENDING THREADS
+        threadGroup.interrupt();
+
+      timer.cancel();
+      timer = null;
+    }
+
+    @Override
+    public String toString() {
+      return getClass().getSimpleName();
+    }
+  }
+
+  /**
+   * Shutdown OrientDB profiler.
+   */
+  private class OShutdownProfilerHandler implements OShutdownHandler {
+    @Override
+    public int getPriority() {
+      return SHUTDOWN_PROFILER_PRIORITY;
+    }
+
+    @Override
+    public void shutdown() throws Exception {
+      // NOTE: DON'T REMOVE PROFILER TO AVOID NPE AROUND THE CODE IF ANY THREADS IS STILL WORKING
+      profiler.shutdown();
+    }
+
+    @Override
+    public String toString() {
+      return getClass().getSimpleName();
+    }
+  }
+
+  /**
+   * Calls all shutdown listeners.
+   */
+  private class OShutdownCallListenersHandler implements OShutdownHandler {
+    @Override
+    public int getPriority() {
+      return SHUTDOWN_CALL_LISTENERS;
+    }
+
+    @Override
+    public void shutdown() throws Exception {
+      purgeWeakShutdownListeners();
+      for (final WeakHashSetValueHolder<OOrientShutdownListener> wl : weakShutdownListeners)
+        try {
+          if (wl != null) {
+            final OOrientShutdownListener l = wl.get();
+            if (l != null) {
+              l.onShutdown();
+            }
+          }
+
+        } catch (Exception e) {
+          OLogManager.instance().error(this, "Error during orient shutdown", e);
+        }
+
+      // CALL THE SHUTDOWN ON ALL THE LISTENERS
+      for (OOrientListener l : browseListeners()) {
+        if (l != null)
+          try {
+            l.onShutdown();
+          } catch (Exception e) {
+            OLogManager.instance().error(this, "Error during orient shutdown", e);
+          }
+
+      }
+
+      System.gc();
+    }
+
+    @Override
+    public String toString() {
+      return getClass().getSimpleName();
     }
   }
 }
---
+++ core/src/main/java/com/orientechnologies/orient/core/shutdown/OShutdownHandler.java
@@ -0,0 +1,51 @@
+package com.orientechnologies.orient.core.shutdown;
+
+import com.orientechnologies.orient.core.Orient;
+
+/**
+ * Handler which is used inside of shutdown priority queue.
+ * The higher priority we have the earlier this handler will be executed.
+ * <p>
+ * There are set of predefined priorities which are used for system shutdown handlers which allows to add your handlers before , between and
+ * after them.
+ *
+ * @see Orient#addShutdownHandler(OShutdownHandler)
+ * @see Orient#shutdown()
+ */
+public interface OShutdownHandler {
+  /**
+   * Priority of {@link com.orientechnologies.orient.core.Orient.OShutdownWorkersHandler} handler.
+   */
+  int SHUTDOWN_WORKERS_PRIORITY = 1000;
+
+  /**
+   * Priority of {@link com.orientechnologies.orient.core.Orient.OShutdownEnginesHandler} handler.
+   */
+  int SHUTDOWN_ENGINES_PRIORITY = 1100;
+
+  /**
+   * Priority of {@link com.orientechnologies.orient.core.Orient.OShutdownPendingThreadsHandler} handler.
+   */
+  int SHUTDOWN_PENDING_THREADS_PRIORITY = 1200;
+
+  /**
+   * Priority of {@link com.orientechnologies.orient.core.Orient.OShutdownProfilerHandler} handler.
+   */
+  int SHUTDOWN_PROFILER_PRIORITY = 1300;
+
+  /**
+   * Priority of {@link com.orientechnologies.orient.core.Orient.OShutdownCallListenersHandler} handler.
+   */
+  int SHUTDOWN_CALL_LISTENERS = 1400;
+
+  /**
+   * @return Handlers priority.
+   */
+  int getPriority();
+
+  /**
+   * Code which executed during system shutdown. During call of {@link Orient#shutdown()} method which is called during JVM
+   * shutdown.
+   */
+  void shutdown() throws Exception;
+}
