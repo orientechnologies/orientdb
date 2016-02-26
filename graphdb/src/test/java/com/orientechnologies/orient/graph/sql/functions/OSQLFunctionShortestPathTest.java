6a94e69f80e35d29251d7c442beaa55c6ade09d7
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2016-02-26 13:00:00 +0100

    Add maxDepth to shortestPath() function
    Resolves: #5771

--- graphdb/src/test/java/com/orientechnologies/orient/graph/sql/functions/OSQLFunctionShortestPathTest.java
+++ graphdb/src/test/java/com/orientechnologies/orient/graph/sql/functions/OSQLFunctionShortestPathTest.java
@@ -16,8 +16,8 @@ import static org.junit.Assert.assertEquals;
 
 public class OSQLFunctionShortestPathTest {
 
-  private OrientGraph              graph;
-  private Map<Integer, Vertex>     vertices = new HashMap<Integer, Vertex>();
+  private OrientGraph graph;
+  private Map<Integer, Vertex> vertices = new HashMap<Integer, Vertex>();
 
   private OSQLFunctionShortestPath function;
 
@@ -45,8 +45,6 @@ public class OSQLFunctionShortestPathTest {
     vertices.get(2).setProperty("node_id", "B");
     vertices.get(3).setProperty("node_id", "C");
     vertices.get(4).setProperty("node_id", "D");
-
-
 
     graph.addEdge(null, vertices.get(1), vertices.get(2), "Edge1");
     graph.addEdge(null, vertices.get(2), vertices.get(3), "Edge1");
@@ -113,4 +111,47 @@ public class OSQLFunctionShortestPathTest {
     }
   }
 
+  @Test
+  public void testMaxDepth1() throws Exception {
+    Map<String, Object> additionalParams = new HashMap<String, Object>();
+    additionalParams.put(OSQLFunctionShortestPath.PARAM_MAX_DEPTH, 11);
+    final List<ORID> result = function
+        .execute(null, null, null, new Object[] { vertices.get(1), vertices.get(20), null, null, additionalParams },
+            new OBasicCommandContext());
+
+    assertEquals(11, result.size());
+  }
+
+  @Test
+  public void testMaxDepth2() throws Exception {
+    Map<String, Object> additionalParams = new HashMap<String, Object>();
+    additionalParams.put(OSQLFunctionShortestPath.PARAM_MAX_DEPTH, 12);
+    final List<ORID> result = function
+        .execute(null, null, null, new Object[] { vertices.get(1), vertices.get(20), null, null, additionalParams },
+            new OBasicCommandContext());
+
+    assertEquals(11, result.size());
+  }
+
+  @Test
+  public void testMaxDepth3() throws Exception {
+    Map<String, Object> additionalParams = new HashMap<String, Object>();
+    additionalParams.put(OSQLFunctionShortestPath.PARAM_MAX_DEPTH, 10);
+    final List<ORID> result = function
+        .execute(null, null, null, new Object[] { vertices.get(1), vertices.get(20), null, null, additionalParams },
+            new OBasicCommandContext());
+
+    assertEquals(0, result.size());
+  }
+
+  @Test
+  public void testMaxDepth4() throws Exception {
+    Map<String, Object> additionalParams = new HashMap<String, Object>();
+    additionalParams.put(OSQLFunctionShortestPath.PARAM_MAX_DEPTH, 3);
+    final List<ORID> result = function
+        .execute(null, null, null, new Object[] { vertices.get(1), vertices.get(20), null, null, additionalParams },
+            new OBasicCommandContext());
+
+    assertEquals(0, result.size());
+  }
 }
