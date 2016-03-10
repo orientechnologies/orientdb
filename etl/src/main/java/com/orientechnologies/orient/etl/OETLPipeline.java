1c48228a5bd3186a944761350a5200a6c5b91018
Tglman <tglman@tglman.com>
2016-03-10 12:12:22 +1100

    refactor of the etl loader for support parallel processing with multiple pipeline.

--- etl/src/main/java/com/orientechnologies/orient/etl/OETLPipeline.java
+++ etl/src/main/java/com/orientechnologies/orient/etl/OETLPipeline.java
@@ -39,15 +39,15 @@ import com.tinkerpop.blueprints.impls.orient.OrientBaseGraph;
  * @author Luca Garulli (l.garulli-at-orientechnologies.com)
  */
 public class OETLPipeline {
-  protected final OETLProcessor      processor;
-  protected final List<OTransformer> transformers;
-  protected final OLoader            loader;
-  protected final OCommandContext    context;
-  protected final LOG_LEVELS         logLevel;
-  protected final int                maxRetries;
-  protected boolean                  haltOnError;
-  protected ODatabaseDocumentTx      db;
-  protected OrientBaseGraph          graph;
+  protected final OETLProcessor       processor;
+  protected final List<OTransformer>  transformers;
+  protected final OLoader             loader;
+  protected final OCommandContext     context;
+  protected final LOG_LEVELS          logLevel;
+  protected final int                 maxRetries;
+  protected       boolean             haltOnError;
+  protected       ODatabaseDocumentTx db;
+  protected       OrientBaseGraph     graph;
 
   public OETLPipeline(final OETLProcessor processor, final List<OTransformer> transformers, final OLoader loader,
       final LOG_LEVELS logLevel, final int maxRetries, final boolean haltOnError) {
@@ -62,12 +62,11 @@ public class OETLPipeline {
 
     for (OTransformer t : this.transformers)
       t.setPipeline(this);
-    this.loader.setPipeline(this);
 
   }
 
   public synchronized void begin() {
-    loader.begin();
+    loader.beginLoader(this);
     for (OTransformer t : transformers)
       t.begin();
   }
@@ -118,18 +117,18 @@ public class OETLPipeline {
         }
         if (current != null)
           // LOAD
-          loader.load(current, context);
+          loader.load(this, current, context);
 
         return current;
       } catch (ONeedRetryException e) {
-        loader.rollback();
+        loader.rollback(this);
         retry++;
         processor.out(INFO, "Error in pipeline execution, retry = %d/%d (exception=%s)", retry, maxRetries, e);
       } catch (OETLProcessHaltedException e) {
         processor.out(ERROR, "Pipeline execution halted");
         processor.getStats().incrementErrors();
 
-        loader.rollback();
+        loader.rollback(this);
         throw e;
 
       } catch (Exception e) {
@@ -140,12 +139,16 @@ public class OETLPipeline {
           return null;
         }
 
-        loader.rollback();
+        loader.rollback(this);
         throw OException.wrapException(new OETLProcessHaltedException("Halt"), e);
 
       }
     } while (retry < maxRetries);
 
     return this;
+  }
+
+  public void end() {
+    loader.endLoader(this);
   }
 }
