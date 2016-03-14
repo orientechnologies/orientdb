25fb627949905aaff95af7193d61adc3f85099a2
Tglman <tglman@tglman.com>
2016-03-14 16:07:43 +1100

    add tests for tracking data in nested collections.

--- core/src/test/java/com/orientechnologies/orient/core/db/record/ODocumentTrackingNestedCollectionsTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/db/record/ODocumentTrackingNestedCollectionsTest.java
@@ -4,13 +4,14 @@ import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.id.ORID;
 import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.record.impl.ONestedMultiValueChangeEvent;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -28,11 +29,11 @@ public class ODocumentTrackingNestedCollectionsTest {
 
   @After
   public void after() {
-    db.close();
+    db.drop();
   }
 
   @Test
-  public void testTrackingNestedList() {
+  public void testTrackingNestedSet() {
 
     ORID orid;
     ODocument document = new ODocument();
@@ -62,6 +63,104 @@ public class ODocumentTrackingNestedCollectionsTest {
     subObjects = (Set) objects.iterator().next();
 
     assertTrue(!subObjects.isEmpty());
+
+  }
+
+  @Test
+  public void testChangesValuesNestedTrackingSet() {
+
+    ODocument document = new ODocument();
+    Set objects = new HashSet();
+
+    document.field("objects", objects);
+    Set subObjects = new HashSet();
+    objects.add(subObjects);
+
+    ODocument nestedDoc = new ODocument();
+    subObjects.add(nestedDoc);
+
+    document.save();
+
+    objects = document.field("objects");
+    subObjects = (Set) objects.iterator().next();
+    subObjects.add("one");
+
+    OMultiValueChangeTimeLine<Object, Object> timeLine = document.getCollectionTimeLine("objects");
+
+    assertEquals(1, timeLine.getMultiValueChangeEvents().size());
+    assertTrue(timeLine.getMultiValueChangeEvents().get(0) instanceof ONestedMultiValueChangeEvent);
+    ONestedMultiValueChangeEvent nesetedEvent = (ONestedMultiValueChangeEvent) timeLine.getMultiValueChangeEvents().get(0);
+    assertEquals(1, nesetedEvent.getTimeLine().getMultiValueChangeEvents().size());
+    List<OMultiValueChangeEvent<?, ?>> multiValueChangeEvents = nesetedEvent.getTimeLine().getMultiValueChangeEvents();
+    assertEquals("one", multiValueChangeEvents.get(0).getValue());
+
+  }
+
+  @Test
+  public void testChangesValuesNestedTrackingList() {
+
+    ODocument document = new ODocument();
+    List objects = new ArrayList();
+
+    document.field("objects", objects);
+    List subObjects = new ArrayList();
+    objects.add(subObjects);
+
+    ODocument nestedDoc = new ODocument();
+    subObjects.add(nestedDoc);
+
+    document.save();
+
+    objects = document.field("objects");
+    subObjects = (List) objects.iterator().next();
+    subObjects.add("one");
+    subObjects.add(new ODocument());
+
+    OMultiValueChangeTimeLine<Object, Object> timeLine = document.getCollectionTimeLine("objects");
+
+    assertEquals(1, timeLine.getMultiValueChangeEvents().size());
+    assertTrue(timeLine.getMultiValueChangeEvents().get(0) instanceof ONestedMultiValueChangeEvent);
+    ONestedMultiValueChangeEvent nesetedEvent = (ONestedMultiValueChangeEvent) timeLine.getMultiValueChangeEvents().get(0);
+    assertEquals(2, nesetedEvent.getTimeLine().getMultiValueChangeEvents().size());
+    List<OMultiValueChangeEvent<?, ?>> multiValueChangeEvents = nesetedEvent.getTimeLine().getMultiValueChangeEvents();
+    assertEquals(1, multiValueChangeEvents.get(0).getKey());
+    assertEquals("one", multiValueChangeEvents.get(0).getValue());
+    assertEquals(2, multiValueChangeEvents.get(1).getKey());
+    assertTrue(multiValueChangeEvents.get(1).getValue() instanceof ODocument);
+
+  }
+
+  @Test
+  public void testChangesValuesNestedTrackingMap() {
+
+    ODocument document = new ODocument();
+    Map objects = new HashMap();
+
+    document.field("objects", objects);
+    Map subObjects = new HashMap();
+    objects.put("first", subObjects);
+
+    ODocument nestedDoc = new ODocument();
+    subObjects.put("one", nestedDoc);
+
+    document.save();
+
+    objects = document.field("objects");
+    subObjects = (Map) objects.values().iterator().next();
+    subObjects.put("one", "String");
+    subObjects.put("two", new ODocument());
+
+    OMultiValueChangeTimeLine<Object, Object> timeLine = document.getCollectionTimeLine("objects");
+
+    assertEquals(1, timeLine.getMultiValueChangeEvents().size());
+    assertTrue(timeLine.getMultiValueChangeEvents().get(0) instanceof ONestedMultiValueChangeEvent);
+    ONestedMultiValueChangeEvent nesetedEvent = (ONestedMultiValueChangeEvent) timeLine.getMultiValueChangeEvents().get(0);
+    assertEquals(2, nesetedEvent.getTimeLine().getMultiValueChangeEvents().size());
+    List<OMultiValueChangeEvent<?, ?>> multiValueChangeEvents = nesetedEvent.getTimeLine().getMultiValueChangeEvents();
+    assertEquals("one", multiValueChangeEvents.get(0).getKey());
+    assertEquals("String", multiValueChangeEvents.get(0).getValue());
+    assertEquals("two", multiValueChangeEvents.get(1).getKey());
+    assertTrue(multiValueChangeEvents.get(1).getValue() instanceof ODocument);
 
   }
 
