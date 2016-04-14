c1b3dad183385d703fcdfa18a9ce5ecaa11c0eed
Tglman <tglman@tglman.com>
2016-04-14 15:23:32 +0200

    ported fix for special DST dates

---
+++ core/src/test/java/com/orientechnologies/orient/core/record/impl/ODateConversionTestCase.java
@@ -0,0 +1,72 @@
+package com.orientechnologies.orient.core.record.impl;/*
+ * Copyright 2016 OrientDB LTD (info--at--orientdb.com)
+ * All Rights Reserved. Commercial License.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * OrientDB LTD and its suppliers, if any.  The intellectual and
+ * technical concepts contained herein are proprietary to
+ * OrientDB LTD and its suppliers and may be covered by United
+ * Kingdom and Foreign Patents, patents in process, and are protected by trade
+ * secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from OrientDB LTD.
+ *
+ * For more information: http://www.orientdb.com
+ */
+
+import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.serialization.serializer.record.ORecordSerializer;
+import com.orientechnologies.orient.core.serialization.serializer.record.binary.ORecordSerializerBinary;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Gabriele Ponzi
+ * @email <gabriele.ponzi--at--gmail.com>
+ */
+
+public class ODateConversionTestCase {
+
+  private ORecordSerializer serializer = new ORecordSerializerBinary();
+
+  @Test
+  public void testDateSerializationWithDST() throws ParseException {
+
+    // write on the db a vertex with a date:
+    // 1975-05-31 23:00:00 GMT OR 1975-06-01 01:00:00 (GMT+1) +DST (+2 total)
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    Date dateToInsert = format.parse("1975-06-01 01:00:00");
+
+    ODocument document = new ODocument();
+    document.field("date", dateToInsert, OType.DATE);
+    byte[] res = serializer.toStream(document, false);
+    ODocument extr = (ODocument) serializer.fromStream(res, new ODocument(), new String[] {});
+    final String[] fields = extr.fieldNames();
+
+    assertNotNull(fields);
+    assertEquals(fields.length, 1);
+    assertEquals(fields[0], "date");
+    //It is correct that is 1 am because this date does not exist at midnight.
+    assertEquals(document.field("date"), extr.field("date"));
+
+  }
+
+}
+
+
