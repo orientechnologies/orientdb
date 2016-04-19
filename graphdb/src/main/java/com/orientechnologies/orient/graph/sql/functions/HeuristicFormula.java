bbc2c84372b8d82a2640e145bcc4b93f1ddda540
Saeed Tabrizi <saeed@nowcando.com>
2016-04-19 22:30:07 +0430

    A Star funtion now added to graph functions

---
+++ graphdb/src/main/java/com/orientechnologies/orient/graph/sql/functions/HeuristicFormula.java
@@ -0,0 +1,47 @@
+/*
+  *
+  *  *  Copyright 2014 Orient Technologies LTD (info(at)orientechnologies.com)
+  *  *
+  *  *  Licensed under the Apache License, Version 2.0 (the "License");
+  *  *  you may not use this file except in compliance with the License.
+  *  *  You may obtain a copy of the License at
+  *  *
+  *  *       http://www.apache.org/licenses/LICENSE-2.0
+  *  *
+  *  *  Unless required by applicable law or agreed to in writing, software
+  *  *  distributed under the License is distributed on an "AS IS" BASIS,
+  *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  *  *  See the License for the specific language governing permissions and
+  *  *  limitations under the License.
+  *  *
+  *  * For more information: http://www.orientechnologies.com
+  *
+  */
+package com.orientechnologies.orient.graph.sql.functions;
+
+import com.orientechnologies.orient.core.command.OCommandContext;
+import com.orientechnologies.orient.core.command.OCommandExecutorAbstract;
+import com.orientechnologies.orient.core.id.ORID;
+import com.orientechnologies.orient.core.sql.functions.math.OSQLFunctionMathAbstract;
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.impls.orient.OrientBaseGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientVertex;
+
+import java.util.*;
+
+
+/**
+ *  heuristic formula enum.
+ *
+ * @author Saeed Tabrizi (saeed a_t  nowcando.com)
+ *
+ */
+public enum HeuristicFormula {
+    MANHATAN,
+    MAXAXIS,
+    DIAGONAL,
+    EUCLIDEAN,
+    EUCLIDEANNOSQR,
+    CUSTOM
+}
