a6f1293bc6e2a24ce78d69e89c66f6ebd9b9a1f1
lvca <l.garulli@gmail.com>
2016-05-09 10:51:51 +0200

    Load Balancing: avoided dependency between graph api and remote
    Issue #5358

--- graphdb/src/main/java/com/tinkerpop/blueprints/impls/orient/OrientBaseGraph.java
+++ graphdb/src/main/java/com/tinkerpop/blueprints/impls/orient/OrientBaseGraph.java
@@ -473,8 +473,8 @@ public abstract class OrientBaseGraph extends OrientConfigurableGraph implements
           final OIndex index = indexManager.getIndex(indexName);
           ODocument metadata = index.getConfiguration().field("metadata");
 
-          String recordMapIndexName =  null;
-          if(metadata != null){
+          String recordMapIndexName = null;
+          if (metadata != null) {
             recordMapIndexName = metadata.field(OrientIndex.CONFIG_RECORD_MAP_NAME);
           }
 
@@ -1163,7 +1163,7 @@ public abstract class OrientBaseGraph extends OrientConfigurableGraph implements
       try {
         if (closeDb) {
           getDatabase().close();
-          if(getDatabase().isPooled()){
+          if (getDatabase().isPooled()) {
             database = null;
           }
         }
@@ -1957,7 +1957,7 @@ public abstract class OrientBaseGraph extends OrientConfigurableGraph implements
     if (pool == null) {
       database = new ODatabaseDocumentTx(url);
 
-      final OStorageRemote.CONNECTION_STRATEGY connMode = settings.getConnectionStrategy();
+      final String connMode = settings.getConnectionStrategy();
       getDatabase().setProperty(OStorageRemote.PARAM_CONNECTION_STRATEGY, connMode);
 
       if (getDatabase().getStorage().getUnderlying() instanceof OAbstractPaginatedStorage)
