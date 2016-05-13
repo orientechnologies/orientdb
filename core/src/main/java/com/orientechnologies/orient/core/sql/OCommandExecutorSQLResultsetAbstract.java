4e1078e1bbda64bdd22d98998f367017cbfdc6ba
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2016-05-13 17:37:02 +0200

    Fix backslash escapes in LET classes
    Resolves: #4996

--- core/src/main/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLResultsetAbstract.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLResultsetAbstract.java
@@ -297,7 +297,7 @@ public abstract class OCommandExecutorSQLResultsetAbstract extends OCommandExecu
 
       parserOptionalKeyword("=");
 
-      parserNextWord(false, " =><,\r\n");
+      parserNextWord(false, " =><,\r\n", true);
 
       // PARSE THE VALUE
       String letValueAsString = parserGetLastWord();
