747079587a804fc68bc9dd6a40d0ce753a0dddda
robfrank <ro.franchini@gmail.com>
2016-05-18 17:02:33 +0200

    releasing OrientDB 2.2.0

--- core/src/main/java/com/orientechnologies/orient/core/OConstants.java
+++ core/src/main/java/com/orientechnologies/orient/core/OConstants.java
@@ -20,15 +20,15 @@
 package com.orientechnologies.orient.core;
 
 public class OConstants {
-  public static final String  ORIENT_VERSION       = "2.2.0-SNAPSHOT";
+  public static final String  ORIENT_VERSION       = "2.2.0";
 
   public static final int     ORIENT_VERSION_MAJOR = 2;
   public static final int     ORIENT_VERSION_MINOR = 2;
   public static final int     ORIENT_VERSION_HOFIX = 0;
-  public static final boolean SNAPSHOT             = true;
+  public static final boolean SNAPSHOT             = false;
 
   public static final String  ORIENT_URL           = "www.orientdb.com";
-  public static final String  COPYRIGHT            = "Copyrights (c) 2015 OrientDB LTD";
+  public static final String  COPYRIGHT            = "Copyrights (c) 2016 OrientDB LTD";
 
   /**
    * Returns the complete text of the current OrientDB version.
