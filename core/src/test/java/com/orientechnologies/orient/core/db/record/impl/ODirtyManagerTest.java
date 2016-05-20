6b38717a9b611655f08f5c1983f8e2b62479ce16
Tglman <tglman@tglman.com>
2016-05-20 21:13:25 +0100

    fixed issue on loop dirty management on collection in case of noTx, issue #6157

--- core/src/test/java/com/orientechnologies/orient/core/db/record/impl/ODirtyManagerTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/db/record/impl/ODirtyManagerTest.java
@@ -3,12 +3,7 @@ package com.orientechnologies.orient.core.db.record.impl;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import org.testng.annotations.Test;
 
@@ -355,8 +350,8 @@ public class ODirtyManagerTest {
     assertEquals(1, manager.getPointed(doc).size());
     assertTrue(manager.getPointed(doc).contains(link));
   }
-  
-  @Test(enabled =false)
+
+  @Test(enabled = false)
   public void testLinkSetNoConvertRemove() {
     ODocument doc = new ODocument();
     doc.field("test", "ddd");
@@ -365,7 +360,7 @@ public class ODirtyManagerTest {
     set.add(link);
     doc.field("set", set, OType.LINKSET);
     doc.removeField("set");
-    
+
     ODirtyManager manager = ORecordInternal.getDirtyManager(doc);
     assertEquals(2, manager.getNewRecords().size());
     assertEquals(0, manager.getPointed(doc).size());
@@ -427,6 +422,86 @@ public class ODirtyManagerTest {
     // TODO: double check this, it's an overhead
     assertEquals(1, manager.getPointed(embeddedMapDoc).size());
     assertTrue(manager.getPointed(doc).contains(link));
+
+  }
+
+  @Test
+  public void testLinkListLoop() {
+    ODocument doc1 = new ODocument().field("name", "doc1");
+    ODocument doc2 = new ODocument().field("name", "doc2");
+    ODocument doc3 = new ODocument().field("name", "doc3");
+    doc1.field("other", Arrays.asList(doc2, doc3));
+    doc2.field("other", Arrays.asList(doc1, doc3));
+    doc3.field("other", Arrays.asList(doc1, doc2));
+    ODocumentInternal.convertAllMultiValuesToTrackedVersions(doc1);
+    ODirtyManager manager = ORecordInternal.getDirtyManager(doc1);
+    assertEquals(3, manager.getNewRecords().size());
+
+    assertTrue(manager.getPointed(doc1).contains(doc2));
+    assertTrue(manager.getPointed(doc1).contains(doc3));
+
+    assertTrue(manager.getPointed(doc2).contains(doc1));
+    assertTrue(manager.getPointed(doc2).contains(doc3));
+
+    assertTrue(manager.getPointed(doc3).contains(doc1));
+    assertTrue(manager.getPointed(doc3).contains(doc2));
+
+  }
+
+
+  @Test
+  public void testLinkSetLoop() {
+    ODocument doc1 = new ODocument().field("name", "doc1");
+    ODocument doc2 = new ODocument().field("name", "doc2");
+    ODocument doc3 = new ODocument().field("name", "doc3");
+    doc1.field("other", new HashSet(Arrays.asList(doc2, doc3)));
+    doc2.field("other", new HashSet(Arrays.asList(doc1, doc3)));
+    doc3.field("other", new HashSet(Arrays.asList(doc1, doc2)));
+    ODocumentInternal.convertAllMultiValuesToTrackedVersions(doc1);
+    ODirtyManager manager = ORecordInternal.getDirtyManager(doc1);
+    assertEquals(3, manager.getNewRecords().size());
+
+    assertTrue(manager.getPointed(doc1).contains(doc2));
+    assertTrue(manager.getPointed(doc1).contains(doc3));
+
+    assertTrue(manager.getPointed(doc2).contains(doc1));
+    assertTrue(manager.getPointed(doc2).contains(doc3));
+
+    assertTrue(manager.getPointed(doc3).contains(doc1));
+    assertTrue(manager.getPointed(doc3).contains(doc2));
+
+  }
+
+  @Test
+  public void testLinkMapLoop() {
+    ODocument doc1 = new ODocument().field("name", "doc1");
+    ODocument doc2 = new ODocument().field("name", "doc2");
+    ODocument doc3 = new ODocument().field("name", "doc3");
+    Map<String, OIdentifiable> map1 = new HashMap<String, OIdentifiable>();
+    map1.put("a", doc2);
+    map1.put("b", doc3);
+    doc1.field("other", map1);
+    Map<String, OIdentifiable> map2 = new HashMap<String, OIdentifiable>();
+    map2.put("a", doc1);
+    map2.put("b", doc3);
+
+    doc2.field("other", map2);
+    Map<String, OIdentifiable> map3 = new HashMap<String, OIdentifiable>();
+    map3.put("a", doc1);
+    map3.put("b", doc2);
+    doc3.field("other", map3);
+    ODocumentInternal.convertAllMultiValuesToTrackedVersions(doc1);
+    ODirtyManager manager = ORecordInternal.getDirtyManager(doc1);
+    assertEquals(3, manager.getNewRecords().size());
+
+    assertTrue(manager.getPointed(doc1).contains(doc2));
+    assertTrue(manager.getPointed(doc1).contains(doc3));
+
+    assertTrue(manager.getPointed(doc2).contains(doc1));
+    assertTrue(manager.getPointed(doc2).contains(doc3));
+
+    assertTrue(manager.getPointed(doc3).contains(doc1));
+    assertTrue(manager.getPointed(doc3).contains(doc2));
 
   }
 
