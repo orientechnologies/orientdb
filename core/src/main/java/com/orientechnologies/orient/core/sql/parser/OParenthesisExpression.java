173da8a98952f4410f6ce4d96661f874bab59a23
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2016-05-26 13:46:42 +0200

    Fix parenthesis expressions in RETURN statement of MATCH statements

--- core/src/main/java/com/orientechnologies/orient/core/sql/parser/OParenthesisExpression.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/parser/OParenthesisExpression.java
@@ -2,6 +2,9 @@
 /* JavaCCOptions:MULTI=true,NODE_USES_PARSER=false,VISITOR=true,TRACK_TOKENS=true,NODE_PREFIX=O,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 package com.orientechnologies.orient.core.sql.parser;
 
+import com.orientechnologies.orient.core.command.OCommandContext;
+import com.orientechnologies.orient.core.db.record.OIdentifiable;
+
 import java.util.Map;
 
 public class OParenthesisExpression extends OMathExpression {
@@ -20,6 +23,16 @@ public class OParenthesisExpression extends OMathExpression {
   /** Accept the visitor. **/
   public Object jjtAccept(OrientSqlVisitor visitor, Object data) {
     return visitor.visit(this, data);
+  }
+
+  @Override public Object execute(OIdentifiable iCurrentRecord, OCommandContext ctx) {
+    if(expression!=null){
+      return expression.execute(iCurrentRecord, ctx);
+    }
+    if(statement!=null){
+      throw new UnsupportedOperationException("Execution of select in parentheses is not supported");
+    }
+    return super.execute(iCurrentRecord, ctx);
   }
 
   public void toString(Map<Object, Object> params, StringBuilder builder) {
