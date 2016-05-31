0d179f92c5d4532f2b0ab4ee70599beab09854c9
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2016-05-31 12:13:46 +0200

    Fix null pointer on Http Graph response

--- graphdb/src/main/java/com/orientechnologies/orient/server/network/protocol/http/OHttpGraphResponse.java
+++ graphdb/src/main/java/com/orientechnologies/orient/server/network/protocol/http/OHttpGraphResponse.java
@@ -23,6 +23,7 @@ import com.orientechnologies.common.collection.OMultiValue;
 import com.orientechnologies.orient.core.db.ODatabaseRecordThreadLocal;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.serialization.serializer.OJSONWriter;
 import com.tinkerpop.blueprints.Direction;
@@ -75,14 +76,22 @@ public class OHttpGraphResponse extends OHttpResponse {
         // IGNORE IT
         continue;
 
-      if (entry instanceof ODocument)
-        if (((ODocument) entry).getSchemaClass().isVertexType())
+      entry = ((OIdentifiable)entry).getRecord();
+
+      if (entry == null || !(entry instanceof OIdentifiable))
+        // IGNORE IT
+        continue;
+
+      if (entry instanceof ODocument) {
+        OClass schemaClass = ((ODocument) entry).getSchemaClass();
+        if (schemaClass!=null && schemaClass.isVertexType())
           vertices.add(graph.getVertex(entry));
-        else if (((ODocument) entry).getSchemaClass().isEdgeType())
+        else if (schemaClass!=null && schemaClass.isEdgeType())
           edges.add(graph.getEdge(entry));
         else
           // IGNORE IT
           continue;
+      }
     }
 
     final StringWriter buffer = new StringWriter();
