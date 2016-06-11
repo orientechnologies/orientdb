a3986772aa625c147921f4228be2bb32d90b712a
Michael MacFadden <michael@macfadden.org>
2016-06-10 18:11:58 -0700

    Updated the date format in the response headers of the http protocol to be RFC 7231 compliant

--- server/src/main/java/com/orientechnologies/orient/server/network/protocol/http/OHttpResponse.java
+++ server/src/main/java/com/orientechnologies/orient/server/network/protocol/http/OHttpResponse.java
@@ -32,6 +32,7 @@ import com.orientechnologies.orient.server.OClientConnection;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.zip.GZIPOutputStream;
 
@@ -44,6 +45,15 @@ public class OHttpResponse {
   public static final String   JSON_FORMAT       = "type,indent:-1,rid,version,attribSameRow,class,keepTypes,alwaysFetchEmbeddedDocuments";
   public static final char[]   URL_SEPARATOR     = { '/' };
   private static final Charset utf8              = Charset.forName("utf8");
+  
+  // Set up a date formatter that prints the date in the Http-date format as
+  // per RFC 7231, section 7.1.1.1
+  private static final SimpleDateFormat sdf;
+  static {
+    sdf = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+    sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+  }
+      
   public final String          httpVersion;
   private final OutputStream   out;
   public String                headers;
@@ -146,7 +156,7 @@ public class OHttpResponse {
       writeLine(headers);
     }
 
-    writeLine("Date: " + new Date());
+    writeLine("Date: " + sdf.format(new Date()));
     writeLine("Content-Type: " + iContentType + "; charset=" + characterSet);
     writeLine("Server: " + serverInfo);
     writeLine("Connection: " + (iKeepAlive ? "Keep-Alive" : "close"));
