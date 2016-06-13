2b67228c3375e795fba6d0f61ccb96c04abd9627
lvca <l.garulli@gmail.com>
2016-06-13 19:25:19 +0200

    StressTool: fixed slowdown due to the print. Now it prints every 1k times

--- tools/src/main/java/com/orientechnologies/orient/stresstest/output/OConsoleWriter.java
+++ tools/src/main/java/com/orientechnologies/orient/stresstest/output/OConsoleWriter.java
@@ -24,63 +24,62 @@ import com.orientechnologies.orient.stresstest.operations.OOperationsSet;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Takes care of updating the console with a completion percentage
- * while the stress test is working
+ * Takes care of updating the console with a completion percentage while the stress test is working
  *
  * @author Andrea Iacono
  */
 public class OConsoleWriter {
+  private long          consoleTotalUpdates = 0l;
 
-    private final int totalCreates;
-    private final int totalReads;
-    private final int totalUpdates;
-    private final int totalDeletes;
+  private final int     totalCreates;
+  private final int     totalReads;
+  private final int     totalUpdates;
+  private final int     totalDeletes;
 
-    private AtomicInteger actualCreates = new AtomicInteger(0);
-    private AtomicInteger actualReads = new AtomicInteger(0);
-    private AtomicInteger actualUpdates = new AtomicInteger(0);
-    private AtomicInteger actualDeletes = new AtomicInteger(0);
+  private AtomicInteger actualCreates       = new AtomicInteger(0);
+  private AtomicInteger actualReads         = new AtomicInteger(0);
+  private AtomicInteger actualUpdates       = new AtomicInteger(0);
+  private AtomicInteger actualDeletes       = new AtomicInteger(0);
 
-    public OConsoleWriter(OOperationsSet operationsSet, int threadsNumber, int iterationsNumber) {
-        totalCreates = operationsSet.getNumberOfCreates() * threadsNumber * iterationsNumber;
-        totalReads = operationsSet.getNumberOfReads() * threadsNumber * iterationsNumber;
-        totalUpdates = operationsSet.getNumberOfUpdates() * threadsNumber * iterationsNumber;
-        totalDeletes = operationsSet.getNumberOfDeletes() * threadsNumber * iterationsNumber;
+  public OConsoleWriter(OOperationsSet operationsSet, int threadsNumber, int iterationsNumber) {
+    totalCreates = operationsSet.getNumberOfCreates() * threadsNumber * iterationsNumber;
+    totalReads = operationsSet.getNumberOfReads() * threadsNumber * iterationsNumber;
+    totalUpdates = operationsSet.getNumberOfUpdates() * threadsNumber * iterationsNumber;
+    totalDeletes = operationsSet.getNumberOfDeletes() * threadsNumber * iterationsNumber;
+  }
+
+  public void printMessage(String message) {
+    System.out.println(message);
+  }
+
+  public void updateConsole() {
+    if (++consoleTotalUpdates % 1000 == 0) {
+      System.out.print(String.format("\rStress test in progress %d%% [Creates: %d%% - Reads: %d%% - Updates: %d%% - Deletes: %d%%]",
+          ((int) (100 * (actualCreates.get() + actualDeletes.get() + actualReads.get() + actualUpdates.get())
+              / (float) (totalCreates + totalDeletes + totalReads + totalUpdates))),
+          ((int) ((actualCreates.get() / (float) totalCreates) * 100)), ((int) ((actualReads.get() / (float) totalReads) * 100)),
+          ((int) ((actualUpdates.get() / (float) totalUpdates) * 100)),
+          ((int) ((actualDeletes.get() / (float) totalDeletes) * 100)), actualReads.get(), totalReads));
     }
+  }
 
-    public void printMessage(String message) {
-        System.out.println(message);
-    }
+  public void addCreate() {
+    actualCreates.incrementAndGet();
+    updateConsole();
+  }
 
-    private void updateConsole() {
-        System.out.print(String.format(
-                "\rStress test in progress %d%% [Creates: %d%% - Reads: %d%% - Updates: %d%% - Deletes: %d%%]",
-                ((int) (100 * (actualCreates.get() + actualDeletes.get() + actualReads.get() + actualUpdates.get()) / (float) (totalCreates + totalDeletes + totalReads + totalUpdates))),
-                ((int) ((actualCreates.get() / (float) totalCreates) * 100)),
-                ((int) ((actualReads.get() / (float) totalReads) * 100)),
-                ((int) ((actualUpdates.get() / (float) totalUpdates) * 100)),
-                ((int) ((actualDeletes.get() / (float) totalDeletes) * 100)),
-                actualReads.get(), totalReads)
-        );
-    }
+  public void addRead() {
+    actualReads.incrementAndGet();
+    updateConsole();
+  }
 
-    public void addCreate() {
-        actualCreates.incrementAndGet();
-        updateConsole();
-    }
+  public void addUpdate() {
+    actualUpdates.incrementAndGet();
+    updateConsole();
+  }
 
-    public void addRead() {
-        actualReads.incrementAndGet();
-        updateConsole();
-    }
-
-    public void addUpdate() {
-        actualUpdates.incrementAndGet();
-        updateConsole();
-    }
-
-    public void addDelete() {
-        actualDeletes.incrementAndGet();
-        updateConsole();
-    }
+  public void addDelete() {
+    actualDeletes.incrementAndGet();
+    updateConsole();
+  }
 }
