d86a6f0a31b1ad22c33a582cc42c20fccea5f6c0
Tglman <tglman@tglman.com>
2016-06-16 16:06:11 +0100

    minor fix on setup and teardown of two test cases

--- graphdb/src/test/java/com/tinkerpop/blueprints/impls/orient/SimplePropertyLinkTest.java
+++ graphdb/src/test/java/com/tinkerpop/blueprints/impls/orient/SimplePropertyLinkTest.java
@@ -1,6 +1,8 @@
 package com.tinkerpop.blueprints.impls.orient;
 
 import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -10,9 +12,20 @@ import static org.junit.Assert.assertTrue;
  */
 public class SimplePropertyLinkTest {
 
+  private OrientGraph graph;
+
+  @Before
+  public void before() {
+    graph = new OrientGraph("memory:" + SimplePropertyLinkTest.class.getSimpleName());
+  }
+
+  @After
+  public void after() {
+    graph.drop();
+  }
+
   @Test
   public void testSimplePropertyLink() {
-    OrientGraph graph = new OrientGraph("memory:" + SimplePropertyLinkTest.class.getSimpleName());
     OrientVertex v1 = graph.addVertex(null);
     OrientVertex v2 = graph.addVertex(null);
     v1.setProperty("link", v2);
@@ -21,7 +34,6 @@ public class SimplePropertyLinkTest {
     graph.commit();
     graph.getRawGraph().getLocalCache().clear();
     assertTrue(((OIdentifiable) graph.getVertex(v1.getIdentity()).getProperty("link")).getIdentity().isPersistent());
-
   }
 
 }
