orientechnologies/orientdb

commit 74ea6f870c9a79127b6d328c6bed148d30e8696e

add sorting of new records following temporary ids to avoid random order/different id in creation, issue #6323

core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/OAbstractPaginatedStorage.java
@@ -1286,11 +1286,28 @@ public abstract class OAbstractPaginatedStorage extends OStorageAbstract
 
     final Iterable<ORecordOperation> entries = (Iterable<ORecordOperation>) clientTx.getAllRecordEntries();
 
+    final Set<ORecordOperation> newRecords = new TreeSet<ORecordOperation>(new Comparator<ORecordOperation>() {
+      @Override
+      public int compare(ORecordOperation o1, ORecordOperation o2) {
+        long diff = (o1.getRecord().getIdentity().getClusterPosition() - o2.getRecord().getIdentity().getClusterPosition());
+        //convert long to int comparation
+        if (diff == 0)
+          return 0;
+        else if (diff < 0)
+          return -1;
+        else
+          return 1;
+      }
+    });
+
     for (ORecordOperation txEntry : entries) {
       if (txEntry.type == ORecordOperation.CREATED || txEntry.type == ORecordOperation.UPDATED) {
         final ORecord record = txEntry.getRecord();
         if (record instanceof ODocument)
           ((ODocument) record).validate();
+      }
+      if (txEntry.type == ORecordOperation.CREATED) {
+        newRecords.add(txEntry);
       }
     }
 
@@ -1311,10 +1328,10 @@ public abstract class OAbstractPaginatedStorage extends OStorageAbstract
           startStorageTx(clientTx);
 
           Map<ORecordOperation, OPhysicalPosition> positions = new IdentityHashMap<ORecordOperation, OPhysicalPosition>();
-          for (ORecordOperation txEntry : entries) {
+          for (ORecordOperation txEntry : newRecords) {
             ORecord rec = txEntry.getRecord();
 
-            if (txEntry.type == ORecordOperation.CREATED && rec.isDirty()) {
+            if (rec.isDirty()) {
               ORecordId rid = (ORecordId) rec.getIdentity().copy();
               ORecordId oldRID = rid.copy();
               int clusterId = rid.clusterId;

commit 07b72340ddbf3a2c1f3643278d501f637afd7bed

Fixed bug introduced by commit 74ea6f870c9a79127b6d328c6bed148d30e8696e

core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/OAbstractPaginatedStorage.java
@@ -1288,15 +1288,8 @@ public abstract class OAbstractPaginatedStorage extends OStorageAbstract
 
     final Set<ORecordOperation> newRecords = new TreeSet<ORecordOperation>(new Comparator<ORecordOperation>() {
       @Override
-      public int compare(ORecordOperation o1, ORecordOperation o2) {
-        long diff = (o1.getRecord().getIdentity().getClusterPosition() - o2.getRecord().getIdentity().getClusterPosition());
-        //convert long to int comparation
-        if (diff == 0)
-          return 0;
-        else if (diff < 0)
-          return -1;
-        else
-          return 1;
+      public int compare(final ORecordOperation o1, final ORecordOperation o2) {
+        return o1.getRecord().getIdentity().compareTo(o2.getRecord().getIdentity());
       }
     });
 