6d633d7990e6faf5cb78d2cd09e5b5b39a837bd6
Tglman <tglman@tglman.com>
2016-07-06 12:05:39 +0100

    Revert "Fixed 2 issues in binary protocol when auto-merge strategy is used + fixed another bug on update"
    This reverts commit 736bcc205c7330229f09d4a8d925d8a8f46e82b9.

--- client/src/main/java/com/orientechnologies/orient/client/remote/OStorageRemote.java
+++ client/src/main/java/com/orientechnologies/orient/client/remote/OStorageRemote.java
@@ -56,6 +56,7 @@ import com.orientechnologies.orient.core.record.ORecordInternal;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.security.OCredentialInterceptor;
 import com.orientechnologies.orient.core.security.OSecurityManager;
+import com.orientechnologies.orient.core.serialization.OSerializableStream;
 import com.orientechnologies.orient.core.serialization.serializer.record.string.ORecordSerializerSchemaAware2CSV;
 import com.orientechnologies.orient.core.serialization.serializer.stream.OStreamSerializerAnyStreamable;
 import com.orientechnologies.orient.core.sql.query.OLiveQuery;
@@ -1306,19 +1307,13 @@ public class OStorageRemote extends OStorageAbstract implements OStorageProxy {
             }
 
             final int updatedRecords = network.readInt();
+            ORecordId rid;
             for (int i = 0; i < updatedRecords; ++i) {
-              final ORecordId rid = network.readRID();
-              final int v = network.readVersion();
+              rid = network.readRID();
 
-              final byte[] updateContent = network.getSrvProtocolVersion() >= 37 ? network.readBytes() : null;
-
-              final ORecordOperation rop = iTx.getRecordEntry(rid);
-              if (rop != null) {
-                if( updateContent != null && updateContent.length>0)
-                  ORecordInternal.fill(rop.getRecord(), rop.getRecord().getIdentity(), v, updateContent, false);
-                else
-                  ORecordInternal.setVersion(rop.getRecord(), v);
-              }
+              ORecordOperation rop = iTx.getRecordEntry(rid);
+              if (rop != null)
+                ORecordInternal.setVersion(rop.getRecord(), network.readVersion());
             }
 
             if (network.getSrvProtocolVersion() >= 20)
