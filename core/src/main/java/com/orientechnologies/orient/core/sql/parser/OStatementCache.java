4b36b98d3187345dd8ff500f6043e8b9b76002a2
Tglman <tglman@tglman.com>
2016-07-07 16:50:50 +0100

    minor refactor on statement cache api

--- core/src/main/java/com/orientechnologies/orient/core/sql/parser/OStatementCache.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/parser/OStatementCache.java
@@ -53,7 +53,7 @@ public class OStatementCache {
       return parse(statement);
     }
 
-    OStatementCache resource = ((OSharedContext)db.getStorage().getResource(OSharedContext.class.getName(),null)).getStatementCache();
+    OStatementCache resource = db.getSharedContext().getStatementCache();
     return resource.get(statement);
   }
 
