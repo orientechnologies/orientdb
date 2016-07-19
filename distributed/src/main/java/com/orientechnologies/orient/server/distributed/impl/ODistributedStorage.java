1cdcfd759896575fa701b5aec5ef2c51333acf0a
lvca <l.garulli@gmail.com>
2016-07-19 00:00:28 -0500

    Distributed: small fix on the assignment of cluster id

--- distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/ODistributedStorage.java
+++ distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/ODistributedStorage.java
@@ -1767,7 +1767,7 @@ public class ODistributedStorage implements OStorage, OFreezableStorageComponent
 
   protected String checkForCluster(final ORecordId iRecordId, final String localNodeName, final ODistributedConfiguration dbCfg) {
 
-    if (iRecordId.isNew())
+    if (iRecordId.getClusterId() < 0)
       throw new IllegalArgumentException("RID " + iRecordId + " is not valid");
 
     String clusterName = getClusterNameByRID(iRecordId);
