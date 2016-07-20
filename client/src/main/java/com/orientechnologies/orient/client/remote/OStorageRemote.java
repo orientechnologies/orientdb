52f36cd1e1a01442e93f6ce2f2fd6b21d380cc43
lvca <l.garulli@gmail.com>
2016-07-19 21:21:08 -0500

    Fixed issues in binary protocol. Now it switches properly to another server if a server is not reachable

--- client/src/main/java/com/orientechnologies/orient/client/remote/OStorageRemote.java
+++ client/src/main/java/com/orientechnologies/orient/client/remote/OStorageRemote.java
@@ -263,7 +263,7 @@ public class OStorageRemote extends OStorageAbstract implements OStorageProxy {
 
   }
 
-  private int handleIOException(int retry, OChannelBinaryAsynchClient network, Exception e) {
+  private int handleIOException(int retry, final OChannelBinaryAsynchClient network, final Exception e) {
     OLogManager.instance().warn(this, "Caught I/O errors, trying to reconnect (error: %s)", e.getMessage());
     OLogManager.instance().debug(this, "I/O error stack: ", e);
     connectionManager.remove(network);
@@ -1821,18 +1821,17 @@ public class OStorageRemote extends OStorageAbstract implements OStorageProxy {
     do {
       do {
         OChannelBinaryAsynchClient network = null;
-        network = getNetwork(currentURL);
         try {
+          network = getNetwork(currentURL);
           openRemoteDatabase(network);
           return currentURL;
         } catch (OIOException e) {
           if (network != null) {
             // REMOVE THE NETWORK CONNECTION IF ANY
             connectionManager.remove(network);
-            network = null;
           }
 
-          OLogManager.instance().error(this, "Cannot open database with url " + currentURL, e);
+          OLogManager.instance().debug(this, "Cannot open database with url " + currentURL, e);
 
         } catch (OException e) {
           connectionManager.release(network);
@@ -1848,7 +1847,6 @@ public class OStorageRemote extends OStorageAbstract implements OStorageProxy {
               // IGNORE ANY EXCEPTION
               OLogManager.instance().debug(this, "Cannot remove connection or database url=" + currentURL, e);
             }
-            network = null;
           }
 
           OLogManager.instance().error(this, "Cannot open database url=" + currentURL, e);
@@ -2091,6 +2089,8 @@ public class OStorageRemote extends OStorageAbstract implements OStorageProxy {
     do {
       try {
         network = connectionManager.acquire(iCurrentURL, clientConfiguration, connectionOptions, asynchEventListener);
+      } catch (OIOException cause) {
+        throw cause;
       } catch (Exception cause) {
         throw OException.wrapException(new OStorageException("Cannot open a connection to remote server: " + iCurrentURL), cause);
       }
