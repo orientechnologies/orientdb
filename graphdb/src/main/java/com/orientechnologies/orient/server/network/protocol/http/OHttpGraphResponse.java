203125fb85e1aa4d3edf603d227bb0e4788e446d
maggiolo00 <maggiolo00@gmail.com>
2016-07-28 16:14:51 +0200

    Changed Edge props From/To -> In/Out as before

--- graphdb/src/main/java/com/orientechnologies/orient/server/network/protocol/http/OHttpGraphResponse.java
+++ graphdb/src/main/java/com/orientechnologies/orient/server/network/protocol/http/OHttpGraphResponse.java
@@ -132,8 +132,8 @@ public class OHttpGraphResponse extends OHttpResponse {
       json.writeAttribute("@rid", edge.getIdentity());
       json.writeAttribute("@class", edge.getRecord().getClassName());
 
-      json.writeAttribute("from", edge.getVertex(Direction.OUT).getId());
-      json.writeAttribute("to", edge.getVertex(Direction.IN).getId());
+      json.writeAttribute("out", edge.getVertex(Direction.OUT).getId());
+      json.writeAttribute("in", edge.getVertex(Direction.IN).getId());
 
       for (String field : edge.getPropertyKeys()) {
         final Object v = edge.getProperty(field);
