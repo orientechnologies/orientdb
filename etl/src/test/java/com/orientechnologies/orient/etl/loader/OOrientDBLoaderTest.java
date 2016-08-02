4581ed0b07c0d55a3b68f890a0c280abd5dd7b91
Roberto Franchini <ro.franchini@gmail.com>
2016-08-02 11:16:08 +0200

    add a test that loads document on a db with predefined schema
    refs: https://github.com/orientechnologies/orientdb/issues/6419

--- etl/src/test/java/com/orientechnologies/orient/etl/loader/OOrientDBLoaderTest.java
+++ etl/src/test/java/com/orientechnologies/orient/etl/loader/OOrientDBLoaderTest.java
@@ -1,10 +1,12 @@
 package com.orientechnologies.orient.etl.loader;
 
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.exception.OConfigurationException;
 import com.orientechnologies.orient.core.id.ORID;
 import com.orientechnologies.orient.core.index.OIndexManagerProxy;
 import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.OCommandSQL;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 import com.orientechnologies.orient.etl.OETLBaseTest;
 import com.tinkerpop.blueprints.Vertex;
@@ -76,14 +78,42 @@ public class OOrientDBLoaderTest extends OETLBaseTest {
     process(
         "{source: { content: { value: 'name,surname,@class\nJay,Miner,Person' } }, extractor : { csv: {} }, loader: { orientdb: {\n"
             + "      dbURL: \"memory:OETLBaseTest\",\n" + "      dbUser: \"admin\",\n" + "      dbPassword: \"admin\",\n"
-            + "      dbAutoCreate: true,\n      tx: false,\n"
-            + "      batchCommit: 1000,\n" + "      wal : false,\n" + "      dbType: \"document\" , \"classes\": [\n"
-            + "        {\n" + "          \"name\": \"Person\"\n" + "        },\n" + "        {\n"
-            + "          \"name\": \"UpdateDetails\"\n" + "        }\n" + "      ]      } } }");
+            + "      dbAutoCreate: true,\n      tx: false,\n" + "      batchCommit: 1000,\n" + "      wal : false,\n"
+            + "      dbType: \"document\" , \"classes\": [\n" + "        {\n" + "          \"name\": \"Person\"\n" + "        },\n"
+            + "        {\n" + "          \"name\": \"UpdateDetails\"\n" + "        }\n" + "      ]      } } }");
 
     graph.makeActive();
 
     ODatabaseDocumentTx db = graph.getRawGraph();
+
+    List<?> res = db.query(new OSQLSynchQuery<ODocument>("SELECT FROM Person"));
+
+    assertThat(res.size()).isEqualTo(1);
+
+  }
+
+  @Test
+  public void shouldSaveDocumentsWithPredefinedSchema() {
+
+    //create class
+    ODatabaseDocument db = new ODatabaseDocumentTx(graph.getRawGraph().getURL()).open("admin", "admin");
+
+    db.command(new OCommandSQL("CREATE Class Person")).execute();
+    db.command(new OCommandSQL("CREATE property Person.name STRING")).execute();
+    db.command(new OCommandSQL("CREATE property Person.surname STRING")).execute();
+
+    db.close();
+
+    //store data
+    process(
+        "{source: { content: { value: 'name,surname,@class\nJay,Miner,Person' } }, extractor : { csv: {} }, loader: { orientdb: {\n"
+            + "      dbURL: \"memory:OETLBaseTest\",\n" + "      dbUser: \"admin\",\n" + "      dbPassword: \"admin\",\n"
+            + "      dbAutoCreate: true,\n      tx: false,\n" + "      batchCommit: 1000,\n" + "      wal : false,\n"
+            + "      dbType: \"document\" } } }");
+
+    graph.makeActive();
+
+    db = graph.getRawGraph();
 
     List<?> res = db.query(new OSQLSynchQuery<ODocument>("SELECT FROM Person"));
 
