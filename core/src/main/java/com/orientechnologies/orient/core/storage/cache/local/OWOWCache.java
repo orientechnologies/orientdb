273119ea5a6296b44d77725a605fb5b400689909
laa <lomakin.andrey@gmail.com>
2016-08-08 16:05:31 +0300

    Prjhub issue #7370 was fixed

--- core/src/main/java/com/orientechnologies/orient/core/storage/cache/local/OWOWCache.java
+++ core/src/main/java/com/orientechnologies/orient/core/storage/cache/local/OWOWCache.java
@@ -649,6 +649,7 @@ public class OWOWCache extends OAbstractWriteCache implements OWriteCache, OCach
               if (pageGroup != null) {
                 pagePointers[n].decrementReadersReferrer();
                 pagePointers[n] = pageGroup.page;
+                pagePointers[n].incrementReadersReferrer();
               }
             }
           }
