4f4f62c1c10b94dce0a7acad3cdac05dde25fb3c
Roberto Franchini <ro.franchini@gmail.com>
2016-08-19 17:17:04 +0200

    fixes setup of JAVA_OPTS_SCRIPT variable
    refs https://github.com/orientechnologies/orientdb/issues/6504

--- server/script/server.sh
+++ server/script/server.sh
@@ -72,7 +72,6 @@ export JAVA
 
 LOG_FILE=$ORIENTDB_HOME/config/orientdb-server-log.properties
 WWW_PATH=$ORIENTDB_HOME/www
-JAVA_OPTS_SCRIPT="-Djna.nosys=true -XX:+HeapDumpOnOutOfMemoryError -XX:MaxDirectMemorySize=512g -Djava.awt.headless=true -Dfile.encoding=UTF8 -Drhino.opt.level=9"
 ORIENTDB_PID=$ORIENTDB_HOME/bin/orient.pid
 
 if [ -f "$ORIENTDB_PID" ]; then
