a358c1888d178e6582b96f8743a0b7b34f1f5ca9
lvca <l.garulli@gmail.com>
2016-08-27 19:29:12 -0500

    Removed % from non accepted character for classes and properties and fixed encoding problem with Graph API
    Fixed issue #5424

--- core/src/main/java/com/orientechnologies/orient/core/metadata/schema/OSchemaShared.java
+++ core/src/main/java/com/orientechnologies/orient/core/metadata/schema/OSchemaShared.java
@@ -143,7 +143,7 @@ public class OSchemaShared extends ODocumentWrapperNoClass implements OSchema, O
 
     for (int i = 0; i < nameSize; ++i) {
       final char c = iName.charAt(i);
-      if (c == ':' || c == ',' || c == ';' || c == ' ' || c == '%' || c == '=')
+      if (c == ':' || c == ',' || c == ';' || c == ' ' || c == '=')
         // INVALID CHARACTER
         return c;
     }
