b00dce63969d48c83b28c0d4c3cc6cdb1d10c5e6
lvca <l.garulli@gmail.com>
2016-09-01 08:28:33 -0500

    Fixed compilation problem after manual merge

--- distributed/src/main/java/com/orientechnologies/orient/server/hazelcast/OHazelcastPlugin.java
+++ distributed/src/main/java/com/orientechnologies/orient/server/hazelcast/OHazelcastPlugin.java
@@ -43,11 +43,7 @@ import com.orientechnologies.orient.server.OServer;
 import com.orientechnologies.orient.server.config.OServerParameterConfiguration;
 import com.orientechnologies.orient.server.distributed.*;
 import com.orientechnologies.orient.server.distributed.ODistributedServerLog.DIRECTION;
-import com.orientechnologies.orient.server.distributed.impl.OClusterHealthChecker;
-import com.orientechnologies.orient.server.distributed.impl.ODistributedAbstractPlugin;
-import com.orientechnologies.orient.server.distributed.impl.ODistributedDatabaseImpl;
-import com.orientechnologies.orient.server.distributed.impl.ODistributedMessageServiceImpl;
-import com.orientechnologies.orient.server.distributed.impl.ODistributedStorage;
+import com.orientechnologies.orient.server.distributed.impl.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -79,11 +75,6 @@ public class OHazelcastPlugin extends ODistributedAbstractPlugin implements Memb
   protected OHazelcastDistributedMap   configurationMap;
 
   public OHazelcastPlugin() {
-  }
-
-  // Must be set before startup() is called.
-  public void setHazelcastConfig(final Config config) {
-    hazelcastConfig = config;
   }
 
   // Must be set before config() is called.
