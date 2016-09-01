171e8eabba6289036c0aac7a9cbeceaef7f9f2d7
lvca <l.garulli@gmail.com>
2016-08-31 20:05:17 -0500

    Stresstest: Fixed issue on vertex reload

--- graphdb/src/main/java/com/orientechnologies/orient/graph/stresstest/OGraphInsertWorkload.java
+++ graphdb/src/main/java/com/orientechnologies/orient/graph/stresstest/OGraphInsertWorkload.java
@@ -148,8 +148,11 @@ public class OGraphInsertWorkload extends OBaseGraphWorkload {
 
             lastVertex = ((OWorkLoadContext) context).lastVertexToConnect;
           } catch (ONeedRetryException e) {
-            lastVertex.reload();
-            ((OWorkLoadContext) context).lastVertexToConnect.reload();
+            if (lastVertex.getIdentity().isPersistent())
+              lastVertex.reload();
+
+            if (((OWorkLoadContext) context).lastVertexToConnect.getIdentity().isPersistent())
+              ((OWorkLoadContext) context).lastVertexToConnect.reload();
           }
       }
     } finally {
@@ -158,7 +161,8 @@ public class OGraphInsertWorkload extends OBaseGraphWorkload {
   }
 
   protected void manageNeedRetryException(OBaseWorkLoadContext context, ONeedRetryException e) {
-    ((OWorkLoadContext) context).lastVertexToConnect.reload();
+    if (((OWorkLoadContext) context).lastVertexToConnect.getIdentity().isPersistent())
+      ((OWorkLoadContext) context).lastVertexToConnect.reload();
   }
 
   @Override
