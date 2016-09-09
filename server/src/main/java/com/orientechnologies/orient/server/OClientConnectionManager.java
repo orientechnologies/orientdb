b9a4cbd3478de70cd776255cbdb622b99992d827
Tglman <tglman@tglman.com>
2016-09-09 11:38:44 +0100

    removed troubling disconnect from distributed configuration push

--- server/src/main/java/com/orientechnologies/orient/server/OClientConnectionManager.java
+++ server/src/main/java/com/orientechnologies/orient/server/OClientConnectionManager.java
@@ -407,11 +407,8 @@ public class OClientConnectionManager {
           OLogManager.instance().info(this, "Timeout on sending updated cluster configuration to the remote client %s",
               c.getRemoteAddress());
         }
-      } catch (IOException e) {
-        disconnect(c);
       } catch (Exception e) {
         OLogManager.instance().warn(this, "Cannot push cluster configuration to the client %s", e, c.getRemoteAddress());
-        disconnect(c);
       }
     }
   }
