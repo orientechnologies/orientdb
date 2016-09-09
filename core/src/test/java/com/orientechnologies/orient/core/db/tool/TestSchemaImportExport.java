fbe1d1ec3e4159ecb72938ab806a2bbd7c3b29e0
Tglman <tglman@tglman.com>
2016-09-09 18:18:01 +0100

    add test for export import of default values, issue #6514

--- core/src/test/java/com/orientechnologies/orient/core/db/tool/TestSchemaImportExport.java
+++ core/src/test/java/com/orientechnologies/orient/core/db/tool/TestSchemaImportExport.java
@@ -4,8 +4,9 @@ import com.orientechnologies.orient.core.command.OCommandOutputListener;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
+import com.orientechnologies.orient.core.metadata.schema.OProperty;
+import com.orientechnologies.orient.core.metadata.schema.OType;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -14,7 +15,6 @@ import java.io.IOException;
 public class TestSchemaImportExport {
 
   @Test
-  @Ignore
   public void testExportImportCustomData() throws IOException {
     ODatabaseDocumentTx db = new ODatabaseDocumentTx("memory:" + TestSchemaImportExport.class.getSimpleName());
     db.create();
@@ -38,6 +38,37 @@ public class TestSchemaImportExport {
       OClass clas1 = db1.getMetadata().getSchema().getClass("Test");
       Assert.assertNotNull(clas1);
       Assert.assertEquals(clas1.getCustom("testcustom"), "test");
+    } finally {
+      db1.drop();
+    }
+  }
+
+  @Test
+  public void testExportImportDefaultValue() throws IOException {
+    ODatabaseDocumentTx db = new ODatabaseDocumentTx("memory:" + TestSchemaImportExport.class.getSimpleName());
+    db.create();
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    try {
+      OClass clazz = db.getMetadata().getSchema().createClass("Test");
+      clazz.createProperty("bla", OType.STRING).setDefaultValue("something");
+      ODatabaseExport exp = new ODatabaseExport(db, output, new MockOutputListener());
+      exp.exportDatabase();
+    } finally {
+      db.drop();
+    }
+
+    ODatabaseDocumentTx db1 = new ODatabaseDocumentTx("memory:imp_" + TestSchemaImportExport.class.getSimpleName());
+    db1.create();
+    try {
+      ODatabaseImport imp = new ODatabaseImport(db1, new ByteArrayInputStream(output.toByteArray()), new MockOutputListener());
+      imp.importDatabase();
+      db1.close();
+      db1.open("admin", "admin");
+      OClass clas1 = db1.getMetadata().getSchema().getClass("Test");
+      Assert.assertNotNull(clas1);
+      OProperty prop1 = clas1.getProperty("bla");
+      Assert.assertNotNull(prop1);
+      Assert.assertEquals(prop1.getDefaultValue(), "something");
     } finally {
       db1.drop();
     }
