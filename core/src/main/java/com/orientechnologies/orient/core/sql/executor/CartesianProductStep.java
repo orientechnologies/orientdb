8bfc7a287921141132de70b78d77107304fe873a
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2016-10-15 13:07:33 +0200

    Implement prettyPrint() of Cartesian Product execution step

--- core/src/main/java/com/orientechnologies/orient/core/sql/executor/CartesianProductStep.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/executor/CartesianProductStep.java
@@ -3,6 +3,7 @@ package com.orientechnologies.orient.core.sql.executor;
 import com.orientechnologies.common.concur.OTimeoutException;
 import com.orientechnologies.orient.core.command.OCommandContext;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,13 +11,14 @@ import java.util.List;
  */
 public class CartesianProductStep extends AbstractExecutionStep {
 
-  private List<OExecutionPlan> subPlans;
+  private List<OInternalExecutionPlan> subPlans = new ArrayList<>();
 
   public CartesianProductStep(OCommandContext ctx) {
     super(ctx);
   }
 
   @Override public OTodoResultSet syncPull(OCommandContext ctx, int nRecords) throws OTimeoutException {
+//    return new OInternalResultSet();
     throw new UnsupportedOperationException("cartesian product is not yet implemented in MATCH statement");
     //TODO
   }
@@ -29,7 +31,120 @@ public class CartesianProductStep extends AbstractExecutionStep {
 
   }
 
-  public void addSubPlan(OExecutionPlan subPlan) {
+  public void addSubPlan(OInternalExecutionPlan subPlan) {
     this.subPlans.add(subPlan);
+  }
+
+  @Override public String prettyPrint(int depth, int indent) {
+    String result = "";
+    String ind = OExecutionStepInternal.getIndent(depth, indent);
+
+    int[] blockSizes = new int[subPlans.size()];
+
+    for (int i = 0; i < subPlans.size(); i++) {
+      OInternalExecutionPlan currentPlan = subPlans.get(subPlans.size() - 1 - i);
+      String partial = currentPlan.prettyPrint(0, indent);
+
+      String[] partials = partial.split("\n");
+      blockSizes[subPlans.size() - 1 - i] = partials.length + 2;
+      result = "+-------------------------\n" + result;
+      for (int j = 0; j < partials.length; j++) {
+        String p = partials[partials.length - 1 - j];
+        if (result.length() > 0) {
+          result = appendPipe(p) + "\n" + result;
+        } else {
+          result = appendPipe(p);
+        }
+      }
+      result = "+-------------------------\n" + result;
+    }
+    result = addArrows(result, blockSizes);
+    result += foot(blockSizes);
+    result = ind + result;
+    result = result.replaceAll("\n", "\n" + ind);
+    result = head(depth, indent, subPlans.size()) + "\n" + result;
+    return result;
+  }
+
+  private String addArrows(String input, int[] blockSizes) {
+    String result = "";
+    String[] rows = input.split("\n");
+    int rowNum = 0;
+    for (int block = 0; block < blockSizes.length; block++) {
+      int blockSize = blockSizes[block];
+      for (int subRow = 0; subRow < blockSize; subRow++) {
+        for (int col = 0; col < blockSizes.length * 3; col++) {
+          if (isHorizontalRow(col, subRow, block, blockSize)) {
+            result += "-";
+          } else if (isPlus(col, subRow, block, blockSize)) {
+            result += "+";
+          } else if (isVerticalRow(col, subRow, block, blockSize)) {
+            result += "|";
+          } else {
+            result += " ";
+          }
+        }
+        result += rows[rowNum] + "\n";
+        rowNum++;
+      }
+    }
+
+    return result;
+  }
+
+  private boolean isHorizontalRow(int col, int subRow, int block, int blockSize) {
+    if (col < block * 3 + 2) {
+      return false;
+    }
+    if (subRow == blockSize / 2) {
+      return true;
+    }
+    return false;
+  }
+
+  private boolean isPlus(int col, int subRow, int block, int blockSize) {
+    if (col == block * 3 + 1) {
+      if (subRow == blockSize / 2) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean isVerticalRow(int col, int subRow, int block, int blockSize) {
+    if (col == block * 3 + 1) {
+      if (subRow > blockSize / 2) {
+        return true;
+      }
+    } else if (col < block * 3 + 1 && col % 3 == 1) {
+      return true;
+    }
+
+    return false;
+  }
+
+  private String head(int depth, int indent, int nItems) {
+    String ind = OExecutionStepInternal.getIndent(depth, indent);
+    return ind + "+ CARTESIAN PRODUCT";
+  }
+
+  private String foot(int[] blockSizes) {
+    String result = "";
+    for (int i = 0; i < blockSizes.length; i++) {
+      result += " V ";//TODO
+    }
+    return result;
+  }
+
+  private String spaces(int num) {
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < num; i++) {
+      result.append(" ");
+    }
+    return result.toString();
+  }
+
+  private String appendPipe(String p) {
+    return "| " + p;
   }
 }
