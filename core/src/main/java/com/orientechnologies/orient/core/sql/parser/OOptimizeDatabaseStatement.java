e891270949409c9f5f7bc8a70597fb0abb107db4
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2016-10-17 16:15:40 +0200

    Implement OPTIMIZE DATABASE with the new executor

--- core/src/main/java/com/orientechnologies/orient/core/sql/parser/OOptimizeDatabaseStatement.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/parser/OOptimizeDatabaseStatement.java
@@ -2,14 +2,28 @@
 /* JavaCCOptions:MULTI=true,NODE_USES_PARSER=false,VISITOR=true,TRACK_TOKENS=true,NODE_PREFIX=O,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 package com.orientechnologies.orient.core.sql.parser;
 
+import com.orientechnologies.common.log.OLogManager;
+import com.orientechnologies.orient.core.command.OCommandContext;
+import com.orientechnologies.orient.core.db.ODatabaseDocumentInternal;
+import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.core.db.record.ridbag.ORidBag;
+import com.orientechnologies.orient.core.id.ORID;
+import com.orientechnologies.orient.core.intent.OIntentMassiveInsert;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.executor.OInternalResultSet;
+import com.orientechnologies.orient.core.sql.executor.OResultInternal;
+import com.orientechnologies.orient.core.sql.executor.OTodoResultSet;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class OOptimizeDatabaseStatement extends OStatement {
+public class OOptimizeDatabaseStatement extends OSimpleExecStatement {
 
   protected List<OCommandLineOption> options = new ArrayList<OCommandLineOption>();
+  private   int                      batch   = 1000;
 
   public OOptimizeDatabaseStatement(int id) {
     super(id);
@@ -17,6 +31,20 @@ public class OOptimizeDatabaseStatement extends OStatement {
 
   public OOptimizeDatabaseStatement(OrientSql p, int id) {
     super(p, id);
+  }
+
+  @Override public OTodoResultSet executeSimple(OCommandContext ctx) {
+    OResultInternal result = new OResultInternal();
+    result.setProperty("operation", "optimize databae");
+
+    if (isOptimizeEdges()) {
+      String edges = optimizeEdges();
+      result.setProperty("optimizeEdges", edges);
+    }
+
+    OInternalResultSet rs = new OInternalResultSet();
+    rs.add(result);
+    return rs;
   }
 
   @Override public void toString(Map<Object, Object> params, StringBuilder builder) {
@@ -31,6 +59,126 @@ public class OOptimizeDatabaseStatement extends OStatement {
     OOptimizeDatabaseStatement result = new OOptimizeDatabaseStatement(-1);
     result.options = options == null ? null : options.stream().map(OCommandLineOption::copy).collect(Collectors.toList());
     return result;
+  }
+
+  private String optimizeEdges() {
+    final ODatabaseDocumentInternal db = getDatabase();
+
+    db.declareIntent(new OIntentMassiveInsert());
+    try {
+      long transformed = 0;
+      if (db.getTransaction().isActive())
+        db.commit();
+
+      db.begin();
+
+      try {
+
+        final long totalEdges = db.countClass("E");
+        long browsedEdges = 0;
+        long lastLapBrowsed = 0;
+        long lastLapTime = System.currentTimeMillis();
+
+        for (ODocument doc : db.browseClass("E")) {
+          if (Thread.currentThread().isInterrupted())
+            break;
+
+          browsedEdges++;
+
+          if (doc != null) {
+            if (doc.fields() == 2) {
+              final ORID edgeIdentity = doc.getIdentity();
+
+              final ODocument outV = doc.field("out");
+              final ODocument inV = doc.field("in");
+
+              // OUTGOING
+              final Object outField = outV.field("out_" + doc.getClassName());
+              if (outField instanceof ORidBag) {
+                final Iterator<OIdentifiable> it = ((ORidBag) outField).iterator();
+                while (it.hasNext()) {
+                  OIdentifiable v = it.next();
+                  if (edgeIdentity.equals(v)) {
+                    // REPLACE EDGE RID WITH IN-VERTEX RID
+                    it.remove();
+                    ((ORidBag) outField).add(inV.getIdentity());
+                    break;
+                  }
+                }
+              }
+
+              outV.save();
+
+              // INCOMING
+              final Object inField = inV.field("in_" + doc.getClassName());
+              if (outField instanceof ORidBag) {
+                final Iterator<OIdentifiable> it = ((ORidBag) inField).iterator();
+                while (it.hasNext()) {
+                  OIdentifiable v = it.next();
+                  if (edgeIdentity.equals(v)) {
+                    // REPLACE EDGE RID WITH IN-VERTEX RID
+                    it.remove();
+                    ((ORidBag) inField).add(outV.getIdentity());
+                    break;
+                  }
+                }
+              }
+
+              inV.save();
+
+              doc.delete();
+
+              if (++transformed % batch == 0) {
+                db.commit();
+                db.begin();
+              }
+
+              final long now = System.currentTimeMillis();
+
+              if (verbose() && (now - lastLapTime > 2000)) {
+                final long elapsed = now - lastLapTime;
+
+                OLogManager.instance()
+                    .info(this, "Browsed %,d of %,d edges, transformed %,d so far (%,d edges/sec)", browsedEdges, totalEdges,
+                        transformed, (((browsedEdges - lastLapBrowsed) * 1000 / elapsed)));
+
+                lastLapTime = System.currentTimeMillis();
+                lastLapBrowsed = browsedEdges;
+              }
+            }
+          }
+        }
+
+        // LAST COMMIT
+        db.commit();
+
+      } finally {
+        if (db.getTransaction().isActive())
+          db.rollback();
+      }
+      return "Transformed " + transformed + " regular edges in lightweight edges";
+
+    } finally {
+      db.declareIntent(null);
+    }
+  }
+
+  private boolean isOptimizeEdges() {
+    for (OCommandLineOption option : options) {
+      if (option.name.getStringValue().equalsIgnoreCase("LWEDGES")) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean verbose() {
+    for (OCommandLineOption option : options) {
+      if (option.name.getStringValue().equalsIgnoreCase("NOVERBOSE")) {
+        return false;
+      }
+    }
+    return true;
   }
 
   @Override public boolean equals(Object o) {
