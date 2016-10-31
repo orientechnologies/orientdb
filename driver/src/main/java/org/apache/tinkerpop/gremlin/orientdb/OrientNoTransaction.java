b8121d56a342fb63b1a43016d11a1cde19bed500
Marvin Froeder <marvin.froeder@vizexplorer.com>
2016-10-31 21:24:04 +1300

    Code format

--- driver/src/main/java/org/apache/tinkerpop/gremlin/orientdb/OrientNoTransaction.java
+++ driver/src/main/java/org/apache/tinkerpop/gremlin/orientdb/OrientNoTransaction.java
@@ -2,28 +2,28 @@ package org.apache.tinkerpop.gremlin.orientdb;
 
 public class OrientNoTransaction extends OrientTransaction {
 
-	public OrientNoTransaction(OrientGraph g) {
-		super(g);
-	}
+    public OrientNoTransaction(OrientGraph g) {
+        super(g);
+    }
 
-	@Override
-	public boolean isOpen() {
-		return false;
-	}
+    @Override
+    public boolean isOpen() {
+        return false;
+    }
 
-	@Override
-	protected void doOpen() {
-		// do nothing, no transaction
-	}
+    @Override
+    protected void doOpen() {
+        // do nothing, no transaction
+    }
 
-	@Override
-	protected void doCommit() throws TransactionException {
-		// do nothing, no transaction
-	}
+    @Override
+    protected void doCommit() throws TransactionException {
+        // do nothing, no transaction
+    }
 
-	@Override
-	protected void doRollback() throws TransactionException {
-		// do nothing, no transaction
-	}
+    @Override
+    protected void doRollback() throws TransactionException {
+        // do nothing, no transaction
+    }
 
 }
--- driver/src/test/java/org/apache/tinkerpop/gremlin/orientdb/TransactionIdsTest.java
+++ driver/src/test/java/org/apache/tinkerpop/gremlin/orientdb/TransactionIdsTest.java
@@ -7,41 +7,41 @@ import org.junit.Test;
 
 public class TransactionIdsTest {
 
-	@Test
-	public void withTransaction() {
-		final String labelVertex = "VertexLabel";
-		OrientGraph graph = new OrientGraphFactory("memory:myGraph").getTx();
-		Vertex v1 = graph.addVertex(labelVertex);
-		graph.tx().commit();
+    @Test
+    public void withTransaction() {
+        final String labelVertex = "VertexLabel";
+        OrientGraph graph = new OrientGraphFactory("memory:myGraph").getTx();
+        Vertex v1 = graph.addVertex(labelVertex);
+        graph.tx().commit();
 
-		v1 = graph.traversal().V().next();
+        v1 = graph.traversal().V().next();
 
-		Vertex v2 = graph.addVertex(labelVertex);
-		graph.tx().commit();
+        Vertex v2 = graph.addVertex(labelVertex);
+        graph.tx().commit();
 
-		GraphTraversal<Vertex, Edge> traversal = graph.traversal().V(v2.id()).outE().as("edge").otherV().hasId(v2)
-				.select("edge");
-		traversal.hasNext();
+        GraphTraversal<Vertex, Edge> traversal = graph.traversal().V(v2.id()).outE().as("edge").otherV().hasId(v2)
+                .select("edge");
+        traversal.hasNext();
 
-		graph.close();
-	}
+        graph.close();
+    }
 
-	@Test
-	public void withoutTransaction() {
-		final String labelVertex = "VertexLabel";
-		OrientGraph graph = new OrientGraphFactory("memory:myGraph").getNoTx();
-		Vertex v1 = graph.addVertex(labelVertex);
-		graph.tx().commit();
+    @Test
+    public void withoutTransaction() {
+        final String labelVertex = "VertexLabel";
+        OrientGraph graph = new OrientGraphFactory("memory:myGraph").getNoTx();
+        Vertex v1 = graph.addVertex(labelVertex);
+        graph.tx().commit();
 
-		v1 = graph.traversal().V().next();
+        v1 = graph.traversal().V().next();
 
-		Vertex v2 = graph.addVertex(labelVertex);
+        Vertex v2 = graph.addVertex(labelVertex);
 
-		GraphTraversal<Vertex, Edge> traversal = graph.traversal().V(v2.id()).outE().as("edge").otherV().hasId(v2)
-				.select("edge");
-		traversal.hasNext();
+        GraphTraversal<Vertex, Edge> traversal = graph.traversal().V(v2.id()).outE().as("edge").otherV().hasId(v2)
+                .select("edge");
+        traversal.hasNext();
 
-		graph.close();
-	}
+        graph.close();
+    }
 
 }
