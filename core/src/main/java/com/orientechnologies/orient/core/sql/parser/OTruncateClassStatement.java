47904e5a103aa73e47b933ccc25704d143353b9a
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2016-11-03 11:14:38 +0100

    Implement TRUNCATE CLASS statement in new SQL executor

--- core/src/main/java/com/orientechnologies/orient/core/sql/parser/OTruncateClassStatement.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/parser/OTruncateClassStatement.java
@@ -2,9 +2,22 @@
 /* JavaCCOptions:MULTI=true,NODE_USES_PARSER=false,VISITOR=true,TRACK_TOKENS=true,NODE_PREFIX=O,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 package com.orientechnologies.orient.core.sql.parser;
 
+import com.orientechnologies.common.exception.OException;
+import com.orientechnologies.orient.core.cache.OCommandCache;
+import com.orientechnologies.orient.core.command.OCommandContext;
+import com.orientechnologies.orient.core.db.ODatabase;
+import com.orientechnologies.orient.core.exception.OCommandExecutionException;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OSchema;
+import com.orientechnologies.orient.core.sql.executor.OInternalResultSet;
+import com.orientechnologies.orient.core.sql.executor.OResultInternal;
+import com.orientechnologies.orient.core.sql.executor.OTodoResultSet;
+
+import java.io.IOException;
+import java.util.Collection;
 import java.util.Map;
 
-public class OTruncateClassStatement extends OStatement {
+public class OTruncateClassStatement extends ODDLStatement {
 
   protected OIdentifier className;
   protected boolean polymorphic = false;
@@ -16,6 +29,88 @@ public class OTruncateClassStatement extends OStatement {
 
   public OTruncateClassStatement(OrientSql p, int id) {
     super(p, id);
+  }
+
+  @Override public OTodoResultSet executeDDL(OCommandContext ctx) {
+    ODatabase db = ctx.getDatabase();
+    OSchema schema = db.getMetadata().getSchema();
+    OClass clazz = schema.getClass(className.getStringValue());
+    if (clazz == null) {
+      throw new OCommandExecutionException("Schema Class not found: " + className);
+    }
+
+    final long recs = clazz.count(polymorphic);
+    if (recs > 0 && !unsafe) {
+      if (clazz.isSubClassOf("V")) {
+        throw new OCommandExecutionException(
+            "'TRUNCATE CLASS' command cannot be used on not empty vertex classes. Apply the 'UNSAFE' keyword to force it (at your own risk)");
+      } else if (clazz.isSubClassOf("E")) {
+        throw new OCommandExecutionException(
+            "'TRUNCATE CLASS' command cannot be used on not empty edge classes. Apply the 'UNSAFE' keyword to force it (at your own risk)");
+      }
+    }
+
+
+    OInternalResultSet rs = new OInternalResultSet();
+    Collection<OClass> subclasses = clazz.getAllSubclasses();
+    if (polymorphic && !unsafe) {// for multiple inheritance
+      for (OClass subclass : subclasses) {
+        long subclassRecs = clazz.count();
+        if (subclassRecs > 0) {
+          if (subclass.isSubClassOf("V")) {
+            throw new OCommandExecutionException(
+                "'TRUNCATE CLASS' command cannot be used on not empty vertex classes (" + subclass.getName()
+                    + "). Apply the 'UNSAFE' keyword to force it (at your own risk)");
+          } else if (subclass.isSubClassOf("E")) {
+            throw new OCommandExecutionException(
+                "'TRUNCATE CLASS' command cannot be used on not empty edge classes (" + subclass.getName()
+                    + "). Apply the 'UNSAFE' keyword to force it (at your own risk)");
+          }
+        }
+      }
+    }
+
+    try {
+      clazz.truncate();
+      OResultInternal result = new OResultInternal();
+      result.setProperty("operation", "drop class");
+      result.setProperty("className", className.getStringValue());
+      rs.add(result);
+      invalidateCommandCache(clazz, db);
+      if (polymorphic) {
+        for (OClass subclass : subclasses) {
+          subclass.truncate();
+          result = new OResultInternal();
+          result.setProperty("operation", "drop class");
+          result.setProperty("className", className.getStringValue());
+          rs.add(result);
+          invalidateCommandCache(subclass, db);
+        }
+      }
+    } catch (IOException e) {
+      throw OException.wrapException(new OCommandExecutionException("Error on executing command"), e);
+    }
+
+
+    return rs;
+  }
+
+  private void invalidateCommandCache(OClass clazz, ODatabase db) {
+    if (clazz == null) {
+      return;
+    }
+    OCommandCache commandCache = db.getMetadata().getCommandCache();
+    if (commandCache != null && commandCache.isEnabled()) {
+      int[] clusterIds = clazz.getClusterIds();
+      if (clusterIds != null) {
+        for (int i : clusterIds) {
+          String clusterName = getDatabase().getClusterNameById(i);
+          if (clusterName != null) {
+            commandCache.invalidateResultsOfCluster(clusterName);
+          }
+        }
+      }
+    }
   }
 
   /**
---
+++ core/src/test/java/com/orientechnologies/orient/core/sql/executor/OTruncateClassStatementExecutionTest.java
@@ -0,0 +1,208 @@
+package com.orientechnologies.orient.core.sql.executor;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.core.index.OIndex;
+import com.orientechnologies.orient.core.index.OIndexCursor;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.metadata.schema.OSchema;
+import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.OCommandSQL;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @author Luigi Dell'Aquila (l.dellaquila-(at)-orientdb.com)
+ */
+public class OTruncateClassStatementExecutionTest {
+  static ODatabaseDocument database;
+
+  @BeforeClass public static void beforeClass() {
+    database = new ODatabaseDocumentTx("memory:OTruncateClassStatementExecutionTest");
+    database.create();
+  }
+
+  @AfterClass public static void afterClass() {
+    database.close();
+  }
+
+  @SuppressWarnings("unchecked") @Test public void testTruncateClass() {
+
+    OSchema schema = database.getMetadata().getSchema();
+    OClass testClass = getOrCreateClass(schema);
+
+    final OIndex<?> index = getOrCreateIndex(testClass);
+    schema.save();
+
+    database.command("truncate class test_class");
+
+    database.save(new ODocument(testClass).field("name", "x").field("data", Arrays.asList(1, 2)));
+    database.save(new ODocument(testClass).field("name", "y").field("data", Arrays.asList(3, 0)));
+
+    database.command(new OCommandSQL("truncate class test_class")).execute();
+
+    database.save(new ODocument(testClass).field("name", "x").field("data", Arrays.asList(5, 6, 7)));
+    database.save(new ODocument(testClass).field("name", "y").field("data", Arrays.asList(8, 9, -1)));
+
+    OTodoResultSet result = database.query("select from test_class");
+    //    Assert.assertEquals(result.size(), 2);
+
+    Set<Integer> set = new HashSet<Integer>();
+    while (result.hasNext()) {
+      set.addAll((Collection<Integer>) result.next().getProperty("data"));
+    }
+    result.close();
+    Assert.assertTrue(set.containsAll(Arrays.asList(5, 6, 7, 8, 9, -1)));
+
+    Assert.assertEquals(index.getSize(), 6);
+
+    OIndexCursor cursor = index.cursor();
+    Map.Entry<Object, OIdentifiable> entry = cursor.nextEntry();
+    while (entry != null) {
+      Assert.assertTrue(set.contains((Integer) entry.getKey()));
+      entry = cursor.nextEntry();
+    }
+
+    schema.dropClass("test_class");
+  }
+
+  @Test public void testTruncateVertexClass() {
+    database.command("create class TestTruncateVertexClass extends V");
+    database.command("create vertex TestTruncateVertexClass set name = 'foo'");
+
+    try {
+      database.command("truncate class TestTruncateVertexClass");
+      Assert.fail();
+    } catch (Exception e) {
+    }
+    OTodoResultSet result = database.query("select from TestTruncateVertexClass");
+    Assert.assertTrue(result.hasNext());
+    result.close();
+
+    database.command("truncate class TestTruncateVertexClass unsafe");
+    result = database.query("select from TestTruncateVertexClass");
+    Assert.assertFalse(result.hasNext());
+    result.close();
+
+  }
+
+  @Test public void testTruncateVertexClassSubclasses() {
+
+    database.command("create class TestTruncateVertexClassSuperclass");
+    database.command("create class TestTruncateVertexClassSubclass extends TestTruncateVertexClassSuperclass");
+
+    database.command("insert into TestTruncateVertexClassSuperclass set name = 'foo'");
+    database.command("insert into TestTruncateVertexClassSubclass set name = 'bar'");
+
+    OTodoResultSet result = database.query("select from TestTruncateVertexClassSuperclass");
+    for (int i = 0; i < 2; i++) {
+      Assert.assertTrue(result.hasNext());
+      result.next();
+    }
+    Assert.assertFalse(result.hasNext());
+    result.close();
+
+    database.command("truncate class TestTruncateVertexClassSuperclass ");
+    result = database.query("select from TestTruncateVertexClassSubclass");
+    Assert.assertTrue(result.hasNext());
+    result.next();
+    Assert.assertFalse(result.hasNext());
+    result.close();
+
+    database.command("truncate class TestTruncateVertexClassSuperclass polymorphic");
+    result = database.query("select from TestTruncateVertexClassSubclass");
+    Assert.assertFalse(result.hasNext());
+    result.close();
+
+  }
+
+  @Test public void testTruncateVertexClassSubclassesWithIndex() {
+
+    database.command("create class TestTruncateVertexClassSuperclassWithIndex");
+    database.command("create property TestTruncateVertexClassSuperclassWithIndex.name STRING");
+    database.command(
+        "create index TestTruncateVertexClassSuperclassWithIndex_index on TestTruncateVertexClassSuperclassWithIndex (name) NOTUNIQUE");
+
+    database.command("create class TestTruncateVertexClassSubclassWithIndex extends TestTruncateVertexClassSuperclassWithIndex");
+
+    database.command("insert into TestTruncateVertexClassSuperclassWithIndex set name = 'foo'");
+    database.command("insert into TestTruncateVertexClassSubclassWithIndex set name = 'bar'");
+
+    OTodoResultSet result = database.query("select from index:TestTruncateVertexClassSuperclassWithIndex_index");
+    Assert.assertEquals(toList(result).size(), 2);
+    result.close();
+
+    database.command("truncate class TestTruncateVertexClassSubclassWithIndex");
+    result = database.query("select from index:TestTruncateVertexClassSuperclassWithIndex_index");
+    Assert.assertEquals(toList(result).size(), 1);
+    result.close();
+
+    database.command("truncate class TestTruncateVertexClassSuperclassWithIndex polymorphic");
+    result = database.query("select from index:TestTruncateVertexClassSuperclassWithIndex_index");
+    Assert.assertEquals(toList(result).size(), 0);
+    result.close();
+
+  }
+
+  private List<OResult> toList(OTodoResultSet input) {
+    List<OResult> result = new ArrayList<>();
+    while (input.hasNext()) {
+      result.add(input.next());
+    }
+    return result;
+  }
+
+  private OIndex<?> getOrCreateIndex(OClass testClass) {
+    OIndex<?> index = database.getMetadata().getIndexManager().getIndex("test_class_by_data");
+    if (index == null) {
+      testClass.createProperty("data", OType.EMBEDDEDLIST, OType.INTEGER);
+      index = testClass.createIndex("test_class_by_data", OClass.INDEX_TYPE.UNIQUE, "data");
+    }
+    return index;
+  }
+
+  private OClass getOrCreateClass(OSchema schema) {
+    OClass testClass;
+    if (schema.existsClass("test_class")) {
+      testClass = schema.getClass("test_class");
+    } else {
+      testClass = schema.createClass("test_class");
+    }
+    schema.save();
+    return testClass;
+  }
+
+  @SuppressWarnings("unchecked") @Test public void testTruncateClassWithCommandCache() {
+
+    OSchema schema = database.getMetadata().getSchema();
+    OClass testClass = getOrCreateClass(schema);
+
+    boolean ccWasEnabled = database.getMetadata().getCommandCache().isEnabled();
+    database.getMetadata().getCommandCache().enable();
+
+    database.command("truncate class test_class");
+
+    database.save(new ODocument(testClass).field("name", "x").field("data", Arrays.asList(1, 2)));
+    database.save(new ODocument(testClass).field("name", "y").field("data", Arrays.asList(3, 0)));
+
+    OTodoResultSet result = database.query("select from test_class");
+    Assert.assertEquals(toList(result).size(), 2);
+
+    database.command("truncate class test_class");
+
+    result = database.query("select from test_class");
+    Assert.assertEquals(toList(result).size(), 0);
+
+    schema.dropClass("test_class");
+    if (!ccWasEnabled) {
+      database.getMetadata().getCommandCache().disable();
+    }
+  }
+
+}
