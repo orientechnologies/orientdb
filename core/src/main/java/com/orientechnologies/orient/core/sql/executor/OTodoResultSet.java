b06cf394815caa045ea0b13dc5c933740fb80e3b
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2016-11-04 14:21:07 +0100

    Add stream APIs to ResultSet

--- core/src/main/java/com/orientechnologies/orient/core/sql/executor/OTodoResultSet.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/executor/OTodoResultSet.java
@@ -1,16 +1,30 @@
 package com.orientechnologies.orient.core.sql.executor;
 
+import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.core.record.OEdge;
+import com.orientechnologies.orient.core.record.OElement;
+import com.orientechnologies.orient.core.record.OVertex;
+
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * Created by luigidellaquila on 07/07/16.
  */
-public interface OTodoResultSet {
+public interface OTodoResultSet extends Spliterator<OResult>, Iterator<OResult> {
 
-  boolean hasNext();
+  @Override boolean hasNext();
 
-  OResult next();
+  @Override OResult next();
+
+  default void remove() {
+    throw new UnsupportedOperationException();
+  }
 
   void close();
 
@@ -20,5 +34,114 @@ public interface OTodoResultSet {
 
   default void reset() {
     throw new UnsupportedOperationException("Implement RESET on " + getClass().getSimpleName());
+  }
+
+  default boolean tryAdvance(Consumer<? super OResult> action) {
+    if (hasNext()) {
+      action.accept(next());
+      return true;
+    }
+    return false;
+  }
+
+  default void forEachRemaining(Consumer<? super OResult> action) {
+    Spliterator.super.forEachRemaining(action);
+  }
+
+  default OTodoResultSet trySplit() {
+    return null;
+  }
+
+  default long estimateSize() {
+    return Long.MAX_VALUE;
+  }
+
+  default int characteristics() {
+    return ORDERED;
+  }
+
+  default Stream<OResult> stream() {
+    return StreamSupport.stream(this, false);
+  }
+
+  default Stream<OElement> elementStream() {
+    return StreamSupport.stream(new Spliterator<OElement>() {
+      @Override public boolean tryAdvance(Consumer<? super OElement> action) {
+        while (hasNext()) {
+          OIdentifiable elem = next().getElement();
+          if (elem instanceof OElement) {
+            action.accept((OElement) elem);
+            return true;
+          }
+        }
+        return false;
+      }
+
+      @Override public Spliterator<OElement> trySplit() {
+        return null;
+      }
+
+      @Override public long estimateSize() {
+        return Long.MAX_VALUE;
+      }
+
+      @Override public int characteristics() {
+        return ORDERED;
+      }
+    }, false);
+  }
+
+  default Stream<OVertex> vertexStream() {
+    return StreamSupport.stream(new Spliterator<OVertex>() {
+      @Override public boolean tryAdvance(Consumer<? super OVertex> action) {
+        while (hasNext()) {
+          OElement elem = (OElement) next().getElement();
+          if (elem != null && elem.isVertex()) {
+            action.accept((OVertex) elem);
+            return true;
+          }
+        }
+        return false;
+      }
+
+      @Override public Spliterator<OVertex> trySplit() {
+        return null;
+      }
+
+      @Override public long estimateSize() {
+        return Long.MAX_VALUE;
+      }
+
+      @Override public int characteristics() {
+        return ORDERED;
+      }
+    }, false);
+  }
+
+  default Stream<OEdge> edgeStream() {
+    return StreamSupport.stream(new Spliterator<OEdge>() {
+      @Override public boolean tryAdvance(Consumer<? super OEdge> action) {
+        while (hasNext()) {
+          OElement elem = (OElement) next().getElement();
+          if (elem != null && elem.isEdge()) {
+            action.accept((OEdge) elem);
+            return true;
+          }
+        }
+        return false;
+      }
+
+      @Override public Spliterator<OEdge> trySplit() {
+        return null;
+      }
+
+      @Override public long estimateSize() {
+        return Long.MAX_VALUE;
+      }
+
+      @Override public int characteristics() {
+        return ORDERED;
+      }
+    }, false);
   }
 }
---
+++ core/src/test/java/com/orientechnologies/orient/core/sql/executor/OTodoResultSetTest.java
@@ -0,0 +1,48 @@
+package com.orientechnologies.orient.core.sql.executor;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Optional;
+
+/**
+ * Created by luigidellaquila on 04/11/16.
+ */
+public class OTodoResultSetTest {
+  @Test
+  public void testResultStream(){
+    OInternalResultSet rs = new OInternalResultSet();
+    for(int i=0;i<10;i++){
+      OResultInternal item = new OResultInternal();
+      item.setProperty("i", i);
+      rs.add(item);
+    }
+    Optional<Integer> result = rs.stream().map(x -> (int) x.getProperty("i")).reduce((a, b) -> a + b);
+    Assert.assertTrue(result.isPresent());
+    Assert.assertEquals(45, result.get().intValue());
+  }
+
+  @Test
+  public void testResultEmptyVertexStream(){
+    OInternalResultSet rs = new OInternalResultSet();
+    for(int i=0;i<10;i++){
+      OResultInternal item = new OResultInternal();
+      item.setProperty("i", i);
+      rs.add(item);
+    }
+    Optional<Integer> result = rs.vertexStream().map(x -> (int) x.getProperty("i")).reduce((a, b) -> a + b);
+    Assert.assertFalse(result.isPresent());
+  }
+
+  @Test
+  public void testResultEdgeVertexStream(){
+    OInternalResultSet rs = new OInternalResultSet();
+    for(int i=0;i<10;i++){
+      OResultInternal item = new OResultInternal();
+      item.setProperty("i", i);
+      rs.add(item);
+    }
+    Optional<Integer> result = rs.vertexStream().map(x -> (int) x.getProperty("i")).reduce((a, b) -> a + b);
+    Assert.assertFalse(result.isPresent());
+  }
+}
