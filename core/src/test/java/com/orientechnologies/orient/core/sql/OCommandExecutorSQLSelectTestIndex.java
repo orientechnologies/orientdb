43302f5c9af3bc3bdbbdd64d2007f8a2d71e2686
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2016-11-30 11:14:29 +0100

    Fix usage of indexes on [collection] CONTAINS <field>
    Resolves: #6953

--- core/src/test/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLSelectTestIndex.java
+++ core/src/test/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLSelectTestIndex.java
@@ -20,6 +20,7 @@ package com.orientechnologies.orient.core.sql;
 
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 import org.junit.Test;
 
 import java.util.List;
@@ -28,8 +29,7 @@ import static org.junit.Assert.assertEquals;
 
 public class OCommandExecutorSQLSelectTestIndex {
 
-  @Test
-  public void testIndexSqlEmbeddedList() {
+  @Test public void testIndexSqlEmbeddedList() {
 
     ODatabaseDocumentTx databaseDocumentTx = new ODatabaseDocumentTx("memory:embeddedList");
     databaseDocumentTx.create();
@@ -48,8 +48,7 @@ public class OCommandExecutorSQLSelectTestIndex {
     }
   }
 
-  @Test
-  public void testIndexOnHierarchyChange() {
+  @Test public void testIndexOnHierarchyChange() {
     //issue #5743
     ODatabaseDocumentTx databaseDocumentTx = new ODatabaseDocumentTx(
         "memory:OCommandExecutorSQLSelectTestIndex_testIndexOnHierarchyChange");
@@ -66,6 +65,35 @@ public class OCommandExecutorSQLSelectTestIndex {
 
       List<ODocument> results = databaseDocumentTx.command(new OCommandSQL("SELECT * FROM Derived WHERE uuid='abcdef'")).execute();
       assertEquals(results.size(), 1);
+
+    } finally {
+      databaseDocumentTx.drop();
+    }
+  }
+
+  @Test
+  public void testListContainsField() {
+    ODatabaseDocumentTx databaseDocumentTx = new ODatabaseDocumentTx(
+        "memory:OCommandExecutorSQLSelectTestIndex_testListContainsField");
+    databaseDocumentTx.create();
+    try {
+      databaseDocumentTx.command(new OCommandSQL("CREATE CLASS Foo")).execute();
+      databaseDocumentTx.command(new OCommandSQL("CREATE PROPERTY Foo.name String")).execute();
+      databaseDocumentTx.command(new OCommandSQL("INSERT INTO Foo SET name = 'foo'")).execute();
+
+      List<?> result = databaseDocumentTx.query(new OSQLSynchQuery<Object>("SELECT * FROM Foo WHERE ['foo', 'bar'] CONTAINS name"));
+      assertEquals(result.size(), 1);
+
+      result = databaseDocumentTx.query(new OSQLSynchQuery<Object>("SELECT * FROM Foo WHERE name IN ['foo', 'bar']"));
+      assertEquals(result.size(), 1);
+
+      databaseDocumentTx.command(new OCommandSQL("CREATE INDEX Foo.name UNIQUE_HASH_INDEX")).execute();
+
+      result = databaseDocumentTx.query(new OSQLSynchQuery<Object>("SELECT * FROM Foo WHERE ['foo', 'bar'] CONTAINS name"));
+      assertEquals(result.size(), 1);
+
+      result = databaseDocumentTx.query(new OSQLSynchQuery<Object>("SELECT * FROM Foo WHERE name IN ['foo', 'bar']"));
+      assertEquals(result.size(), 1);
 
     } finally {
       databaseDocumentTx.drop();
