3830680f26596900aa3a90eb51b1a07ca051b43d
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2016-12-29 15:33:38 +0100

    Fix ALTER SEQUENCE to automatically reset the sequence

--- core/src/main/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLAlterSequence.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLAlterSequence.java
@@ -15,73 +15,79 @@ import java.util.Map;
  * @since 3/5/2015
  */
 public class OCommandExecutorSQLAlterSequence extends OCommandExecutorSQLAbstract implements OCommandDistributedReplicateRequest {
-  public static final String KEYWORD_ALTER     = "ALTER";
-  public static final String KEYWORD_SEQUENCE  = "SEQUENCE";
-  public static final String KEYWORD_START     = "START";
-  public static final String KEYWORD_INCREMENT = "INCREMENT";
-  public static final String KEYWORD_CACHE     = "CACHE";
+    public static final String KEYWORD_ALTER = "ALTER";
+    public static final String KEYWORD_SEQUENCE = "SEQUENCE";
+    public static final String KEYWORD_START = "START";
+    public static final String KEYWORD_INCREMENT = "INCREMENT";
+    public static final String KEYWORD_CACHE = "CACHE";
 
-  private String                 sequenceName;
-  private OSequence.CreateParams params;
+    private String sequenceName;
+    private OSequence.CreateParams params;
 
-  @Override public OCommandExecutorSQLAlterSequence parse(OCommandRequest iRequest) {
-    final OCommandRequestText textRequest = (OCommandRequestText) iRequest;
+    @Override
+    public OCommandExecutorSQLAlterSequence parse(OCommandRequest iRequest) {
+        final OCommandRequestText textRequest = (OCommandRequestText) iRequest;
 
-    String queryText = textRequest.getText();
-    String originalQuery = queryText;
-    try {
-      queryText = preParse(queryText, iRequest);
-      textRequest.setText(queryText);
+        String queryText = textRequest.getText();
+        String originalQuery = queryText;
+        try {
+            queryText = preParse(queryText, iRequest);
+            textRequest.setText(queryText);
 
-      init((OCommandRequestText) iRequest);
+            init((OCommandRequestText) iRequest);
 
-      final ODatabaseDocumentInternal database = getDatabase();
-      final StringBuilder word = new StringBuilder();
+            final ODatabaseDocumentInternal database = getDatabase();
+            final StringBuilder word = new StringBuilder();
 
-      parserRequiredKeyword(KEYWORD_ALTER);
-      parserRequiredKeyword(KEYWORD_SEQUENCE);
-      this.sequenceName = parserRequiredWord(false, "Expected <sequence name>");
-      this.params = new OSequence.CreateParams();
+            parserRequiredKeyword(KEYWORD_ALTER);
+            parserRequiredKeyword(KEYWORD_SEQUENCE);
+            this.sequenceName = parserRequiredWord(false, "Expected <sequence name>");
+            this.params = new OSequence.CreateParams();
 
-      String temp;
-      while ((temp = parseOptionalWord(true)) != null) {
-        if (parserIsEnded()) {
-          break;
+            String temp;
+            while ((temp = parseOptionalWord(true)) != null) {
+                if (parserIsEnded()) {
+                    break;
+                }
+
+                if (temp.equals(KEYWORD_START)) {
+                    String startAsString = parserRequiredWord(true, "Expected <start value>");
+                    this.params.start = Long.parseLong(startAsString);
+                } else if (temp.equals(KEYWORD_INCREMENT)) {
+                    String incrementAsString = parserRequiredWord(true, "Expected <increment value>");
+                    this.params.increment = Integer.parseInt(incrementAsString);
+                } else if (temp.equals(KEYWORD_CACHE)) {
+                    String cacheAsString = parserRequiredWord(true, "Expected <cache value>");
+                    this.params.cacheSize = Integer.parseInt(cacheAsString);
+                }
+            }
+        } finally {
+            textRequest.setText(originalQuery);
         }
-
-        if (temp.equals(KEYWORD_START)) {
-          String startAsString = parserRequiredWord(true, "Expected <start value>");
-          this.params.start = Long.parseLong(startAsString);
-        } else if (temp.equals(KEYWORD_INCREMENT)) {
-          String incrementAsString = parserRequiredWord(true, "Expected <increment value>");
-          this.params.increment = Integer.parseInt(incrementAsString);
-        } else if (temp.equals(KEYWORD_CACHE)) {
-          String cacheAsString = parserRequiredWord(true, "Expected <cache value>");
-          this.params.cacheSize = Integer.parseInt(cacheAsString);
-        }
-      }
-    } finally {
-      textRequest.setText(originalQuery);
-    }
-    return this;
-  }
-
-  @Override public Object execute(Map<Object, Object> iArgs) {
-    if (this.sequenceName == null) {
-      throw new OCommandExecutionException("Cannot execute the command because it has not been parsed yet");
+        return this;
     }
 
-    final ODatabaseDocument database = getDatabase();
-    OSequence sequence = database.getMetadata().getSequenceLibrary().getSequence(this.sequenceName);
+    @Override
+    public Object execute(Map<Object, Object> iArgs) {
+        if (this.sequenceName == null) {
+            throw new OCommandExecutionException("Cannot execute the command because it has not been parsed yet");
+        }
 
-    return sequence.updateParams(this.params);
-  }
+        final ODatabaseDocument database = getDatabase();
+        OSequence sequence = database.getMetadata().getSequenceLibrary().getSequence(this.sequenceName);
 
-  @Override public String getSyntax() {
-    return "ALTER SEQUENCE <sequence> [START <value>] [INCREMENT <value>] [CACHE <value>]";
-  }
+        boolean result = sequence.updateParams(this.params);
+        sequence.reset();
+        return result;
+    }
 
-  @Override public QUORUM_TYPE getQuorumType() {
-    return QUORUM_TYPE.ALL;
-  }
+    @Override
+    public String getSyntax() {
+        return "ALTER SEQUENCE <sequence> [START <value>] [INCREMENT <value>] [CACHE <value>]";
+    }
+
+    @Override
+    public QUORUM_TYPE getQuorumType() {
+        return QUORUM_TYPE.ALL;
+    }
 }
