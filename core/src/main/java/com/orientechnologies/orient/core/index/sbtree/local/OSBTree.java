63d1448a5f306f73c308d8339ddc998794442f6b
Tglman <tglman@tglman.com>
2017-01-09 21:20:48 +0000

    fixed missing atomic operation count down in sbtree

--- core/src/main/java/com/orientechnologies/orient/core/index/sbtree/local/OSBTree.java
+++ core/src/main/java/com/orientechnologies/orient/core/index/sbtree/local/OSBTree.java
@@ -304,9 +304,10 @@ public class OSBTree<K, V> extends ODurableComponent {
             boolean doReleasePage = true;
             try {
               final Object result = validator.validate(key, oldValue, value);
-              if (result == OIndexEngine.Validator.IGNORE)
+              if (result == OIndexEngine.Validator.IGNORE) {
+                endAtomicOperation(false, null);
                 return false;
-              else
+              } else
                 doReleasePage = false;
 
               value = (V) result;
@@ -394,8 +395,10 @@ public class OSBTree<K, V> extends ODurableComponent {
               final V oldValueValue = oldValue == null ? null : readValue(oldValue, atomicOperation);
 
               final Object result = validator.validate(null, oldValueValue, value);
-              if (result == OIndexEngine.Validator.IGNORE)
+              if (result == OIndexEngine.Validator.IGNORE) {
+                endAtomicOperation(false, null);
                 return false;
+              }
 
               value = (V) result;
             }
