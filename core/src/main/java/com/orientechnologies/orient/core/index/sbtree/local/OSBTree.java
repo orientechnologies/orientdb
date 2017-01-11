1644964eb33c823396536648f7975571a5163332
Sergey Sitnikov <serge.sitnikov@gmail.com>
2017-01-11 15:44:35 +0700

    more cleaner atomic operation ending while ignoring validated sb-tree put

--- core/src/main/java/com/orientechnologies/orient/core/index/sbtree/local/OSBTree.java
+++ core/src/main/java/com/orientechnologies/orient/core/index/sbtree/local/OSBTree.java
@@ -862,26 +862,30 @@ public class OSBTree<K, V> extends ODurableComponent {
               getChanges(atomicOperation, keyBucketCacheEntry));
 
           if (validator != null) {
-            final V oldValue = bucketSearchResult.itemIndex > -1 ?
-                readValue(keyBucket.getValue(bucketSearchResult.itemIndex), atomicOperation) :
-                null;
+            boolean failure = true; // assuming validation throws by default
+            boolean ignored = false;
 
-            boolean doReleasePage = true;
             try {
+              final V oldValue = bucketSearchResult.itemIndex > -1 ?
+                  readValue(keyBucket.getValue(bucketSearchResult.itemIndex), atomicOperation) :
+                  null;
+
               final Object result = validator.validate(key, oldValue, value);
               if (result == OIndexEngine.Validator.IGNORE) {
-                endAtomicOperation(false, null);
+                ignored = true;
+                failure = false;
                 return false;
               }
-              else
-                doReleasePage = false;
 
               value = (V) result;
+              failure = false;
             } finally {
-              if (doReleasePage) {
+              if (failure || ignored) {
                 keyBucketCacheEntry.releaseExclusiveLock();
                 releasePage(atomicOperation, keyBucketCacheEntry);
               }
+              if (ignored) // in case of a failure atomic operation will be ended in a usual way below
+                endAtomicOperation(false, null);
             }
           }
 
@@ -953,6 +957,7 @@ public class OSBTree<K, V> extends ODurableComponent {
 
           int sizeDiff = 0;
 
+          boolean ignored = false;
           cacheEntry.acquireExclusiveLock();
           try {
             final ONullBucket<V> nullBucket = new ONullBucket<V>(cacheEntry, getChanges(atomicOperation, cacheEntry),
@@ -964,7 +969,7 @@ public class OSBTree<K, V> extends ODurableComponent {
 
               final Object result = validator.validate(null, oldValueValue, value);
               if (result == OIndexEngine.Validator.IGNORE) {
-                endAtomicOperation(false, null);
+                ignored = true;
                 return false;
               }
 
@@ -978,6 +983,8 @@ public class OSBTree<K, V> extends ODurableComponent {
           } finally {
             cacheEntry.releaseExclusiveLock();
             releasePage(atomicOperation, cacheEntry);
+            if (ignored)
+              endAtomicOperation(false, null);
           }
 
           sizeDiff++;
