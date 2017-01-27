2a1d6bae7567daf09e92e387980f4e8ce41f6c13
maggiolo00 <maggiolo00@gmail.com>
2017-01-27 18:56:48 +0100

    Added method registerResultTransformer

--- core/src/main/java/com/orientechnologies/orient/core/command/script/transformer/OScriptTransformer.java
+++ core/src/main/java/com/orientechnologies/orient/core/command/script/transformer/OScriptTransformer.java
@@ -1,5 +1,6 @@
 package com.orientechnologies.orient.core.command.script.transformer;
 
+import com.orientechnologies.orient.core.command.script.transformer.result.OResultTransformer;
 import com.orientechnologies.orient.core.sql.executor.OResult;
 import com.orientechnologies.orient.core.sql.executor.OResultSet;
 
@@ -12,4 +13,6 @@ public interface OScriptTransformer {
   OResult toResult(Object value);
 
   boolean doesHandleResult(Object value);
+
+  void registerResultTransformer(Class clazz, OResultTransformer resultTransformer);
 }
