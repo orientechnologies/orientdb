4b071564f308ae4ace1e079732e3fc2c5f7f85cc
Roberto Franchini <ro.franchini@gmail.com>
2017-01-27 16:24:14 +0100

    removes dependency and use of orientdb-grapg API (TP2) in favour of orientdb-core graph API -  all config files  and scripts are on distribution now -  console is OConsoleDatabaseApp
    refs https://github.com/orientechnologies/orientdb/issues/7117

---
+++ distribution/config/dserver.bat
@@ -0,0 +1,81 @@
+@echo off
+rem
+rem Copyright (c) Orient Technologies LTD (http://www.orientechnologies.com)
+rem
+
+echo            .
+echo           .`        `
+echo           ,      `:.
+echo          `,`    ,:`
+echo          .,.   :,,
+echo          .,,  ,,,
+echo     .    .,.:::::  ````                                 :::::::::     :::::::::
+echo     ,`   .::,,,,::.,,,,,,`;;                      .:    ::::::::::    :::    :::
+echo     `,.  ::,,,,,,,:.,,.`  `                       .:    :::      :::  :::     :::
+echo      ,,:,:,,,,,,,,::.   `        `         ``     .:    :::      :::  :::     :::
+echo       ,,:.,,,,,,,,,: `::, ,,   ::,::`   : :,::`  ::::   :::      :::  :::    :::
+echo        ,:,,,,,,,,,,::,:   ,,  :.    :   ::    :   .:    :::      :::  :::::::
+echo         :,,,,,,,,,,:,::   ,,  :      :  :     :   .:    :::      :::  :::::::::
+echo   `     :,,,,,,,,,,:,::,  ,, .::::::::  :     :   .:    :::      :::  :::     :::
+echo   `,...,,:,,,,,,,,,: .:,. ,, ,,         :     :   .:    :::      :::  :::     :::
+echo     .,,,,::,,,,,,,:  `: , ,,  :     `   :     :   .:    :::      :::  :::     :::
+echo       ...,::,,,,::.. `:  .,,  :,    :   :     :   .:    :::::::::::   :::     :::
+echo            ,::::,,,. `:   ,,   :::::    :     :   .:    :::::::::     ::::::::::
+echo            ,,:` `,,.
+echo           ,,,    .,`
+echo          ,,.     `,                                          GRAPH DATABASE
+echo        ``        `.
+echo                  ``                                         www.orientdb.org
+echo                  `
+
+rem Guess ORIENTDB_HOME if not defined
+set CURRENT_DIR=%cd%
+
+if exist "%JAVA_HOME%\bin\java.exe" goto setJavaHome
+set JAVA=java
+goto okJava
+
+:setJavaHome
+set JAVA="%JAVA_HOME%\bin\java"
+
+:okJava
+if not "%ORIENTDB_HOME%" == "" goto gotHome
+set ORIENTDB_HOME=%CURRENT_DIR%
+if exist "%ORIENTDB_HOME%\bin\server.bat" goto okHome
+cd ..
+set ORIENTDB_HOME=%cd%
+cd %CURRENT_DIR%
+
+:gotHome
+if exist "%ORIENTDB_HOME%\bin\server.bat" goto okHome
+echo The ORIENTDB_HOME environment variable is not defined correctly
+echo This environment variable is needed to run this program
+goto end
+
+:okHome
+rem Get remaining unshifted command line arguments and save them in the
+set CMD_LINE_ARGS=
+
+:setArgs
+if ""%1""=="""" goto doneSetArgs
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+shift
+goto setArgs
+
+:doneSetArgs
+
+if NOT exist "%CONFIG_FILE%" set CONFIG_FILE=%ORIENTDB_HOME%/config/orientdb-server-config.xml
+
+set LOG_FILE=%ORIENTDB_HOME%/config/orientdb-server-log.properties
+set WWW_PATH=%ORIENTDB_HOME%/www
+set ORIENTDB_SETTINGS=-Dprofiler.enabled=true
+set JAVA_OPTS_SCRIPT=-Xmx512m -Djna.nosys=true -XX:MaxDirectMemorySize=512g -XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true -Dfile.encoding=UTF8 -Drhino.opt.level=9 -Ddistributed=true
+
+rem ORIENTDB MAXIMUM HEAP. USE SYNTAX -Xmx<memory>, WHERE <memory> HAS THE TOTAL MEMORY AND SIZE UNIT. EXAMPLE: -Xmx512m
+set MAXHEAP=-Xmx512m
+rem ORIENTDB MAXIMUM DISKCACHE IN MB, EXAMPLE 8192 FOR 8GB
+set MAXDISKCACHE=
+
+call %JAVA% -server %JAVA_OPTS% %MAXHEAP% %JAVA_OPTS_SCRIPT% %ORIENTDB_SETTINGS% %MAXDISKCACHE% -Djava.util.logging.config.file="%LOG_FILE%" -Dorientdb.config.file="%CONFIG_FILE%" -Dorientdb.www.path="%WWW_PATH%" -Dorientdb.build.number="@BUILD@" -cp "%ORIENTDB_HOME%\lib\*;%ORIENTDB_HOME%\plugins\*" %CMD_LINE_ARGS% com.orientechnologies.orient.server.OServerMain
+
+:end
---
+++ distribution/script/shutdown.bat
@@ -0,0 +1,57 @@
+@echo off
+rem
+rem Copyright (c) Orient Technologies LTD (http://www.orientechnologies.com)
+rem
+rem HISTORY:
+rem 2012-07-31: Added -w option
+rem
+rem
+
+rem Guess ORIENTDB_HOME if not defined
+set CURRENT_DIR=%cd%
+
+if exist "%JAVA_HOME%\bin\java.exe" goto setJavaHome
+set JAVA=java
+goto okJava
+
+:setJavaHome
+set JAVA="%JAVA_HOME%\bin\java"
+
+:okJava
+if not "%ORIENTDB_HOME%" == "" goto gotHome
+set ORIENTDB_HOME=%CURRENT_DIR%
+if exist "%ORIENTDB_HOME%\bin\server.bat" goto okHome
+cd ..
+set ORIENTDB_HOME=%cd%
+cd %CURRENT_DIR%
+
+:gotHome
+if exist "%ORIENTDB_HOME%\bin\server.bat" goto okHome
+echo The ORIENTDB_HOME environment variable is not defined correctly
+echo This environment variable is needed to run this program
+goto end
+
+:okHome
+rem Get remaining unshifted command line arguments and save them in the
+set CMD_LINE_ARGS=
+
+:setArgs
+if ""%1""=="""" goto doneSetArgs
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+shift
+goto setArgs
+
+:doneSetArgs
+if NOT exist "%CONFIG_FILE%" set CONFIG_FILE=%ORIENTDB_HOME%/config/orientdb-server-config.xml
+
+set LOG_FILE=%ORIENTDB_HOME%/config/orientdb-server-log.properties
+set LOG_LEVEL=warning
+set WWW_PATH=%ORIENTDB_HOME%/www
+set JAVA_OPTS=-Djava.awt.headless=true
+
+rem It's better to set MaxDirectMemorySize, since no one sure OServerShutdownMain will refer inside it
+set ORIENTDB_SETTINGS=-XX:MaxDirectMemorySize=512g
+
+call %JAVA% -client %JAVA_OPTS% %ORIENTDB_SETTINGS% -Dorientdb.config.file="%CONFIG_FILE%" -cp "%ORIENTDB_HOME%\lib\orientdb-tools-@VERSION@.jar;%ORIENTDB_HOME%\lib\*" com.orientechnologies.orient.server.OServerShutdownMain %CMD_LINE_ARGS%
+
+:end
