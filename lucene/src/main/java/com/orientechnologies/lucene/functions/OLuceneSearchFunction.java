3432c514154b33f45897f7bb2e1661cdcc2e3a86
Roberto Franchini <ro.franchini@gmail.com>
2017-02-07 19:29:28 +0100

    adds SEARCH_INDEX function

--- lucene/src/main/java/com/orientechnologies/lucene/functions/OLuceneSearchFunction.java
+++ lucene/src/main/java/com/orientechnologies/lucene/functions/OLuceneSearchFunction.java
@@ -6,18 +6,22 @@ import com.orientechnologies.orient.core.db.ODatabaseDocumentInternal;
 import com.orientechnologies.orient.core.db.ODatabaseRecordThreadLocal;
 import com.orientechnologies.orient.core.db.record.OIdentifiable;
 import com.orientechnologies.orient.core.index.OIndex;
+import com.orientechnologies.orient.core.metadata.OMetadataInternal;
+import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.functions.OIndexableSQLFunction;
 import com.orientechnologies.orient.core.sql.functions.OSQLFunctionAbstract;
-import com.orientechnologies.orient.core.sql.parser.*;
+import com.orientechnologies.orient.core.sql.parser.OBinaryCompareOperator;
+import com.orientechnologies.orient.core.sql.parser.OExpression;
+import com.orientechnologies.orient.core.sql.parser.OFromClause;
 
 /**
  * Created by frank on 15/01/2017.
  */
 public class OLuceneSearchFunction extends OSQLFunctionAbstract implements OIndexableSQLFunction {
-  public static final String NAME = "LUCENE_SEARCH";
+  public static final String NAME = "SEARCH_INDEX";
 
   public OLuceneSearchFunction() {
-    super(NAME, 2, 2);
+    super(NAME, 2, 3);
   }
 
   @Override
@@ -27,14 +31,17 @@ public class OLuceneSearchFunction extends OSQLFunctionAbstract implements OInde
 
   protected OIndex searchForIndex(OFromClause target, OExpression[] args) {
 
-    // TODO Check if target is a class otherwise exception
+    // TODO:  Check if target is a class otherwise exception (seems useless)
+    OMetadataInternal metadata = getDb().getMetadata();
 
-    OFromItem item = target.getItem();
-    OIdentifier identifier = item.getIdentifier();
+//    OFromItem item = target.getItem();
+//    OIdentifier identifier = item.getIdentifier();
+//
+//    OClass aClass = metadata.getSchema().getClass(identifier.getValue());
+
     String indexName = args[0].toString();
 
-    System.out.println("indexName = " + indexName);
-    OIndex<?> index = getDb().getMetadata().getIndexManager().getIndex(indexName);
+    OIndex<?> index = metadata.getIndexManager().getIndex(indexName);
     return index;
   }
 
@@ -45,13 +52,13 @@ public class OLuceneSearchFunction extends OSQLFunctionAbstract implements OInde
   @Override
   public Object execute(Object iThis, OIdentifiable iCurrentRecord, Object iCurrentResult, Object[] iParams,
       OCommandContext iContext) {
-
-    return null;
+    System.out.println("execute::" + iCurrentRecord.getIdentity());
+    return true;
   }
 
   @Override
   public String getSyntax() {
-    return "SYNTAXT FOR LUCENE_SEARCH";
+    return "SEARCH_INDEX( indexName, [ metdatada {} ] )";
   }
 
   @Override
@@ -69,35 +76,50 @@ public class OLuceneSearchFunction extends OSQLFunctionAbstract implements OInde
     if (index != null) {
 
       OExpression exp = args[1];
-      System.out.println("exp = " + exp);
-      return (OLuceneResultSet) index.get(exp.toString());
+      System.out.println("query= " + exp);
+
+      if (args.length == 3) {
+        ODocument metadata = new ODocument().fromJSON(args[2].toString());
+      }
+      OLuceneResultSet luceneResultSet = (OLuceneResultSet) index.get(exp.toString());
+
+      System.out.println("luceneResultSet.size() = " + luceneResultSet.size());
+      return luceneResultSet;
     }
     return null;
+  }
+
+  @Override
+  public Object getResult() {
+    System.out.println("getResult");
+    return super.getResult();
   }
 
   @Override
   public long estimate(OFromClause target, OBinaryCompareOperator operator, Object rightValue, OCommandContext ctx,
       OExpression... args) {
     System.out.println("estimate");
-    System.out.println("target = " + target);
     return 0;
   }
 
   @Override
   public boolean canExecuteWithoutIndex(OFromClause target, OBinaryCompareOperator operator, Object rightValue, OCommandContext ctx,
       OExpression... args) {
+    System.out.println("canExecuteWithoutIndex");
     return true;
   }
 
   @Override
   public boolean allowsIndexedExecution(OFromClause target, OBinaryCompareOperator operator, Object rightValue, OCommandContext ctx,
       OExpression... args) {
+    System.out.println("allowsIndexedExecution:: " + true);
     return true;
   }
 
   @Override
   public boolean shouldExecuteAfterSearch(OFromClause target, OBinaryCompareOperator operator, Object rightValue,
       OCommandContext ctx, OExpression... args) {
-    return true;
+    System.out.println("shouldExecuteAfterSearch= " + target);
+    return false;
   }
 }
--- lucene/src/test/java/com/orientechnologies/lucene/functions/OLuceneSearchFunctionTest.java
+++ lucene/src/test/java/com/orientechnologies/lucene/functions/OLuceneSearchFunctionTest.java
@@ -2,23 +2,26 @@ package com.orientechnologies.lucene.functions;
 
 import com.orientechnologies.lucene.test.BaseLuceneTest;
 import com.orientechnologies.orient.core.command.script.OCommandScript;
-import com.orientechnologies.orient.core.db.ODatabase;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.OCommandSQL;
-import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
+import com.orientechnologies.orient.core.sql.executor.OResultSet;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.InputStream;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by frank on 15/01/2017.
  */
-public class OLuceneSearchFunctionTest extends BaseLuceneTest{
+public class OLuceneSearchFunctionTest extends BaseLuceneTest {
 
   @Test
-  public void name() throws Exception {
+  @Ignore
+  public void shouldSearchOnIndex() throws Exception {
     InputStream stream = ClassLoader.getSystemResourceAsStream("testLuceneIndex.sql");
 
     db.command(new OCommandScript("sql", getScriptFromStream(stream))).execute();
@@ -30,8 +33,36 @@ public class OLuceneSearchFunctionTest extends BaseLuceneTest{
         "create index Song.author on Song (author) FULLTEXT ENGINE LUCENE METADATA {\"analyzer\":\"" + StandardAnalyzer.class
             .getName() + "\"}")).execute();
 
-    List<ODocument> results = db.command(new OCommandSQL("SELECT from song where LUCENE_SEARCH(Song.title, 'BELIEVE') = true")).execute();
+    List<ODocument> results = db.command(new OCommandSQL("SELECT from song where SEARCH_INDEX(Song.title, 'BELIEVE') = true"))
+        .execute();
 
-    System.out.println("results = " + results);
+    assertThat(results).hasSize(2);
+
+    OResultSet resultSet = db.query("SELECT from Song where SEARCH_INDEX(Song.title, 'BELIEVE') = true");
+
+    assertThat(resultSet).hasSize(2);
+
+//    //no params: match tentative
+//    db.query("SELECT from song where LUCENE_SEARCH(['title'], ?, {} ) = true");
+//
+//    //field name:
+//    db.query("SELECT from song where LUCENE_SEARCH(?, title ) = true");
+//
+//    //field name plus metadata:
+//    db.query("SELECT from song where LUCENE_SEARCH('pippo', title , metadata {}) = true");
+//
+//    //only metadata
+//    db.query("SELECT from song where LUCENE_SEARCH('title:pippo', {}) = true");
+//
+//    db.query("SELECT from song where LUCENE_SEARCH('title:pippo') = true");
+
+    /**
+     * fields : []
+     * indeName: string
+     * allowLeadingwilcard
+     * bootsfiled
+     *
+     *
+     */
   }
 }
