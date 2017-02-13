06de3586edf3bb2d1a05cec8a1bd0e4277dd1d8f
maggiolo00 <maggiolo00@gmail.com>
2017-02-13 10:36:01 +0100

    Disabled debugTools

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -27,14 +27,14 @@ if ('production' === ENV) {
 } else {
 
   _decorateModuleRef = (modRef: any) => {
-    const appRef = modRef.injector.get(ApplicationRef);
-    const cmpRef = appRef.components[0];
-
-
-    let _ng = (<any>window).ng;
-    enableDebugTools(cmpRef);
-    (<any>window).ng.probe = _ng.probe;
-    (<any>window).ng.coreTokens = _ng.coreTokens;
+    // const appRef = modRef.injector.get(ApplicationRef);
+    // const cmpRef = appRef.components[0];
+    //
+    //
+    // let _ng = (<any>window).ng;
+    // enableDebugTools(cmpRef);
+    // (<any>window).ng.probe = _ng.probe;
+    // (<any>window).ng.coreTokens = _ng.coreTokens;
     return modRef;
   };
 
