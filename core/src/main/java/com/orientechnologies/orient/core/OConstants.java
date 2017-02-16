d9bace82ea8437117fd48114fc255e791056014b
Roberto Franchini <ro.franchini@gmail.com>
2017-02-16 15:42:20 +0100

    updates to 2.2.17 for relase process

--- core/src/main/java/com/orientechnologies/orient/core/OConstants.java
+++ core/src/main/java/com/orientechnologies/orient/core/OConstants.java
@@ -20,12 +20,12 @@
 package com.orientechnologies.orient.core;
 
 public class OConstants {
-  public static final String  ORIENT_VERSION       = "2.2.17-SNAPSHOT";
+  public static final String  ORIENT_VERSION       = "2.2.17";
 
   public static final int     ORIENT_VERSION_MAJOR = 2;
   public static final int     ORIENT_VERSION_MINOR = 2;
   public static final int     ORIENT_VERSION_HOFIX = 17;
-  public static final boolean SNAPSHOT             = true;
+  public static final boolean SNAPSHOT             = false;
 
   public static final String  ORIENT_URL           = "www.orientdb.com";
   public static final String  COPYRIGHT            = "Copyrights (c) 2017 OrientDB LTD";
