63bc54e7e1edbb070d8779625d4ddeaee404d975
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2017-03-07 17:02:47 +0100

    Fix DELETE EDGE TO x

--- core/src/main/java/com/orientechnologies/orient/core/sql/executor/FetchEdgesToVerticesStep.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/executor/FetchEdgesToVerticesStep.java
@@ -2,15 +2,36 @@ package com.orientechnologies.orient.core.sql.executor;
 
 import com.orientechnologies.common.concur.OTimeoutException;
 import com.orientechnologies.orient.core.command.OCommandContext;
+import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.core.exception.OCommandExecutionException;
+import com.orientechnologies.orient.core.record.ODirection;
+import com.orientechnologies.orient.core.record.OEdge;
+import com.orientechnologies.orient.core.record.OElement;
 import com.orientechnologies.orient.core.sql.parser.OIdentifier;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by luigidellaquila on 21/02/17.
  */
 public class FetchEdgesToVerticesStep extends AbstractExecutionStep {
+  private final String      toAlias;
+  private final OIdentifier targetCluster;
+  private final OIdentifier targetClass;
+
+  private boolean inited = false;
+  private Iterator        toIter;
+  private OEdge           nextEdge;
+  private Iterator<OEdge> currentToEdgesIter;
+
   public FetchEdgesToVerticesStep(String toAlias, OIdentifier targetClass, OIdentifier targetCluster, OCommandContext ctx) {
     super(ctx);
-    //TODO
+    this.toAlias = toAlias;
+    this.targetClass = targetClass;
+    this.targetCluster = targetCluster;
   }
 
   @Override
@@ -20,11 +41,135 @@ public class FetchEdgesToVerticesStep extends AbstractExecutionStep {
 
   @Override
   public OResultSet syncPull(OCommandContext ctx, int nRecords) throws OTimeoutException {
-    throw new UnsupportedOperationException();//TODO
+    getPrev().ifPresent(x -> x.syncPull(ctx, nRecords));
+    init();
+
+    return new OResultSet() {
+      int currentBatch = 0;
+
+      @Override
+      public boolean hasNext() {
+        return (currentBatch < nRecords && nextEdge != null);
+      }
+
+      @Override
+      public OResult next() {
+        if (!hasNext()) {
+          throw new IllegalStateException();
+        }
+        OEdge edge = nextEdge;
+        fetchNextEdge();
+        OResultInternal result = new OResultInternal();
+        result.setElement(edge);
+        return result;
+      }
+
+      @Override
+      public void close() {
+        if (toIter instanceof OResultSet) {
+          ((OResultSet) toIter).close();
+        }
+      }
+
+      @Override
+      public Optional<OExecutionPlan> getExecutionPlan() {
+        return null;
+      }
+
+      @Override
+      public Map<String, Long> getQueryStats() {
+        return null;
+      }
+    };
+  }
+
+  private void init() {
+    synchronized (this) {
+      if (this.inited) {
+        return;
+      }
+      inited = true;
+    }
+
+    Object toValues = null;
+
+    toValues = ctx.getVariable(toAlias);
+    if (toValues instanceof Iterable && !(toValues instanceof OIdentifiable)) {
+      toValues = ((Iterable) toValues).iterator();
+    } else if (!(toValues instanceof Iterator)) {
+      toValues = Collections.singleton(toValues).iterator();
+    }
+
+    this.toIter = (Iterator) toValues;
+
+    fetchNextEdge();
+  }
+
+  private void fetchNextEdge() {
+    this.nextEdge = null;
+    while (true) {
+      while (this.currentToEdgesIter == null || !this.currentToEdgesIter.hasNext()) {
+        if (this.toIter == null) {
+          return;
+        }
+        if (this.toIter.hasNext()) {
+          Object from = toIter.next();
+          if (from instanceof OResult) {
+            from = ((OResult) from).toElement();
+          }
+          if (from instanceof OIdentifiable && !(from instanceof OElement)) {
+            from = ((OIdentifiable) from).getRecord();
+          }
+          if (from instanceof OElement && ((OElement) from).isVertex()) {
+            Iterable<OEdge> edges = ((OElement) from).asVertex().get().getEdges(ODirection.IN);
+            currentToEdgesIter = edges.iterator();
+          } else {
+            throw new OCommandExecutionException("Invalid vertex: " + from);
+          }
+        } else {
+          return;
+        }
+      }
+      OEdge edge = this.currentToEdgesIter.next();
+      if (matchesClass(edge) && matchesCluster(edge)) {
+        this.nextEdge = edge;
+        return;
+      }
+    }
+  }
+
+  private boolean matchesCluster(OEdge edge) {
+    if (targetCluster == null) {
+      return true;
+    }
+    int clusterId = edge.getIdentity().getClusterId();
+    String clusterName = ctx.getDatabase().getClusterNameById(clusterId);
+    return clusterName.equals(targetCluster.getStringValue());
+  }
+
+  private boolean matchesClass(OEdge edge) {
+    if (targetClass == null) {
+      return true;
+    }
+    return edge.getSchemaType().get().isSubClassOf(targetClass.getStringValue());
   }
 
   @Override
   public void asyncPull(OCommandContext ctx, int nRecords, OExecutionCallback callback) throws OTimeoutException {
 
+  }
+
+  @Override
+  public String prettyPrint(int depth, int indent) {
+    String spaces = OExecutionStepInternal.getIndent(depth, indent);
+    String result = spaces + "+ FOR EACH x in " + toAlias + "\n";
+    result += spaces + "       FETCH EDGES TO x";
+    if (targetClass != null) {
+      result += "\n" + spaces + "       (target class " + targetClass + ")";
+    }
+    if (targetCluster != null) {
+      result += "\n" + spaces + "       (target cluster " + targetCluster + ")";
+    }
+    return result;
   }
 }
