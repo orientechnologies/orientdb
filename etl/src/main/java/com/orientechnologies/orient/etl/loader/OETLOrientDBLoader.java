c7a69aa0271e8694392d5f92b53e507666c3ceb3
Roberto Franchini <ro.franchini@gmail.com>
2017-03-13 14:28:06 +0100

    dropped log message

--- etl/src/main/java/com/orientechnologies/orient/etl/loader/OETLOrientDBLoader.java
+++ etl/src/main/java/com/orientechnologies/orient/etl/loader/OETLOrientDBLoader.java
@@ -104,7 +104,6 @@ public class OETLOrientDBLoader extends OETLAbstractLoader implements OETLLoader
     if (input instanceof OVertex) {
       final OVertex v = (OVertex) input;
 
-      log(Level.FINE, "v::" + v.getSchemaType().get());
       try {
         v.save(clusterName);
       } catch (ORecordDuplicatedException e) {
