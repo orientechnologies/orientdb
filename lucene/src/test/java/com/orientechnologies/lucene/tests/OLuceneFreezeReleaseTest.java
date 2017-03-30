ee951bb7b418e19c939a1d545872093f3a28114b
Roberto Franchini <ro.franchini@gmail.com>
2017-03-30 14:56:35 +0200

    ports test to new 3.0 API

--- lucene/src/test/java/com/orientechnologies/lucene/tests/OLuceneFreezeReleaseTest.java
+++ lucene/src/test/java/com/orientechnologies/lucene/tests/OLuceneFreezeReleaseTest.java
@@ -1,31 +1,21 @@
 package com.orientechnologies.lucene.tests;
 
-import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
-import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
 import com.orientechnologies.orient.core.metadata.schema.OSchema;
 import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.impl.ODocument;
-import com.orientechnologies.orient.core.sql.OCommandSQL;
 import com.orientechnologies.orient.core.sql.executor.OResultSet;
-import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by Enrico Risa on 23/09/16.
  */
-public class OLuceneFreezeReleaseTest {
+public class OLuceneFreezeReleaseTest extends OLuceneBaseTest {
 
   @Test
   public void freezeReleaseTest() {
-
-    ODatabaseDocument db = new ODatabaseDocumentTx("plocal:target/freezeRelease");
-
-    db.create();
 
     OSchema schema = db.getMetadata().getSchema();
     OClass person = schema.createClass("Person");
@@ -35,31 +25,24 @@ public class OLuceneFreezeReleaseTest {
 
     db.save(new ODocument("Person").field("name", "John"));
 
-    try {
+    OResultSet results = db.command("select from Person where search_class('John')=true");
 
-      OResultSet results = db.command("select from Person where search_class('John')=true");
+    assertThat(results).hasSize(1);
+    results.close();
 
-      assertThat(results).hasSize(1);
-      results.close();
+    db.freeze();
 
-      db.freeze();
+    results = db.command("select from Person where search_class('John')=true");
+    assertThat(results).hasSize(1);
+    results.close();
 
-      results = db.command("select from Person where search_class('John')=true");
-      assertThat(results).hasSize(1);
-      results.close();
+    db.release();
 
-      db.release();
+    db.save(new ODocument("Person").field("name", "John"));
 
-      db.save(new ODocument("Person").field("name", "John"));
-
-      results = db.command("select from Person where search_class('John')=true");
-      assertThat(results).hasSize(2);
-      results.close();
-
-    } finally {
-
-      db.drop();
-    }
+    results = db.command("select from Person where search_class('John')=true");
+    assertThat(results).hasSize(2);
+    results.close();
 
   }
 
@@ -67,10 +50,6 @@ public class OLuceneFreezeReleaseTest {
   @Test
   public void freezeReleaseMisUsageTest() {
 
-    ODatabaseDocument db = new ODatabaseDocumentTx("plocal:target/freezeRelease");
-
-    db.create();
-
     OSchema schema = db.getMetadata().getSchema();
     OClass person = schema.createClass("Person");
     person.createProperty("name", OType.STRING);
@@ -79,35 +58,28 @@ public class OLuceneFreezeReleaseTest {
 
     db.save(new ODocument("Person").field("name", "John"));
 
-    try {
+    OResultSet results = db.command("select from Person where search_class('John')=true");
 
-      OResultSet results = db.command("select from Person where search_class('John')=true");
+    assertThat(results).hasSize(1);
+    results.close();
 
-      assertThat(results).hasSize(1);
-      results.close();
+    db.freeze();
 
-      db.freeze();
+    db.freeze();
 
-      db.freeze();
+    results = db.command("select from Person where search_class('John')=true");
 
-      results = db.command("select from Person where search_class('John')=true");
+    assertThat(results).hasSize(1);
+    results.close();
 
-      assertThat(results).hasSize(1);
-      results.close();
+    db.release();
+    db.release();
 
-      db.release();
-      db.release();
+    db.save(new ODocument("Person").field("name", "John"));
 
-      db.save(new ODocument("Person").field("name", "John"));
-
-      results = db.command("select from Person where search_class('John')=true");
-      assertThat(results).hasSize(2);
-      results.close();
-
-    } finally {
-
-      db.drop();
-    }
+    results = db.command("select from Person where search_class('John')=true");
+    assertThat(results).hasSize(2);
+    results.close();
 
   }
 }
