e7e1e017b5297b6ad9247673f27bcd6ecb44ab6d
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2017-04-02 09:11:39 +0200

    Implement HA SYNC DATABASE command in new SQL executor

--- distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/ODatabaseDocumentDistributed.java
+++ distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/ODatabaseDocumentDistributed.java
@@ -1,9 +1,10 @@
 package com.orientechnologies.orient.server.distributed.impl;
 
 import com.orientechnologies.orient.core.db.ODatabaseDocumentInternal;
-import com.orientechnologies.orient.core.db.OrientDBConfig;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentEmbedded;
+import com.orientechnologies.orient.core.exception.OCommandExecutionException;
 import com.orientechnologies.orient.core.storage.OStorage;
+import com.orientechnologies.orient.server.distributed.ODistributedException;
 import com.orientechnologies.orient.server.hazelcast.OHazelcastPlugin;
 
 /**
@@ -24,5 +25,23 @@ public class ODatabaseDocumentDistributed extends ODatabaseDocumentEmbedded {
     database.internalOpen(getUser().getName(), null, getConfig(), false);
     this.activateOnCurrentThread();
     return database;
+  }
+
+  @Override
+  public boolean sync() {
+    final OStorage stg = getStorage();
+    if (!(stg instanceof ODistributedStorage))
+      throw new ODistributedException("SYNC DATABASE command cannot be executed against a non distributed server");
+
+    final ODistributedStorage dStg = (ODistributedStorage) stg;
+
+    final OHazelcastPlugin dManager = (OHazelcastPlugin) dStg.getDistributedManager();
+    if (dManager == null || !dManager.isEnabled())
+      throw new OCommandExecutionException("OrientDB is not started in distributed mode");
+
+    final String databaseName = getName();
+
+    return dManager.installDatabase(true, databaseName, dStg.getDistributedConfiguration().getDocument(), false, true);
+
   }
 }
