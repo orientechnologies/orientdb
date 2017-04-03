37742ea8cfbfc90ed9794d5c0956ed77f653282e
maggiolo00 <maggiolo00@gmail.com>
2017-04-03 18:54:37 +0200

    Added Network Serializer v37

---
+++ core/src/test/java/com/orientechnologies/orient/core/record/impl/ODocumentSerializerNetworkTestV1.java
@@ -0,0 +1,17 @@
+package com.orientechnologies.orient.core.record.impl;
+
+import com.orientechnologies.orient.core.serialization.serializer.record.binary.ORecordSerializerNetworkV37;
+import org.junit.Before;
+
+/**
+ * Created by tglman on 05/10/15.
+ */
+public class ODocumentSerializerNetworkTestV1 extends ODocumentSchemalessBinarySerializationTest {
+
+
+
+  @Before
+  public void createSerializer() {
+    serializer = new ORecordSerializerNetworkV37();
+  }
+}
