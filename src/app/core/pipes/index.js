ef6adde1528d6a3fdd039952a95c013cce82ad2e
G4br13l3 <gabriele.ponzi@gmail.com>
2017-04-18 17:08:37 +0100

    Deleted compiled js from the project.

--- src/app/core/pipes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./formatArray.pipe"));
-__export(require("./formatError.pipe"));
-__export(require("./objectKeys.pipe"));
--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,11 @@
-"use strict";
-var API = (function () {
-    var m = window.location.pathname.match(/(.*\/)studio\/index.html/);
-    return m && m[1] ? m[1] : '/api/';
+let API = (function () {
+  var m = window.location.pathname.match(/(.*\/)studio\/index.html/);
+  return m && m[1] ? m[1] : '/api/';
 })();
-exports.API = API;
-var STUDIO_VERSION = "2.2";
-exports.STUDIO_VERSION = STUDIO_VERSION;
-var POLLING = 5000;
-exports.POLLING = POLLING;
+
+
+let STUDIO_VERSION = "2.2";
+
+let POLLING = 5000;
+
+export {API, STUDIO_VERSION, POLLING};
