8411a0868c5feba85488f8de5d6818b3368a8adb
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2017-05-08 10:43:21 +0200

    Fix GROUP BY with NULL values
    Resolves: #7166

--- tests/src/test/java/com/orientechnologies/orient/test/database/auto/SQLSelectGroupByTest.java
+++ tests/src/test/java/com/orientechnologies/orient/test/database/auto/SQLSelectGroupByTest.java
@@ -92,8 +92,11 @@ public class SQLSelectGroupByTest extends DocumentDBBaseTest {
     Assert.assertTrue(result.size() > 1);
     last = null;
     for (ODocument d : result) {
-      if (last != null)
-        Assert.assertTrue(last.compareTo((String) d.field("location")) > 0);
+      Object current = d.field("location");
+      if(current!=null) {
+        if (last != null)
+          Assert.assertTrue(last.compareTo((String) current) > 0);
+      }
       last = d.field("location");
     }
   }
