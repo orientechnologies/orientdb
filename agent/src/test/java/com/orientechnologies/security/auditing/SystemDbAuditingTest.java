ad4a8bc21e01d6d57b31c74d545a1b35ae0a6e8d
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2017-05-10 13:03:30 +0200

    Add a sleep on auditing test (after class create) to give auditing mechanisms time to execute

--- agent/src/test/java/com/orientechnologies/security/auditing/SystemDbAuditingTest.java
+++ agent/src/test/java/com/orientechnologies/security/auditing/SystemDbAuditingTest.java
@@ -21,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * @author sdipro
  * @since 02/06/16
+<<<<<<< HEAD
  *
  * Launches a new OServer (using the security.json resource).
  * Creates a test database.
@@ -30,31 +31,41 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Drops the 'TestClass' class.
  * Queries the system database auditing log for 'TestClass'.
  * Asserts that the "dropped class" event is there.
+=======
+ * <p>
+ * Launches a new OServer (using the security.json resource). Creates a test database. Creates a new class called 'TestClass'.
+ * Queries the system database auditing log for 'TestClass'. Asserts that the "created class" event is there. Drops the 'TestClass'
+ * class. Queries the system database auditing log for 'TestClass'. Asserts that the "dropped class" event is there.
+>>>>>>> 064809e... Add a sleep on auditing test (after class create) to give auditing mechanisms time to execute
  */
 public class SystemDbAuditingTest extends AbstractSecurityTest {
 
-  private static final String TESTDB = "SystemDbAuditingTestDB";
+  private static final String TESTDB       = "SystemDbAuditingTestDB";
   private static final String DATABASE_URL = "remote:localhost/" + TESTDB;
 
   private static OServer server;
 
   @BeforeClass
   public static void beforeClass() throws Exception {
-  	 setup(TESTDB);
+    setup(TESTDB);
 
-  	 createFile(SERVER_DIRECTORY + "/config/orientdb-server-config.xml", SystemDbAuditingTest.class.getResourceAsStream("/com/orientechnologies/security/auditing/orientdb-server-config.xml"));
-  	 createFile(SERVER_DIRECTORY + "/config/security.json", SystemDbAuditingTest.class.getResourceAsStream("/com/orientechnologies/security/auditing/security.json"));
+    createFile(SERVER_DIRECTORY + "/config/orientdb-server-config.xml",
+        SystemDbAuditingTest.class.getResourceAsStream("/com/orientechnologies/security/auditing/orientdb-server-config.xml"));
+    createFile(SERVER_DIRECTORY + "/config/security.json",
+        SystemDbAuditingTest.class.getResourceAsStream("/com/orientechnologies/security/auditing/security.json"));
 
 
     server = new OServer();
-    server.setServerRootDirectory(SERVER_DIRECTORY);   
+    server.setServerRootDirectory(SERVER_DIRECTORY);
 
     server.startup(new File(SERVER_DIRECTORY + "/config/orientdb-server-config.xml"));
 
     server.activate();
 
-  	 createDirectory(SERVER_DIRECTORY + "/databases/" + TESTDB);
-    createFile(SERVER_DIRECTORY + "/databases/" + TESTDB + "/auditing-config.json", SystemDbAuditingTest.class.getResourceAsStream("/com/orientechnologies/security/auditing/auditing-config.json"));
+
+    createDirectory(SERVER_DIRECTORY + "/databases/" + TESTDB);
+    createFile(SERVER_DIRECTORY + "/databases/" + TESTDB + "/auditing-config.json",
+        SystemDbAuditingTest.class.getResourceAsStream("/com/orientechnologies/security/auditing/auditing-config.json"));
 
     OServerAdmin serverAd = new OServerAdmin("remote:localhost");
     serverAd.connect("root", "D2AFD02F20640EC8B7A5140F34FCA49D2289DB1F0D0598BB9DE8AAA75A0792F3");
@@ -72,15 +83,22 @@ public class SystemDbAuditingTest extends AbstractSecurityTest {
   @Test
   public void createDropClassTest() throws IOException {
     ODatabaseDocumentTx db = new ODatabaseDocumentTx("remote:localhost/" + TESTDB);
-    db.open("admin","admin");
+    db.open("admin", "admin");
 
     server.getSystemDatabase().execute(null, "delete from OAuditingLog where database = ?", TESTDB);
 
     db.command(new OCommandSQL("create class TestClass")).execute();
 
+
+    try {
+      Thread.sleep(1000);//let auditing log happen (remove this and make auditing more reliable!!!)
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     String query = "select from OAuditingLog where database = ? and note = ?";
 
-    List<ODocument> result = (List<ODocument>)server.getSystemDatabase().execute(null, query, TESTDB, "I created a class: TestClass");
+    List<ODocument> result = (List<ODocument>) server.getSystemDatabase()
+        .execute(null, query, TESTDB, "I created a class: TestClass");
 
     assertThat(result).isNotNull();
 
@@ -89,7 +107,8 @@ public class SystemDbAuditingTest extends AbstractSecurityTest {
     // Drop Class Test
     db.command(new OCommandSQL("drop class TestClass")).execute();
 
-    result = (List<ODocument>)server.getSystemDatabase().execute(null, query, TESTDB, "I dropped a class: TestClass");
+
+    result = (List<ODocument>) server.getSystemDatabase().execute(null, query, TESTDB, "I dropped a class: TestClass");
 
     assertThat(result).isNotNull();
 
