9847ea08b8e42ce39c27247969e67f101d9866c1
Fabrizio Fortino <ffortino@gilt.com>
2017-05-19 00:13:01 +0100

    custom sql functions server plugin

---
+++ core/src/main/java/com/orientechnologies/orient/core/sql/functions/OCustomSQLFunctionFactory.java
@@ -0,0 +1,77 @@
+package com.orientechnologies.orient.core.sql.functions;
+
+import com.orientechnologies.common.exception.OException;
+import com.orientechnologies.common.log.OLogManager;
+import com.orientechnologies.orient.core.exception.OCommandExecutionException;
+import com.orientechnologies.orient.core.sql.functions.misc.OSQLStaticReflectiveFunction;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Factory for custom SQL functions.
+ *
+ * @author Fabrizio Fortino
+ */
+public class OCustomSQLFunctionFactory implements OSQLFunctionFactory {
+  private static final Map<String, Object> FUNCTIONS = new HashMap<>();
+
+  public static void register(final String prefix, final Class<?> clazz) {
+    final Map<String, List<Method>> methodsMap = Arrays.stream(clazz.getMethods())
+        .filter(m -> Modifier.isStatic(m.getModifiers()))
+        .collect(Collectors.groupingBy(Method::getName));
+
+    for (Map.Entry<String, List<Method>> entry : methodsMap.entrySet()) {
+      final String name = prefix + entry.getKey();
+      if (FUNCTIONS.containsKey(name)) {
+        OLogManager.instance().warn(null, "Unable to register reflective function with name " + name);
+      } else {
+        List<Method> methodsList = methodsMap.get(entry.getKey());
+        Method[] methods = new Method[methodsList.size()];
+        int i = 0;
+        int minParams = 0;
+        int maxParams = 0;
+        for (Method m : methodsList) {
+          methods[i++] = m;
+          minParams = minParams < m.getParameterTypes().length ? minParams : m.getParameterTypes().length;
+          maxParams = maxParams > m.getParameterTypes().length ? maxParams : m.getParameterTypes().length;
+        }
+        FUNCTIONS.put(name.toLowerCase(), new OSQLStaticReflectiveFunction(name, minParams, maxParams, methods));
+      }
+    }
+
+  }
+
+  @Override
+  public Set<String> getFunctionNames() {
+    return FUNCTIONS.keySet();
+  }
+
+  @Override
+  public boolean hasFunction(final String name) {
+    return FUNCTIONS.containsKey(name);
+  }
+
+  @Override
+  public OSQLFunction createFunction(final String name) {
+    final Object obj = FUNCTIONS.get(name);
+
+    if (obj == null)
+      throw new OCommandExecutionException("Unknown function name :" + name);
+
+    if (obj instanceof OSQLFunction)
+      return (OSQLFunction) obj;
+    else {
+      // it's a class
+      final Class<?> clazz = (Class<?>) obj;
+      try {
+        return (OSQLFunction) clazz.newInstance();
+      } catch (Exception e) {
+        throw OException.wrapException(new OCommandExecutionException("Error in creation of function " + name
+            + "(). Probably there is not an empty constructor or the constructor generates errors"), e);
+      }
+    }
+  }
+}
---
+++ server/src/main/java/com/orientechnologies/orient/server/handler/OCustomSQLFunctionPlugin.java
@@ -0,0 +1,81 @@
+package com.orientechnologies.orient.server.handler;
+
+import com.orientechnologies.common.exception.OException;
+import com.orientechnologies.common.io.OIOUtils;
+import com.orientechnologies.common.parser.OSystemVariableResolver;
+import com.orientechnologies.orient.core.exception.OConfigurationException;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.functions.OCustomSQLFunctionFactory;
+import com.orientechnologies.orient.server.OServer;
+import com.orientechnologies.orient.server.config.OServerParameterConfiguration;
+import com.orientechnologies.orient.server.plugin.OServerPluginAbstract;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Server Plugin to register custom SQL functions.
+ *
+ * @author Fabrizio Fortino
+ */
+public class OCustomSQLFunctionPlugin extends OServerPluginAbstract {
+
+  private static final char PREFIX_NAME_SEPARATOR = '_';
+
+  private ODocument configuration;
+
+  @Override
+  public String getName() {
+    return "custom-sql-functions-manager";
+  }
+
+  @Override
+  public void config(OServer oServer, OServerParameterConfiguration[] iParams) {
+    configuration = new ODocument();
+
+    final File configFile = Arrays.stream(iParams)
+        .filter(p -> p.name.equalsIgnoreCase("config"))
+        .map(p -> p.value.trim())
+        .map(OSystemVariableResolver::resolveSystemVariables)
+        .map(File::new)
+        .filter(File::exists)
+        .findFirst()
+        .orElseThrow(() -> new OConfigurationException("Custom SQL functions configuration file not found"));
+
+    try {
+      final String configurationContent = OIOUtils.readFileAsString(configFile);
+      configuration = new ODocument().fromJSON(configurationContent);
+    } catch (IOException e) {
+      throw OException.wrapException(new OConfigurationException(
+          "Cannot load Custom SQL configuration file '" + configFile + "'. No custom functions will be disabled"), e);
+    }
+  }
+
+  @Override
+  public void startup() {
+    if (configuration.field("enabled")) {
+      List<Map<String, String>> functions = configuration.field("functions");
+      for (Map<String, String> function: functions) {
+        final String prefix = function.get("prefix");
+        final String clazz = function.get("class");
+        if (prefix == null || clazz == null) {
+          throw new OConfigurationException("Unable to load functions without prefix and / or class ");
+        }
+        if (!prefix.matches("^[\\pL]+$")) {
+          throw new OConfigurationException("Unable to load functions with prefix '" + prefix + "'. Prefixes can be letters only");
+        }
+
+        try {
+          Class functionsClass = Class.forName(clazz);
+          OCustomSQLFunctionFactory.register(prefix + PREFIX_NAME_SEPARATOR, functionsClass);
+        } catch (ClassNotFoundException e) {
+          throw new OConfigurationException("Unable to load class " + clazz + " for custom functions with prefix " + prefix);
+        }
+
+      }
+    }
+  }
+}
