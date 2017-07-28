bb39c548deb5f5b1dd7dd17f12ae099f05fa8273
Tglman <tglman@tglman.com>
2017-07-28 15:07:00 +0100

    fixed static usage in database importer, moved inner classes in their own files

---
+++ core/src/main/java/com/orientechnologies/orient/core/db/tool/importer/OImportConvertersFactory.java
@@ -0,0 +1,37 @@
+package com.orientechnologies.orient.core.db.tool.importer;
+
+import com.orientechnologies.orient.core.db.record.OIdentifiable;
+import com.orientechnologies.orient.core.db.record.ridbag.ORidBag;
+import com.orientechnologies.orient.core.id.ORID;
+import com.orientechnologies.orient.core.id.ORecordId;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by tglman on 28/07/17.
+ */
+public final class OImportConvertersFactory {
+  public static final ORID                     BROKEN_LINK = new ORecordId(-1, -42);
+  public static final OImportConvertersFactory INSTANCE    = new OImportConvertersFactory();
+
+  public OValuesConverter getConverter(Object value, OConverterData converterData) {
+    if (value instanceof Map)
+      return new OMapConverter(converterData);
+
+    if (value instanceof List)
+      return new OListConverter(converterData);
+
+    if (value instanceof Set)
+      return new OSetConverter(converterData);
+
+    if (value instanceof ORidBag)
+      return new ORidBagConverter(converterData);
+
+    if (value instanceof OIdentifiable)
+      return new OLinkConverter(converterData);
+
+    return null;
+  }
+}
---
+++ core/src/main/java/com/orientechnologies/orient/core/db/tool/importer/OSetConverter.java
@@ -0,0 +1,36 @@
+package com.orientechnologies.orient.core.db.tool.importer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by tglman on 28/07/17.
+ */
+public final class OSetConverter extends OAbstractCollectionConverter<Set> {
+  public OSetConverter(OConverterData converterData) {
+    super(converterData);
+  }
+
+  @Override
+  public Set convert(Set value) {
+    boolean updated = false;
+    final Set result;
+
+    result = new HashSet();
+
+    final ResultCallback callback = new ResultCallback() {
+      @Override
+      public void add(Object item) {
+        result.add(item);
+      }
+    };
+
+    for (Object item : value)
+      updated = convertSingleValue(item, callback, updated);
+
+    if (updated)
+      return result;
+
+    return value;
+  }
+}
