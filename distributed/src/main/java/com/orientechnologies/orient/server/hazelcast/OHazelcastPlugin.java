cb0ca9bb03fe073d7b07985148abbd8371ddcfc5
Tglman <tglman@tglman.com>
2017-11-06 23:24:32 +0000

    add check for the remove of the lock manager node to avoid execution of distributed lock operation on unreachable node

--- distributed/src/main/java/com/orientechnologies/orient/server/hazelcast/OHazelcastPlugin.java
+++ distributed/src/main/java/com/orientechnologies/orient/server/hazelcast/OHazelcastPlugin.java
@@ -1501,6 +1501,23 @@ public class OHazelcastPlugin extends ODistributedAbstractPlugin
         .debug(this, nodeName, nodeLeftName, DIRECTION.NONE, "Distributed server '%s' is unreachable", nodeLeftName);
 
     try {
+      if (nodeLeftName.equals(getLockManagerRequester().getServer()))
+        electNewLockManager();
+
+      getLockManagerExecutor().handleUnreachableServer(nodeLeftName);
+      getLockManagerRequester().handleUnreachableServer(nodeLeftName);
+
+    } catch (Exception e) {
+      // IGNORE IT
+      ODistributedServerLog.debug(this, nodeName, nodeLeftName, DIRECTION.NONE, "Error on electing new lockManager", e);
+    }
+
+    if (nodeLeftName.equals(getLockManagerRequester().getServer())) {
+      // IF AFTER RE-ELECTION THE DISTRIBUTED LOCK MANAGER IS STILL THE REMOVED SERVER ABORT REMOVE.
+      return;
+    }
+
+    try {
       // REMOVE INTRA SERVER CONNECTION
       closeRemoteServer(nodeLeftName);
 
@@ -1510,13 +1527,9 @@ public class OHazelcastPlugin extends ODistributedAbstractPlugin
           l.onNodeLeft(nodeLeftName);
         } catch (Exception e) {
           // IGNORE IT
+          ODistributedServerLog
+              .debug(this, nodeName, nodeLeftName, DIRECTION.NONE, "Error on calling onNodeLeft event on '%s'", e, l);
         }
-
-      if (nodeLeftName.equals(getLockManagerRequester().getServer()))
-        electNewLockManager();
-
-      getLockManagerExecutor().handleUnreachableServer(nodeLeftName);
-      getLockManagerRequester().handleUnreachableServer(nodeLeftName);
 
       // UNLOCK ANY PENDING LOCKS
       if (messageService != null) {
