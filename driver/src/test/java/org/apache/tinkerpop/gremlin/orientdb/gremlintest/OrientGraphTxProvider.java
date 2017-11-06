86055943f235ae90b8c2d1c2f81921dc46a78e57
maggiolo00 <maggiolo00@gmail.com>
2017-11-06 15:43:14 +0100

    Removed ThreadLocal implementation for tx with OrientGraph

--- driver/src/test/java/org/apache/tinkerpop/gremlin/orientdb/gremlintest/OrientGraphTxProvider.java
+++ driver/src/test/java/org/apache/tinkerpop/gremlin/orientdb/gremlintest/OrientGraphTxProvider.java
@@ -10,18 +10,20 @@ import static org.apache.tinkerpop.gremlin.orientdb.OrientGraph.CONFIG_TRANSACTI
 
 public class OrientGraphTxProvider extends OrientGraphProvider {
 
-    static {
-        IGNORED_TESTS.put(TransactionTest.class,
-                asList("shouldExecuteWithCompetingThreads", "shouldAllowReferenceOfEdgeIdOutsideOfOriginalThreadManual",
-                        "shouldAllowReferenceOfVertexIdOutsideOfOriginalThreadManual", "shouldSupportTransactionIsolationCommitCheck",
-                        "shouldNotShareTransactionReadWriteConsumersAcrossThreads"));
-    }
+  static {
+    IGNORED_TESTS.put(TransactionTest.class,
+        asList("shouldExecuteWithCompetingThreads", "shouldAllowReferenceOfEdgeIdOutsideOfOriginalThreadManual",
+            "shouldAllowReferenceOfVertexIdOutsideOfOriginalThreadManual", "shouldSupportTransactionIsolationCommitCheck",
+            "shouldNotShareTransactionReadWriteConsumersAcrossThreads", "shouldNotShareTransactionCloseConsumersAcrossThreads",
+            "shouldNotifyTransactionListenersInSameThreadOnlyOnCommitSuccess",
+            "shouldNotifyTransactionListenersInSameThreadOnlyOnRollbackSuccess"));
+  }
 
-    @Override
-    public Map<String, Object> getBaseConfiguration(String graphName, Class<?> test, String testMethodName,
-            LoadGraphWith.GraphData loadGraphWith) {
-        Map<String, Object> baseConfiguration = super.getBaseConfiguration(graphName, test, testMethodName, loadGraphWith);
-        baseConfiguration.put(CONFIG_TRANSACTIONAL, true);
-        return baseConfiguration;
-    }
+  @Override
+  public Map<String, Object> getBaseConfiguration(String graphName, Class<?> test, String testMethodName,
+      LoadGraphWith.GraphData loadGraphWith) {
+    Map<String, Object> baseConfiguration = super.getBaseConfiguration(graphName, test, testMethodName, loadGraphWith);
+    baseConfiguration.put(CONFIG_TRANSACTIONAL, true);
+    return baseConfiguration;
+  }
 }
