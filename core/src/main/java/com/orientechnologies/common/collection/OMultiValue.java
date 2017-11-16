f880a721d3d6b5b07c62b5a1090bb022cc24119b
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2017-11-16 12:21:27 +0100

    Fix UPDATE ADD of a map in an embeddedlist
    Resolves: https://github.com/orientechnologies/orientjs/issues/285

--- core/src/main/java/com/orientechnologies/common/collection/OMultiValue.java
+++ core/src/main/java/com/orientechnologies/common/collection/OMultiValue.java
@@ -478,8 +478,7 @@ public class OMultiValue {
 
         } else if (iToAdd instanceof Map<?, ?>) {
           // MAP
-          for (Entry<Object, Object> entry : ((Map<Object, Object>) iToAdd).entrySet())
-            coll.add(entry.getValue());
+          coll.add(iToAdd);
         } else if (iToAdd instanceof Iterator<?>) {
           // ITERATOR
           for (Iterator<?> it = (Iterator<?>) iToAdd; it.hasNext(); )
