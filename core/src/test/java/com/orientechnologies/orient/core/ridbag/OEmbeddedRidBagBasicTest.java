699c17e540c3b404229efd0ece15fc7efa246344
Tglman <tglman@tglman.com>
2017-11-22 15:23:38 +0000

    add test case of ridbag serialization invalid rid assert

--- core/src/test/java/com/orientechnologies/orient/core/ridbag/OEmbeddedRidBagBasicTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/ridbag/OEmbeddedRidBagBasicTest.java
@@ -2,8 +2,13 @@ package com.orientechnologies.orient.core.ridbag;
 
 import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.db.record.ridbag.ORidBag;
 import com.orientechnologies.orient.core.db.record.ridbag.embedded.OEmbeddedRidBag;
+import com.orientechnologies.orient.core.exception.ODatabaseException;
+import com.orientechnologies.orient.core.exception.OSerializationException;
 import com.orientechnologies.orient.core.id.ORecordId;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.record.impl.ODocument;
 import org.testng.annotations.Test;
 
 import java.util.UUID;
@@ -32,6 +37,40 @@ public class OEmbeddedRidBagBasicTest {
       assertEquals(bag.size(), 1);
 
       assertEquals(null, bag1.iterator().next());
+    } finally {
+      db.drop();
+    }
+
+  }
+
+  @Test(expectedExceptions = ODatabaseException.class)
+  public void embeddedRidBagInvalidClusterTest() {
+    ODatabaseDocument db = new ODatabaseDocumentTx("memory:" + OEmbeddedRidBag.class.getSimpleName());
+    db.create();
+    db.getMetadata().getSchema().createClass("test");
+    try {
+      ODocument document = new ODocument("test");
+      ORidBag ridBag = new ORidBag();
+      ridBag.add(new ORecordId(-2, -30));
+      document.field("ridbag", ridBag);
+      db.save(document);
+    } finally {
+      db.drop();
+    }
+
+  }
+
+  @Test(expectedExceptions = ODatabaseException.class)
+  public void embeddedRidBagInvalidClusterPositionTest() {
+    ODatabaseDocument db = new ODatabaseDocumentTx("memory:" + OEmbeddedRidBag.class.getSimpleName());
+    db.create();
+    OClass cls = db.getMetadata().getSchema().createClass("test");
+    try {
+      ODocument document = new ODocument("test");
+      ORidBag ridBag = new ORidBag();
+      ridBag.add(new ORecordId(cls.getDefaultClusterId(), -30));
+      document.field("ridbag", ridBag);
+      db.save(document);
     } finally {
       db.drop();
     }
