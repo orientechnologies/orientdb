2191fb425327753b20497a807e5d7cab345d31b2
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2018-02-13 12:32:50 +0100

    Fix automatic tx when not in hooks

--- core/src/main/java/com/orientechnologies/orient/core/db/document/ODatabaseDocumentAbstract.java
+++ core/src/main/java/com/orientechnologies/orient/core/db/document/ODatabaseDocumentAbstract.java
@@ -2011,7 +2011,7 @@ public abstract class ODatabaseDocumentAbstract extends OListenerManger<ODatabas
     ODirtyManager dirtyManager = ORecordInternal.getDirtyManager(iRecord);
     if (iRecord instanceof OElement && dirtyManager != null && dirtyManager.getReferences() != null && !dirtyManager.getReferences()
         .isEmpty()) {
-      if (((OElement) iRecord).isVertex() || ((OElement) iRecord).isEdge() && !getTransaction().isActive() && inHook.isEmpty()) {
+      if ((((OElement) iRecord).isVertex() || ((OElement) iRecord).isEdge()) && !getTransaction().isActive() && inHook.isEmpty()) {
         return saveGraph(iRecord, iClusterName, iMode, iForceCreate, iRecordCreatedCallback, iRecordUpdatedCallback);
       }
     }
