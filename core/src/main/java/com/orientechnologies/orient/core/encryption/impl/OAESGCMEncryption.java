856564a6509d7cd7817eda54c86705a66067f2b9
Markus Kreusch <mail@markuskreusch.de>
2018-04-12 21:44:52 +0200

    Added OAESGCMEncryption

---
+++ core/src/main/java/com/orientechnologies/orient/core/encryption/impl/OAESGCMEncryption.java
@@ -0,0 +1,158 @@
+package com.orientechnologies.orient.core.encryption.impl;
+
+import com.orientechnologies.common.exception.OException;
+import com.orientechnologies.orient.core.config.OGlobalConfiguration;
+import com.orientechnologies.orient.core.encryption.OEncryption;
+import com.orientechnologies.orient.core.exception.OInvalidStorageEncryptionKeyException;
+import com.orientechnologies.orient.core.exception.OSecurityException;
+
+import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.ByteBuffer;
+import java.security.*;
+import java.util.Base64;
+
+import static java.lang.String.format;
+import static javax.crypto.Cipher.DECRYPT_MODE;
+import static javax.crypto.Cipher.ENCRYPT_MODE;
+
+/**
+ * OEncryption implementation using AES/GCM/NoPadding with a 12 byte nonce and 16 byte tag size.
+ *
+ * @author Skymatic / Markus Kreusch (markus.kreusch--(at)--skymatic.de)
+ */
+public class OAESGCMEncryption extends OAbstractEncryption {
+
+  public static final String NAME = "aes/gcm";
+
+  private static final String ALGORITHM_NAME = "AES";
+  private static final String TRANSFORMATION = "AES/GCM/NoPadding";
+
+  private static final int GCM_NONCE_SIZE_IN_BYTES = 12;
+  private static final int GCM_TAG_SIZE_IN_BYTES = 16;
+
+  private static final String MISSING_KEY_ERROR = "AESGCMEncryption encryption has been selected, but no key was found. Please configure it by passing the key as property at database create/open. The property key is: '%s'";
+  private static final String INVALID_KEY_ERROR = "Failed to initialize AESGCMEncryption. Assure the key is a 128, 192 or 256 bits long BASE64 value";
+  private static final String ENCRYPTION_NOT_INITIALIZED_ERROR = "OAESGCMEncryption not properly initialized";
+  private static final String SECURE_RANDOM_CREATION_ERROR = "OAESGCMEncryption could not create SecureRandom";
+  private static final String AUTHENTICATION_ERROR = "Authentication of encrypted data failed. The encrypted data may have been altered.";
+
+
+  private boolean initialized;
+  private SecretKey key;
+  private SecureRandom random;
+
+  @Override
+  public String name() {
+    return NAME;
+  }
+
+  public OAESGCMEncryption() {
+  }
+
+  public OEncryption configure(final String base64EncodedKey) {
+    initialized = false;
+
+    key = createKey(base64EncodedKey);
+    random = createSecureRandom();
+
+    this.initialized = true;
+    return this;
+  }
+
+  public byte[] encryptOrDecrypt(final int mode, final byte[] input, final int offset, final int length) throws Exception {
+    assertInitialized();
+    return encryptOrDecrypt(mode, ByteBuffer.wrap(input, offset, length)).array();
+  }
+
+  private SecretKey createKey(String base64EncodedKey) {
+    if (base64EncodedKey == null) {
+      throw new OSecurityException(format(MISSING_KEY_ERROR, OGlobalConfiguration.STORAGE_ENCRYPTION_KEY.getKey()));
+    }
+    try {
+      final byte[] keyBytes = Base64.getDecoder().decode(base64EncodedKey);
+      validateKeySize(keyBytes);
+      return new SecretKeySpec(keyBytes, ALGORITHM_NAME);
+    } catch (Exception e) {
+      throw OException.wrapException(new OInvalidStorageEncryptionKeyException(INVALID_KEY_ERROR), e);
+    }
+  }
+
+  private SecureRandom createSecureRandom() {
+    try {
+      return SecureRandom.getInstanceStrong();
+    } catch (NoSuchAlgorithmException e) {
+      throw OException.wrapException(new OSecurityException(SECURE_RANDOM_CREATION_ERROR), e);
+    }
+  }
+
+  private ByteBuffer encryptOrDecrypt(int mode, ByteBuffer input) throws GeneralSecurityException {
+    switch (mode) {
+      case ENCRYPT_MODE:
+        return encrypt(input);
+      case Cipher.DECRYPT_MODE:
+        return decrypt(input);
+      default:
+        throw new IllegalArgumentException(format("Unexpected mode %d", mode));
+    }
+  }
+
+  private ByteBuffer encrypt(ByteBuffer input) throws GeneralSecurityException {
+    Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+    byte[] nonce = randomNonce();
+    cipher.init(ENCRYPT_MODE, key, gcmParameterSpec(nonce));
+
+    ByteBuffer output = ByteBuffer.allocate(GCM_NONCE_SIZE_IN_BYTES + cipher.getOutputSize(input.remaining()));
+    output.put(nonce);
+    cipher.doFinal(input, output);
+    output.flip();
+
+    return output;
+  }
+
+  private ByteBuffer decrypt(ByteBuffer input) throws GeneralSecurityException {
+    Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+    byte[] nonce = readNonce(input);
+    cipher.init(DECRYPT_MODE, key, gcmParameterSpec(nonce));
+
+    ByteBuffer output = ByteBuffer.allocate(cipher.getOutputSize(input.remaining()));
+    try {
+      cipher.doFinal(input, output);
+    } catch (AEADBadTagException e) {
+      throw OException.wrapException(new OSecurityException(AUTHENTICATION_ERROR), e);
+    }
+    output.flip();
+
+    return output;
+  }
+
+  private void validateKeySize(byte[] keyBytes) {
+    if (keyBytes.length != 16 && keyBytes.length != 24 && keyBytes.length != 32) {
+      throw new OInvalidStorageEncryptionKeyException(INVALID_KEY_ERROR);
+    }
+  }
+
+  private void assertInitialized() {
+    if (!initialized) {
+      throw new OSecurityException(ENCRYPTION_NOT_INITIALIZED_ERROR);
+    }
+  }
+
+  private byte[] randomNonce() {
+    byte[] nonce = new byte[GCM_NONCE_SIZE_IN_BYTES];
+    random.nextBytes(nonce);
+    return nonce;
+  }
+
+  private byte[] readNonce(ByteBuffer input) {
+    byte[] nonce = new byte[GCM_NONCE_SIZE_IN_BYTES];
+    input.get(nonce);
+    return nonce;
+  }
+
+  private GCMParameterSpec gcmParameterSpec(byte[] nonce) {
+    return new GCMParameterSpec(GCM_TAG_SIZE_IN_BYTES * Byte.SIZE, nonce);
+  }
+
+}
---
+++ core/src/test/java/com/orientechnologies/orient/core/encryption/impl/OAESGCMEncryptionTest.java
@@ -0,0 +1,229 @@
+package com.orientechnologies.orient.core.encryption.impl;
+
+import com.orientechnologies.common.io.OFileUtils;
+import com.orientechnologies.orient.core.config.OGlobalConfiguration;
+import com.orientechnologies.orient.core.db.ODatabase;
+import com.orientechnologies.orient.core.db.ODatabaseDocumentInternal;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.exception.OInvalidStorageEncryptionKeyException;
+import com.orientechnologies.orient.core.exception.OSecurityException;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.OCommandSQL;
+import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
+import com.orientechnologies.orient.core.storage.OStorage;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * @author Skymatic / Markus Kreusch (markus.kreusch--(at)--skymatic.de)
+ */
+public class OAESGCMEncryptionTest extends AbstractEncryptionTest {
+
+  private static final String DBNAME_DATABASETEST = "testCreatedAESGCMEncryptedDatabase";
+  private static final String DBNAME_CLUSTERTEST  = "testCreatedAESGCMEncryptedCluster";
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  @Test
+  public void testOAESGCMEncryptionWithoutKey() {
+    expectedException.expect(OSecurityException.class);
+    expectedException.expectMessage("no key");
+
+    testEncryption(OAESGCMEncryption.NAME);
+  }
+
+  @Test
+  public void testOAESEncryptedInvalidKeyDueToInvalidBase64() {
+    expectedException.expect(OInvalidStorageEncryptionKeyException.class);
+
+    testEncryption(OAESGCMEncryption.NAME, "T1JJRU:UREJfSVNf;09PTF9TT#9DT09M");
+  }
+
+  @Test
+  public void testOAESEncryptedInvalidKeyDueToInvalidKeySize() {
+    expectedException.expect(OInvalidStorageEncryptionKeyException.class);
+
+    testEncryption(OAESGCMEncryption.NAME, "T1JJRU5UREJfSVNfQ09PTF9TT19DT09MX1NP");
+  }
+
+  @Test
+  public void testOAESEncryptedWith128BitKey() {
+    testEncryption(OAESGCMEncryption.NAME, "T1JJRU5UREJfSVNfQ09PTA==");
+  }
+
+  @Test
+  public void testOAESEncryptedWith192BitKey() {
+    testEncryption(OAESGCMEncryption.NAME, "T1JJRU5UREJfSVNfQ09PTF9TT19DT09M");
+  }
+
+  @Test
+  public void testOAESEncryptedWith256BitKey() {
+    testEncryption(OAESGCMEncryption.NAME, "T1JJRU5UREJfSVNfQ09PTF9TT19DT09MX1NPX0NPT0w=");
+  }
+
+  @Test
+  public void testCreatedAESEncryptedDatabase() {
+    String buildDirectory = System.getProperty("buildDirectory", ".");
+
+    final String dbPath = buildDirectory + File.separator + DBNAME_DATABASETEST;
+    OFileUtils.deleteRecursively(new File(dbPath));
+
+    final ODatabase db = new ODatabaseDocumentTx("plocal:" + dbPath);
+
+    db.setProperty(OGlobalConfiguration.STORAGE_ENCRYPTION_METHOD.getKey(), "aes/gcm");
+    db.setProperty(OGlobalConfiguration.STORAGE_ENCRYPTION_KEY.getKey(), "T1JJRU5UREJfSVNfQ09PTA==");
+
+    db.create();
+
+    try {
+      db.command(new OCommandSQL("create class TestEncryption")).execute();
+      db.command(new OCommandSQL("insert into TestEncryption set name = 'Jay'")).execute();
+
+      List result = db.query(new OSQLSynchQuery<ODocument>("select from TestEncryption"));
+      Assert.assertEquals(result.size(), 1);
+      db.close();
+
+      db.open("admin", "admin");
+      OStorage storage = ((ODatabaseDocumentInternal) db).getStorage();
+      db.close();
+
+      storage.close(true, false);
+
+      db.setProperty(OGlobalConfiguration.STORAGE_ENCRYPTION_KEY.getKey(), "T1JJRU5UREJfSVNfQ09PTA==");
+      db.open("admin", "admin");
+      result = db.query(new OSQLSynchQuery<ODocument>("select from TestEncryption"));
+      Assert.assertEquals(result.size(), 1);
+      storage = ((ODatabaseDocumentInternal) db).getStorage();
+      db.close();
+
+      storage.close(true, false);
+
+      db.setProperty(OGlobalConfiguration.STORAGE_ENCRYPTION_KEY.getKey(), "invalidPassword");
+      try {
+        db.open("admin", "admin");
+        storage = ((ODatabaseDocumentInternal) db).getStorage();
+        Assert.fail();
+      } catch (OSecurityException e) {
+        Assert.assertTrue(true);
+      } finally {
+        db.activateOnCurrentThread();
+        db.close();
+        storage.close(true, false);
+      }
+
+      db.setProperty(OGlobalConfiguration.STORAGE_ENCRYPTION_KEY.getKey(), "T1JJRU5UREJfSVNfQ09PTA=-");
+      try {
+        db.open("admin", "admin");
+        storage = ((ODatabaseDocumentInternal) db).getStorage();
+        Assert.fail();
+      } catch (OSecurityException e) {
+        Assert.assertTrue(true);
+      } finally {
+        db.activateOnCurrentThread();
+        db.close();
+        storage.close(true, false);
+      }
+
+      db.setProperty(OGlobalConfiguration.STORAGE_ENCRYPTION_KEY.getKey(), "T1JJRU5UREJfSVNfQ09PTA==");
+      db.open("admin", "admin");
+      result = db.query(new OSQLSynchQuery<ODocument>("select from TestEncryption"));
+      Assert.assertEquals(result.size(), 1);
+
+    } finally {
+      db.activateOnCurrentThread();
+      if (db.isClosed())
+        db.open("admin", "admin");
+
+      db.drop();
+    }
+  }
+
+  @Test
+  public void testCreatedAESEncryptedCluster() {
+    final String buildDirectory = System.getProperty("buildDirectory", ".");
+    final String dbPath = buildDirectory + File.separator + DBNAME_CLUSTERTEST;
+
+    OFileUtils.deleteRecursively(new File(dbPath));
+    final ODatabase db = new ODatabaseDocumentTx("plocal:" + dbPath);
+
+    db.setProperty(OGlobalConfiguration.STORAGE_ENCRYPTION_KEY.getKey(), "T1JJRU5UREJfSVNfQ09PTA==");
+
+    db.create();
+    try {
+      db.command(new OCommandSQL("create class TestEncryption")).execute();
+      db.command(new OCommandSQL("alter class TestEncryption encryption aes")).execute();
+      db.command(new OCommandSQL("insert into TestEncryption set name = 'Jay'")).execute();
+
+      List result = db.query(new OSQLSynchQuery<ODocument>("select from TestEncryption"));
+      Assert.assertEquals(result.size(), 1);
+      db.close();
+
+      db.open("admin", "admin");
+      OStorage storage = ((ODatabaseDocumentInternal) db).getStorage();
+
+      db.close();
+
+      storage.close(true, false);
+
+      db.setProperty(OGlobalConfiguration.STORAGE_ENCRYPTION_KEY.getKey(), "T1JJRU5UREJfSVNfQ09PTA==");
+      db.open("admin", "admin");
+      result = db.query(new OSQLSynchQuery<ODocument>("select from TestEncryption"));
+      Assert.assertEquals(result.size(), 1);
+      storage = ((ODatabaseDocumentInternal) db).getStorage();
+      db.close();
+
+      storage.close(true, false);
+
+      db.setProperty(OGlobalConfiguration.STORAGE_ENCRYPTION_KEY.getKey(), "invalidPassword");
+      try {
+        db.open("admin", "admin");
+        storage = ((ODatabaseDocumentInternal) db).getStorage();
+        db.query(new OSQLSynchQuery<ODocument>("select from TestEncryption"));
+
+        result = db.query(new OSQLSynchQuery<ODocument>("select from OUser"));
+        Assert.assertFalse(result.isEmpty());
+
+        Assert.fail();
+      } catch (OSecurityException e) {
+        Assert.assertTrue(true);
+      } finally {
+        db.close();
+        storage.close(true, false);
+      }
+
+      db.setProperty(OGlobalConfiguration.STORAGE_ENCRYPTION_KEY.getKey(), "T1JJRU5UREJfSVNfQ09PTA=-");
+      try {
+        db.open("admin", "admin");
+        storage = ((ODatabaseDocumentInternal) db).getStorage();
+        db.query(new OSQLSynchQuery<ODocument>("select from TestEncryption"));
+        Assert.fail();
+      } catch (OSecurityException e) {
+        Assert.assertTrue(true);
+      } finally {
+        db.activateOnCurrentThread();
+        db.close();
+        storage.close(true, false);
+      }
+
+      db.setProperty(OGlobalConfiguration.STORAGE_ENCRYPTION_KEY.getKey(), "T1JJRU5UREJfSVNfQ09PTA==");
+      db.open("admin", "admin");
+      result = db.query(new OSQLSynchQuery<ODocument>("select from TestEncryption"));
+      Assert.assertEquals(result.size(), 1);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      db.activateOnCurrentThread();
+      if (db.isClosed())
+        db.open("admin", "admin");
+
+      db.drop();
+    }
+  }
+}
