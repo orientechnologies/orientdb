e781b3b7675a0f8e6e91534c83f9b7e3c81d1995
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2018-04-17 18:13:29 +0200

    Refactor live query

--- core/src/main/java/com/orientechnologies/orient/core/query/live/OLiveQueryQueueThreadV2.java
+++ core/src/main/java/com/orientechnologies/orient/core/query/live/OLiveQueryQueueThreadV2.java
@@ -21,33 +21,23 @@ package com.orientechnologies.orient.core.query.live;
 
 import com.orientechnologies.common.log.OLogManager;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.LinkedBlockingQueue;
-
 /**
  * @author Luigi Dell'Aquila (l.dellaquila-(at)-orientdb.com)
  */
 public class OLiveQueryQueueThreadV2 extends Thread {
 
-  private final BlockingQueue<OLiveQueryHookV2.OLiveQueryOp>            queue;
-  private final ConcurrentMap<Integer, OLiveQueryListenerV2> subscribers;
-  private boolean                                          stopped = false;
+  private final OLiveQueryHookV2.OLiveQueryOps ops;
 
-  private OLiveQueryQueueThreadV2(BlockingQueue<OLiveQueryHookV2.OLiveQueryOp> queue, ConcurrentMap<Integer, OLiveQueryListenerV2> subscribers) {
-    this.queue = queue;
-    this.subscribers = subscribers;
-  }
+  private boolean stopped = false;
 
-  public OLiveQueryQueueThreadV2() {
-    this(new LinkedBlockingQueue<OLiveQueryHookV2.OLiveQueryOp>(), new ConcurrentHashMap<Integer, OLiveQueryListenerV2>());
+  public OLiveQueryQueueThreadV2(OLiveQueryHookV2.OLiveQueryOps ops) {
     setName("LiveQueryQueueThreadV2");
+    this.ops = ops;
     this.setDaemon(true);
   }
 
   public OLiveQueryQueueThreadV2 clone() {
-    return new OLiveQueryQueueThreadV2(this.queue, this.subscribers);
+    return new OLiveQueryQueueThreadV2(this.ops);
   }
 
   @Override
@@ -55,14 +45,14 @@ public class OLiveQueryQueueThreadV2 extends Thread {
     while (!stopped) {
       OLiveQueryHookV2.OLiveQueryOp next = null;
       try {
-        next = queue.take();
+        next = ops.getQueue().take();
       } catch (InterruptedException ignore) {
         break;
       }
       if (next == null) {
         continue;
       }
-      for (OLiveQueryListenerV2 listener : subscribers.values()) {
+      for (OLiveQueryListenerV2 listener : ops.getSubscribers().values()) {
         try {
           listener.onLiveResult(next);
         } catch (Exception e) {
@@ -78,27 +68,4 @@ public class OLiveQueryQueueThreadV2 extends Thread {
     this.interrupt();
   }
 
-  public void enqueue(OLiveQueryHookV2.OLiveQueryOp item) {
-    queue.offer(item);
-  }
-
-  public Integer subscribe(Integer id, OLiveQueryListenerV2 iListener) {
-    subscribers.put(id, iListener);
-    return id;
-  }
-
-  public void unsubscribe(Integer id) {
-    OLiveQueryListenerV2 res = subscribers.remove(id);
-    if (res != null) {
-      res.onLiveResultEnd();
-    }
-  }
-
-  public boolean hasListeners() {
-    return !subscribers.isEmpty();
-  }
-
-  public boolean hasToken(Integer key) {
-    return subscribers.containsKey(key);
-  }
 }
