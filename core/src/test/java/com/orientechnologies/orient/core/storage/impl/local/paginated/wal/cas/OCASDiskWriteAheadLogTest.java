9e037ff30efbd208bede1606b354423b966ecdf4
laa <lomakin.andrey@gmail.com>
2018-04-19 16:13:55 +0300

    Limits are added after 3/4 of WAL.

--- core/src/test/java/com/orientechnologies/orient/core/storage/impl/local/paginated/wal/cas/OCASDiskWriteAheadLogTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/storage/impl/local/paginated/wal/cas/OCASDiskWriteAheadLogTest.java
@@ -3004,7 +3004,7 @@ public class OCASDiskWriteAheadLogTest {
       final OLogSequenceNumber begin = wal.begin();
       final OLogSequenceNumber end = wal.end();
 
-      final long segment = begin.getSegment() + (end.getSegment() - begin.getSegment()) / 2;
+      final long segment = begin.getSegment() + 3 * (end.getSegment() - begin.getSegment()) / 4;
 
       final OLogSequenceNumber lsn = chooseRandomRecord(random, addedRecords.tailMap(new OLogSequenceNumber(segment, 0), false));
       if (lsn == null) {
