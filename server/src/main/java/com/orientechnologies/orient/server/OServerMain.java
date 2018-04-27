d12f1e1cba668952707338cd83e8d836b39c07f7
Marko Djurovic <mdjurovi@callidussoftware.com>
2018-04-27 12:08:07 +0200

    removed wait for input on server start

--- server/src/main/java/com/orientechnologies/orient/server/OServerMain.java
+++ server/src/main/java/com/orientechnologies/orient/server/OServerMain.java
@@ -39,9 +39,7 @@ public class OServerMain {
   }
 
   public static void main(final String[] args) throws Exception {
-    // STARTS ORIENTDB IN A NON DAEMON THREAD TO PREVENT EXIT
-    
-    System.in.read();
+    // STARTS ORIENTDB IN A NON DAEMON THREAD TO PREVENT EXIT       
     
     final Thread t = new Thread() {
       @Override
