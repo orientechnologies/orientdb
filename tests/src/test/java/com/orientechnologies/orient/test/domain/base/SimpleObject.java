9d0ef3b884ccef86e2d7d074749096b8787081f5
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2018-05-11 15:14:53 +0200

    Fix detaching of maps and collections in Object db
    Resolves: #8256

---
+++ tests/src/test/java/com/orientechnologies/orient/test/domain/base/SimpleObject.java
@@ -0,0 +1,38 @@
+package com.orientechnologies.orient.test.domain.base;
+
+import javax.persistence.Id;
+import javax.persistence.Version;
+import java.util.Map;
+
+public class SimpleObject{
+
+  @Id
+  private Object rid;
+
+  @Version
+  private Object version;
+
+  private String objectId;
+
+  private Map<String, String> templatePartsIds;
+
+  public String getObjectId() {
+    return objectId;
+  }
+
+  public void setObjectId(String objectId) {
+    this.objectId = objectId;
+  }
+
+  public Map<String, String> getTemplatePartsIds() {
+    return templatePartsIds;
+  }
+
+  public void setTemplatePartsIds(Map<String, String> templatePartsIds) {
+    this.templatePartsIds = templatePartsIds;
+  }
+
+  public Object getRid() {
+    return rid;
+  }
+}
