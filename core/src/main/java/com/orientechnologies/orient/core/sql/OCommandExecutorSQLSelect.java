57aa1df078911f48f17f576ac1a51b104e1578b1
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2018-05-14 09:53:39 +0200

    add db in context in old SQL executor

--- core/src/main/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLSelect.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/OCommandExecutorSQLSelect.java
@@ -45,7 +45,6 @@ import com.orientechnologies.orient.core.db.record.OIdentifiable;
 import com.orientechnologies.orient.core.db.record.ridbag.ORidBag;
 import com.orientechnologies.orient.core.exception.OCommandExecutionException;
 import com.orientechnologies.orient.core.exception.OQueryParsingException;
-import com.orientechnologies.orient.core.hook.ORecordHook;
 import com.orientechnologies.orient.core.id.OContextualRecordId;
 import com.orientechnologies.orient.core.id.ORID;
 import com.orientechnologies.orient.core.id.ORecordId;
@@ -121,34 +120,34 @@ public class OCommandExecutorSQLSelect extends OCommandExecutorSQLResultsetAbstr
 
   private static final AsyncResult PARALLEL_END_EXECUTION_THREAD = new AsyncResult(null, null);
 
-  private final OOrderByOptimizer                         orderByOptimizer     = new OOrderByOptimizer();
-  private final OMetricRecorder                           metricRecorder       = new OMetricRecorder();
-  private final OFilterOptimizer                          filterOptimizer      = new OFilterOptimizer();
-  private final OFilterAnalyzer                           filterAnalyzer       = new OFilterAnalyzer();
-  private       Map<String, String>                       projectionDefinition = null;
+  private final OOrderByOptimizer           orderByOptimizer     = new OOrderByOptimizer();
+  private final OMetricRecorder             metricRecorder       = new OMetricRecorder();
+  private final OFilterOptimizer            filterOptimizer      = new OFilterOptimizer();
+  private final OFilterAnalyzer             filterAnalyzer       = new OFilterAnalyzer();
+  private       Map<String, String>         projectionDefinition = null;
   // THIS HAS BEEN KEPT FOR COMPATIBILITY; BUT IT'S USED THE PROJECTIONS IN GROUPED-RESULTS
-  private       Map<String, Object>                       projections          = null;
-  private       List<OPair<String, String>>               orderedFields        = new ArrayList<OPair<String, String>>();
-  private       List<String>                              groupByFields;
-  private       ConcurrentHashMap<Object, ORuntimeResult> groupedResult        = new ConcurrentHashMap<Object, ORuntimeResult>();
-  private       boolean                                   aggregate            = false;
-  private       List<String>                              unwindFields;
-  private       Object                                    expandTarget;
-  private       int                                       fetchLimit           = -1;
-  private       OIdentifiable                             lastRecord;
-  private       String                                    fetchPlan;
-  private       boolean                                   fullySortedByIndex   = false;
-  private       LOCKING_STRATEGY                          lockingStrategy      = LOCKING_STRATEGY.DEFAULT;
+  private       Map<String, Object>         projections          = null;
+  private       List<OPair<String, String>> orderedFields        = new ArrayList<OPair<String, String>>();
+  private List<String> groupByFields;
+  private ConcurrentHashMap<Object, ORuntimeResult> groupedResult = new ConcurrentHashMap<Object, ORuntimeResult>();
+  private boolean                                   aggregate     = false;
+  private List<String> unwindFields;
+  private Object       expandTarget;
+  private int fetchLimit = -1;
+  private OIdentifiable lastRecord;
+  private String        fetchPlan;
+  private boolean          fullySortedByIndex = false;
+  private LOCKING_STRATEGY lockingStrategy    = LOCKING_STRATEGY.DEFAULT;
 
-  private          Boolean                         isAnyFunctionAggregates = null;
-  private volatile boolean                         parallel                = false;
+  private          Boolean isAnyFunctionAggregates = null;
+  private volatile boolean parallel                = false;
   private volatile boolean                         parallelRunning;
   private final    ArrayBlockingQueue<AsyncResult> resultQueue;
 
   private ConcurrentHashMap<ORID, ORID> uniqueResult;
-  private boolean                       noCache    = false;
-  private int                           tipLimitThreshold;
-  private String                        NULL_VALUE = "null";
+  private boolean noCache = false;
+  private int tipLimitThreshold;
+  private String NULL_VALUE = "null";
 
   private AtomicLong tmpQueueOffer = new AtomicLong();
   private Object     resultLock    = new Object();
@@ -1523,6 +1522,7 @@ public class OCommandExecutorSQLSelect extends OCommandExecutorSQLResultsetAbstr
   public void initContext() {
     if (context == null) {
       context = new OBasicCommandContext();
+      ((OBasicCommandContext) context).setDatabase(getDatabase());
     }
 
     metricRecorder.setContext(context);
