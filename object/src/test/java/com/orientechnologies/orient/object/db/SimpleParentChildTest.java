02a22263c8a3c724d4205756c14a349712d2701b
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2018-05-21 15:21:41 +0200

    Add test case for issue #8272 - sets on Object DB

--- object/src/test/java/com/orientechnologies/orient/object/db/SimpleParentChildTest.java
+++ object/src/test/java/com/orientechnologies/orient/object/db/SimpleParentChildTest.java
@@ -1,16 +1,18 @@
 package com.orientechnologies.orient.object.db;
 
 import com.orientechnologies.orient.core.db.object.ODatabaseObject;
+import com.orientechnologies.orient.core.id.ORecordId;
 import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.object.db.entity.ObjectWithSet;
 import com.orientechnologies.orient.object.db.entity.SimpleChild;
 import com.orientechnologies.orient.object.db.entity.SimpleParent;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by tglman on 17/02/17.
@@ -19,12 +21,15 @@ public class SimpleParentChildTest {
 
   private ODatabaseObject database;
 
+  String url = "memory:" + SimpleParentChildTest.class.getSimpleName();
+
   @Before
   public void before() {
-    database = new OObjectDatabaseTx("memory:" + SimpleParentChildTest.class.getSimpleName());
+    database = new OObjectDatabaseTx(url);
     database.create();
     database.getEntityManager().registerEntityClass(SimpleChild.class);
     database.getEntityManager().registerEntityClass(SimpleParent.class);
+    database.getEntityManager().registerEntityClass(ObjectWithSet.class);
   }
 
   @After
@@ -42,6 +47,32 @@ public class SimpleParentChildTest {
     database.getLocalCache().clear();
     ODocument doc = ((OObjectDatabaseTx) database).getUnderlying().load(ret.getId().getIdentity());
     assertEquals(doc.fieldType("child"), OType.LINK);
+  }
+
+  @Test
+  public void testWithSets() {
+    ObjectWithSet parent = new ObjectWithSet();
+    ObjectWithSet child = new ObjectWithSet();
+    parent.addFriend(child);
+    child.setName("child1");
+    ObjectWithSet savedParent = database.save(parent);
+    String parentId = savedParent.getId();
+
+    this.database.close();
+    this.database = new OObjectDatabaseTx(url);
+    this.database.open("admin", "admin");
+
+    ObjectWithSet retrievedParent = this.database.load(new ORecordId(parentId));
+    ObjectWithSet retrievedChild = retrievedParent.getFriends().iterator().next();
+    retrievedChild.setName("child2");
+    this.database.save(retrievedParent);
+
+    this.database.close();
+    this.database = new OObjectDatabaseTx(url);
+    this.database.open("admin", "admin");
+
+    retrievedParent = this.database.load(new ORecordId(parentId));
+    Assert.assertEquals("child2", retrievedParent.getFriends().iterator().next().getName());
   }
 
 }
