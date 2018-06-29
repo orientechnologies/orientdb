f1d1a8c58ed0328b7b062514dd48717c7e929638
laa <lomakin.andrey@gmail.com>
2018-06-29 22:27:03 +0300

    Length of LSN chunk size was changed.

--- core/src/main/java/com/orientechnologies/orient/core/config/OGlobalConfiguration.java
+++ core/src/main/java/com/orientechnologies/orient/core/config/OGlobalConfiguration.java
@@ -309,7 +309,7 @@ public enum OGlobalConfiguration {
       16),
 
   DISK_CACHE_LSN_CHUNK_SIZE("storage.diskCache.ringChunkSize",
-      "Maximum size of chunk which should be flushed by write cache background thread in LSN mode", Integer.class, 512),
+      "Maximum size of chunk which should be flushed by write cache background thread in LSN mode", Integer.class, 32),
 
   DISK_CACHE_EXCLUSIVE_PAGES_BOUNDARY("storage.diskCache.exclusiveBoundary",
       "Portion of exclusive pages in write cache after which we will start to flush only exclusive pages", Float.class, 0.7),
