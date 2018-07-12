267f2e3d11e3cec8b46f7f560e647fde231ab688
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2018-07-12 11:18:39 +0200

    add test case for SQL CREATE VIEW

---
+++ core/src/test/java/com/orientechnologies/orient/core/sql/executor/OCreateViewStatementExecutionTest.java
@@ -0,0 +1,40 @@
+package com.orientechnologies.orient.core.sql.executor;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.metadata.schema.OSchema;
+import com.orientechnologies.orient.core.metadata.schema.OView;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Luigi Dell'Aquila (l.dellaquila-(at)-orientdb.com)
+ */
+public class OCreateViewStatementExecutionTest {
+  static ODatabaseDocument db;
+
+  @BeforeClass
+  public static void beforeClass() {
+    db = new ODatabaseDocumentTx("memory:OCreateViewStatementExecutionTest");
+    db.create();
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    db.close();
+  }
+
+  @Test
+  public void testPlain() {
+    String className = "testPlain";
+    OResultSet result = db.command("create view " + className + "  FROM (SELECT FROM V)");
+    OSchema schema = db.getMetadata().getSchema();
+    OView view = schema.getView(className);
+    Assert.assertNotNull(view);
+    Assert.assertEquals(className, view.getName());
+    result.close();
+  }
+
+}
