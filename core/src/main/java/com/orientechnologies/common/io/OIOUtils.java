55f8738df0913001fb52429110fee8b668547e06
laa <lomakin.andrey@gmail.com>
2018-07-26 21:51:27 +0300

    Write only aligned memory blocks in pwritev

--- core/src/main/java/com/orientechnologies/common/io/OIOUtils.java
+++ core/src/main/java/com/orientechnologies/common/io/OIOUtils.java
@@ -507,7 +507,7 @@ public class OIOUtils {
       final int bufferIndex = (int) written / bufferLimit;
       final ByteBuffer buffer = buffers[bufferIndex];
       final int bufferOffset = (int) (written - bufferIndex * buffer.limit());
-      buffer.position(bufferOffset);
+      written -= bufferOffset;//we need to write only aligned memory blocks
 
       try {
         written += ONative.instance().pwritev(fd, position + written, buffers, bufferIndex);
