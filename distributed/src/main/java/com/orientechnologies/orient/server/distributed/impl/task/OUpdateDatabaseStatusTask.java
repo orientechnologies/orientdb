71e948418456a066c61162990906a90a1632343f
Tglman <tglman@tglman.com>
2018-08-10 16:06:58 +0100

    made sure that after the sync the node is aware of all the latest states of all the nodes

--- distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/task/OUpdateDatabaseStatusTask.java
+++ distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/task/OUpdateDatabaseStatusTask.java
@@ -22,6 +22,8 @@ package com.orientechnologies.orient.server.distributed.impl.task;
 import com.orientechnologies.orient.core.command.OCommandDistributedReplicateRequest;
 import com.orientechnologies.orient.core.config.OGlobalConfiguration;
 import com.orientechnologies.orient.core.db.ODatabaseDocumentInternal;
+import com.orientechnologies.orient.core.serialization.OStreamable;
+import com.orientechnologies.orient.core.storage.impl.local.OAbstractPaginatedStorage;
 import com.orientechnologies.orient.core.storage.impl.local.paginated.wal.OLogSequenceNumber;
 import com.orientechnologies.orient.server.OServer;
 import com.orientechnologies.orient.server.distributed.ODistributedDatabase;
@@ -47,6 +49,31 @@ public class OUpdateDatabaseStatusTask extends OAbstractRemoteTask {
   private String             status;
   private OLogSequenceNumber lsn;
 
+  public static class OUpdateResult implements OStreamable {
+    private OLogSequenceNumber sequenceNumber;
+
+    public OUpdateResult() {
+    }
+
+    public OUpdateResult(OLogSequenceNumber sequenceNumber) {
+      this.sequenceNumber = sequenceNumber;
+    }
+
+    @Override
+    public void toStream(DataOutput out) throws IOException {
+      sequenceNumber.toStream(out);
+    }
+
+    @Override
+    public void fromStream(DataInput in) throws IOException {
+      sequenceNumber = new OLogSequenceNumber(in);
+    }
+
+    public OLogSequenceNumber getSequenceNumber() {
+      return sequenceNumber;
+    }
+  }
+
   public OUpdateDatabaseStatusTask() {
   }
 
@@ -61,10 +88,14 @@ public class OUpdateDatabaseStatusTask extends OAbstractRemoteTask {
       final ODatabaseDocumentInternal database) throws Exception {
 
     ODistributedDatabase database1 = iManager.getMessageService().getDatabase(databaseName);
-    if(database1 != null) {
+    if (database1 != null) {
       database1.getSyncConfiguration().setLastLSN(getNodeSource(), lsn, false);
     }
-    return true;
+    if (((OAbstractPaginatedStorage) database.getStorage().getUnderlying()).getLSN() != null) {
+      return new OUpdateResult(((OAbstractPaginatedStorage) database.getStorage().getUnderlying()).getLSN());
+    } else {
+      return null;
+    }
   }
 
   @Override
@@ -123,4 +154,5 @@ public class OUpdateDatabaseStatusTask extends OAbstractRemoteTask {
   public String toString() {
     return getName();
   }
+
 }
