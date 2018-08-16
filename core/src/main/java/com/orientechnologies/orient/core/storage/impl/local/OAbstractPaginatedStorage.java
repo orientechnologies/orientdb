afd0c1254ddf55ca1f529c2f527f2037f60910c9
laa <lomakin.andrey@gmail.com>
2018-08-16 11:46:26 +0300

    Ensure that atomic locks released only once

--- core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/OAbstractPaginatedStorage.java
+++ core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/OAbstractPaginatedStorage.java
@@ -2204,7 +2204,7 @@ public abstract class OAbstractPaginatedStorage extends OStorageAbstract
       throw logAndPrepareForRethrow(ee);
     } catch (Error ee) {
       handleJVMError(ee);
-      atomicOperationsManager.alarmClearOfAtomicOperation();
+      OAtomicOperationsManager.alarmClearOfAtomicOperation();
       throw logAndPrepareForRethrow(ee);
     } catch (Throwable t) {
       throw logAndPrepareForRethrow(t);
