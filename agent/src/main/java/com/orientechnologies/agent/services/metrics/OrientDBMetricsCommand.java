6ef67536c9327fe078f7582b3a1131f2f11ec691
wolf4ood <maggiolo00@gmail.com>
2018-08-28 16:44:32 +0200

    Added list of current metrics

--- agent/src/main/java/com/orientechnologies/agent/services/metrics/OrientDBMetricsCommand.java
+++ agent/src/main/java/com/orientechnologies/agent/services/metrics/OrientDBMetricsCommand.java
@@ -2,6 +2,8 @@ package com.orientechnologies.agent.services.metrics;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.orientechnologies.agent.profiler.OMetricsRegistry;
+import com.orientechnologies.agent.profiler.metrics.OMetric;
+import com.orientechnologies.agent.profiler.metrics.OMetricSet;
 import com.orientechnologies.orient.server.network.protocol.http.OHttpRequest;
 import com.orientechnologies.orient.server.network.protocol.http.OHttpResponse;
 import com.orientechnologies.orient.server.network.protocol.http.OHttpUtils;
@@ -13,17 +15,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Enrico Risa on 06/08/2018.
  */
 public class OrientDBMetricsCommand extends OServerCommandAuthenticatedServerAbstract {
 
-  private static final String[] NAMES = { "GET|metrics", "GET|metrics/*", "POST|metrics/config" };
+  private static final String[] NAMES = { "GET|metrics", "GET|metrics/*", "POST|metrics/config", "POST|metrics/config" };
 
   private OMetricsRegistry       registry;
   private OrientDBMetricsService service;
-  private ObjectMapper mapper = new ObjectMapper();
+  private ObjectMapper           mapper = new ObjectMapper();
 
   public OrientDBMetricsCommand(OMetricsRegistry registry, OrientDBMetricsService settings) {
     super("server.metrics");
@@ -69,9 +73,28 @@ public class OrientDBMetricsCommand extends OServerCommandAuthenticatedServerAbs
         String valueAsString = mapper.writeValueAsString(settings);
         iResponse.send(OHttpUtils.STATUS_OK_CODE, OHttpUtils.STATUS_OK_DESCRIPTION, OHttpUtils.CONTENT_JSON, valueAsString, null);
         break;
+      case "list":
+        Map<String, String> metrics = getMetricsLists(registry.getMetrics(), "");
+        String metricsAsString = mapper.writeValueAsString(metrics);
+        iResponse.send(OHttpUtils.STATUS_OK_CODE, OHttpUtils.STATUS_OK_DESCRIPTION, OHttpUtils.CONTENT_JSON, metricsAsString, null);
+        break;
       }
 
     }
+  }
+
+  private Map<String, String> getMetricsLists(Map<String, OMetric> metrics, String prefix) {
+
+    Map<String, String> m = new HashMap<>();
+    metrics.forEach((k, v) -> {
+      if (v instanceof OMetricSet) {
+        m.putAll(getMetricsLists(((OMetricSet) v).getMetrics(), ((OMetricSet) v).prefix()));
+      } else {
+        m.put(prefix + "." + k, v.getDescription());
+      }
+    });
+    return m;
+
   }
 
   private void doPost(OHttpRequest iRequest, OHttpResponse iResponse, String[] parts) throws IOException {
