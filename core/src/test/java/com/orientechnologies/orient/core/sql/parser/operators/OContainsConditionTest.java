81e91ae2a3922ea18eff522dc4b7616ef2aa16cb
Marko Djurovic <markodjurovic@yahoo.com>
2018-09-17 17:37:46 +0200

    test for iterable

--- core/src/test/java/com/orientechnologies/orient/core/sql/parser/operators/OContainsConditionTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/sql/parser/operators/OContainsConditionTest.java
@@ -24,6 +24,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -54,6 +56,30 @@ public class OContainsConditionTest {
     List<String> right = new ArrayList<>();
     right.add("foo");
     right.add(null);
+    Assert.assertTrue(op.execute(left, right));
+  }
+  
+  @Test
+  public void testIterable() {
+    Iterable left = new Iterable() {
+      private final List<Integer> ls = Arrays.asList(3, 1, 2);
+      
+      @Override
+      public Iterator iterator() {
+        return ls.iterator();
+      }
+    };
+    
+    Iterable right = new Iterable() {
+      private final List<Integer> ls = Arrays.asList(2, 3);
+      
+      @Override
+      public Iterator iterator() {
+        return ls.iterator();
+      }
+    };
+    
+    OContainsCondition op = new OContainsCondition(-1);
     Assert.assertTrue(op.execute(left, right));
   }
 }
