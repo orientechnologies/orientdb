d4ce6d82ea9ed04ce221452f9ac7dced81f7d53f
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2018-10-04 09:10:24 +0200

    Fix method signatures for OEnterpriseEndpoint

--- core/src/main/java/com/orientechnologies/orient/core/enterprise/OEnterpriseEndpoint.java
+++ core/src/main/java/com/orientechnologies/orient/core/enterprise/OEnterpriseEndpoint.java
@@ -5,7 +5,7 @@ import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 public interface OEnterpriseEndpoint {
   void haSetDbStatus(ODatabaseDocument db, String nodeName, String status);
 
-  void haSetRole(ODatabaseDocument db, String nodeName, String status);
+  void haSetRole(ODatabaseDocument db, String nodeName, String role);
 
-  void haSetOwner(ODatabaseDocument db, String clusterName, String status);
+  void haSetOwner(ODatabaseDocument db, String clusterName, String owner);
 }
