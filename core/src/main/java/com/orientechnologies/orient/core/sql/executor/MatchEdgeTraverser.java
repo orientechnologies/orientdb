d6155918e0bb65c8467445342bd848dbcda3f69f
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2018-12-27 17:15:11 +0100

    Add lazy traversal on MATCH execution
    Resolves: #8703

--- core/src/main/java/com/orientechnologies/orient/core/sql/executor/MatchEdgeTraverser.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/executor/MatchEdgeTraverser.java
@@ -330,19 +330,49 @@ public class MatchEdgeTraverser {
       return Collections.singleton(new OResultInternal((OIdentifiable) qR));
     }
     if (qR instanceof Iterable) {
-      Iterable iterable = (Iterable) qR;
-      List<OResultInternal> result = new ArrayList<>();
-      for (Object o : iterable) {
-        if (o instanceof OIdentifiable) {
-          result.add(new OResultInternal((OIdentifiable) o));
-        } else if (o instanceof OResultInternal) {
-          result.add((OResultInternal) o);
-        } else if (o == null) {
-          continue;
-        } else {
-          throw new UnsupportedOperationException();
+      final Iterator<Object> iter = ((Iterable) qR).iterator();
+      Iterable<OResultInternal> result = () -> new Iterator<OResultInternal>() {
+        private OResultInternal nextElement;
+
+        @Override
+        public boolean hasNext() {
+          if (nextElement == null) {
+            fetchNext();
+          }
+          return nextElement != null;
         }
-      }
+
+        @Override
+        public OResultInternal next() {
+          if (nextElement == null) {
+            fetchNext();
+          }
+          if (nextElement == null) {
+            throw new IllegalStateException();
+          }
+          OResultInternal res = nextElement;
+          nextElement = null;
+          return res;
+        }
+
+        public void fetchNext() {
+          while (iter.hasNext()) {
+            Object o = iter.next();
+            if (o instanceof OIdentifiable) {
+              nextElement = new OResultInternal((OIdentifiable) o);
+              break;
+            } else if (o instanceof OResultInternal) {
+              nextElement = (OResultInternal) o;
+              break;
+            } else if (o == null) {
+              continue;
+            } else {
+              throw new UnsupportedOperationException();
+            }
+          }
+        }
+      };
+
       return result;
     }
     return Collections.EMPTY_LIST;
