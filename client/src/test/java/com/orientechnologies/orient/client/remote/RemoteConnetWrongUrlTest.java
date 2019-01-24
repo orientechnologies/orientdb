7dc0dc09ef2ce50cf6ef96ef981ad3c6f340e644
Tglman <tglman@tglman.com>
2019-01-24 14:28:16 +0000

    removed thread local in case of open failed in remote

--- client/src/test/java/com/orientechnologies/orient/client/remote/RemoteConnetWrongUrlTest.java
+++ client/src/test/java/com/orientechnologies/orient/client/remote/RemoteConnetWrongUrlTest.java
@@ -1,9 +1,12 @@
 package com.orientechnologies.orient.client.remote;
 
+import com.orientechnologies.orient.core.db.ODatabaseRecordThreadLocal;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.exception.ODatabaseException;
 import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
 
 public class RemoteConnetWrongUrlTest {
 
@@ -12,6 +15,17 @@ public class RemoteConnetWrongUrlTest {
     ODatabaseDocument doc = new ODatabaseDocumentTx("remote:wrong:2424/test");
     doc.open("user", "user");
 
+  }
+
+  @Test
+  public void testConnectWrongUrlTL() {
+    try {
+      ODatabaseDocument doc = new ODatabaseDocumentTx("remote:wrong:2424/test");
+      doc.open("user", "user");
+    } catch (ODatabaseException e) {
+
+    }
+    assertNull(ODatabaseRecordThreadLocal.instance().getIfDefined());
   }
 
 }
