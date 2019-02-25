645d6159158c74da7bb022b4d6535cea5f9fd42f
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2019-02-25 15:48:10 +0100

    Fix query execution planning with batch variables and multiple indexes (index union)

--- core/src/test/java/com/orientechnologies/orient/core/sql/executor/OSelectStatementExecutionTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/sql/executor/OSelectStatementExecutionTest.java
@@ -1400,7 +1400,6 @@ public class OSelectStatementExecutionTest {
     OExecutionPlan p2 = p.get();
     Assert.assertTrue(p2 instanceof OSelectExecutionPlan);
     OSelectExecutionPlan plan = (OSelectExecutionPlan) p2;
-    Assert.assertEquals(2, plan.getSteps().size());
     Assert.assertEquals(ParallelExecStep.class, plan.getSteps().get(0).getClass());
     ParallelExecStep parallel = (ParallelExecStep) plan.getSteps().get(0);
     Assert.assertEquals(2, parallel.getSubExecutionPlans().size());
