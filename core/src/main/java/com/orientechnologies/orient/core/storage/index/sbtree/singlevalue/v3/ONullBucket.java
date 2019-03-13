d440e1f82f3c73d20a2a84437616c05d0eeb569c
laa <lomakin.andrey@gmail.com>
2019-03-13 15:33:41 +0200

    Copy of CellBTreeSingleValue

---
+++ core/src/main/java/com/orientechnologies/orient/core/storage/index/sbtree/singlevalue/v3/ONullBucket.java
@@ -0,0 +1,69 @@
+/*
+ *
+ *  *  Copyright 2010-2016 OrientDB LTD (http://orientdb.com)
+ *  *
+ *  *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  *  You may obtain a copy of the License at
+ *  *
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *  Unless required by applicable law or agreed to in writing, software
+ *  *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  See the License for the specific language governing permissions and
+ *  *  limitations under the License.
+ *  *
+ *  * For more information: http://orientdb.com
+ *
+ */
+package com.orientechnologies.orient.core.storage.index.sbtree.singlevalue.v3;
+
+import com.orientechnologies.common.serialization.types.OShortSerializer;
+import com.orientechnologies.orient.core.id.ORID;
+import com.orientechnologies.orient.core.id.ORecordId;
+import com.orientechnologies.orient.core.storage.cache.OCacheEntry;
+import com.orientechnologies.orient.core.storage.impl.local.paginated.base.ODurablePage;
+
+/**
+ * Bucket which is intended to save values stored in sbtree under <code>null</code> key. Bucket has following layout:
+ * <ol>
+ * <li>First byte is flag which indicates presence of value in bucket</li>
+ * <li>Second byte indicates whether value is presented by link to the "bucket list" where actual value is stored or real value
+ * passed be user.</li>
+ * <li>The rest is serialized value whether link or passed in value.</li>
+ * </ol>
+ *
+ * @author Andrey Lomakin (a.lomakin-at-orientdb.com)
+ * @since 4/15/14
+ */
+final class ONullBucket extends ODurablePage {
+  ONullBucket(final OCacheEntry cacheEntry, final boolean isNew) {
+    super(cacheEntry);
+
+    if (isNew) {
+      setByteValue(NEXT_FREE_POSITION, (byte) 0);
+    }
+  }
+
+  public void setValue(final ORID value) {
+    setByteValue(NEXT_FREE_POSITION, (byte) 1);
+
+    setShortValue(NEXT_FREE_POSITION + 1, (short) value.getClusterId());
+    setLongValue(NEXT_FREE_POSITION + 1 + OShortSerializer.SHORT_SIZE, value.getClusterPosition());
+  }
+
+  public ORID getValue() {
+    if (getByteValue(NEXT_FREE_POSITION) == 0) {
+      return null;
+    }
+
+    final int clusterId = getShortValue(NEXT_FREE_POSITION + 1);
+    final long clusterPosition = getLongValue(NEXT_FREE_POSITION + 1 + OShortSerializer.SHORT_SIZE);
+    return new ORecordId(clusterId, clusterPosition);
+  }
+
+  void removeValue() {
+    setByteValue(NEXT_FREE_POSITION, (byte) 0);
+  }
+}
