94549ee82431dd3b4c53a1aedb4b5107e4763b71
wolf4ood <enrico.risa@gmail.com>
2019-04-05 23:17:37 +0200

    Added nested steps in TraversalMetricTransformer

--- driver/src/main/java/org/apache/tinkerpop/gremlin/orientdb/executor/transformer/OTraversalMetricTransformer.java
+++ driver/src/main/java/org/apache/tinkerpop/gremlin/orientdb/executor/transformer/OTraversalMetricTransformer.java
@@ -4,6 +4,7 @@ import com.orientechnologies.orient.core.command.script.transformer.result.OResu
 import com.orientechnologies.orient.core.sql.executor.OResult;
 import com.orientechnologies.orient.core.sql.executor.OResultInternal;
 import org.apache.tinkerpop.gremlin.process.traversal.util.DefaultTraversalMetrics;
+import org.apache.tinkerpop.gremlin.process.traversal.util.ImmutableMetrics;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -13,21 +14,25 @@ import java.util.stream.Collectors;
  * Created by Enrico Risa on 26/05/2017.
  */
 public class OTraversalMetricTransformer implements OResultTransformer<DefaultTraversalMetrics> {
-    @Override
-    public OResult transform(DefaultTraversalMetrics value) {
+  @Override
+  public OResult transform(DefaultTraversalMetrics value) {
 
-        OResultInternal result = new OResultInternal();
-        result.setProperty("time (ms)", value.getDuration(TimeUnit.MILLISECONDS));
+    OResultInternal result = new OResultInternal();
+    result.setProperty("time (ms)", value.getDuration(TimeUnit.MILLISECONDS));
 
-        List<OResultInternal> steps = value.getMetrics().stream().map((m) -> {
-            OResultInternal internal = new OResultInternal();
-            internal.setProperty("id", m.getId());
-            internal.setProperty("time (ms)", m.getDuration(TimeUnit.MILLISECONDS));
-            internal.setProperty("name", m.getName());
-            return internal;
-        }).collect(Collectors.toList());
+    List<OResultInternal> steps = value.getMetrics().stream().map(this::mapMetric).collect(Collectors.toList());
 
-        result.setProperty("steps", steps);
-        return result;
-    }
+    result.setProperty("steps", steps);
+    return result;
+  }
+
+  private OResultInternal mapMetric(ImmutableMetrics m) {
+    OResultInternal internal = new OResultInternal();
+    internal.setProperty("id", m.getId());
+    internal.setProperty("time (ms)", m.getDuration(TimeUnit.MILLISECONDS));
+    internal.setProperty("name", m.getName());
+    internal.setProperty("counts", m.getCounts());
+    internal.setProperty("nested", m.getNested().stream().map(this::mapMetric).collect(Collectors.toList()));
+    return internal;
+  }
 }
