15679a89267bcdfbfa6393ddd95bd0a61a00c5d7
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2019-06-17 11:54:58 +0200

    remove dead code

--- core/src/main/java/com/orientechnologies/orient/core/serialization/serializer/record/string/ORecordSerializerStringAbstract.java
+++ core/src/main/java/com/orientechnologies/orient/core/serialization/serializer/record/string/ORecordSerializerStringAbstract.java
@@ -383,8 +383,6 @@ public abstract class ORecordSerializerStringAbstract implements ORecordSerializ
       return OType.LINK;
     else if (iCharType == 'n')
       return OType.LINKSET;
-    else if (iCharType == 'x')
-      return OType.LINK;
     else if (iCharType == 'u')
       return OType.CUSTOM;
 
