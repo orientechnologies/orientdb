b830fdd1701c1b39c80f34d30e1467e3e32f53bc
Vitalii Honchar <weaxme@gmail.com>
2019-08-08 11:01:44 +0300

    Created database pool factory which allows manage database pools associated with users
    Issue #8977

--- core/src/test/java/com/orientechnologies/orient/core/db/OrientDBEmbeddedTests.java
+++ core/src/test/java/com/orientechnologies/orient/core/db/OrientDBEmbeddedTests.java
@@ -217,6 +217,30 @@ public class OrientDBEmbeddedTests {
   }
 
   @Test
+  public void testPoolFactory() {
+      OrientDB orientDB = new OrientDB("embedded:testdb", OrientDBConfig.defaultConfig());
+      orientDB.createIfNotExists("testdb", ODatabaseType.MEMORY);
+
+      ODatabasePoolFactory factory = new ODatabasePoolFactory(orientDB, 2);
+
+      ODatabasePool poolAdmin1 = factory.get("testdb", "admin", "admin");
+      ODatabasePool poolAdmin2 = factory.get("testdb", "admin", "admin");
+      ODatabasePool poolReader1 = factory.get("testdb", "reader", "reader");
+      ODatabasePool poolReader2 = factory.get("testdb", "reader", "reader");
+
+      assertEquals(poolAdmin1, poolAdmin2);
+      assertEquals(poolReader1, poolReader2);
+      assertNotEquals(poolAdmin1, poolReader1);
+
+      ODatabasePool poolWriter1 = factory.get("testdb", "writer", "writer");
+      ODatabasePool poolWriter2 = factory.get("testdb", "writer", "writer");
+      assertEquals(poolWriter1, poolWriter2);
+
+      ODatabasePool poolAdmin3 = factory.get("testdb", "admin", "admin");
+      assertNotEquals(poolAdmin1, poolAdmin3);
+  }
+
+  @Test
   public void testOpenKeepClean() {
     OrientDB orientDb = new OrientDB("embedded:./", OrientDBConfig.defaultConfig());
     try {
