64d282a308ff34b156d0fa8003a4fc2b5c5290e3
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2019-08-16 16:10:14 +0200

    Add test cases for security engine

--- core/src/test/java/com/orientechnologies/orient/core/metadata/security/OSecurityEngineTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/metadata/security/OSecurityEngineTest.java
@@ -8,6 +8,7 @@ public class OSecurityEngineTest {
 
   static OrientDB orient;
   private ODatabaseSession db;
+  private static String DB_NAME = "test";
 
   @BeforeClass
   public static void beforeClass() {
@@ -21,14 +22,14 @@ public class OSecurityEngineTest {
 
   @Before
   public void before() {
-    orient.create("test", ODatabaseType.MEMORY);
-    this.db = orient.open("test", "admin", "admin");
+    orient.create(DB_NAME, ODatabaseType.MEMORY);
+    this.db = orient.open(DB_NAME, "admin", "admin");
   }
 
   @After
   public void after() {
     this.db.close();
-    orient.drop("test");
+    orient.drop(DB_NAME);
     this.db = null;
   }
 
@@ -152,5 +153,66 @@ public class OSecurityEngineTest {
             .getPredicateForSecurityResource(db, (OSecurityShared) security, "database.class.Employee", OSecurityPolicy.Scope.READ);
 
     Assert.assertEquals("name = 'foo'", pred.toString());
+  }
+
+
+  @Test
+  public void testTwoSuperclasses() {
+    OSecurityInternal security = ((ODatabaseInternal) db).getSharedContext().getSecurity();
+
+    db.createClass("Person");
+    db.createClass("Foo");
+    db.createClass("Employee", "Person", "Foo");
+
+    OSecurityPolicy policy = security.createSecurityPolicy(db, "policy1");
+    policy.setActive(true);
+    policy.setReadRule("name = 'foo'");
+    security.saveSecurityPolicy(db, policy);
+    security.setSecurityPolicy(db, security.getRole(db, "admin"), "database.class.Person", policy);
+
+    policy = security.createSecurityPolicy(db, "policy2");
+    policy.setActive(true);
+    policy.setReadRule("surname = 'bar'");
+    security.saveSecurityPolicy(db, policy);
+    security.setSecurityPolicy(db, security.getRole(db, "admin"), "database.class.Foo", policy);
+
+
+    OBooleanExpression pred = OSecurityEngine
+            .getPredicateForSecurityResource(db, (OSecurityShared) security, "database.class.Employee", OSecurityPolicy.Scope.READ);
+
+    Assert.assertTrue("name = 'foo' AND surname = 'bar'".equals(pred.toString()) || "surname = 'bar' AND name = 'foo'".equals(pred.toString()));
+  }
+
+  @Test
+  public void testTwoRoles() {
+
+    db.command("Update OUser set roles = roles || (select from orole where name = 'reader') where name = 'admin'");
+    db.close();
+    db = orient.open(DB_NAME, "admin", "admin");
+
+
+    OSecurityInternal security = ((ODatabaseInternal) db).getSharedContext().getSecurity();
+
+
+    db.createClass("Person");
+
+    OSecurityPolicy policy = security.createSecurityPolicy(db, "policy1");
+    policy.setActive(true);
+    policy.setReadRule("name = 'foo'");
+    security.saveSecurityPolicy(db, policy);
+    security.setSecurityPolicy(db, security.getRole(db, "admin"), "database.class.Person", policy);
+
+    policy = security.createSecurityPolicy(db, "policy2");
+    policy.setActive(true);
+    policy.setReadRule("surname = 'bar'");
+    security.saveSecurityPolicy(db, policy);
+    security.setSecurityPolicy(db, security.getRole(db, "reader"), "database.class.Person", policy);
+
+
+    OBooleanExpression pred = OSecurityEngine
+            .getPredicateForSecurityResource(db, (OSecurityShared) security, "database.class.Person", OSecurityPolicy.Scope.READ);
+
+
+    Assert.assertTrue("name = 'foo' OR surname = 'bar'".equals(pred.toString()) || "surname = 'bar' OR name = 'foo'".equals(pred.toString()));
   }
 }
