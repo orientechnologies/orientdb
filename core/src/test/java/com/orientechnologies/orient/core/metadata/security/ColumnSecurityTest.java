9cf5ee0a196fad21a8b20aa4b5b3ac6da2a6baaa
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2019-09-18 11:57:21 +0200

    Add column security tests (read operations)

--- core/src/test/java/com/orientechnologies/orient/core/metadata/security/ColumnSecurityTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/metadata/security/ColumnSecurityTest.java
@@ -4,6 +4,9 @@ import com.orientechnologies.orient.core.db.*;
 import com.orientechnologies.orient.core.index.OIndexException;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
 import com.orientechnologies.orient.core.metadata.schema.OType;
+import com.orientechnologies.orient.core.record.OElement;
+import com.orientechnologies.orient.core.sql.executor.OResult;
+import com.orientechnologies.orient.core.sql.executor.OResultSet;
 import org.junit.*;
 
 public class ColumnSecurityTest {
@@ -145,5 +148,149 @@ public class ColumnSecurityTest {
     security.saveSecurityPolicy(db, policy);
     security.setSecurityPolicy(db, security.getRole(db, "reader"), "database.class.Person.name", policy);
   }
+
+
+  @Test
+  public void testFilterOneProperty() {
+    OSecurityInternal security = ((ODatabaseInternal) db).getSharedContext().getSecurity();
+
+    db.createClass("Person");
+
+    OSecurityPolicy policy = security.createSecurityPolicy(db, "testPolicy");
+    policy.setActive(true);
+    policy.setReadRule("name = 'foo'");
+    security.saveSecurityPolicy(db, policy);
+    security.setSecurityPolicy(db, security.getRole(db, "reader"), "database.class.Person.name", policy);
+
+    OElement elem = db.newElement("Person");
+    elem.setProperty("name", "foo");
+    db.save(elem);
+
+    elem = db.newElement("Person");
+    elem.setProperty("name", "bar");
+    db.save(elem);
+
+    db.close();
+    this.db = orient.open(DB_NAME, "reader", "reader");
+    OResultSet rs = db.query("select from Person");
+    boolean fooFound = false;
+    boolean nullFound = false;
+
+    for (int i = 0; i < 2; i++) {
+      OResult item = rs.next();
+      if ("foo".equals(item.getProperty("name"))) {
+        fooFound = true;
+      }
+      if (item.getProperty("name") == null) {
+        nullFound = true;
+      }
+    }
+
+    Assert.assertFalse(rs.hasNext());
+    rs.close();
+
+    Assert.assertTrue(fooFound);
+    Assert.assertTrue(nullFound);
+  }
+
+  @Test
+  public void testPredicateWithQuery() {
+    OSecurityInternal security = ((ODatabaseInternal) db).getSharedContext().getSecurity();
+
+    db.createClass("Person");
+
+    OSecurityPolicy policy = security.createSecurityPolicy(db, "testPolicy");
+    policy.setActive(true);
+    policy.setReadRule("name IN (select 'foo' as foo)");
+    security.saveSecurityPolicy(db, policy);
+    security.setSecurityPolicy(db, security.getRole(db, "reader"), "database.class.Person.name", policy);
+
+    OElement elem = db.newElement("Person");
+    elem.setProperty("name", "foo");
+    db.save(elem);
+
+    elem = db.newElement("Person");
+    elem.setProperty("name", "bar");
+    db.save(elem);
+
+    OResultSet rs = db.query("select from Person");
+    boolean fooFound = false;
+    boolean barFound = false;
+
+    for (int i = 0; i < 2; i++) {
+      OResult item = rs.next();
+      if ("foo".equals(item.getProperty("name"))) {
+        fooFound = true;
+      }
+      if ("bar".equals(item.getProperty("name"))) {
+        barFound = true;
+      }
+    }
+
+    Assert.assertFalse(rs.hasNext());
+    rs.close();
+
+    Assert.assertTrue(fooFound);
+    Assert.assertTrue(barFound);
+
+    db.close();
+    this.db = orient.open(DB_NAME, "reader", "reader");
+    rs = db.query("select from Person");
+    fooFound = false;
+    boolean nullFound = false;
+
+    for (int i = 0; i < 2; i++) {
+      OResult item = rs.next();
+      if ("foo".equals(item.getProperty("name"))) {
+        fooFound = true;
+      }
+      if (item.getProperty("name") == null) {
+        nullFound = true;
+      }
+    }
+
+    Assert.assertFalse(rs.hasNext());
+    rs.close();
+
+    Assert.assertTrue(fooFound);
+    Assert.assertTrue(nullFound);
+  }
+
+  @Test
+  public void testFilterOnePropertyWithQuery() {
+    OSecurityInternal security = ((ODatabaseInternal) db).getSharedContext().getSecurity();
+
+    db.createClass("Person");
+
+    OSecurityPolicy policy = security.createSecurityPolicy(db, "testPolicy");
+    policy.setActive(true);
+    policy.setReadRule("name = 'foo'");
+    security.saveSecurityPolicy(db, policy);
+    security.setSecurityPolicy(db, security.getRole(db, "reader"), "database.class.Person.name", policy);
+
+    OElement elem = db.newElement("Person");
+    elem.setProperty("name", "foo");
+    elem.setProperty("surname", "foo");
+    db.save(elem);
+
+    elem = db.newElement("Person");
+    elem.setProperty("name", "bar");
+    elem.setProperty("surname", "bar");
+    db.save(elem);
+
+    db.close();
+    this.db = orient.open(DB_NAME, "reader", "reader");
+    OResultSet rs = db.query("select from Person where name = 'foo' OR name = 'bar'");
+
+    OResult item = rs.next();
+    Assert.assertEquals("foo", item.getProperty("name"));
+
+
+    Assert.assertFalse(rs.hasNext());
+    rs.close();
+  }
+
+
+
 
 }
