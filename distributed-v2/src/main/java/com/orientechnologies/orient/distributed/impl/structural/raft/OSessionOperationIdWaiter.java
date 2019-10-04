adf432c9a74f02a059cdec68374158e69222776d
Tglman <tglman@tglman.com>
2019-10-04 17:03:47 +0100

    add support for waiting for it's own request in structural distributed flow

---
+++ distributed-v2/src/main/java/com/orientechnologies/orient/distributed/impl/structural/raft/OSessionOperationIdWaiter.java
@@ -0,0 +1,25 @@
+package com.orientechnologies.orient.distributed.impl.structural.raft;
+
+import com.orientechnologies.common.concur.lock.OInterruptedException;
+import com.orientechnologies.common.exception.OException;
+import com.orientechnologies.orient.distributed.impl.coordinator.transaction.OSessionOperationId;
+
+public class OSessionOperationIdWaiter {
+
+  private volatile OSessionOperationId lastReceived;
+
+  public synchronized void notify(OSessionOperationId id) {
+    this.lastReceived = id;
+    this.notifyAll();
+  }
+
+  public synchronized void waitIfNeeded(OSessionOperationId id) {
+    while (lastReceived != null && id.getSequential() > lastReceived.getSequential()) {
+      try {
+        this.wait();
+      } catch (InterruptedException e) {
+        throw OException.wrapException(new OInterruptedException("Interrupted waiting for apply of own requests"), e);
+      }
+    }
+  }
+}
