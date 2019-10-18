e090f86161d6e10b7606fedbf9d3d90c90045965
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2019-10-18 12:00:54 +0200

    Implement UDP unicast node manager

---
+++ core/src/main/java/com/orientechnologies/orient/core/db/config/OUDPUnicastConfiguration.java
@@ -0,0 +1,58 @@
+package com.orientechnologies.orient.core.db.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OUDPUnicastConfiguration {
+
+  public static class Address {
+    private String address;
+    private int port;
+
+    public Address(String address, int port) {
+      this.address = address;
+      this.port = port;
+    }
+
+    public String getAddress() {
+      return address;
+    }
+
+    public int getPort() {
+      return port;
+    }
+  }
+
+  private boolean enabled = true;
+  private int port = 4321;
+  private List<Address> discoveryAddresses = new ArrayList<>();
+
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
+  }
+
+  public List<Address> getDiscoveryAddresses() {
+    return discoveryAddresses;
+  }
+
+  public void setDiscoveryAddresses(List<Address> discoveryAddresses) {
+    this.discoveryAddresses = discoveryAddresses;
+  }
+
+  public static OUDPUnicastConfigurationBuilder builder() {
+    return new OUDPUnicastConfigurationBuilder();
+  }
+
+}
