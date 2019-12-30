e9844bbd5814412ebb37e8840e048d7e12ffd6af
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2019-12-30 15:32:51 +0100

    Add serialization tests for ONetworkConfirm

---
+++ distributed-v2/src/test/java/com/orientechnologies/orient/distributed/impl/coordinator/network/ONetworkConfirmTest.java
@@ -0,0 +1,35 @@
+package com.orientechnologies.orient.distributed.impl.coordinator.network;
+
+import com.orientechnologies.orient.distributed.impl.log.OLogId;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.*;
+
+public class ONetworkConfirmTest {
+
+  @Test
+  public void testSerialize() throws IOException {
+    OLogId oplogId = new OLogId(10, 20, 19);
+    ONetworkConfirm ack = new ONetworkConfirm(oplogId);
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DataOutputStream out = new DataOutputStream(baos);
+    ack.write(out);
+    byte[] serialized = baos.toByteArray();
+    out.close();
+    baos.close();
+    ByteArrayInputStream bais = new ByteArrayInputStream(serialized);
+    DataInputStream in = new DataInputStream(bais);
+
+    ONetworkConfirm ack2 = new ONetworkConfirm();
+    ack2.read(in);
+    in.close();
+    bais.close();
+
+    Assert.assertEquals(ack.getCommand(), ack2.getCommand());
+    Assert.assertEquals(ack.getId(), ack2.getId());
+  }
+}
+
+
