164c1c2f20f1ac9517b64ff09e719bf1057eab3d
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2019-12-31 09:50:47 +0100

    Add test cases for HA SQL statements

---
+++ core/src/test/java/com/orientechnologies/orient/core/sql/parser/OHaStatusStatementTest.java
@@ -0,0 +1,22 @@
+package com.orientechnologies.orient.core.sql.parser;
+
+import org.junit.Test;
+
+public class OHaStatusStatementTest extends OParserTestAbstract {
+
+  @Test
+  public void testPlain() {
+    checkRightSyntax("HA STATUS -servers");
+    checkRightSyntax("HA STATUS -db");
+    checkRightSyntax("HA STATUS -latency");
+    checkRightSyntax("HA STATUS -messages");
+    checkRightSyntax("HA STATUS -locks");
+    checkRightSyntax("HA STATUS -all");
+    checkRightSyntax("HA STATUS -all -output=text");
+    checkRightSyntax("HA STATUS -servers -db -latency -messages -locks ");
+
+    checkWrongSyntax("HA STATUS servers");
+  }
+
+
+}
