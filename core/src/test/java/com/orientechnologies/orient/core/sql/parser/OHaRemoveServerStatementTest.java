1ffe17ad3b03d26e89d1ebb3fb3efd2fee9faa44
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2019-12-31 09:41:22 +0100

    Fix HA SET statement and add test cases

---
+++ core/src/test/java/com/orientechnologies/orient/core/sql/parser/OHaRemoveServerStatementTest.java
@@ -0,0 +1,16 @@
+package com.orientechnologies.orient.core.sql.parser;
+
+import org.junit.Test;
+
+public class OHaRemoveServerStatementTest extends OParserTestAbstract {
+
+  @Test
+  public void testPlain() {
+    checkRightSyntax("HA REMOVE SERVER foo");
+    checkRightSyntax("ha remove server foo");
+
+    checkWrongSyntax("ha remove server");
+  }
+
+
+}
