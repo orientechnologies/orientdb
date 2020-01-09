4c5f8ccf6b6452c8f74782edcd6e24b1909bf905
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2020-01-09 17:04:14 +0100

    Add test cases for encryption

---
+++ core/src/test/java/com/orientechnologies/orient/core/encryption/impl/ONothingEncryptionTest.java
@@ -0,0 +1,21 @@
+package com.orientechnologies.orient.core.encryption.impl;
+
+import com.orientechnologies.orient.core.exception.OSecurityException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Base64;
+
+public class ONothingEncryptionTest {
+
+  @Test
+  public void test() {
+    ONothingEncryption encryption = new ONothingEncryption();
+    String key = new String(Base64.getEncoder().encode(new byte[]{1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8}));
+    encryption.configure(key);
+
+    byte[] original = "this is a test string to encrypt".getBytes();
+    byte[] encrypted = encryption.encrypt(original);
+    Assert.assertArrayEquals(original, encryption.decrypt(encrypted));
+  }
+}
