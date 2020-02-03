c54d86d3e1e56f51218bade9bcdc0bc49a3aca87
Tglman <tglman@tglman.com>
2020-02-03 09:25:53 +0000

    review implementation of transaction sequence implementation for distributed

--- distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/task/transaction/OTransactionId.java
+++ distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/task/transaction/OTransactionId.java
@@ -1,50 +1,19 @@
 package com.orientechnologies.orient.server.distributed.impl.task.transaction;
 
-import java.util.Optional;
-import java.util.UUID;
-
 public class OTransactionId {
-  private UUID           requester;
-  private long           sequence;
-  private Optional<Long> lastSuccessful;
-  private Optional<UUID> lastRequester;
+  private int  position;
+  private long sequence;
 
-  public OTransactionId(UUID requester, long sequence, Optional<Long> lastSuccessful, Optional<UUID> lastRequester) {
-    this.requester = requester;
+  public OTransactionId(int position, long sequence) {
+    this.position = position;
     this.sequence = sequence;
-    this.lastSuccessful = lastSuccessful;
-    this.lastRequester = lastRequester;
   }
 
-  public UUID getRequester() {
-    return requester;
-  }
-
-  public void setRequester(UUID requester) {
-    this.requester = requester;
+  public int getPosition() {
+    return position;
   }
 
   public long getSequence() {
     return sequence;
-  }
-
-  public void setSequence(long sequence) {
-    this.sequence = sequence;
-  }
-
-  public Optional<Long> getLastSuccessful() {
-    return lastSuccessful;
-  }
-
-  public void setLastSuccessful(Optional<Long> lastSuccessful) {
-    this.lastSuccessful = lastSuccessful;
-  }
-
-  public Optional<UUID> getLastRequester() {
-    return lastRequester;
-  }
-
-  public void setLastRequester(Optional<UUID> lastRequester) {
-    this.lastRequester = lastRequester;
   }
 }
