46a6b1a9c8fb57ce6aa9db3b1a9358e786faed61
Enrico Risa <enrico.risa@gmail.com>
2020-02-06 11:18:59 +0100

    Added integration test + default empty config

---
+++ server/src/test/java/com/orientechnologies/orient/server/script/JSScriptServerTest.java
@@ -0,0 +1,51 @@
+package com.orientechnologies.orient.server.script;
+
+import com.orientechnologies.orient.core.db.ODatabaseType;
+import com.orientechnologies.orient.core.db.OrientDB;
+import com.orientechnologies.orient.core.db.OrientDBConfig;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.sql.executor.OResultSet;
+import com.orientechnologies.orient.server.OServer;
+import org.junit.*;
+import org.junit.rules.TestName;
+
+public class JSScriptServerTest {
+
+  @Rule
+  public TestName name = new TestName();
+
+  private OServer server;
+
+  @Before
+  public void before() throws Exception {
+
+    server = OServer.startFromStreamConfig(getClass().getResourceAsStream("orientdb-server-config.xml"));
+  }
+
+  @Test
+  public void jsPackagesFromConfigTest() {
+
+    OrientDB orientDB = new OrientDB("remote:localhost", "root", "root", OrientDBConfig.defaultConfig());
+    orientDB.create(name.getMethodName(), ODatabaseType.MEMORY);
+    try (ODatabaseDocument db = orientDB.open(name.getMethodName(), "admin", "admin")) {
+
+      try (OResultSet resultSet = db.execute("javascript", "new java.math.BigDecimal(1.0);")) {
+        Assert.assertEquals(1, resultSet.stream().count());
+      }
+
+      try (OResultSet resultSet = db.execute("javascript", "new java.util.ArrayList();")) {
+        Assert.assertEquals(1, resultSet.stream().count());
+      }
+
+    } finally {
+
+      orientDB.drop(name.getMethodName());
+      orientDB.close();
+    }
+  }
+
+  @After
+  public void after() {
+    server.shutdown();
+  }
+}
