6e30e7fd8f390701494e86364e455254431f3e67
Enrico Risa <enrico.risa@gmail.com>
2020-03-12 12:15:12 +0100

    Added local node name to metrics api

--- agent/src/main/java/com/orientechnologies/agent/services/metrics/OrientDBMetricsCommand.java
+++ agent/src/main/java/com/orientechnologies/agent/services/metrics/OrientDBMetricsCommand.java
@@ -33,12 +33,12 @@ import java.util.stream.Collectors;
  */
 public class OrientDBMetricsCommand extends OServerCommandAuthenticatedServerAbstract {
 
-  private static final String[] NAMES = {"GET|metrics", "GET|metrics/*", "POST|metrics/config", "POST|metrics/config"};
+  private static final String[] NAMES = { "GET|metrics", "GET|metrics/*", "POST|metrics/config", "POST|metrics/config" };
 
-  private OEnterpriseServer enterpriseServer;
-  private OMetricsRegistry registry;
+  private OEnterpriseServer      enterpriseServer;
+  private OMetricsRegistry       registry;
   private OrientDBMetricsService service;
-  private ObjectMapper mapper = new ObjectMapper();
+  private ObjectMapper           mapper = new ObjectMapper();
 
   public OrientDBMetricsCommand(OEnterpriseServer enterpriseServer, OMetricsRegistry registry, OrientDBMetricsService settings) {
     super(EnterprisePermissions.SERVER_METRICS.toString());
@@ -114,15 +114,15 @@ public class OrientDBMetricsCommand extends OServerCommandAuthenticatedServerAbs
         } else {
 
           Map<String, ODocument> responses = enterpriseServer.getNodesManager().sendAll(new NewEnterpriseStatsTask()).stream()
-                  .collect(Collectors.toMap(OperationResponseFromNode::getSenderNodeName, (r) -> {
-                    NodeResponse node = r.getNodeResponse();
-                    if (node != null && node.getResponseType() == 1) {
-                      ResponseOk ok = (ResponseOk) node;
-                      EnterpriseStatsResponse response = (EnterpriseStatsResponse) ok.getPayload();
-                      return new ODocument().fromJSON(response.getStats());
-                    }
-                    return new ODocument();
-                  }));
+              .collect(Collectors.toMap(OperationResponseFromNode::getSenderNodeName, (r) -> {
+                NodeResponse node = r.getNodeResponse();
+                if (node != null && node.getResponseType() == 1) {
+                  ResponseOk ok = (ResponseOk) node;
+                  EnterpriseStatsResponse response = (EnterpriseStatsResponse) ok.getPayload();
+                  return new ODocument().fromJSON(response.getStats());
+                }
+                return new ODocument();
+              }));
 
           metrics.field("clusterStats", responses);
 
@@ -137,8 +137,9 @@ public class OrientDBMetricsCommand extends OServerCommandAuthenticatedServerAbs
         metrics.field("clusterStats", singleNodeStats);
       }
       metrics.setProperty("distributed", manager != null);
+      metrics.setProperty("nodeName", manager != null ? manager.getLocalNodeName() : "orientdb");
       iResponse
-              .send(OHttpUtils.STATUS_OK_CODE, OHttpUtils.STATUS_OK_DESCRIPTION, OHttpUtils.CONTENT_JSON, metrics.toJSON(""), null);
+          .send(OHttpUtils.STATUS_OK_CODE, OHttpUtils.STATUS_OK_DESCRIPTION, OHttpUtils.CONTENT_JSON, metrics.toJSON(""), null);
     } else {
       String command = parts[1];
 
@@ -150,8 +151,8 @@ public class OrientDBMetricsCommand extends OServerCommandAuthenticatedServerAbs
           try {
             TextFormat.write004(writer, CollectorRegistry.defaultRegistry.filteredMetricFamilySamples(Collections.emptySet()));
             iResponse
-                    .send(OHttpUtils.STATUS_OK_CODE, OHttpUtils.STATUS_OK_DESCRIPTION, OHttpUtils.CONTENT_TEXT_PLAIN, writer.toString(),
-                            null);
+                .send(OHttpUtils.STATUS_OK_CODE, OHttpUtils.STATUS_OK_DESCRIPTION, OHttpUtils.CONTENT_TEXT_PLAIN, writer.toString(),
+                    null);
           } finally {
             try {
               writer.close();
@@ -168,7 +169,7 @@ public class OrientDBMetricsCommand extends OServerCommandAuthenticatedServerAbs
       case "list":
         Map<String, MetricInfo> metrics = getMetricsLists(registry.getMetrics(), "");
         String metricsAsString = mapper
-                .writeValueAsString(new MetricList(metrics.entrySet().stream().map(v -> v.getValue()).collect(Collectors.toList())));
+            .writeValueAsString(new MetricList(metrics.entrySet().stream().map(v -> v.getValue()).collect(Collectors.toList())));
         iResponse.send(OHttpUtils.STATUS_OK_CODE, OHttpUtils.STATUS_OK_DESCRIPTION, OHttpUtils.CONTENT_JSON, metricsAsString, null);
         break;
       }
