f5825107b7fba60fb8b94126ff354b0b7f4fb067
Enrico Risa <enrico.risa@gmail.com>
2020-03-17 16:46:15 +0100

    added cluster messagging metrics

--- src/app/administration/stats/cluster/overview/clusteroverview.component.ts
+++ src/app/administration/stats/cluster/overview/clusteroverview.component.ts
@@ -21,8 +21,13 @@ class ClusterOverviewComponent implements OnInit, OnChanges {
   @Input()
   private stats;
   handle: any;
-
-  
+  messages: any[];
+  members = [];
+  totalMessages: {};
+  totalMessagesServer: {};
+  total: number;
+  latenciesTotal: {};
+  totalLatency: number;
 
   constructor(private metrics: MetricService) {}
 
@@ -42,7 +47,62 @@ class ClusterOverviewComponent implements OnInit, OnChanges {
   fetchMetrics() {
     this.metrics.getMetrics().then(data => {
       this.stats = data;
+      this.refreshDistributedStats();
     });
+  }
+
+  refreshDistributedStats() {
+    this.members = this.stats.members;
+
+    let messages = [];
+
+    let totalMessages = {};
+    let totalMessagesServer = {};
+    let total = 0;
+
+    let totalLatency = 0;
+    let latenciesTotal = {};
+
+    this.members.forEach(function(m) {
+      messages = messages.concat(
+        Object.keys(m.messages).filter(function(k) {
+          if (!totalMessages[k]) {
+            totalMessages[k] = 0;
+          }
+          if (!totalMessagesServer[m.name]) {
+            totalMessagesServer[m.name] = 0;
+          }
+          totalMessagesServer[m.name] += m.messages[k];
+          total += m.messages[k];
+          totalMessages[k] += m.messages[k];
+          return messages.indexOf(k) == -1;
+        })
+      );
+      if (!latenciesTotal[m.name]) {
+        latenciesTotal[m.name] = {};
+        latenciesTotal[m.name].in = 0;
+        latenciesTotal[m.name].out = 0;
+      }
+      Object.keys(m.latencies).forEach(function(n) {
+        if (!latenciesTotal[n]) {
+          latenciesTotal[n] = {};
+          latenciesTotal[n].in = 0;
+          latenciesTotal[n].out = 0;
+        }
+        if (n !== m.name) {
+          totalLatency += m.latencies[n].entries;
+          latenciesTotal[m.name].out += m.latencies[n].entries;
+          latenciesTotal[n].in += m.latencies[n].entries;
+        }
+      });
+    });
+
+    this.messages = messages;
+    this.totalMessages = totalMessages;
+    this.totalMessagesServer = totalMessagesServer;
+    this.total = total;
+    this.latenciesTotal = latenciesTotal;
+    this.totalLatency = totalLatency;
   }
 }
 
