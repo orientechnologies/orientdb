7f8d84adb84e2fc2f39086e565305c9c7861a7ee
Tglman <tglman@tglman.com>
2020-04-21 13:10:25 +0100

    copied lock manager implementation from new distriubted module

---
+++ distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/lock/ODistributedLockManager.java
@@ -0,0 +1,17 @@
+package com.orientechnologies.orient.server.distributed.impl.lock;
+
+import com.orientechnologies.common.util.OPair;
+import com.orientechnologies.orient.core.id.ORID;
+
+import java.util.List;
+import java.util.SortedSet;
+
+public interface ODistributedLockManager {
+
+  void lock(SortedSet<ORID> rids, SortedSet<OPair<String, String>> indexKeys, OnLocksAcquired acquired);
+
+  void unlock(List<OLockGuard> guards);
+
+  void lockResource(String name, OnLocksAcquired acquired);
+
+}
