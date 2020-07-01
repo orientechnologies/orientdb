269ccd6a70d506b951b05b71d684c66448d05de4
pxsalehi <pxsalehi@gmail.com>
2020-07-01 16:45:55 +0200

    use watch to wait for ready replicas

--- distributed/src/test/java/com/orientechnologies/orient/server/distributed/BasicSyncIT.java
+++ distributed/src/test/java/com/orientechnologies/orient/server/distributed/BasicSyncIT.java
@@ -34,66 +34,20 @@ public class BasicSyncIT {
     setup.start();
 
     OrientDB remote =
-        new OrientDB(setup.getRemoteAddress(), "root", "test", OrientDBConfig.defaultConfig());
+        new OrientDB(
+            "remote:" + setup.getAddress(server0, TestSetup.PortType.BINARY),
+            "root",
+            "test",
+            OrientDBConfig.defaultConfig());
     remote.create("test", ODatabaseType.PLOCAL);
     remote.close();
     System.out.println("created database 'test'");
   }
 
   @Test
-  public void sync2()
-      throws ClassNotFoundException, InstantiationException, IllegalAccessException, IOException,
-          InterruptedException, ApiException {
-    //    String remoteAddress = "remote:localhost";
-    //    String remoteAddress = "remote:" +
-    // nodeParams.get(0).get(StatefulSetTemplateKeys.ORIENTDB_BINARY_ADDRESS);
-    //    try (OrientDB remote = new OrientDB(remoteAddress, OrientDBConfig.defaultConfig())) {
-    //      try (ODatabaseSession session = remote.open("test", "admin", "admin")) {
-    //        session.createClass("One");
-    //        session.save(session.newElement("One"));
-    //        session.save(session.newElement("One"));
-    //      }
-    //      server2.shutdown();
-    //      try (ODatabaseSession session = remote.open("test", "admin", "admin")) {
-    //        session.save(session.newElement("One"));
-    //      }
-    //      System.out.println("saved session.");
-    //    }
-    setup.shutdownServer(server2);
-    Thread.sleep(10000);
-    setup.startServer(server2);
-    //    server0.shutdown();
-    //    server1.shutdown();
-    //    // Starting the servers in reverse shutdown order to trigger miss sync
-    //    server0 = OServer.startFromClasspathConfig("orientdb-simple-dserver-config-0.xml");
-    //    server1 = OServer.startFromClasspathConfig("orientdb-simple-dserver-config-1.xml");
-    //    server2 = OServer.startFromClasspathConfig("orientdb-simple-dserver-config-2.xml");
-    //    // Test server 0
-    //    try (OrientDB remote = new OrientDB("remote:localhost", OrientDBConfig.defaultConfig())) {
-    //      try (ODatabaseSession session = remote.open("test", "admin", "admin")) {
-    //        assertEquals(session.countClass("One"), 3);
-    //      }
-    //    }
-    //    // Test server 1
-    //    try (OrientDB remote = new OrientDB("remote:localhost:2425",
-    // OrientDBConfig.defaultConfig())) {
-    //      try (ODatabaseSession session = remote.open("test", "admin", "admin")) {
-    //        assertEquals(session.countClass("One"), 3);
-    //      }
-    //    }
-    //    // Test server 2
-    //    try (OrientDB remote = new OrientDB("remote:localhost:2426",
-    // OrientDBConfig.defaultConfig())) {
-    //      try (ODatabaseSession session = remote.open("test", "admin", "admin")) {
-    //        assertEquals(session.countClass("One"), 3);
-    //      }
-    //    }
-  }
-
-  @Test
-  @Ignore
   public void sync() {
-    try (OrientDB remote = new OrientDB("remote:localhost", OrientDBConfig.defaultConfig())) {
+    String remoteAddress = "remote:" + setup.getAddress(server0, TestSetup.PortType.BINARY);
+    try (OrientDB remote = new OrientDB(remoteAddress, OrientDBConfig.defaultConfig())) {
       try (ODatabaseSession session = remote.open("test", "admin", "admin")) {
         session.createClass("One");
         session.save(session.newElement("One"));
@@ -111,19 +65,21 @@ public class BasicSyncIT {
     setup.startServer(server1);
     setup.startServer(server2);
     // Test server 0
-    try (OrientDB remote = new OrientDB("remote:localhost", OrientDBConfig.defaultConfig())) {
+    try (OrientDB remote = new OrientDB(remoteAddress, OrientDBConfig.defaultConfig())) {
       try (ODatabaseSession session = remote.open("test", "admin", "admin")) {
         assertEquals(session.countClass("One"), 3);
       }
     }
     // Test server 1
-    try (OrientDB remote = new OrientDB("remote:localhost:2425", OrientDBConfig.defaultConfig())) {
+    String server1Address = "remote:" + setup.getAddress(server1, TestSetup.PortType.BINARY);
+    try (OrientDB remote = new OrientDB(server1Address, OrientDBConfig.defaultConfig())) {
       try (ODatabaseSession session = remote.open("test", "admin", "admin")) {
         assertEquals(session.countClass("One"), 3);
       }
     }
     // Test server 2
-    try (OrientDB remote = new OrientDB("remote:localhost:2426", OrientDBConfig.defaultConfig())) {
+    String server2Address = "remote:" + setup.getAddress(server2, TestSetup.PortType.BINARY);
+    try (OrientDB remote = new OrientDB(server2Address, OrientDBConfig.defaultConfig())) {
       try (ODatabaseSession session = remote.open("test", "admin", "admin")) {
         assertEquals(session.countClass("One"), 3);
       }
