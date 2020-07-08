a908ab19d0d5cd54f7cda89efbaf84e8a0c18c7b
pxsalehi <pxsalehi@gmail.com>
2020-07-08 15:24:22 +0200

    bring back init container, limit RBAC

--- distributed/src/test/java/com/orientechnologies/orient/server/distributed/BasicSyncIT.java
+++ distributed/src/test/java/com/orientechnologies/orient/server/distributed/BasicSyncIT.java
@@ -21,9 +21,6 @@ public class BasicSyncIT {
   private TestConfig config;
   private String server0, server1, server2;
 
-  // todo: Do not tear down after each test. wait for tear down to remove everything, or use random
-  // names!
-
   @Before
   public void before() throws Exception {
     config = new SimpleDServerConfig();
@@ -33,9 +30,6 @@ public class BasicSyncIT {
     setup = TestSetupUtil.create(config);
     System.out.println("created config");
     setup.start();
-
-    System.out.println("waiting ...");
-    Thread.sleep(120000);
 
     OrientDB remote = setup.createRemote(server0, "root", "test", OrientDBConfig.defaultConfig());
     remote.create("test", ODatabaseType.PLOCAL);
@@ -50,27 +44,21 @@ public class BasicSyncIT {
         session.createClass("One");
         session.save(session.newElement("One"));
         session.save(session.newElement("One"));
-        System.out.println("created class and elements.");
       }
-      System.out.println("shutting down server2");
-
+      // TODO: Without this sleep, test fails in a distributed setup!
       try {
         Thread.sleep(5000);
       } catch (InterruptedException e) {
-        e.printStackTrace();
       }
 
       setup.shutdownServer(server2);
       try (ODatabaseSession session = remote.open("test", "admin", "admin")) {
         session.save(session.newElement("One"));
-        System.out.println("created another element.");
       }
     }
-
-    System.out.println("shutting down server0 and server1");
     setup.shutdownServer(server0);
     setup.shutdownServer(server1);
-    System.out.println("starting servers again");
+
     setup.startServer(server0);
     setup.startServer(server1);
     setup.startServer(server2);
@@ -101,11 +89,6 @@ public class BasicSyncIT {
         session.createClass("One");
         session.save(session.newElement("One"));
         session.save(session.newElement("One"));
-      }
-      try {
-        Thread.sleep(10000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
       }
       setup.shutdownServer(server2);
       try (ODatabaseSession session = remote.open("test", "admin", "admin")) {
@@ -141,10 +124,10 @@ public class BasicSyncIT {
   @After
   public void after() {
     try {
-//      OrientDB remote = setup.createRemote(server0, "root", "test", OrientDBConfig.defaultConfig());
-//      remote.drop("test");
-//      remote.close();
-//      System.out.println("dropped and closed!");
+      OrientDB remote = setup.createRemote(server0, "root", "test", OrientDBConfig.defaultConfig());
+      remote.drop("test");
+      remote.close();
+      System.out.println("dropped and closed!");
     } finally {
       setup.teardown();
       ODatabaseDocumentTx.closeAll();
