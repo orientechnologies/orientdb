bb747be29582191206cdd1dd0c3ee930fbe2a4c5
U-GLOBAL\D037559 <daniel.ritter@sap.com>
2020-08-03 11:18:12 +0200

    re-activate vpm

--- core/src/main/java/com/orientechnologies/orient/core/index/engine/v1/OCellBTreeSingleValueIndexEngine.java
+++ core/src/main/java/com/orientechnologies/orient/core/index/engine/v1/OCellBTreeSingleValueIndexEngine.java
@@ -15,6 +15,9 @@ import com.orientechnologies.orient.core.storage.impl.local.paginated.atomicoper
 import com.orientechnologies.orient.core.storage.index.sbtree.singlevalue.OCellBTreeSingleValue;
 import com.orientechnologies.orient.core.storage.index.sbtree.singlevalue.v1.CellBTreeSingleValueV1;
 import com.orientechnologies.orient.core.storage.index.sbtree.singlevalue.v3.CellBTreeSingleValueV3;
+import com.orientechnologies.orient.core.storage.index.versionmap.OVersionPositionMap;
+import com.orientechnologies.orient.core.storage.index.versionmap.OVersionPositionMapV0;
+
 import java.io.IOException;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -25,7 +28,7 @@ public final class OCellBTreeSingleValueIndexEngine
   private static final String NULL_BUCKET_FILE_EXTENSION = ".nbt";
 
   private final OCellBTreeSingleValue<Object> sbTree;
-  // TODO: [DR] private final OVersionPositionMap versionPositionMap;
+  private final OVersionPositionMap versionPositionMap;
   private final String name;
   private final int id;
 
@@ -45,8 +48,8 @@ public final class OCellBTreeSingleValueIndexEngine
     } else {
       throw new IllegalStateException("Invalid tree version " + version);
     }
-    // TODO: [DR] activate and check input
-    // versionPositionMap = new OVersionPositionMapV0(storage, null, null, DATA_FILE_EXTENSION);
+    versionPositionMap =
+        new OVersionPositionMapV0(storage, name, name + DATA_FILE_EXTENSION, OVersionPositionMap.DEF_EXTENSION);
   }
 
   @Override
