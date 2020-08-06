c676254fa239cb34a46cfc4f045e85bed582ec50
laa <lomakin.andrey@gmail.com>
2020-08-06 13:31:58 +0300

    Component name included inside of exception.

--- core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/paginated/atomicoperations/OAtomicOperationsManager.java
+++ core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/paginated/atomicoperations/OAtomicOperationsManager.java
@@ -48,6 +48,7 @@ import java.util.Objects;
  * @since 12/3/13
  */
 public class OAtomicOperationsManager {
+
   private static volatile ThreadLocal<OAtomicOperation> currentOperation = new ThreadLocal<>();
 
   static {
@@ -214,7 +215,8 @@ public class OAtomicOperationsManager {
     } catch (Exception e) {
       throw OException.wrapException(
           new OStorageException(
-              "Exception during execution of component operation inside of storage "
+              "Exception during execution of component operation inside component " + lockName +
+                  " in storage "
                   + storage.getName()),
           e);
     } finally {
@@ -428,7 +430,7 @@ public class OAtomicOperationsManager {
    * Acquires exclusive lock with the given lock name in the given atomic operation.
    *
    * @param operation the atomic operation to acquire the lock in.
-   * @param lockName the lock name to acquire.
+   * @param lockName  the lock name to acquire.
    */
   public void acquireExclusiveLockTillOperationComplete(
       OAtomicOperation operation, String lockName) {
