9eb5b28164e1bf2c1032a77f66ea9008746195b2
laa <lomakin.andrey@gmail.com>
2020-08-20 18:24:19 +0300

    Error in push message unit test was fixed.

--- server/src/test/java/com/orientechnologies/orient/server/network/PushMessageUnitTest.java
+++ server/src/test/java/com/orientechnologies/orient/server/network/PushMessageUnitTest.java
@@ -8,6 +8,7 @@ import com.orientechnologies.orient.client.remote.message.OBinaryPushResponse;
 import com.orientechnologies.orient.core.config.OContextConfiguration;
 import com.orientechnologies.orient.enterprise.channel.binary.OChannelDataInput;
 import com.orientechnologies.orient.enterprise.channel.binary.OChannelDataOutput;
+import com.orientechnologies.orient.server.NoOpMemoryManager;
 import com.orientechnologies.orient.server.OServer;
 import com.orientechnologies.orient.server.network.protocol.binary.ONetworkProtocolBinary;
 import org.junit.Before;
@@ -120,6 +121,8 @@ public class PushMessageUnitTest {
     this.channelBinaryClient = new MockPipeChannel(inputClient, outputClient);
     this.channelBinaryServer = new MockPipeChannel(inputServer, outputServer);
     Mockito.when(server.getContextConfiguration()).thenReturn(new OContextConfiguration());
+    Mockito.when(server.getMemoryManager()).thenReturn(new NoOpMemoryManager());
+
     Mockito.when(remote.getNetwork(Mockito.anyString())).thenReturn(channelBinaryClient);
     Mockito.when(remote.createPush((byte) 100)).thenReturn(new MockPushRequest());
     Mockito.when(remote.createPush((byte) 101)).thenReturn(new MockPushRequestNoResponse());
