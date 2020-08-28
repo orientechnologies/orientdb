ce95f1f1fdad0d6709e63e182f5bee84585f41a5
pxsalehi <pxsalehi@gmail.com>
2020-08-28 15:12:59 +0200

    move ConcurrentIndexDefinitionIT to new setup

--- distributed/src/test/java/com/orientechnologies/orient/server/distributed/ConcurrentIndexDefinitionIT.java
+++ distributed/src/test/java/com/orientechnologies/orient/server/distributed/ConcurrentIndexDefinitionIT.java
@@ -1,8 +1,5 @@
 package com.orientechnologies.orient.server.distributed;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import com.orientechnologies.orient.core.db.ODatabaseSession;
 import com.orientechnologies.orient.core.db.ODatabaseType;
 import com.orientechnologies.orient.core.db.OrientDB;
@@ -10,28 +7,38 @@ import com.orientechnologies.orient.core.db.OrientDBConfig;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
 import com.orientechnologies.orient.core.metadata.schema.OType;
-import com.orientechnologies.orient.server.OServer;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import com.orientechnologies.orient.setup.SetupConfig;
+import com.orientechnologies.orient.setup.TestSetup;
+import com.orientechnologies.orient.setup.TestSetupUtil;
+import com.orientechnologies.orient.setup.configs.SimpleDServerConfig;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class ConcurrentIndexDefinitionIT {
 
-  private OServer server0;
-  private OServer server1;
-  private OServer server2;
+  private TestSetup setup;
+  private SetupConfig config;
+  private String server0, server1, server2;
   private OrientDB remote;
 
   @Before
   public void before() throws Exception {
-    server0 = OServer.startFromClasspathConfig("orientdb-simple-dserver-config-0.xml");
-    server1 = OServer.startFromClasspathConfig("orientdb-simple-dserver-config-1.xml");
-    server2 = OServer.startFromClasspathConfig("orientdb-simple-dserver-config-2.xml");
-    remote = new OrientDB("remote:localhost", "root", "test", OrientDBConfig.defaultConfig());
+    config = new SimpleDServerConfig();
+    server0 = SimpleDServerConfig.SERVER0;
+    server1 = SimpleDServerConfig.SERVER1;
+    server2 = SimpleDServerConfig.SERVER2;
+    setup = TestSetupUtil.create(config);
+    setup.setup();
+    remote = setup.createRemote(server0, "root", "test", OrientDBConfig.defaultConfig());
     remote.create("test", ODatabaseType.PLOCAL);
   }
 
@@ -63,8 +70,7 @@ public class ConcurrentIndexDefinitionIT {
     assertTrue(session.getMetadata().getSchema().existsClass("Test1"));
     assertFalse(session.getMetadata().getSchema().getClass("Test1").getIndexes().isEmpty());
 
-    OrientDB remote1 =
-        new OrientDB("remote:localhost:2425", "root", "test", OrientDBConfig.defaultConfig());
+    OrientDB remote1 = setup.createRemote(server1, "root", "test", OrientDBConfig.defaultConfig());
     ODatabaseSession session1 = remote1.open("test", "admin", "admin");
     assertTrue(session1.getMetadata().getSchema().existsClass("Test"));
     assertFalse(session1.getMetadata().getSchema().getClass("Test").getIndexes().isEmpty());
@@ -74,8 +80,7 @@ public class ConcurrentIndexDefinitionIT {
     session1.close();
     remote1.close();
 
-    OrientDB remote2 =
-        new OrientDB("remote:localhost:2426", "root", "test", OrientDBConfig.defaultConfig());
+    OrientDB remote2 = setup.createRemote(server2, "root", "test", OrientDBConfig.defaultConfig());
     // Make sure the created database is propagated
     ODatabaseSession session2 = remote2.open("test", "admin", "admin");
     assertTrue(session2.getMetadata().getSchema().existsClass("Test"));
@@ -89,12 +94,12 @@ public class ConcurrentIndexDefinitionIT {
 
   @After
   public void after() {
-    remote.drop("test");
-    remote.close();
-
-    server0.shutdown();
-    server1.shutdown();
-    server2.shutdown();
+    try {
+      remote.drop("test");
+      remote.close();
+    } finally {
+      setup.teardown();
+    }
     ODatabaseDocumentTx.closeAll();
   }
 }
