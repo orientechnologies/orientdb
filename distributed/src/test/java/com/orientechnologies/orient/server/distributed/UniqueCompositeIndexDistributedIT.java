b2a1528dd2d19e963acdb7121ad9ec6eb2577f4e
pxsalehi <pxsalehi@gmail.com>
2020-08-28 16:38:44 +0200

    move UniqueCompositeIndexDistributedIT to the new setup

--- distributed/src/test/java/com/orientechnologies/orient/server/distributed/UniqueCompositeIndexDistributedIT.java
+++ distributed/src/test/java/com/orientechnologies/orient/server/distributed/UniqueCompositeIndexDistributedIT.java
@@ -1,8 +1,5 @@
 package com.orientechnologies.orient.server.distributed;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import com.orientechnologies.orient.core.db.ODatabaseSession;
 import com.orientechnologies.orient.core.db.ODatabaseType;
 import com.orientechnologies.orient.core.db.OrientDB;
@@ -13,33 +10,41 @@ import com.orientechnologies.orient.core.metadata.schema.OClass;
 import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.OElement;
 import com.orientechnologies.orient.core.sql.executor.OResultSet;
-import com.orientechnologies.orient.server.OServer;
+import com.orientechnologies.orient.setup.SetupConfig;
+import com.orientechnologies.orient.setup.TestSetup;
+import com.orientechnologies.orient.setup.TestSetupUtil;
+import com.orientechnologies.orient.setup.configs.SimpleDServerConfig;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class UniqueCompositeIndexDistributedIT {
 
-  private OServer server0;
-  private OServer server1;
-  private OServer server2;
+  private TestSetup setup;
+  private SetupConfig config;
+  private String server0, server1, server2;
   private OrientDB remote;
   private ODatabaseSession session;
-  private String indexName;
 
   @Before
   public void before() throws Exception {
-    server0 = OServer.startFromClasspathConfig("orientdb-simple-dserver-config-0.xml");
-    server1 = OServer.startFromClasspathConfig("orientdb-simple-dserver-config-1.xml");
-    server2 = OServer.startFromClasspathConfig("orientdb-simple-dserver-config-2.xml");
-    remote = new OrientDB("remote:localhost", "root", "test", OrientDBConfig.defaultConfig());
+    config = new SimpleDServerConfig();
+    server0 = SimpleDServerConfig.SERVER0;
+    server1 = SimpleDServerConfig.SERVER1;
+    server2 = SimpleDServerConfig.SERVER2;
+    setup = TestSetupUtil.create(config);
+    setup.setup();
+
+    remote = setup.createRemote(server0, "root", "test", OrientDBConfig.defaultConfig());
     remote.create("test", ODatabaseType.PLOCAL);
     session = remote.open("test", "admin", "admin");
     OClass clazz = session.createClass("Test");
     clazz.createProperty("test", OType.STRING);
     clazz.createProperty("testa", OType.STRING);
     OIndex idx = clazz.createIndex("cu", OClass.INDEX_TYPE.UNIQUE, "test", "testa");
-    indexName = idx.getName();
   }
 
   @Test
@@ -64,14 +69,14 @@ public class UniqueCompositeIndexDistributedIT {
 
   @After
   public void after() throws InterruptedException {
-    session.activateOnCurrentThread();
-    session.close();
-    remote.drop("test");
-    remote.close();
-
-    server0.shutdown();
-    server1.shutdown();
-    server2.shutdown();
-    ODatabaseDocumentTx.closeAll();
+    try {
+      session.activateOnCurrentThread();
+      session.close();
+      remote.drop("test");
+      remote.close();
+    } finally {
+      setup.teardown();
+      ODatabaseDocumentTx.closeAll();
+    }
   }
 }
