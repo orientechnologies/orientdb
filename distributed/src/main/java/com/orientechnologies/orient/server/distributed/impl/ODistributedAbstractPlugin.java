8c6d1c035c65337c5d818f97b9ab307fa2df829e
laa <lomakin.andrey@gmail.com>
2020-10-06 17:40:11 +0300

    Re-try in backup if atomic moves are not supported.

--- distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/ODistributedAbstractPlugin.java
+++ distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/ODistributedAbstractPlugin.java
@@ -107,6 +107,7 @@ import com.orientechnologies.orient.server.plugin.OServerPluginAbstract;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.AtomicMoveNotSupportedException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -951,9 +952,9 @@ public abstract class ODistributedAbstractPlugin extends OServerPluginAbstract
         return false;
       }
     }
-      
+
     final ODistributedDatabaseImpl distrDatabase = messageService.registerDatabase(databaseName, null);
-    
+
     try {
       installingDatabases.add(databaseName);
       return executeInDistributedDatabaseLock(databaseName, 20000, null,
@@ -1575,27 +1576,10 @@ public abstract class ODistributedAbstractPlugin extends OServerPluginAbstract
     final File backupFullPath = new File(backupPath);
     try {
       if (backupFullPath.exists()) {
-        // delete directory and its content
-        Files.walkFileTree(
-            backupFullPath.toPath(),
-            new SimpleFileVisitor<Path>() {
-              @Override
-              public FileVisitResult postVisitDirectory(Path dir, IOException exc)
-                  throws IOException {
-                Files.delete(dir);
-                return FileVisitResult.CONTINUE;
-              }
-
-              @Override
-              public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
-                  throws IOException {
-                Files.delete(file);
-                return FileVisitResult.CONTINUE;
-              }
-            });
-      } else {
-        Files.createDirectories(backupFullPath.toPath());
+        deleteRecursively(backupFullPath);
       }
+
+      Files.createDirectories(backupFullPath.toPath());
 
       // move the database on current node
       ODistributedServerLog.warn(
@@ -1610,11 +1594,22 @@ public abstract class ODistributedAbstractPlugin extends OServerPluginAbstract
 
       final File oldDirectory = new File(dbpath);
       if (oldDirectory.exists() && oldDirectory.isDirectory()) {
-        Files.move(
-            oldDirectory.toPath(),
-            backupFullPath.toPath(),
-            StandardCopyOption.REPLACE_EXISTING,
-            StandardCopyOption.ATOMIC_MOVE);
+        try {
+          Files.move(
+              oldDirectory.toPath(), backupFullPath.toPath(), StandardCopyOption.ATOMIC_MOVE);
+        } catch (AtomicMoveNotSupportedException e) {
+          OLogManager.instance()
+              .errorNoDb(
+                  this,
+                  "Atomic moves not supported during database backup, will try not atomic move",
+                  null);
+          if (backupFullPath.exists()) {
+            deleteRecursively(backupFullPath);
+          }
+          Files.createDirectories(backupFullPath.toPath());
+
+          Files.move(oldDirectory.toPath(), backupFullPath.toPath());
+        }
       }
     } catch (IOException e) {
       ODistributedServerLog.warn(
@@ -1631,12 +1626,34 @@ public abstract class ODistributedAbstractPlugin extends OServerPluginAbstract
     }
   }
 
+  private void deleteRecursively(final File path) throws IOException {
+    // delete directory and its content
+    Files.walkFileTree(
+        path.toPath(),
+        new SimpleFileVisitor<Path>() {
+          @Override
+          public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Files.delete(dir);
+            return FileVisitResult.CONTINUE;
+          }
+
+          @Override
+          public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+              throws IOException {
+            Files.delete(file);
+            return FileVisitResult.CONTINUE;
+          }
+        });
+  }
+
   /**
    * Installs a database from the network.
    */
   protected void installDatabaseFromNetwork(final String dbPath, final String databaseName,
-      final ODistributedDatabaseImpl distrDatabase, final String iNode, final ODistributedDatabaseChunk firstChunk,
-      final boolean delta, final File uniqueClustersBackupDirectory, final OModifiableDistributedConfiguration cfg) {
+      final ODistributedDatabaseImpl distrDatabase, final String iNode,
+      final ODistributedDatabaseChunk firstChunk,
+      final boolean delta, final File uniqueClustersBackupDirectory,
+      final OModifiableDistributedConfiguration cfg) {
 
     final String localNodeName = nodeName;
 
