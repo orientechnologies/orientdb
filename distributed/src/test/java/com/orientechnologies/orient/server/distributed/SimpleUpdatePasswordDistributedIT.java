4b203a1e98bc3110080f419bde2fccbede1bc401
Tglman <tglman@tglman.com>
2020-10-16 13:43:20 +0100

    migrated new distributed test to dynamic test setup

--- distributed/src/test/java/com/orientechnologies/orient/server/distributed/SimpleUpdatePasswordDistributedIT.java
+++ distributed/src/test/java/com/orientechnologies/orient/server/distributed/SimpleUpdatePasswordDistributedIT.java
@@ -9,24 +9,30 @@ import com.orientechnologies.orient.core.db.OrientDBConfig;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
 import com.orientechnologies.orient.core.sql.executor.OResultSet;
 import com.orientechnologies.orient.server.OServer;
+import com.orientechnologies.orient.setup.TestSetup;
+import com.orientechnologies.orient.setup.TestSetupUtil;
+import com.orientechnologies.orient.setup.configs.SimpleDServerConfig;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SimpleUpdatePasswordDistributedIT {
 
-  private OServer server0;
-  private OServer server1;
-  private OServer server2;
   private OrientDB remote;
   private ODatabaseSession session;
+  private SimpleDServerConfig config;
+  private TestSetup setup;
 
   @Before
   public void before() throws Exception {
-    server0 = OServer.startFromClasspathConfig("orientdb-simple-dserver-config-0.xml");
-    server1 = OServer.startFromClasspathConfig("orientdb-simple-dserver-config-1.xml");
-    server2 = OServer.startFromClasspathConfig("orientdb-simple-dserver-config-2.xml");
-    remote = new OrientDB("remote:localhost", "root", "test", OrientDBConfig.defaultConfig());
+    config = new SimpleDServerConfig();
+    setup = TestSetupUtil.create(config);
+    setup.setup();
+
+    remote =
+        setup.createRemote(
+            SimpleDServerConfig.SERVER0, "root", "test", OrientDBConfig.defaultConfig());
     remote.create("test", ODatabaseType.PLOCAL);
     session = remote.open("test", "admin", "admin");
   }
@@ -44,12 +50,12 @@ public class SimpleUpdatePasswordDistributedIT {
 
   @After
   public void after() throws InterruptedException {
-    remote.drop("test");
-    remote.close();
-
-    server0.shutdown();
-    server1.shutdown();
-    server2.shutdown();
+    try {
+      remote.drop("test");
+      remote.close();
+    } finally {
+      setup.teardown();
+    }
     ODatabaseDocumentTx.closeAll();
   }
 }
