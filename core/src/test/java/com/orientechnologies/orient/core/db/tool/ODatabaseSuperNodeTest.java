ccbb9bb625d3a183176f36914c5513d3de139b17
U-GLOBAL\D037559 <daniel.ritter@sap.com>
2020-12-14 17:17:24 +0100

    added supernode test

--- core/src/test/java/com/orientechnologies/orient/core/db/tool/ODatabaseSuperNodeTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/db/tool/ODatabaseSuperNodeTest.java
@@ -5,55 +5,99 @@ import com.orientechnologies.orient.core.db.*;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.orientechnologies.orient.core.record.OEdge;
+import com.orientechnologies.orient.core.record.OVertex;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ODatabaseSuperNodeTest {
 
-  @Test
-  public void exportImportOnlySchemaTest() throws IOException {
-    final String databaseName = "test";
+  public static void main(String[] args) {
+    final ODatabaseSuperNodeTest tester = new ODatabaseSuperNodeTest();
+
+    final String databaseName = "superNode_export";
     final String exportDbUrl =
         "memory:target/export_" + ODatabaseSuperNodeTest.class.getSimpleName();
-    final OrientDB orientDB = createDatabase(databaseName, exportDbUrl);
+    final OrientDB orientDB = tester.createDatabase(databaseName, exportDbUrl);
 
-    final ByteArrayOutputStream output = new ByteArrayOutputStream();
-    try (final ODatabaseSession db = orientDB.open(databaseName, "admin", "admin")) {
-      db.createClass("SimpleClass");
+    final List<Integer> numberEdges = Arrays.asList(100000, 500000, 1000000, 5000000, 10000000);
+    final List<Long> results = new ArrayList<>(numberEdges.size());
 
-      final ODatabaseExport export =
-          new ODatabaseExport(
-              (ODatabaseDocumentInternal) db,
-              output,
-              new OCommandOutputListener() {
-                @Override
-                public void onMessage(String iText) {}
-              });
-      export.setOptions(" -excludeAll -includeSchema=true");
-      export.exportDatabase();
+    for (int numberEdge : numberEdges) {
+      final ByteArrayOutputStream output = new ByteArrayOutputStream();
+      try (final ODatabaseSession session = orientDB.open(databaseName, "admin", "admin")) {
+        session.createClassIfNotExist("SuperNodeClass", "V");
+        session.createClassIfNotExist("NonSuperEdgeClass", "E");
+
+        // session.begin();
+        final OVertex fromNode = session.newVertex("SuperNodeClass");
+        fromNode.save();
+        final OVertex toNode = session.newVertex("SuperNodeClass");
+        toNode.save();
+
+        for (int i = 0; i < numberEdge; i++) {
+          final OEdge edge = session.newEdge(fromNode, toNode, "NonSuperEdgeClass");
+          edge.save();
+        }
+        session.commit();
+
+        final ODatabaseExport export =
+            new ODatabaseExport(
+                (ODatabaseDocumentInternal) session,
+                output,
+                new OCommandOutputListener() {
+                  @Override
+                  public void onMessage(String iText) {}
+                });
+        // export.setOptions(" -excludeAll -includeSchema=true");
+        // List of export options can be found in `ODatabaseImpExpAbstract`
+        export.setOptions(" -includeSchema=true");
+        final long start = System.nanoTime();
+        export.exportDatabase();
+        final long time = (System.nanoTime() - start) / 1000000;
+        results.add(time);
+        System.out.println("Export-" + numberEdge + "(ms)=" + time);
+
+        Thread.sleep(2000);
+      } catch (final IOException e) {
+        e.printStackTrace();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
 
-    final String importDbUrl =
+    for (int i = 0; i < results.size(); i++) {
+      System.out.println("Export-" + numberEdges.get(i) + "(ms)=" + results.get(i));
+    }
+
+    /*final String importDbUrl =
         "memory:target/import_" + ODatabaseSuperNodeTest.class.getSimpleName();
-    createDatabase(databaseName, importDbUrl);
+    tester.createDatabase(databaseName, importDbUrl);
 
     try (final ODatabaseSession db = orientDB.open(databaseName, "admin", "admin")) {
       final ODatabaseImport importer =
           new ODatabaseImport(
               (ODatabaseDocumentInternal) db,
-              new ByteArrayInputStream(output.toByteArray()),
+              "export_ODatabaseSuperNodeTest/" + databaseName,
               new OCommandOutputListener() {
                 @Override
                 public void onMessage(String iText) {}
               });
       importer.importDatabase();
-      Assert.assertTrue(db.getMetadata().getSchema().existsClass("SimpleClass"));
-    }
+      Assert.assertTrue(db.getMetadata().getSchema().existsClass("SuperNodeClass"));
+    } catch (final IOException e) {
+      e.printStackTrace();
+    }*/
     orientDB.drop(databaseName);
+    // orientDB.drop(importDatabaseName);
     orientDB.close();
   }
 
-  private OrientDB createDatabase(String database, String url) {
+  private OrientDB createDatabase(final String database, final String url) {
     final OrientDB orientDB = new OrientDB(url, OrientDBConfig.defaultConfig());
     orientDB.create(database, ODatabaseType.PLOCAL);
     return orientDB;
