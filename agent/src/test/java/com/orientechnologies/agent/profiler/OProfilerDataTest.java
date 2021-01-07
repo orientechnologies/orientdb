0e6e4f16760b1a20643b769fb9183d9209fa7a71
Luigi Dell'Aquila <luigi.dellaquila@gmail.com>
2021-01-07 16:11:13 +0100

    Add test cases

--- agent/src/test/java/com/orientechnologies/agent/profiler/OProfilerDataTest.java
+++ agent/src/test/java/com/orientechnologies/agent/profiler/OProfilerDataTest.java
@@ -6,11 +6,41 @@ import org.junit.Test;
 public class OProfilerDataTest {
 
   @Test
+  public void testBasics() {
+    OProfilerData data = new OProfilerData();
+    Assert.assertNull(data.getChrono(null));
+    Assert.assertEquals(Long.MAX_VALUE, data.getRecordingTo());
+
+    data.updateCounter(null, 10);
+    Assert.assertEquals(-1L, data.getCounter(null));
+
+    Assert.assertNull(data.getHookValue(null));
+    Assert.assertNull(data.getHookValue("foobar"));
+
+    Assert.assertNotNull(data.getCountersAsString());
+    Assert.assertNotNull(data.getChronosAsString());
+    Assert.assertNotNull(data.getCounters() );
+    Assert.assertNotNull(data.getHooks());
+    Assert.assertNotNull(data.getChronos());
+    Assert.assertNotNull(data.getStats());
+
+    Assert.assertNull(data.getStat(null));
+    Assert.assertNull(data.getStat("foobar"));
+    data.updateStat("foobar", 10L);
+    Assert.assertEquals(10L, data.getStat("foobar").last);
+
+  }
+
+
+
+  @Test
   public void testClear() {
     OProfilerData data = new OProfilerData();
     data.updateCounter("foo", 1);
     data.updateStat("foo", 1);
 
+
+    Assert.assertEquals(-1, data.getCounter("asdf"));
     Assert.assertEquals(1, data.getCounter("foo"));
     data.clear("bar");
     Assert.assertEquals(1, data.getCounter("foo"));
@@ -35,5 +65,7 @@ public class OProfilerDataTest {
     String[] result = data.getStatsAsString();
     result[0].contains("foo");
     result[0].contains("123456");
+
+    data.getChronosAsString();
   }
 }
