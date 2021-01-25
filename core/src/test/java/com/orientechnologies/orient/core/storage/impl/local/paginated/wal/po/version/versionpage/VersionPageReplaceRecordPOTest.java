dbe08b501dfed38fbdc929a6af2e3ee4d361813c
laa <lomakin.andrey@gmail.com>
2021-01-25 11:26:14 +0200

    Profiling of direct memory allocations has been implemented - compilation fixes.

--- core/src/test/java/com/orientechnologies/orient/core/storage/impl/local/paginated/wal/po/version/versionpage/VersionPageReplaceRecordPOTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/storage/impl/local/paginated/wal/po/version/versionpage/VersionPageReplaceRecordPOTest.java
@@ -1,6 +1,7 @@
 package com.orientechnologies.orient.core.storage.impl.local.paginated.wal.po.version.versionpage;
 
 import com.orientechnologies.common.directmemory.OByteBufferPool;
+import com.orientechnologies.common.directmemory.ODirectMemoryAllocator.Intention;
 import com.orientechnologies.common.directmemory.OPointer;
 import com.orientechnologies.orient.core.storage.cache.OCacheEntry;
 import com.orientechnologies.orient.core.storage.cache.OCacheEntryImpl;
@@ -19,7 +20,7 @@ public class VersionPageReplaceRecordPOTest {
     final int pageSize = OVersionPage.PAGE_SIZE;
     final OByteBufferPool byteBufferPool = new OByteBufferPool(pageSize);
     try {
-      final OPointer pointer = byteBufferPool.acquireDirect(false);
+      final OPointer pointer = byteBufferPool.acquireDirect(false, Intention.TEST);
       final OCachePointer cachePointer = new OCachePointer(pointer, byteBufferPool, 0, 0);
       final OCacheEntry entry = new OCacheEntryImpl(0, 0, cachePointer, false);
 
@@ -31,7 +32,7 @@ public class VersionPageReplaceRecordPOTest {
 
       entry.clearPageOperations();
 
-      final OPointer restoredPointer = byteBufferPool.acquireDirect(false);
+      final OPointer restoredPointer = byteBufferPool.acquireDirect(false, Intention.TEST);
       final OCachePointer restoredCachePointer =
           new OCachePointer(restoredPointer, byteBufferPool, 0, 0);
       final OCacheEntry restoredCacheEntry = new OCacheEntryImpl(0, 0, restoredCachePointer, false);
@@ -88,7 +89,7 @@ public class VersionPageReplaceRecordPOTest {
 
     final OByteBufferPool byteBufferPool = new OByteBufferPool(pageSize);
     try {
-      final OPointer pointer = byteBufferPool.acquireDirect(false);
+      final OPointer pointer = byteBufferPool.acquireDirect(false, Intention.TEST);
       final OCachePointer cachePointer = new OCachePointer(pointer, byteBufferPool, 0, 0);
       final OCacheEntry entry = new OCacheEntryImpl(0, 0, cachePointer, false);
 
