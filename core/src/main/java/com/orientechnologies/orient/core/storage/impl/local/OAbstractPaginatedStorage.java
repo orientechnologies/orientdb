ff2f8677796861d9306ffc7a75a0b9d5a60180dc
laa <lomakin.andrey@gmail.com>
2022-01-17 15:58:01 +0100

    Prevention of recursive flow of exception is case of internal error.

--- core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/OAbstractPaginatedStorage.java
+++ core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/OAbstractPaginatedStorage.java
@@ -1493,8 +1493,10 @@ public abstract class OAbstractPaginatedStorage extends OStorageAbstract
         return;
       }
 
-      error = e;
-      status = STATUS.INTERNAL_ERROR;
+      if (!(e instanceof OInternalErrorException)) {
+        error = e;
+        status = STATUS.INTERNAL_ERROR;
+      }
 
       try {
         makeStorageDirty();
@@ -3927,7 +3929,9 @@ public abstract class OAbstractPaginatedStorage extends OStorageAbstract
 
   public void moveToErrorStateIfNeeded(final Throwable error) {
     if (error != null
-        && !((error instanceof OHighLevelException) || (error instanceof ONeedRetryException))) {
+        && !((error instanceof OHighLevelException)
+            || (error instanceof ONeedRetryException)
+            || (error instanceof OInternalErrorException))) {
       this.error = error;
       status = STATUS.INTERNAL_ERROR;
     }
@@ -6489,7 +6493,8 @@ public abstract class OAbstractPaginatedStorage extends OStorageAbstract
   protected RuntimeException logAndPrepareForRethrow(
       final RuntimeException runtimeException, final boolean putInReadOnlyMode) {
     if (!(runtimeException instanceof OHighLevelException
-        || runtimeException instanceof ONeedRetryException)) {
+        || runtimeException instanceof ONeedRetryException
+        || runtimeException instanceof OInternalErrorException)) {
       if (putInReadOnlyMode) {
         this.error = runtimeException;
         status = STATUS.INTERNAL_ERROR;
@@ -6538,7 +6543,9 @@ public abstract class OAbstractPaginatedStorage extends OStorageAbstract
 
   protected RuntimeException logAndPrepareForRethrow(
       final Throwable throwable, final boolean putInReadOnlyMode) {
-    if (!(throwable instanceof OHighLevelException || throwable instanceof ONeedRetryException)) {
+    if (!(throwable instanceof OHighLevelException
+        || throwable instanceof ONeedRetryException
+        || throwable instanceof OInternalErrorException)) {
       if (putInReadOnlyMode) {
         this.error = throwable;
         status = STATUS.INTERNAL_ERROR;
