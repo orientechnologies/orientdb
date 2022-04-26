a8a07a5b811d5341d5ec51b9816fa9cf6816f26a
Paul Kendall <pkendall64@gmail.com>
2022-04-27 10:42:14 +1200

    Fix formatting

--- distributed/src/main/java/com/orientechnologies/orient/server/hazelcast/OHazelcastLockManager.java
+++ distributed/src/main/java/com/orientechnologies/orient/server/hazelcast/OHazelcastLockManager.java
@@ -4,7 +4,6 @@ import com.hazelcast.core.HazelcastInstance;
 import com.orientechnologies.common.types.OModifiableInteger;
 import com.orientechnologies.orient.server.distributed.ODistributedLockManager;
 import com.orientechnologies.orient.server.distributed.task.ODistributedLockException;
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -37,14 +36,16 @@ public class OHazelcastLockManager implements ODistributedLockManager {
       try {
         if (!hazelcast.getLock(resource).tryLock(timeout, TimeUnit.MILLISECONDS)) {
           throw new ODistributedLockException(
-                  String.format("Timed out after %d ms attempting to obtain lock for resource '%s' on node '%s'",
-                          timeout, resource, nodeSource));
+              String.format(
+                  "Timed out after %d ms attempting to obtain lock for resource '%s' on node '%s'",
+                  timeout, resource, nodeSource));
         }
       } catch (InterruptedException e) {
         Thread.currentThread().interrupt();
         throw new ODistributedLockException(
-                String.format("Interrupted while attempting to obtain lock for resource '%s' on node '%s'",
-                        timeout, resource, nodeSource));
+            String.format(
+                "Interrupted while attempting to obtain lock for resource '%s' on node '%s'",
+                timeout, resource, nodeSource));
       }
     } else {
       hazelcast.getLock(resource).lock();
