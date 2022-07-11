945f06bdcb56b41c2f0911785a569d54f236d131
Tim Whittington <tim.whittington@indexity.io>
2022-07-12 08:59:48 +1200

    Add exception details to some logging that lost stack traces.

--- distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/ODistributedDatabaseImpl.java
+++ distributed/src/main/java/com/orientechnologies/orient/server/distributed/impl/ODistributedDatabaseImpl.java
@@ -159,6 +159,7 @@ public class ODistributedDatabaseImpl implements ODistributedDatabase {
           sender,
           OUT,
           "Error on sending response '%s' back (reqId=%s err=%s)",
+          e,
           response,
           iRequestId,
           e.toString());
