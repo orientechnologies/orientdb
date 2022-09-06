d8e0f2894b57501f3bca0a8f1383ae8fe2ab9cd4
Tglman <tglman@tglman.com>
2022-09-06 11:40:55 +0100

    fix: removed tests not needed anymore

--- core/src/test/java/com/orientechnologies/orient/core/db/tool/ODatabaseImportTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/db/tool/ODatabaseImportTest.java
@@ -9,7 +9,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /** Created by tglman on 23/05/16. */
@@ -59,54 +58,6 @@ public class ODatabaseImportTest {
   }
 
   @Test
-  public void exportImportOnlySchemaTestStreamed() throws IOException {
-    final String databaseName = "testStreamed";
-    final String exportDbUrl =
-        "memory:target/exportStreamed_" + ODatabaseImportTest.class.getSimpleName();
-    final OrientDB orientDB =
-        OCreateDatabaseUtil.createDatabase(
-            databaseName, exportDbUrl, OCreateDatabaseUtil.TYPE_PLOCAL);
-
-    final ByteArrayOutputStream output = new ByteArrayOutputStream();
-    try (final ODatabaseSession db =
-        orientDB.open(databaseName, "admin", OCreateDatabaseUtil.NEW_ADMIN_PASSWORD)) {
-      db.createClass("SimpleClass");
-
-      final ODatabaseExport export =
-          new ODatabaseExport(
-              (ODatabaseDocumentInternal) db,
-              output,
-              new OCommandOutputListener() {
-                @Override
-                public void onMessage(String iText) {}
-              });
-      export.setOptions(" -excludeAll -includeSchema=true");
-      export.exportDatabase();
-    }
-
-    final String importDbUrl =
-        "memory:target/importStreamed_" + ODatabaseImportTest.class.getSimpleName();
-    OCreateDatabaseUtil.createDatabase(databaseName, importDbUrl, OCreateDatabaseUtil.TYPE_PLOCAL);
-    try (final ODatabaseSession db =
-        orientDB.open(databaseName, "admin", OCreateDatabaseUtil.NEW_ADMIN_PASSWORD)) {
-      final ODatabaseImport importer =
-          new ODatabaseImport(
-              (ODatabaseDocumentInternal) db,
-              new ByteArrayInputStream(output.toByteArray()),
-              new OCommandOutputListener() {
-                @Override
-                public void onMessage(String iText) {}
-              });
-      importer.importDatabaseStreamed();
-      Assert.assertTrue(
-          "SimpleClass schema must exist.",
-          db.getMetadata().getSchema().existsClass("SimpleClass"));
-    }
-    orientDB.drop(databaseName);
-    orientDB.close();
-  }
-
-  @Test
   public void exportImportExcludeClusters() throws IOException {
     final String databaseName = "test";
     final String exportDbUrl =
@@ -147,55 +98,6 @@ public class ODatabaseImportTest {
                 public void onMessage(String iText) {}
               });
       importer.importDatabase();
-      Assert.assertTrue(db.getMetadata().getSchema().existsClass("SimpleClass"));
-    }
-    orientDB.drop(databaseName);
-    orientDB.close();
-  }
-
-  // TODO: part of the end to end import renovation
-  @Ignore
-  @Test
-  public void exportImportExcludeClustersStreamed() throws IOException {
-    final String databaseName = "test2";
-    final String exportDbUrl =
-        "memory:target/export2_" + ODatabaseImportTest.class.getSimpleName() + "_excludeclusters";
-    final OrientDB orientDB =
-        OCreateDatabaseUtil.createDatabase(
-            databaseName, exportDbUrl, OCreateDatabaseUtil.TYPE_PLOCAL);
-
-    final ByteArrayOutputStream output = new ByteArrayOutputStream();
-    try (final ODatabaseSession db =
-        orientDB.open(databaseName, "admin", OCreateDatabaseUtil.NEW_ADMIN_PASSWORD)) {
-      db.createClass("SimpleClass");
-
-      final ODatabaseExport export =
-          new ODatabaseExport(
-              (ODatabaseDocumentInternal) db,
-              output,
-              new OCommandOutputListener() {
-                @Override
-                public void onMessage(String iText) {}
-              });
-      export.setOptions(" -includeClusterDefinitions=false");
-      export.exportDatabase();
-    }
-
-    final String importDbUrl =
-        "memory:target/import2_" + ODatabaseImportTest.class.getSimpleName() + "_excludeclusters";
-    OCreateDatabaseUtil.createDatabase(databaseName, importDbUrl, OCreateDatabaseUtil.TYPE_PLOCAL);
-
-    try (final ODatabaseSession db =
-        orientDB.open(databaseName, "admin", OCreateDatabaseUtil.NEW_ADMIN_PASSWORD)) {
-      final ODatabaseImport importer =
-          new ODatabaseImport(
-              (ODatabaseDocumentInternal) db,
-              new ByteArrayInputStream(output.toByteArray()),
-              new OCommandOutputListener() {
-                @Override
-                public void onMessage(String iText) {}
-              });
-      importer.importDatabaseStreamed();
       Assert.assertTrue(db.getMetadata().getSchema().existsClass("SimpleClass"));
     }
     orientDB.drop(databaseName);
