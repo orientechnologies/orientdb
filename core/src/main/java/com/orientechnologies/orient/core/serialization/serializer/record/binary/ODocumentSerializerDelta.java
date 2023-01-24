d8d19179f3d0cf8fcd192d0f4c2c00039c085f9d
Tglman <tglman@tglman.com>
2023-01-24 12:14:58 +0000

    fix: minor fix to handle misstyped records in delta serialization

--- core/src/main/java/com/orientechnologies/orient/core/serialization/serializer/record/binary/ODocumentSerializerDelta.java
+++ core/src/main/java/com/orientechnologies/orient/core/serialization/serializer/record/binary/ODocumentSerializerDelta.java
@@ -219,7 +219,7 @@ public class ODocumentSerializerDelta {
         deserializeDeltaEmbeddedMap(bytes, (OTrackedMap) toUpdate);
         break;
       case EMBEDDED:
-        deserializeDelta(bytes, (ODocument) toUpdate);
+        deserializeDelta(bytes, (ODocument) ((ORecord) toUpdate).getRecord());
         break;
       case LINKLIST:
         deserializeDeltaLinkList(bytes, (ORecordLazyList) toUpdate);
@@ -624,7 +624,7 @@ public class ODocumentSerializerDelta {
         serializeDeltaEmbeddedMap(bytes, (OTrackedMap) value);
         break;
       case EMBEDDED:
-        serializeDelta(bytes, (ODocument) value);
+        serializeDelta(bytes, (ODocument) ((ORecord) value).getRecord());
         break;
       case LINKLIST:
         serializeDeltaLinkList(bytes, (ORecordLazyList) value);
@@ -1064,7 +1064,7 @@ public class ODocumentSerializerDelta {
           cur.field(ODocumentSerializable.CLASS_NAME, value.getClass().getName());
           serialize(cur, bytes);
         } else {
-          serialize((ODocument) value, bytes);
+          serialize((ODocument) ((ORecord) value).getRecord(), bytes);
         }
         break;
       case EMBEDDEDSET:
