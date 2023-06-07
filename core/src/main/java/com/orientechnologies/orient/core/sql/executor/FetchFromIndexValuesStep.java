5d2d6db75911323c39a531a46de55c4d1afdf394
Tglman <tglman@tglman.com>
2023-06-07 12:44:23 +0100

    refactor: removed not needed specific logic from fetch index value

--- core/src/main/java/com/orientechnologies/orient/core/sql/executor/FetchFromIndexValuesStep.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/executor/FetchFromIndexValuesStep.java
@@ -1,24 +1,14 @@
 package com.orientechnologies.orient.core.sql.executor;
 
-import com.orientechnologies.common.exception.OException;
 import com.orientechnologies.orient.core.command.OCommandContext;
-import com.orientechnologies.orient.core.exception.OCommandExecutionException;
 import com.orientechnologies.orient.core.index.OIndex;
 
 /** Created by luigidellaquila on 02/08/16. */
 public class FetchFromIndexValuesStep extends FetchFromIndexStep {
 
-  private boolean asc;
-
   public FetchFromIndexValuesStep(
       OIndex index, boolean asc, OCommandContext ctx, boolean profilingEnabled) {
-    super(index, null, null, ctx, profilingEnabled);
-    this.asc = asc;
-  }
-
-  @Override
-  protected boolean isOrderAsc() {
-    return asc;
+    super(index, null, null, asc, ctx, profilingEnabled);
   }
 
   @Override
@@ -31,23 +21,6 @@ public class FetchFromIndexValuesStep extends FetchFromIndexStep {
       return OExecutionStepInternal.getIndent(depth, indent)
           + "+ FETCH FROM INDEX VAUES DESC "
           + index.getName();
-    }
-  }
-
-  @Override
-  public OResult serialize() {
-    OResultInternal result = (OResultInternal) super.serialize();
-    result.setProperty("asc", asc);
-    return result;
-  }
-
-  @Override
-  public void deserialize(OResult fromResult) {
-    try {
-      super.deserialize(fromResult);
-      this.asc = fromResult.getProperty("asc");
-    } catch (Exception e) {
-      throw OException.wrapException(new OCommandExecutionException(""), e);
     }
   }
 
