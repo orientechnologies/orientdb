2cb4d080be81ad92102dc70d339727418f267ad3
Tglman <tglman@tglman.com>
2023-08-29 17:45:56 +0100

    fix: correcte some license headers, issue #10020

--- tools/src/main/java/com/orientechnologies/orient/output/OOutputStreamManager.java
+++ tools/src/main/java/com/orientechnologies/orient/output/OOutputStreamManager.java
@@ -1,19 +1,21 @@
 /*
- * Copyright 2015 OrientDB LTD (info--at--orientdb.com)
- * All Rights Reserved. Commercial License.
  *
- * NOTICE:  All information contained herein is, and remains the property of
- * OrientDB LTD and its suppliers, if any.  The intellectual and
- * technical concepts contained herein are proprietary to
- * OrientDB LTD and its suppliers and may be covered by United
- * Kingdom and Foreign Patents, patents in process, and are protected by trade
- * secret or copyright law.
+ *  *  Copyright 2010-2016 OrientDB LTD (http://orientdb.com)
+ *  *
+ *  *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  *  You may obtain a copy of the License at
+ *  *
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *  Unless required by applicable law or agreed to in writing, software
+ *  *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  See the License for the specific language governing permissions and
+ *  *  limitations under the License.
+ *  *
+ *  * For more information: http://orientdb.com
  *
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from OrientDB LTD.
- *
- * For more information: http://www.orientdb.com
  */
 
 package com.orientechnologies.orient.output;
