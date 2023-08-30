7dd61acc6f5972214cc3881e36453de877131f9f
Tglman <tglman@tglman.com>
2023-08-30 17:35:08 +0100

    chore: update some license headers

--- agent/src/main/java/com/orientechnologies/agent/http/command/OServerCommandPostSecurityReload.java
+++ agent/src/main/java/com/orientechnologies/agent/http/command/OServerCommandPostSecurityReload.java
@@ -1,19 +1,19 @@
 /*
- * Copyright 2010-2016 OrientDB LTD
- * All Rights Reserved. Commercial License.
+ * Copyright 2016 OrientDB LTD (info(at)orientdb.com)
  *
- * NOTICE:  All information contained herein is, and remains the property of
- * Orient Technologies LTD and its suppliers, if any.  The intellectual and
- * technical concepts contained herein are proprietary to
- * Orient Technologies LTD and its suppliers and may be covered by United
- * Kingdom and Foreign Patents, patents in process, and are protected by trade
- * secret or copyright law.
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Orient Technologies LTD.
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * For more information: http://www.orientdb.com
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *   For more information: http://www.orientdb.com
  */
 package com.orientechnologies.agent.http.command;
 
