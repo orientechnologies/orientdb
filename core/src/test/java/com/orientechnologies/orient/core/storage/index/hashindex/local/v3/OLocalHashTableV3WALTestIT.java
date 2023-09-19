d580b8c4ec180d4a955d5350c7433ff48529bf43
Tglman <tglman@tglman.com>
2023-09-19 12:38:03 +0100

    test: disable view background execution in storage integration test, causing sporious failures

--- core/src/test/java/com/orientechnologies/orient/core/storage/index/hashindex/local/v3/OLocalHashTableV3WALTestIT.java
+++ core/src/test/java/com/orientechnologies/orient/core/storage/index/hashindex/local/v3/OLocalHashTableV3WALTestIT.java
@@ -4,6 +4,7 @@ import com.orientechnologies.common.io.OFileUtils;
 import com.orientechnologies.common.serialization.types.OIntegerSerializer;
 import com.orientechnologies.orient.core.OCreateDatabaseUtil;
 import com.orientechnologies.orient.core.config.OGlobalConfiguration;
+import com.orientechnologies.orient.core.db.ODatabaseDocumentInternal;
 import com.orientechnologies.orient.core.db.ODatabaseInternal;
 import com.orientechnologies.orient.core.db.ODatabaseSession;
 import com.orientechnologies.orient.core.db.OrientDB;
@@ -79,12 +80,16 @@ public class OLocalHashTableV3WALTestIT extends OLocalHashTableV3Base {
     // orientDB.create(ACTUAL_DB_NAME, ODatabaseType.PLOCAL);
     databaseDocumentTx =
         orientDB.open(ACTUAL_DB_NAME, "admin", OCreateDatabaseUtil.NEW_ADMIN_PASSWORD);
+    ((ODatabaseDocumentInternal) databaseDocumentTx).getSharedContext().getViewManager().close();
 
     OCreateDatabaseUtil.createDatabase(EXPECTED_DB_NAME, orientDB, OCreateDatabaseUtil.TYPE_PLOCAL);
     // orientDB.create(EXPECTED_DB_NAME, ODatabaseType.PLOCAL);
     expectedDatabaseDocumentTx =
         orientDB.open(EXPECTED_DB_NAME, "admin", OCreateDatabaseUtil.NEW_ADMIN_PASSWORD);
-
+    ((ODatabaseDocumentInternal) expectedDatabaseDocumentTx)
+        .getSharedContext()
+        .getViewManager()
+        .close();
     expectedStorage =
         ((OLocalPaginatedStorage) ((ODatabaseInternal<?>) expectedDatabaseDocumentTx).getStorage());
     actualStorage =
