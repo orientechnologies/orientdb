690e9ccc268b6720a793867de2d636cfd6e13043
Tglman <tglman@tglman.com>
2024-02-06 19:59:06 +0100

    test: restore old query engine on failing test

--- core/src/test/java/com/orientechnologies/orient/core/index/TxNonUniqueIndexWithCollationTest.java
+++ core/src/test/java/com/orientechnologies/orient/core/index/TxNonUniqueIndexWithCollationTest.java
@@ -26,7 +26,10 @@ import com.orientechnologies.BaseMemoryDatabase;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
 import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.OElement;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.OCommandSQL;
 import com.orientechnologies.orient.core.sql.executor.OResultSet;
+import com.orientechnologies.orient.core.sql.query.OLegacyResultSet;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -96,15 +99,17 @@ public class TxNonUniqueIndexWithCollationTest extends BaseMemoryDatabase {
 
     db.command("update user set name='abd' where name='Aby'").close();
 
-    final OResultSet r =
-        db.query("select * from user where name in ['Abc', 'Abd', 'Abz'] order by name");
+    final OLegacyResultSet<ODocument> r =
+        db.command(
+                new OCommandSQL(
+                    "select * from user where name in ['Abc', 'Abd', 'Abz'] order by name"))
+            .execute();
+    assertEquals(4, r.size());
+    assertEquals("abc", r.get(0).field("name"));
+    assertEquals("abd", r.get(1).field("name"));
+    assertEquals("abd", r.get(2).field("name"));
+    assertEquals("abz", r.get(3).field("name"));
 
-    assertEquals("abc", r.next().getProperty("name"));
-    assertEquals("abd", r.next().getProperty("name"));
-    assertEquals("abd", r.next().getProperty("name"));
-    assertEquals("abz", r.next().getProperty("name"));
-    assertFalse(r.hasNext());
-    r.close();
     db.commit();
   }
 }
