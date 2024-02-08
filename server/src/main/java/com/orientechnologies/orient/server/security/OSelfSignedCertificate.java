df2e6d5932e132c6edd1632392e39b7da7fdbc5d
Tglman <tglman@tglman.com>
2024-02-08 12:45:11 +0100

    fix: removed provider for self signed certificate that create issues

--- server/src/main/java/com/orientechnologies/orient/server/security/OSelfSignedCertificate.java
+++ server/src/main/java/com/orientechnologies/orient/server/security/OSelfSignedCertificate.java
@@ -200,9 +200,8 @@ public class OSelfSignedCertificate {
       var certHolder =
           certBuilder.build(
               new JcaContentSignerBuilder("SHA256WithRSA")
-                  .setProvider("BC")
                   .build(keypair.getPrivate()));
-      return new JcaX509CertificateConverter().setProvider("BC").getCertificate(certHolder);
+      return new JcaX509CertificateConverter().getCertificate(certHolder);
     } catch (OperatorCreationException e) {
       throw new RuntimeException(e);
     }
