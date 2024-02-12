2a88158cd41db3198ec9e11d89637873b01c9ace
Tglman <tglman@tglman.com>
2024-02-12 13:05:07 +0100

    fix: corrected query with content of nested json documents to use embedded class

--- core/src/main/java/com/orientechnologies/orient/core/sql/parser/OExpression.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/parser/OExpression.java
@@ -65,7 +65,7 @@ public class OExpression extends SimpleNode {
       return arrayConcatExpression.execute(iCurrentRecord, ctx);
     }
     if (json != null) {
-      return json.toMap(iCurrentRecord, ctx);
+      return json.toObjectDetermineType(iCurrentRecord, ctx);
     }
     if (booleanValue != null) {
       return booleanValue;
