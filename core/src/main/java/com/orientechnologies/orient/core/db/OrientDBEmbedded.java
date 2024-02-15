3528d7e22c374d3700fb3b94f4e17694b55b61ed
Tglman <tglman@tglman.com>
2024-02-15 17:44:01 +0100

    fix: make sure to call on drop on database drop

--- core/src/main/java/com/orientechnologies/orient/core/db/OrientDBEmbedded.java
+++ core/src/main/java/com/orientechnologies/orient/core/db/OrientDBEmbedded.java
@@ -866,6 +866,7 @@ public class OrientDBEmbedded implements OrientDBInternal {
           it.hasNext(); ) {
         it.next().onDrop(db);
       }
+      db.callOnDropListeners();
       db.close();
     } finally {
       ODatabaseRecordThreadLocal.instance().set(current);
