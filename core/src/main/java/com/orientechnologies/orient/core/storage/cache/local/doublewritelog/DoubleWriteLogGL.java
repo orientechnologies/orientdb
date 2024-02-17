374c88bab8b1142aa280fad5694dbe9665066748
laa <andrii0lomakin@gmail.com>
2024-02-17 21:04:57 +0100

    SYNC flag is removed for DWL channel because we perform sync on data write.

--- core/src/main/java/com/orientechnologies/orient/core/storage/cache/local/doublewritelog/DoubleWriteLogGL.java
+++ core/src/main/java/com/orientechnologies/orient/core/storage/cache/local/doublewritelog/DoubleWriteLogGL.java
@@ -143,8 +143,7 @@ public class DoubleWriteLogGL implements DoubleWriteLog {
   }
 
   private FileChannel createLogFile(Path path) throws IOException {
-    return FileChannel.open(
-        path, StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW, StandardOpenOption.SYNC);
+    return FileChannel.open(path, StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW);
   }
 
   private Path createLogFilePath() {
