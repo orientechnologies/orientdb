f727822882344bbf79d337433379de4bb1698c50
Tglman <tglman@tglman.com>
2024-06-25 15:55:48 +0200

    fix: add compatibility for GSON3 for rid parser

--- driver/src/main/java/org/apache/tinkerpop/gremlin/orientdb/io/graphson/OrientGraphSONV3.java
+++ driver/src/main/java/org/apache/tinkerpop/gremlin/orientdb/io/graphson/OrientGraphSONV3.java
@@ -4,6 +4,7 @@ import static org.apache.tinkerpop.gremlin.orientdb.io.OrientIoRegistry.isORecor
 import static org.apache.tinkerpop.gremlin.orientdb.io.OrientIoRegistry.newORecordId;
 
 import com.orientechnologies.orient.core.id.ORecordId;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -13,6 +14,10 @@ import org.apache.tinkerpop.gremlin.structure.io.graphson.AbstractObjectDeserial
 import org.apache.tinkerpop.gremlin.structure.io.graphson.GraphSONTokens;
 import org.apache.tinkerpop.gremlin.structure.util.detached.DetachedEdge;
 import org.apache.tinkerpop.gremlin.structure.util.detached.DetachedVertex;
+import org.apache.tinkerpop.shaded.jackson.core.JsonParser;
+import org.apache.tinkerpop.shaded.jackson.core.JsonProcessingException;
+import org.apache.tinkerpop.shaded.jackson.core.JsonToken;
+import org.apache.tinkerpop.shaded.jackson.databind.DeserializationContext;
 import org.apache.tinkerpop.shaded.jackson.databind.JsonDeserializer;
 
 /** Created by Enrico Risa on 06/09/2017. */
@@ -85,6 +90,33 @@ public class OrientGraphSONV3 extends OrientGraphSON {
 
     public ORecordIdDeserializer() {
       super(Object.class);
+    }
+
+    @Override
+    public Object deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+        throws IOException, JsonProcessingException {
+      boolean keyString = true;
+      if (jsonParser.currentToken() == JsonToken.START_OBJECT) {
+        Map<String, Object> m = deserializationContext.readValue(jsonParser, LinkedHashMap.class);
+        return createObject(m);
+      } else {
+        final Map<Object, Object> m = new LinkedHashMap<>();
+
+        while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
+          final Object key = deserializationContext.readValue(jsonParser, Object.class);
+          if (!(key instanceof String)) {
+            keyString = false;
+          }
+          jsonParser.nextToken();
+          final Object val = deserializationContext.readValue(jsonParser, Object.class);
+          m.put(key, val);
+        }
+        if (keyString) {
+          return createObject((Map<String, Object>) (Map) m);
+        } else {
+          return m;
+        }
+      }
     }
 
     @Override
