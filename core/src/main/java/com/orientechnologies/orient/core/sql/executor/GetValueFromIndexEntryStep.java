d2c9772469287db2a78fef7846465a7224bdf361
Tglman <tglman@tglman.com>
2024-07-04 14:30:15 +0100

    fix: minor optimization in query engine class/cluster filtering

--- core/src/main/java/com/orientechnologies/orient/core/sql/executor/GetValueFromIndexEntryStep.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/executor/GetValueFromIndexEntryStep.java
@@ -30,6 +30,7 @@ public class GetValueFromIndexEntryStep extends AbstractExecutionStep {
       OCommandContext ctx, int[] filterClusterIds, boolean profilingEnabled) {
     super(ctx, profilingEnabled);
     this.filterClusterIds = filterClusterIds;
+    Arrays.sort(this.filterClusterIds);
   }
 
   @Override
@@ -52,14 +53,8 @@ public class GetValueFromIndexEntryStep extends AbstractExecutionStep {
           return null;
         }
         ORID rid = ((OIdentifiable) finalVal).getIdentity();
-        boolean found = false;
-        for (int filterClusterId : filterClusterIds) {
-          if (rid.getClusterId() < 0 || filterClusterId == rid.getClusterId()) {
-            found = true;
-            break;
-          }
-        }
-        if (!found) {
+        if (rid.getClusterId() >= 0
+            && Arrays.binarySearch(filterClusterIds, rid.getClusterId()) < 0) {
           return null;
         }
       }
