2315e2760b03b53b82d2ffa485ea9d2e78462195
Tglman <tglman@tglman.com>
2024-10-24 19:41:53 +0100

    cleanup: removed legacy graph implementation not used anymore

--- graphdb/src/main/java/com/tinkerpop/blueprints/impls/orient/OrientIndex.java
+++
@@ -1,7 +0,0 @@
-package com.tinkerpop.blueprints.impls.orient;
-
-import com.tinkerpop.blueprints.Index;
-
-public interface OrientIndex<T extends OrientElement> extends Index<T> {
-  void removeElement(final T element);
-}
