579ba2cab3ccda40577c02fed95f23da84e80123
Tglman <tglman@tglman.com>
2025-01-28 16:10:24 +0000

    fix: make sure to skip null key on index rebuild if are ignored by the index type, issue #10367

--- core/src/main/java/com/orientechnologies/orient/core/index/OIndexAbstract.java
+++ core/src/main/java/com/orientechnologies/orient/core/index/OIndexAbstract.java
@@ -658,8 +658,10 @@ public abstract class OIndexAbstract implements OIndexInternal {
         }
 
         try {
-          try (Stream<ORID> stream = getRids(null)) {
-            stream.forEach((rid) -> remove(null, rid));
+          if (!this.getDefinition().isNullValuesIgnored()) {
+            try (Stream<ORID> stream = getRids(null)) {
+              stream.forEach((rid) -> remove(null, rid));
+            }
           }
         } catch (OIndexEngineException e) {
           throw e;
