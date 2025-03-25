83758c5ed0c7e26aa80785d6688b30924b99424c
Tglman <tglman@tglman.com>
2025-03-25 12:43:18 +0000

    fix: make sure to convert the key to the index keys in the index stream major and minor

--- core/src/main/java/com/orientechnologies/orient/core/sql/executor/OMajorIndexStream.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/executor/OMajorIndexStream.java
@@ -4,7 +4,10 @@ import com.orientechnologies.common.util.ORawPair;
 import com.orientechnologies.orient.core.command.OCommandContext;
 import com.orientechnologies.orient.core.id.ORID;
 import com.orientechnologies.orient.core.index.OCompositeKey;
+import com.orientechnologies.orient.core.index.OIndexDefinition;
 import com.orientechnologies.orient.core.index.OIndexInternal;
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class OMajorIndexStream implements OIndexStream {
@@ -16,9 +19,21 @@ public class OMajorIndexStream implements OIndexStream {
   public OMajorIndexStream(OIndexInternal index, Object startKey, boolean include, boolean asc) {
     super();
     this.index = index;
-    this.startKey = startKey;
+    this.startKey = toIndexKey(index.getDefinition(), startKey);
     this.include = include;
     this.asc = asc;
+  }
+
+  private static Object toIndexKey(OIndexDefinition definition, Object rightValue) {
+    if (definition.getFields().size() == 1 && rightValue instanceof Collection) {
+      rightValue = ((Collection) rightValue).iterator().next();
+    }
+    if (rightValue instanceof List) {
+      rightValue = definition.createValue((List<?>) rightValue);
+    } else if (!(rightValue instanceof OCompositeKey)) {
+      rightValue = definition.createValue(rightValue);
+    }
+    return rightValue;
   }
 
   public Stream<ORawPair<Object, ORID>> start(OCommandContext ctx) {
