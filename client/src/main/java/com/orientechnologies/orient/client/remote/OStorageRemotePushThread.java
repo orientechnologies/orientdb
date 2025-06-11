0bc55b2a5060de7db6642a368e7c248d8ed982ae
Tglman <tglman@tglman.com>
2025-06-11 17:51:22 +0100

    fix: make sure to terminate the push subscribe connection if fails to connect

--- client/src/main/java/com/orientechnologies/orient/client/remote/OStorageRemotePushThread.java
+++ client/src/main/java/com/orientechnologies/orient/client/remote/OStorageRemotePushThread.java
@@ -22,7 +22,7 @@ public class OStorageRemotePushThread extends Thread {
   private final String host;
   private final int retryDelay;
   private final long requestTimeout;
-  private OChannelBinary network;
+  private volatile OChannelBinary network;
   private final BlockingQueue<Object> blockingQueue = new SynchronousQueue<>();
   private volatile OBinaryRequest currentRequest;
   private volatile boolean shutDown;
@@ -85,27 +85,31 @@ public class OStorageRemotePushThread extends Thread {
         }
       } catch (IOException | OException e) {
         pushHandler.onPushDisconnect(this.network, e);
-        while (!currentThread().isInterrupted()) {
+        while (!isInterrupted()) {
           try {
             Thread.sleep(retryDelay);
           } catch (InterruptedException x) {
-            currentThread().interrupt();
+            interrupt();
           }
-          if (!currentThread().isInterrupted()) {
+          if (!isInterrupted()) {
             try {
               synchronized (this) {
-                network = pushHandler.getNetwork(this.host);
+                this.network = null;
+                this.network = pushHandler.getNetwork(this.host);
               }
               pushHandler.onPushReconnect(this.host);
               break;
             } catch (OIOException ex) {
+              if (this.network != null) {
+                pushHandler.onPushDisconnect(this.network, ex);
+              }
               // Noting it just retry
             }
           }
         }
       } catch (InterruptedException e) {
         pushHandler.onPushDisconnect(this.network, e);
-        currentThread().interrupt();
+        interrupt();
       } catch (Throwable e) {
         OLogManager.instance().warn(this, "Push thread error ", e);
         throw e;
