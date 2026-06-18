dd3e18cab57ff5b5f98cd1ef63729b7bf5596613
Tglman <tglman@tglman.com>
2026-06-18 11:07:14 +0900

    fix: hardening of binary protocol, directly close connection in case of first request miss authentication

--- server/src/main/java/com/orientechnologies/orient/server/network/protocol/binary/ONetworkProtocolBinary.java
+++ server/src/main/java/com/orientechnologies/orient/server/network/protocol/binary/ONetworkProtocolBinary.java
@@ -99,6 +99,7 @@ public class ONetworkProtocolBinary extends ONetworkProtocol {
   private long requests = 0;
   private HandshakeInfo handshakeInfo;
   private volatile OBinaryPushResponse expectedPushResponse;
+  private boolean onceAuthenticated = false;
   private BlockingQueue<OBinaryPushResponse> pushResponse =
       new SynchronousQueue<OBinaryPushResponse>();
 
@@ -301,8 +302,19 @@ public class ONetworkProtocolBinary extends ONetworkProtocol {
             connection.getData().commandInfo = request.getDescription();
             connection.setProtocol(this); // This is need for the request command
           }
+          onceAuthenticated = true;
         } catch (RuntimeException | IOException ex) {
-          exception = ex;
+          if (!onceAuthenticated) {
+            try {
+              sendError(connection, clientTxId, ex);
+            } catch (IOException e) {
+              logger.error("Error while binary response serialization", e);
+            }
+            sendShutdown();
+            return;
+          } else {
+            exception = ex;
+          }
         }
         // Also in case of session validation error i read the message from the socket.
         try {
